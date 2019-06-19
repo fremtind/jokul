3bfcfe5c3ea8c18bbd762cc087425d55d6edccb4
Nicolai Hagen <nicolai.hagen@sparebank1.no>
2019-06-19 15:04:26 +0200

    feat(dropdown): added chevron icon, and began toggling it
    affects: @fremtind/jkl-dropdown

--- packages/dropdown/example/index.tsx
+++ packages/dropdown/example/index.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {Component} from "react";
 import ReactDOM from "react-dom";
 import "../dropdown.scss";
 
@@ -9,10 +9,34 @@ const testdata = [
     'Jøkkkul'
 ];
 
-const App = () => (
-    <select className="jkl-dropdown">
-        {testdata.map(data => <option>{data}</option>)}
-    </select>
-);
+const Icon = () => {
+    return (
+        <svg className='chevron' style={{transform: 'rotate(180deg)'}} width="26" height="14" viewBox="0 0 26 14" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M1 1L13 13L25 1" stroke="black"/>
+        </svg>
+    );
+};
+
+class App extends Component{
+    state = {
+        test: true,
+    };
+
+    render(){
+        const {
+            test
+        } = this.state;
+
+        console.log("test", test)
+        return (
+            <div className='jkl-dropdown-wrapper'>
+                <select className="jkl-dropdown" onClick={() => this.setState((prevState) => {test: !prevState.test})}>
+                    {testdata.map((data, idx) => <option className='jokul-option' key={idx}>{data}</option>)}
+                </select>
+                <Icon />
+            </div>
+        )
+    }
+};
 
 ReactDOM.render(<App />, document.getElementById("app"));
