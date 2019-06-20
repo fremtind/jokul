0695e97a35634b22bd2bc4e3c6dc6284829d5b3e
Pio Rasch-Halvorsen <pio.rasch-halvorsen@fremtind.no>
2019-06-20 13:37:48 +0200

    refactor(accordion): refactor scss w/nesting and using core variables
    affects: @fremtind/accordion

--- packages/accordion/example/index.tsx
+++ packages/accordion/example/index.tsx
@@ -5,20 +5,23 @@ import chevron from "../chevron.svg";
 
 const App = () => (
     <>
-        <div className="jkl-accordion" style={{ width: 700}}>
+        <div className="jkl-accordion" style={{ width: 700 }}>
             <div className="jkl-accordion__item jkl-accordion__item--expanded">
                 <div className="jkl-accordion__item-title">
                     <div className="jkl-accordion__item-title-text">Tekst som står her</div>
-                    <img className="jkl-accordion__item-title-icon" src={chevron} />
+                    <img className="jkl-accordion__item-title-icon" src={chevron} alt="Vis mindre" />
                 </div>
                 <div className="jkl-accordion__item-content">
-                    Now let's use some more properties. Consider a list of 6 items, all with a fixed dimensions in a matter of aesthetics but they could be auto-sized. We want them to be evenly and nicely distributed on the horizontal axis so that when we resize the browser, everything is fine (without media queries!).
+                    Now let&lsquo;s use some more properties. Consider a list of 6 items, all with a fixed dimensions in
+                    a matter of aesthetics but they could be auto-sized. We want them to be evenly and nicely
+                    distributed on the horizontal axis so that when we resize the browser, everything is fine (without
+                    media queries!).
                 </div>
             </div>
             <div className="jkl-accordion__item">
                 <div className="jkl-accordion__item-title">
                     <div className="jkl-accordion__item-title-text">Second item</div>
-                    <img className="jkl-accordion__item-title-icon" src={chevron}/>
+                    <img className="jkl-accordion__item-title-icon" src={chevron} alt="Vis mer" />
                 </div>
                 <div className="jkl-accordion__item-content">
                     <p>This is a paragraph</p>
