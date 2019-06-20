06479f51e5db944920f6efe37d2907979942f1fc
Pio Rasch-Halvorsen <pio.rasch-halvorsen@fremtind.no>
2019-06-20 12:59:38 +0200

    fix(card-react): add style to story
    affects: @fremtind/jkl-card-react

--- packages/card-react/src/Card.stories.tsx
+++ packages/card-react/src/Card.stories.tsx
@@ -4,6 +4,7 @@ import { storiesOf } from "@storybook/react";
 import { withKnobs } from "@storybook/addon-knobs";
 import { StoryTemplate } from "@fremtind/jkl-utils";
 import { Card } from ".";
+import "@fremtind/jkl-card/card.css"
 
 const stories = storiesOf("Atom/Struktur", module);
 stories.addDecorator(withKnobs);
