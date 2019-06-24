43267a8f749e8767426f3f6e5d2dcb885688fccf
Pio Rasch-Halvorsen <pio.rasch-halvorsen@fremtind.no>
2019-06-24 12:53:45 +0200

    refactor(accordion-react): move Accordion and -Item into separate files
    affects: @fremtind/jkl-accordion-react

--- packages/accordion-react/src/Accordion.tsx
+++ packages/accordion-react/src/Accordion.tsx
@@ -1,6 +1,4 @@
-import React, { ReactNode, useState } from "react";
-// @ts-ignore
-import CoreToggle from "@nrk/core-toggle/jsx";
+import React, { ReactNode } from "react";
 
 interface Props {
     children: ReactNode;
@@ -8,26 +6,4 @@ interface Props {
 
 export function Accordion({ children }: Props) {
     return <div className="jkl-accordion">{children}</div>;
-}
-
-interface ItemProps {
-    title: string;
-    children: ReactNode;
-    startExpanded?: boolean;
-}
-
-export function AccordionItem({ children, title, startExpanded = false }: ItemProps) {
-    const [isOpen, setIsOpen] = useState(startExpanded);
-    const openClassName = isOpen ? " jkl-accordion-item--expanded" : "";
-    return (
-        <div className={`jkl-accordion-item${openClassName}`}>
-            <button className="jkl-accordion-item__title">
-                <div className="jkl-accordion-item__title-text">{title}</div>
-                <div className="jkl-accordion-item__title-icon" />
-            </button>
-            <CoreToggle hidden onToggle={() => setIsOpen(!isOpen)}>
-                <div className="jkl-accordion-item__content">{children}</div>
-            </CoreToggle>
-        </div>
-    );
 }
---
+++ packages/accordion-react/src/AccordionItem.tsx
@@ -0,0 +1,28 @@
+import React, { ReactNode, useState } from "react";
+/* There are no types for core-toggle, so we have to use ts-ignore
+Hopefully someone (us?) will write types for it sometime soon */
+// @ts-ignore
+import CoreToggle from "@nrk/core-toggle/jsx";
+
+interface Props {
+    title: string;
+    children: ReactNode;
+    startExpanded?: boolean;
+}
+
+export function AccordionItem({ children, title, startExpanded = false }: Props) {
+    const [isOpen, setIsOpen] = useState(startExpanded);
+    const openClassName = isOpen ? " jkl-accordion-item--expanded" : "";
+    const onToggle = setIsOpen(!isOpen);
+    return (
+        <div className={`jkl-accordion-item${openClassName}`}>
+            <button className="jkl-accordion-item__title">
+                <div className="jkl-accordion-item__title-text">{title}</div>
+                <div className="jkl-accordion-item__title-icon" />
+            </button>
+            <CoreToggle hidden onToggle={onToggle}>
+                <div className="jkl-accordion-item__content">{children}</div>
+            </CoreToggle>
+        </div>
+    );
+}
