a24fb3a43f104fa911ad59eeb883c54a95b5613a
Leiv Fredrik Berge <leiv.fredr@sparebank1.no>
2019-06-25 09:37:46 +0200

    feat(dropdown): add react wrapper for dropdown
    affects: @fremtind/jkl-dropdown-react, @fremtind/jkl-dropdown
    
    lots of improvements in downshift usage

--- packages/dropdown/example/index.tsx
+++ packages/dropdown/example/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import ReactDOM from "react-dom";
 import Downshift from "downshift";
 import "../dropdown.scss";
@@ -6,46 +6,48 @@ import "../dropdown.scss";
 // Eslint do not understand how Downshift sets keys and label controls as its packed into the render props
 /* eslint-disable jsx-a11y/label-has-associated-control, react/jsx-key */
 const App = () => {
-    const items = [{ value: "Frem" }, { value: "Tind" }, { value: "Fremtind" }, { value: "Jøkkkul" }];
-    const [dropdownOpen, setDropdownOpen] = useState(false);
+    const items = ["Frem", "Tind", "Fremtind", "Jøkkkul"];
 
     return (
-        <Downshift
-            itemToString={(item) => (item ? item.value : "")}
-            isOpen={dropdownOpen}
-            onOuterClick={() => setDropdownOpen(false)}
-        >
-            {({ getLabelProps, getMenuProps, isOpen, highlightedIndex, inputValue, getItemProps }) => (
-                <div
-                    className={`jkl-dropdown ${isOpen ? "jkl-dropdown--open" : ""}`}
-                    onClick={() => setDropdownOpen(!dropdownOpen)}
-                    role="button"
-                    tabIndex={0}
-                    onKeyDown={(e) => e.key === "Enter" && setDropdownOpen(!dropdownOpen)}
-                >
+        <Downshift>
+            {({
+                getInputProps,
+                getToggleButtonProps,
+                getLabelProps,
+                getMenuProps,
+                isOpen,
+                highlightedIndex,
+                inputValue,
+                getItemProps,
+            }) => (
+                <div className={`jkl-dropdown ${isOpen ? "jkl-dropdown--open" : ""}`} {...getInputProps()}>
+                    <button className="jkl-dropdown__toggler" {...getToggleButtonProps()}>
+                        {isOpen ? "Lukk" : "Åpne"}
+                    </button>
                     <label
                         className={`jkl-dropdown__label ${inputValue ? "jkl-dropdown__label--raised" : ""}`}
                         {...getLabelProps()}
                     >
-                        Forsikringstype
+                        Velg
                     </label>
                     <span className={`jkl-dropdown__value ${inputValue ? "jkl-dropdown__value--show" : ""}`}>
                         {inputValue}
                     </span>
+
                     <ul className="jkl-dropdown__option" {...getMenuProps()}>
                         {isOpen
                             ? items.map((item, index) => (
                                   <li
                                       className={`jkl-dropdown__option--item ${
                                           highlightedIndex === index ? "jkl-dropdown__option--item--highlighted" : ""
-                                      } ${inputValue === item.value ? "jkl-dropdown__option--item--selected" : ""}`}
+                                      } ${inputValue === item ? "jkl-dropdown__option--item--selected" : ""}`}
                                       {...getItemProps({
-                                          key: item.value,
+                                          key: item,
                                           index,
                                           item,
                                       })}
                                   >
-                                      {item.value}
+                                      {item}
                                   </li>
                               ))
                             : null}
