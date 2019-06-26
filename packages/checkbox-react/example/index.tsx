c800bab21c4dbf5e93d6fcd8162dd127b8481dd7
Simen Saegrov <saegrov@gmail.com>
2019-06-26 09:33:30 +0200

    fix(checkbox): add core css to checkbox-react example

--- packages/checkbox-react/example/index.tsx
+++ packages/checkbox-react/example/index.tsx
@@ -1,4 +1,5 @@
 import "@fremtind/jkl-checkbox/checkbox.css";
+import "@fremtind/jkl-core/build/css/core.css";
 import React from "react";
 import ReactDOM from "react-dom";
 import { Checkbox } from "../src";
