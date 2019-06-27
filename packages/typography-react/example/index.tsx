9e4544bbdea914eb54a89d97f440f4c193f7c183
Pio Rasch-Halvorsen <pio.rasch-halvorsen@fremtind.no>
2019-06-27 14:26:07 +0200

    fix(normalize): remove margins from paragraph and headings

--- packages/typography-react/example/index.tsx
+++ packages/typography-react/example/index.tsx
@@ -1,23 +1,28 @@
 import "@fremtind/jkl-core/src/typography/typography.scss";
+import "@fremtind/jkl-core/src/normalize.scss";
 import React from "react";
 import ReactDOM from "react-dom";
 import { H1, H2, H3, H4, LeadParagraph, P, SmallText } from "../src";
 
 const Typography = () => (
     <>
-        <H1>H1: Hello World</H1>
+        <H1>H1: Hello World, look at this!</H1>
         <H2>H2: This is typography</H2>
         <H3>H3: components for</H3>
         <H4>H4: jøkul design system</H4>
         <LeadParagraph>
-            LeadParagraph: With these components you can say what you need in your application
+            LeadParagraph: For half-an-hour we were muffled in a cold, damp mist, and total darkness, and had begun to
+            think of going indoors when, all at once, the car burst into the pure and starlit region of the upper air.
         </LeadParagraph>
         <P>
-            P: There is literally no limit for what you can express with these components. Long text, short text, what
-            ever you need, we got you covered.
+            A paragraph (P): The sky of a deep dark blue was hung with innumerable stars, which seemed to float in the
+            limpid ether, and the rolling vapours through which we had passed were drawn like a sable curtain between us
+            and the lower world. The stillness was so profound that we could hear the beating of our own hearts.
         </P>
         <SmallText>
-            SmallText: Even really small text, even if we probably should not have small text everywhere.
+            SmallText: The unwonted sadness of her tone reminded me of her devoted life, and I turned towards her with
+            new interest and sympathy. She was looking at the Evening Star, whose bright beam softened the
+            irregularities of her profile, and made her almost beautiful.
         </SmallText>
 
         <H2 styledAs={"h1"}>Semantic h2, looks like h1</H2>
