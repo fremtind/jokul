a74c4480aa5a628d9fc010c3a2465cc7a24342f2
Leiv Fredrik Berge <leiv.fredr@sparebank1.no>
2019-06-27 13:17:48 +0200

    fix(textinput): fixed spacing
    affects: @fremtind/jkl-text-input-react, @fremtind/jkl-text-input

--- packages/text-input-react/example/index.tsx
+++ packages/text-input-react/example/index.tsx
@@ -7,25 +7,25 @@ const TextFieldDemo = () => {
     const [value, setValue] = useState("");
     return (
         <div style={{ margin: "36px", padding: "36px", border: "1px solid black", background: "#fafafa" }}>
-            <div style={{ paddingTop: "24px" }}>
+            <div style={{ paddingTop: "64px" }}>
                 <TextField label="Fornavn" value={value} onChange={(e) => setValue(e.target.value)} />
             </div>
-            <div style={{ paddingTop: "24px" }}>
+            <div style={{ paddingTop: "64px" }}>
                 <TextField label="Fornavn" value={"Something"} onChange={(e) => setValue(e.target.value)} />
             </div>
-            <div style={{ paddingTop: "24px" }}>
+            <div style={{ paddingTop: "64px" }}>
                 <TextField label="Telefon" type="tel" value={value} onChange={(e) => setValue(e.target.value)} />
             </div>
-            <div style={{ paddingTop: "24px" }}>
+            <div style={{ paddingTop: "64px" }}>
                 <TextField label="Passord" type="password" value={value} onChange={(e) => setValue(e.target.value)} />
             </div>
-            <div style={{ paddingTop: "24px" }}>
+            <div style={{ paddingTop: "64px" }}>
                 <TextField label="Epost" type="email" value={value} onChange={(e) => setValue(e.target.value)} />
             </div>
-            <div style={{ paddingTop: "24px" }}>
+            <div style={{ paddingTop: "64px" }}>
                 <TextArea label="Moreinfo" value={value} onChange={(e) => setValue(e.target.value)} />
             </div>
-            <div style={{ paddingTop: "24px" }}>
+            <div style={{ paddingTop: "64px" }}>
                 <TextArea label="Moreinfo" value={"Something"} onChange={(e) => setValue(e.target.value)} />
             </div>
         </div>
