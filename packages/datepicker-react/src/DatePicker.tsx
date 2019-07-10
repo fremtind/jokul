c64365ca2a81d08a15ea5be6671f9e77348e0609
Leiv Fredrik Berge <leiv.fredr@sparebank1.no>
2019-07-10 14:18:50 +0200

    feat(datepicker): add initial datepicker
    affects: @fremtind/jkl-datepicker-react, @fremtind/jkl-text-input-react

---
+++ packages/datepicker-react/src/DatePicker.tsx
@@ -0,0 +1,63 @@
+import React, { useState, ChangeEvent } from "react";
+// @ts-ignore
+import CoreDatepicker from "@nrk/core-datepicker/jsx";
+// @ts-ignore
+import CoreToggle from "@nrk/core-toggle/jsx";
+import { TextField } from "@fremtind/jkl-text-input-react";
+
+interface ChangeDate {
+    date: Date;
+}
+
+interface Props {
+    label?: string;
+    monthLabel?: string;
+    yearLabel?: string;
+    initialDate?: Date;
+    onChange?: (date: Date) => void;
+    onlyFuture?: boolean;
+}
+
+export function DatePicker({
+    label = "Velg dato",
+    monthLabel = "Måned",
+    yearLabel = "År",
+    initialDate = new Date(),
+    onChange,
+    onlyFuture = true,
+}: Props) {
+    const [today] = useState(Date.now() - (Date.now() % 864e3));
+    const [date, setDate] = useState(initialDate);
+
+    const onDateChange = (e: ChangeEvent<ChangeDate>) => {
+        if (onChange) {
+            onChange(e.target.date);
+        }
+        setDate(e.target.date);
+    };
+
+    return (
+        <div className="jkl-datepicker">
+            <button className="jkl-datepicker__toggler">
+                <TextField label={label} type="text" readOnly value={date.toLocaleDateString()} />
+            </button>
+            <CoreToggle hidden popup>
+                <CoreDatepicker
+                    timestamp={date.getTime()}
+                    disabled={onlyFuture ? (date: number) => date <= today : onlyFuture}
+                    onDatepickerChange={onDateChange}
+                    className="jkl-datepicker__calendar"
+                >
+                    <div className="jkl-datepicker__calendar__header">
+                        <TextField label={yearLabel} type="year" className="jkl-datepicker__calendar__header--year" />
+                        <label className="jkl-datepicker__calendar__header--month">
+                            {monthLabel}
+                            <select />
+                        </label>
+                    </div>
+                    <table />
+                </CoreDatepicker>
+            </CoreToggle>
+        </div>
+    );
+}
--- packages/text-input-react/src/TextField.tsx
+++ packages/text-input-react/src/TextField.tsx
@@ -2,24 +2,35 @@ import React, { ChangeEvent } from "react";
 
 interface Props {
     label: string;
-    value: string;
-    onChange: (value: ChangeEvent<HTMLInputElement>) => void;
-    type?: "text" | "number" | "tel" | "password" | "email";
+    value?: string;
+    onChange?: (value: ChangeEvent<HTMLInputElement>) => void;
+    type?: "text" | "number" | "tel" | "password" | "email" | "year";
     isInvalid?: boolean;
     id?: string;
     autoComplete?: string;
     required?: boolean;
+    readOnly?: boolean;
+    className?: string;
 }
 
-export const TextField = ({ type = "text", isInvalid = false, id, label, ...rest }: Props) => {
+export const TextField = ({
+    type = "text",
+    isInvalid = false,
+    readOnly = false,
+    id,
+    label,
+    className = "",
+    ...rest
+}: Props) => {
     return (
-        <label className={`jkl-text-field`}>
+        <label className={`jkl-text-field ${className}`}>
             <input
                 type={type}
                 aria-invalid={isInvalid}
-                className="jkl-text-field__input"
+                className={`jkl-text-field__input`}
                 id={id}
                 placeholder=" "
+                readOnly={readOnly}
                 {...rest}
             />
             <span className="jkl-text-field__label">{label}</span>
