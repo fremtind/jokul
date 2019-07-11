3b21e7b7aa98b8b0f1d34b27fb0597eae5f71335
Pio Rasch-Halvorsen <pio.rasch-halvorsen@fremtind.no>
2019-07-11 15:21:41 +0200

    revert(dropdown): rebase to master without new dropdown stuff
    affects: @fremtind/portal

--- portal/src/examples/DropdownExample.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { Dropdown } from "@fremtind/jkl-dropdown-react";
-
-export function DropdownExample() {
-    const items = ["Et valg", "Enda et valg", "Nok et valg", "et ganske langt valg for variasjonens skyld"];
-
-    return <Dropdown label="Foreta et valg" items={items} />;
-}
