bc7896a21007820fe6d5daa6121775337c001139
Leiv Fredrik Berge <leiv.fredr@sparebank1.no>
2019-07-11 16:43:38 +0200

    feat(build): add terser and size analyser to rollup
    affects: @fremtind/jkl-accordion-react, @fremtind/jkl-bullet-list-react, @fremtind/jkl-button-react,
    @fremtind/jkl-card-react, @fremtind/jkl-checkbox-react, @fremtind/jkl-core,
    @fremtind/jkl-divider-line-react, @fremtind/jkl-dropdown-react, @fremtind/jkl-footer-react,
    @fremtind/jkl-grid-react, @fremtind/jkl-header-react, @fremtind/jkl-logo-react,
    @fremtind/jkl-radio-button-react, @fremtind/jkl-text-input-react, @fremtind/jkl-typography-react
    
    ISSUES CLOSED: #124

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,8 @@
 import nodeResolve from "rollup-plugin-node-resolve";
 import babel from "rollup-plugin-babel";
 import commonjs from "rollup-plugin-commonjs";
+import { terser } from "rollup-plugin-terser";
+import { sizeSnapshot } from "rollup-plugin-size-snapshot";
 
 const extensions = [".ts", ".tsx"];
 const outputFolder = "build";
@@ -13,7 +15,7 @@ let newVar = {
         babel({
             rootMode: "upward",
             extensions,
-            exclude: ["node_modules/**", "*.stories.*"], // only transpile our source code
+            exclude: ["node_modules/**"], // only transpile our source code
         }),
         commonjs({
             namedExports: {
@@ -21,6 +23,8 @@ let newVar = {
             },
             include: [/node_modules\/prop-types/, /node_modules\/react-is/],
         }),
+        sizeSnapshot({ printInfo: false }),
+        terser(),
     ],
     external: ["react"],
 };
