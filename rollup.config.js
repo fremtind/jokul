e0ae13af0e59d377925019faee7f6646f4290c81
Leiv Fredrik Berge <leiv.fredr@sparebank1.no>
2019-07-12 11:36:13 +0200

    fix(build): uglify and keep original file
    affects: @fremtind/jkl-accordion-react, @fremtind/jkl-bullet-list-react, @fremtind/jkl-button-react,
    @fremtind/jkl-card-react, @fremtind/jkl-checkbox-react, @fremtind/jkl-core,
    @fremtind/jkl-divider-line-react, @fremtind/jkl-dropdown-react, @fremtind/jkl-footer-react,
    @fremtind/jkl-grid-react, @fremtind/jkl-header-react, @fremtind/jkl-logo-react,
    @fremtind/jkl-radio-button-react, @fremtind/jkl-text-input-react, @fremtind/jkl-typography-react

--- rollup.config.js
+++ rollup.config.js
@@ -7,34 +7,40 @@ import { sizeSnapshot } from "rollup-plugin-size-snapshot";
 const extensions = [".ts", ".tsx"];
 const outputFolder = "build";
 
-let newVar = {
-    input: "src/index.ts",
+const defaultPlugins = [
+    nodeResolve({ extensions }),
+    babel({
+        rootMode: "upward",
+        extensions,
+        exclude: ["node_modules/**"], // only transpile our source code
+    }),
+    commonjs({
+        namedExports: {
+            "react-is": ["isForwardRef"],
+        },
+        include: [/node_modules\/prop-types/, /node_modules\/react-is/],
+    }),
+];
 
-    plugins: [
-        nodeResolve({ extensions }),
-        babel({
-            rootMode: "upward",
-            extensions,
-            exclude: ["node_modules/**"], // only transpile our source code
-        }),
-        commonjs({
-            namedExports: {
-                "react-is": ["isForwardRef"],
-            },
-            include: [/node_modules\/prop-types/, /node_modules\/react-is/],
-        }),
-        sizeSnapshot({ printInfo: false }),
-        terser(),
-    ],
-    external: ["react"],
-};
+const uglifiedPlugins = [...defaultPlugins, sizeSnapshot({ printInfo: false }), terser({ ecma: "es5" })];
 
-function configWithOutput(output) {
-    return Object.assign({}, newVar, { output });
+function config(plugins) {
+    return {
+        input: "src/index.ts",
+
+        plugins: plugins,
+        external: ["react"],
+    };
+}
+
+function configWithOutput(config, output) {
+    return Object.assign({}, config, { output });
 }
 
 export default [
-    configWithOutput({ file: `${outputFolder}/cjs/index.js`, format: "commonjs" }),
-    configWithOutput({ file: `${outputFolder}/esm/index.js`, format: "esm" }),
-    configWithOutput({ file: `${outputFolder}/browser/index.js`, format: "esm" }),
+    configWithOutput(config(defaultPlugins), { file: `${outputFolder}/cjs/index.js`, format: "commonjs" }),
+    configWithOutput(config(defaultPlugins), { file: `${outputFolder}/esm/index.js`, format: "esm" }),
+    configWithOutput(config(defaultPlugins), { file: `${outputFolder}/browser/index.js`, format: "esm" }),
+    configWithOutput(config(uglifiedPlugins), { file: `${outputFolder}/esm/index.min.js`, format: "esm" }),
+    configWithOutput(config(uglifiedPlugins), { file: `${outputFolder}/browser/index.min.js`, format: "esm" }),
 ];
