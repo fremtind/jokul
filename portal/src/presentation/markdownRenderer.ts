ec24a21343982b3258b5f672cd67c0a1986d57e1
Pio Rasch-Halvorsen <pio.rasch-halvorsen@fremtind.no>
2019-07-16 15:05:58 +0200

    fix(bullet-list): remove import of paragraph styles and align spacing
    affects: @fremtind/jkl-bullet-list, @fremtind/portal

--- portal/src/presentation/markdownRenderer.ts
+++ portal/src/presentation/markdownRenderer.ts
@@ -12,6 +12,6 @@ export const renderer = {
     paragraph: ({ children }: Props) => createElement("p", { className: "jkl-p" }, children),
     heading: ({ level, children }: HeadingProps) =>
         createElement(`h${level}`, { className: `jkl-h${level + 1}` }, children),
-    list: ({ children }: Props) => createElement("ul", { className: "jkl-bullet-list" }, children),
+    list: ({ children }: Props) => createElement("ul", { className: "jkl-bullet-list jkl-p" }, children),
     listItem: ({ children }: Props) => createElement("li", { className: "jkl-bullet-list__item" }, children),
 };
