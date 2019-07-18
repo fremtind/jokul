625f169e144e0aec3b360d0c42ad0655114a192b
Leiv Fredrik Berge <leiv.fredr@sparebank1.no>
2019-07-18 14:13:54 +0200

    feat(portal): add types to examples in portal
    affects: @fremtind/portal
    
    add fullscreen mode for components

--- portal/src/components/Layout/Layout.tsx
+++ portal/src/components/Layout/Layout.tsx
@@ -6,19 +6,38 @@
  */
 
 import React, { ReactNode } from "react";
+import { LocationProvider } from "@reach/router";
 import Header from "../Header/Header";
 import { Menu } from "../Menu/Menu";
 
 import "./Layout.scss";
 
-export const Layout = ({ children }: { children: ReactNode }) => {
-    return (
-        <>
-            <Header siteTitle="Jøkul" />
-            <Menu />
-            <main className="portal-content">{children}</main>
-        </>
-    );
-};
+interface Props {
+    children: ReactNode;
+    title?: string;
+    mainExample?: ReactNode;
+}
+
+export const Layout = ({ children, title, mainExample }: Props) => (
+    <LocationProvider>
+        {({ location }) => {
+            if (location.search !== "?fullscreen") {
+                return (
+                    <>
+                        <Header siteTitle="Jøkul" />
+                        <Menu />
+
+                        <main className="portal-content">
+                            {title && <h2 className="jkl-h1 portal-content__title">{title}</h2>}
+                            {mainExample}
+                            {children}
+                        </main>
+                    </>
+                );
+            }
+            return <main className="portal-component-only">{mainExample}</main>;
+        }}
+    </LocationProvider>
+);
 
 export default Layout;
