ff689a4c461a6c6be399006d516908e7f9c36b14
Leiv Fredrik Berge <leiv.fredr@sparebank1.no>
2019-07-19 15:03:12 +0200

    fix(portal): hide type for dividerline
    affects: @fremtind/portal

--- portal/src/examples/DividerLineExample.tsx
+++ portal/src/examples/DividerLineExample.tsx
@@ -3,7 +3,7 @@ import { Example } from "../components";
 import { DividerLine } from "@fremtind/jkl-divider-line-react";
 import "@fremtind/jkl-divider-line/divider-line.min.css";
 // @ts-ignore
-import dividerLineType from "!raw-loader!@fremtind/jkl-divider-line-react/build/DividerLine.d.ts";
+//import dividerLineType from "!raw-loader!@fremtind/jkl-divider-line-react/build/DividerLine.d.ts";
 
 const example = `<DividerLine />`;
 
@@ -14,7 +14,7 @@ const DividerLineExample = () => (
     <Example
         exampleComponents={{ DividerLine }}
         exampleCode={example}
-        type={dividerLineType}
+        //type={dividerLineType}
         exampleImport={exampleImport}
     />
 );
