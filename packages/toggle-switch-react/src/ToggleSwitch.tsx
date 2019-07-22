c313137b2f72ff6b120adc1300637743d4fbfb6e
Pio Rasch-Halvorsen <pio.rasch-halvorsen@fremtind.no>
2019-07-22 15:37:03 +0200

    test(toggle-switch-react): add test for onChange method

--- packages/toggle-switch-react/src/ToggleSwitch.tsx
+++ packages/toggle-switch-react/src/ToggleSwitch.tsx
@@ -8,13 +8,14 @@ interface Props {
 }
 
 export const ToggleSwitch = ({ children, defaultChecked, onChange, className = "" }: Props) => {
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => onChange && onChange(e.target.checked);
     return (
         <label data-testid="jkl-toggle-input--label" className={`jkl-toggle-switch ${className}`}>
             <input
                 data-testid="jkl-toggle-input"
                 type="checkbox"
                 defaultChecked={defaultChecked}
-                onChange={(e) => onChange && onChange(e.target.checked)}
+                onChange={handleChange}
             />
             <span className="jkl-toggle-switch__slider" />
             <span className="jkl-toggle-switch__label jkl-p">{children}</span>
