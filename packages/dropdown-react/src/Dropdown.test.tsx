b3b53ded53cb4ce012df2902fa3418e823df0e45
Pio Rasch-Halvorsen <pio.rasch-halvorsen@fremtind.no>
2019-07-22 15:46:50 +0200

    refactor(dropdown-react): remove unused import

--- packages/dropdown-react/src/Dropdown.test.tsx
+++ packages/dropdown-react/src/Dropdown.test.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { cleanup, render, fireEvent } from "@testing-library/react";
+import { cleanup, render } from "@testing-library/react";
 import { Dropdown } from ".";
 
 describe("Dropdown", () => {
