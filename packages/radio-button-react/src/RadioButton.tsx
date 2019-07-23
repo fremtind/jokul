35357ca28a744d9b8769013bc0886c14c1b18f1b
Pio Rasch-Halvorsen <pio.rasch-halvorsen@fremtind.no>
2019-07-23 15:32:02 +0200

    refactor(radio-button-react): remove unnecessary returns

--- packages/radio-button-react/src/RadioButton.tsx
+++ packages/radio-button-react/src/RadioButton.tsx
@@ -8,12 +8,10 @@ interface Props {
     checked?: boolean;
 }
 
-export const RadioButton = ({ value, onChange, name = "", inline = false, checked }: Props) => {
-    return (
-        <label className={"jkl-radio-button".concat(inline ? " jkl-radio-button--inline" : "")}>
-            <input type="radio" name={name} value={value} onChange={onChange} checked={checked} />
-            <span className="jkl-radio-button__input" />
-            <span className="jkl-radio-button__label">{value}</span>
-        </label>
-    );
-};
+export const RadioButton = ({ value, onChange, name = "", inline = false, checked }: Props) => (
+    <label className={"jkl-radio-button".concat(inline ? " jkl-radio-button--inline" : "")}>
+        <input type="radio" name={name} value={value} onChange={onChange} checked={checked} />
+        <span className="jkl-radio-button__input" />
+        <span className="jkl-radio-button__label">{value}</span>
+    </label>
+);
