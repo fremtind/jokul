d66f256b1464d5f414b0fff71f6df9248582a356
Pio Rasch-Halvorsen <pio.rasch-halvorsen@fremtind.no>
2019-07-23 15:16:16 +0200

    fix(radio-button): refactor structure to match other components

--- packages/radio-button-react/src/RadioButton.tsx
+++ packages/radio-button-react/src/RadioButton.tsx
@@ -11,14 +11,8 @@ interface Props {
 export const RadioButton = ({ value, onChange, name = "", inline = false, checked }: Props) => {
     return (
         <label className={"jkl-radio-button".concat(inline ? " jkl-radio-button--inline" : "")}>
-            <input
-                type="radio"
-                name={name}
-                value={value}
-                className="jkl-radio-button__input"
-                onChange={onChange}
-                checked={checked}
-            />
+            <input type="radio" name={name} value={value} onChange={onChange} checked={checked} />
+            <span className="jkl-radio-button__input" />
             <span className="jkl-radio-button__label">{value}</span>
         </label>
     );
