c504990616a11c2ff58a136283223eddae02f886
Leiv Fredrik Berge <leiv.fredr@sparebank1.no>
2019-07-24 13:46:19 +0200

    test(messagebox): add test
    affects: @fremtind/jkl-message-box-react

--- packages/message-box-react/src/MessageBox.test.tsx
+++ packages/message-box-react/src/MessageBox.test.tsx
@@ -1,15 +1,21 @@
 import React from "react";
 import { cleanup, render } from "@testing-library/react";
-import { InfoMessage, ErrorMessage, SuccessMessage } from ".";
+import { InfoMessage, ErrorMessage, SuccessMessage, WarningMessage } from ".";
 
 describe("Message boxes", () => {
     afterEach(cleanup);
 
-    [InfoMessage, ErrorMessage, SuccessMessage].map((E) => {
-        it("should render message title and content", () => {
-            const { getByText } = render(<E title="test">content</E>);
-            getByText("content");
-            getByText("test");
+    [true, false].map((fullWidth) => {
+        [InfoMessage, ErrorMessage, SuccessMessage, WarningMessage].map((E) => {
+            it("should render message title and content", () => {
+                const { getByText } = render(
+                    <E fullWidth={fullWidth} title="test">
+                        content
+                    </E>,
+                );
+                getByText("content");
+                getByText("test");
+            });
         });
     });
 });
