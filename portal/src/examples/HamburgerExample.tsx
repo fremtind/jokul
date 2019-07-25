30f49269350dae8f3a483fd54da590887a3f5226
Leiv Fredrik Berge <leiv.fredr@sparebank1.no>
2019-07-25 11:06:46 +0200

    docs(hamburger): add example to portal
    affects: @fremtind/jkl-hamburger-react, @fremtind/jkl-hamburger, @fremtind/portal

---
+++ portal/src/examples/HamburgerExample.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { Example } from "../components";
+import { Hamburger } from "@fremtind/jkl-hamburger-react";
+import "@fremtind/jkl-hamburger/hamburger.min.css";
+// @ts-ignore
+import hamburgerType from "!raw-loader!@fremtind/jkl-hamburger-react/build/Hamburger.d.ts";
+
+const example = `<div style={{backgroundColor: "#765d4b", padding: "32px"}}>
+    <Hamburger />
+    <Hamburger negative initialIsActive description="Burger" onClick={ (next) => alert(next ? "Is opening" : "Is closing")} />
+</div>`;
+
+const exampleImport = `import { Hamburger } from "@fremtind/jkl-hamburger-react";
+import "@fremtind/jkl-hamburger/hamburger.min.css";`;
+
+const HamburgerExample = () => (
+    <Example
+        exampleComponents={{ Hamburger }}
+        exampleCode={example}
+        type={hamburgerType}
+        exampleImport={exampleImport}
+    />
+);
+
+export default HamburgerExample;
