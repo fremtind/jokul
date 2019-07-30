413351339ae4c903fedd643b542655ca97a19770
Pio Rasch-Halvorsen <pio.rasch-halvorsen@fremtind.no>
2019-07-30 16:03:53 +0200

    fix(text-input-react): pass className to textarea

--- packages/text-input-react/src/TextArea.tsx
+++ packages/text-input-react/src/TextArea.tsx
@@ -3,14 +3,15 @@ import React, { ChangeEvent } from "react";
 interface Props {
     label: string;
     value: string;
-    onChange: (value: ChangeEvent<HTMLTextAreaElement>) => void;
+    onChange?: (value: ChangeEvent<HTMLTextAreaElement>) => void;
+    className?: string;
     isInvalid?: boolean;
     id?: string;
     required?: boolean;
 }
 
-export const TextArea = ({ isInvalid = false, id, label, ...rest }: Props) => (
-    <label className="jkl-text-field jkl-text-area">
+export const TextArea = ({ isInvalid = false, id, label, className = "", ...rest }: Props) => (
+    <label className={`jkl-text-field jkl-text-area ${className}`}>
         <textarea aria-invalid={isInvalid} className="jkl-text-field__input" id={id} placeholder=" " {...rest} />
         <span className="jkl-text-field__label">{label}</span>
     </label>
