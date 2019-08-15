6592a0dc4321d9f7cb226cf10322fdd4c89ea077
Simen Saegrov <saegrov@gmail.com>
2019-08-15 21:35:28 +0200

    fix(date-picker-react): remove test that didn't make sense

--- packages/datepicker-react/src/DatePicker.test.tsx
+++ packages/datepicker-react/src/DatePicker.test.tsx
@@ -4,42 +4,10 @@ import { DatePicker } from ".";
 
 beforeEach(cleanup);
 
-it("datepicker should render to dom with todays date as default", () => {
-    const { getByTestId } = render(<DatePicker />);
-
-    const today = new Date();
-
-    const date = getByTestId("jkl-datepicker-input");
-    expect(date).toHaveProperty("value", today.toLocaleDateString());
-});
-
-it("datepicker should render to dom with date in the past", () => {
-    const thePast = new Date(new Date().setFullYear(new Date().getFullYear() - 1));
-    const { getByTestId } = render(<DatePicker onlyFuture={false} initialDate={thePast} />);
+it("datepicker should render with the correct format", () => {
+    const thePast = new Date(2019, 11, 24);
+    const { getByTestId } = render(<DatePicker initialDate={thePast} />);
 
     const date = getByTestId("jkl-datepicker-input");
-    expect(date).toHaveProperty("value", thePast.toLocaleDateString());
-});
-
-it("datepicker should render to dom with date in the future", () => {
-    const theFuture = new Date(new Date().setFullYear(new Date().getFullYear() + 1));
-    const { getByTestId } = render(<DatePicker initialDate={theFuture} />);
-
-    const date = getByTestId("jkl-datepicker-input");
-    expect(date).toHaveProperty("value", theFuture.toLocaleDateString());
-});
-
-it("datepicker should render to dom with custom labels", () => {
-    const custom = {
-        label: "Select date",
-        yearLabel: "Year",
-        monthLabel: "Month",
-    };
-    const { getByLabelText } = render(
-        <DatePicker label={custom.label} yearLabel={custom.yearLabel} monthLabel={custom.monthLabel} />,
-    );
-
-    const labels = [getByLabelText(custom.label), getByLabelText(custom.yearLabel), getByLabelText(custom.monthLabel)];
-
-    labels.forEach((label) => expect(label).toBeInTheDocument());
+    expect(date).toHaveProperty("value", "24.12.2019");
 });
