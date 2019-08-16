fa160555dc834a34a7572ef2bb03f25b3b1c52c2
Gørild Døhl <51953080+Gorild@users.noreply.github.com>
2019-08-16 12:37:42 +0200

    Update MessageBoxExample.tsx

--- portal/src/examples/MessageBoxExample.tsx
+++ portal/src/examples/MessageBoxExample.tsx
@@ -7,16 +7,24 @@ import messageType from "!raw-loader!@fremtind/jkl-message-box-react/build/Messa
 
 const example = `<>
         <InfoMessage title="Info">
-            Cupidatat Lorem incididunt incididunt in non mollit cillum Lorem eiusmod sunt magna.
+            Vi lagrer alle svarene dine underveis, slik at du kan forstsette der du slapp når det passer for deg.
         </InfoMessage>
         <ErrorMessage title="Error">
-            In non dolore ullamco minim adipisicing ipsum pariatur deserunt ipsum.
+            Vi finner ikke denne kombinasjonen av e-post og passord. Prøv igjen.
         </ErrorMessage>
-        <SuccessMessage title="Success">Ullamco minim aute Lorem adipisicing.</SuccessMessage>
-        <WarningMessage title="Warning">Culpa commodo velit cupidatat anim excepteur est culpa.</WarningMessage>
-        <InfoMessage fullWidth title="Full width info">Reprehenderit magna duis aliquip adipisicing do mollit ullamco. Pariatur quis ullamco eu cillum. Exercitation in mollit amet ex aute culpa do incididunt Lorem et non id quis ex. Pariatur ex cillum anim consectetur eu mollit occaecat ut aliqua mollit magna. Irure ut occaecat aliqua excepteur consectetur aliqua eiusmod qui excepteur consectetur magna aliqua.
-        </InfoMessage>
-</>`;
+        <SuccessMessage title="Success">Saken din er registrert</SuccessMessage>
+        <WarningMessage title="Warning">Du må fylle ut alle feltene før du kan gå videre</WarningMessage>
+        <InfoMessage fullWidth title="Full width info">
+## Slik behandler vi saken din
+* Vi dekker skaden på bilen din. 
+* Skadenummeret ditt er 1352630007. 
+* Vi kontakter deg innen klokka 15 neste arbeidsdag. Da får du vite hvem vi mener er ansvarlig for uhellet og om du må betale egenandel.
+### Dette skjer videre
+* Vi kontakter forsikringsselskapet til den andr parten.
+* Du må oppgi skadenummeret til verkstedet.
+* Kontakt Avis Bilutleie på telefon 66 77 11 06 for å få leiebil. Oppgi skadenummeret ditt. Du har leiebil i inntil 45 dager mens bilen er på verksted.
+</InfoMessage>
+        </>`;
 
 const exampleImport = `import { ErrorMessage, InfoMessage, SuccessMessage, WarningMessage } from "@fremtind/jkl-message-box-react";
 import "@fremtind/jkl-message-box/message-box.min.css";`;
