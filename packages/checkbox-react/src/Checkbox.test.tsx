af4f4e0331e20c7359907b460ac86c52793b1817
Pio Rasch-Halvorsen <pio.rasch-halvorsen@fremtind.no>
2019-08-26 14:28:40 +0200

    test(checkbox): add tests for CheckboxGroup

--- packages/checkbox-react/src/Checkbox.test.tsx
+++ packages/checkbox-react/src/Checkbox.test.tsx
@@ -4,8 +4,14 @@ import { Checkbox } from ".";
 
 afterEach(cleanup);
 
+it("should render name as label if no children are passed", () => {
+    const { getByText } = render(<Checkbox name="I am Groot!" />);
+
+    expect(getByText("I am Groot!")).toBeInTheDocument;
+});
+
 it("should be checked after clicking the label", () => {
-    const { getByText, getByTestId } = render(<Checkbox>I am groot!</Checkbox>);
+    const { getByText, getByTestId } = render(<Checkbox name="iamgroot">I am groot!</Checkbox>);
 
     const label = getByText("I am groot!");
     const input = getByTestId("jkl-checkbox-input");
@@ -18,7 +24,7 @@ it("should be checked after clicking the label", () => {
 });
 
 it("should be checked after clicking the input ", function() {
-    const { getByTestId } = render(<Checkbox>I am groot!</Checkbox>);
+    const { getByTestId } = render(<Checkbox name="iamgroot">I am groot!</Checkbox>);
 
     const input = getByTestId("jkl-checkbox-input");
 
@@ -30,7 +36,11 @@ it("should be checked after clicking the input ", function() {
 });
 
 it("should be checked if checked is true", function() {
-    const { getByTestId } = render(<Checkbox checked={true}>I am groot!</Checkbox>);
+    const { getByTestId } = render(
+        <Checkbox name="iamgroot" checked={true}>
+            I am groot!
+        </Checkbox>,
+    );
 
     const input = getByTestId("jkl-checkbox-input");
 
@@ -41,7 +51,7 @@ it("should be unchecked if checked is true and input is clicked", function() {
     const TestCheckbox = () => {
         const [checked, toggle] = React.useState(true);
         return (
-            <Checkbox checked={checked} onChange={() => toggle(!checked)}>
+            <Checkbox name="iamgroot" checked={checked} onChange={() => toggle(!checked)}>
                 I am groot!
             </Checkbox>
         );
@@ -60,7 +70,11 @@ it("should be unchecked if checked is true and input is clicked", function() {
 
 it("should call the passed onChange method when clicked", () => {
     const onChange = jest.fn();
-    const { getByLabelText } = render(<Checkbox onChange={onChange}>Switch me!</Checkbox>);
+    const { getByLabelText } = render(
+        <Checkbox name="switchme" onChange={onChange}>
+            Switch me!
+        </Checkbox>,
+    );
 
     const input = getByLabelText("Switch me!");
     input.click();
