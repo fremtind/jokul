a2052a7c22d16e256a3747470d92bebd96baf9e0
Leiv Fredrik Berge <leiv.fredr@sparebank1.no>
2019-08-27 16:51:04 +0200

    feat(radiobutton): add error state to radio button
    affects: @fremtind/jkl-radio-button-react, @fremtind/jkl-radio-button, @fremtind/portal

--- packages/radio-button-react/src/RadioButtonChoice.tsx
+++ packages/radio-button-react/src/RadioButtonChoice.tsx
@@ -1,5 +1,6 @@
 import React, { ChangeEvent } from "react";
 import { RadioButton } from "./RadioButton";
+import { SupportLabel } from "@fremtind/jkl-typography-react";
 
 interface Props {
     name: string;
@@ -8,9 +9,20 @@ interface Props {
     selectedValue: string;
     inline?: boolean;
     onChange: (event: ChangeEvent<HTMLInputElement>) => void;
+    helpLabel?: string;
+    errorLabel?: string;
 }
 
-export const RadioButtonChoice = ({ name, legend, choices, selectedValue, onChange, inline = true }: Props) => (
+export const RadioButtonChoice = ({
+    name,
+    legend,
+    choices,
+    selectedValue,
+    onChange,
+    inline = true,
+    errorLabel,
+    helpLabel,
+}: Props) => (
     <fieldset className="jkl-radio-button-choice">
         <legend>{legend || name}</legend>
         {choices.map((choice) => (
@@ -21,7 +33,9 @@ export const RadioButtonChoice = ({ name, legend, choices, selectedValue, onChan
                 inline={inline}
                 checked={choice === selectedValue}
                 onChange={onChange}
+                invalid={!!errorLabel}
             />
         ))}
+        {(helpLabel || errorLabel) && <SupportLabel errorLabel={errorLabel} helpLabel={helpLabel} />}
     </fieldset>
 );
