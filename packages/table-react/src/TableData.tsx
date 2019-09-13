b94847ae2fb2d0d833d4f0e888b8cafc2cb9afda
Simen Saegrov <saegrov@gmail.com>
2019-09-13 14:53:25 +0200

    fix(jkl-table-react): let the TableRow click handler handle navigation

--- packages/table-react/src/TableData.tsx
+++ packages/table-react/src/TableData.tsx
@@ -10,12 +10,17 @@ interface Props {
 
 export function TableData(props: Props) {
     const { row, data, isFirstCell } = props;
+    function onAnchorClick(evt: React.MouseEvent<HTMLAnchorElement>) {
+        // Stop browser link navigation and let the clickHandler on TableRow do the job
+        evt.preventDefault();
+    }
+
     return (
         <td className="jkl-table__data-cell">
             {/* Only create an anchor in the first cell, so that screen readers doesn't read each cell as a link */}
             {/* The downside of this is that only the first cell can be right clicked and get the anchor context menu */}
             {isFirstCell && isAnchorRowData(row) ? (
-                <a className="jkl-sr-only" href={row.href}>
+                <a className="jkl-sr-only" href={row.href} onClick={onAnchorClick}>
                     {row.hrefLabel}{" "}
                 </a>
             ) : (
