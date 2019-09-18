1e3b58b7c2518560727a381b6a7fe0fbaa78445c
Simen Saegrov <saegrov@gmail.com>
2019-09-18 08:42:58 +0200

    docs(table): document needed polyfill for table component

--- portal/src/pages/components/table.tsx
+++ portal/src/pages/components/table.tsx
@@ -1,3 +1,5 @@
+import { WarningMessage } from "@fremtind/jkl-message-box-react/src";
+import { Link } from "@fremtind/jkl-typography-react";
 import React from "react";
 import ReactMarkdown from "react-markdown";
 import { ComponentLayout, Changelog } from "../../components";
@@ -14,6 +16,16 @@ import changelogReact from "!raw-loader!@fremtind/jkl-table-react/CHANGELOG.md";
 export default function TablePage() {
     return (
         <ComponentLayout title="Tabell" mainExample={<TableExample />} packageName="table">
+            <WarningMessage fullWidth title="Fokusring i tabellen" className="jkl-spacing--top-1">
+                Denne komponenten bruker{" "}
+                <span>
+                    <code>:focus-within </code>
+                </span>
+                for å vise fokus-ring ved tab navigasjon som{" "}
+                <Link href="https://caniuse.com/#search=focus-within">ikke er støttet i IE/Edge.</Link> Er du avhenging
+                av riktig visning i browsere som ikke er støttet så kan man bruke{" "}
+                <Link href="https://www.npmjs.com/package/focus-within-polyfill">dette polyfillet</Link>.
+            </WarningMessage>
             <ReactMarkdown renderers={renderer} source={readmeContents} />
             <Changelog changelog={changelog} changelogReact={changelogReact} />
         </ComponentLayout>
