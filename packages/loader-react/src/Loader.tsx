7edc767a65f382377818326194249c2d9d01d50f
Pio Rasch-Halvorsen <pio.rasch-halvorsen@fremtind.no>
2019-09-24 16:00:39 +0200

    fix(loader-react): add missing space in componentClassName function
    affects: @fremtind/jkl-loader-react

--- packages/loader-react/src/Loader.tsx
+++ packages/loader-react/src/Loader.tsx
@@ -7,7 +7,7 @@ interface Props {
 export const Loader = ({ negative = false, inline = false, className }: Props) => {
     const componentClassName = "jkl-loader"
         .concat(negative ? " jkl-loader--negative" : "")
-        .concat(inline ? "jkl-loader--inline" : "")
+        .concat(inline ? " jkl-loader--inline" : "")
         .concat(className ? ` ${className}` : "");
     return (
         <span data-testid="jkl-loader" className={componentClassName}>
