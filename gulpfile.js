2186624a75409cda2000444b3188ef7bbcc843ca
Leiv Fredrik Berge <leiv.fredrik.berge@sparebank1.no>
2019-10-11 12:48:27 +0200

    feat(scss): swap sass compiler to dart-sass
    affects: @fremtind/jkl-accordion, @fremtind/jkl-bullet-list, @fremtind/jkl-button,
    @fremtind/jkl-card-react, @fremtind/jkl-card, @fremtind/jkl-checkbox, @fremtind/jkl-datepicker,
    @fremtind/jkl-divider-line, @fremtind/jkl-dropdown, @fremtind/jkl-field-group-react,
    @fremtind/jkl-field-group, @fremtind/jkl-hamburger, @fremtind/jkl-loader, @fremtind/jkl-logo,
    @fremtind/jkl-message-box, @fremtind/jkl-radio-button, @fremtind/jkl-table-react,
    @fremtind/jkl-table, @fremtind/jkl-text-input, @fremtind/jkl-toggle-switch
    
    dart-sass is the refrence implementation of sass, so it will get the latest features and fixes first
    
    it is also pure javascript, node-sass is a wrapper of libsass, a c++ implementation, with its
    inherited problems of wrapping c++
    node-sass is stil a bit faster than dart-sass, but the
    differenence for us is about 1 second for compiling all scss currently
    this enables us to utilize
    the new module system in scss, which should reduce could reduce css size and improve performance in
    production

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,12 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 const sass = require("gulp-sass");
-const importer = require("node-sass-tilde-importer");
 const postcss = require("gulp-postcss");
 const cssnano = require("cssnano");
 const autoprefixer = require("autoprefixer");
 const rename = require("gulp-rename");
+const Fiber = require("fibers");
 
-sass.compiler = require("node-sass");
+sass.compiler = require("dart-sass");
 /* eslint-enable @typescript-eslint/no-var-requires */
 
 const scssFiles = ["**/*.scss", "!example/*.scss"];
@@ -19,7 +19,9 @@ module.exports = function(gulp) {
     gulp.task("build", function() {
         return gulp
             .src(scssFiles)
-            .pipe(sass({ importer }).on("error", throwSassError))
+            .pipe(
+                sass({ includePaths: ["../../node_modules", "../packages"], fiber: Fiber }).on("error", throwSassError),
+            )
             .pipe(postcss([autoprefixer()]))
             .pipe(gulp.dest("./"))
             .pipe(postcss([cssnano()]))
