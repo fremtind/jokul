19526c3d0bc86e10d4002ed99bb7f4ac002765fb
Frida Thorkildsen Solhaug <frida.solhaug@fremtind.no>
2019-10-17 15:44:33 +0200

    fix: remove unused package

--- packages/icon-react/README.md
+++
@@ -1,9 +0,0 @@
-# [`@fremtind/jkl-icon-react`](https://fremtind.github.io/jokul/components/icon/)
-
-## Beskrivelse
-
-[Bruk og prinsipper](https://fremtind.github.io/jokul/components/icon/) er beskrevet i `@fremtind/jkl-icon` stil-pakken.
-
-## Kom i gang
-
-[Lær hvordan du kan ta i bruk Jøkul](https://fremtind.github.io/jokul/developer/getting-started/)
--- packages/icon-react/src/Icons/IconClear.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-export const IconClear = () => (
-    <svg width="24" height="24" xmlns="http://www.w3.org/2000/svg" fillRule="evenodd" clipRule="evenodd">
-        <path d="M12 11.293l10.293-10.293.707.707-10.293 10.293 10.293 10.293-.707.707-10.293-10.293-10.293 10.293-.707-.707 10.293-10.293-10.293-10.293.707-.707 10.293 10.293z" />
-    </svg>
-);
