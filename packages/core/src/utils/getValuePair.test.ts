a1430ee355e51d028ce2bdfe83d389adccfb5ebc
Pio Rasch-Halvorsen <pio.rasch-halvorsen@fremtind.no>
2019-10-29 11:07:39 +0100

    style(core): fix typo in ValuePair tests
    affects: @fremtind/jkl-core

--- packages/core/src/utils/getValuePair.test.ts
+++ packages/core/src/utils/getValuePair.test.ts
@@ -2,14 +2,14 @@ import { getValuePair } from "./getValuePair";
 import { ValuePair } from "../index";
 
 describe("getValuePair", () => {
-    it("should convert a string to the correct SelectValuePair", () => {
+    it("should convert a string to the correct ValuePair", () => {
         const valuePair = getValuePair("hello");
 
         expect(valuePair.label).toEqual("hello");
         expect(valuePair.value).toEqual(valuePair.label);
     });
 
-    it("should return a SelectValuePair unchanged", () => {
+    it("should return a ValuePair unchanged", () => {
         const valuePair: ValuePair = {
             value: "hello",
             label: "hello",
