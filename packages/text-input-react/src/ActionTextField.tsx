6d9c3655ecd4467f1fe6a8ef41c90ae503604ce9
Vegard Sørlie <vegard.sorlie@fremtind.no>
2019-10-30 12:41:08 +0100

    refactor: refactor ActionTextField to support compact and errorLabel
    affects: @fremtind/jkl-text-input-react, @fremtind/jkl-text-input

--- packages/text-input-react/src/ActionTextField.tsx
+++ packages/text-input-react/src/ActionTextField.tsx
@@ -1,29 +1,71 @@
 import React, { MouseEventHandler } from "react";
-import { TextField } from "./TextField";
 import { ActionIcon, IconVariant } from "./ActionIcon";
 import { TextInputProps } from "./index";
+import { Label, SupportLabel } from "@fremtind/jkl-typography-react";
+import { getWidthAsStyle } from "./index";
 
 interface Props extends TextInputProps {
     action: IconVariant;
     onClick: MouseEventHandler<HTMLButtonElement>;
-    description: string;
     helpLabel?: string;
     errorLabel?: string;
+    type?: string;
 }
 
-export const ActionTextField = ({ action, onClick, description, ...rest }: Props) => {
+export const ActionTextField = ({
+    action,
+    onClick,
+    forceCompact,
+    className,
+    helpLabel,
+    errorLabel,
+    variant,
+    label,
+    type,
+    id,
+    placeholder,
+    readOnly,
+    value,
+    maxLength,
+    width,
+    ...rest
+}: Props) => {
+    const componentClassName = "jkl-text-field jkl-text-field--action".concat(
+        forceCompact ? " jkl-text-field--compact" : "",
+        className ? ` ${className}` : "",
+    );
+
+    const style = getWidthAsStyle(width, maxLength);
+
     return (
-        <div className="jkl-text-field--action">
-            <button
-                type="button"
-                className="jkl-text-field__icon"
-                onClick={onClick}
-                aria-label={description}
-                title={description}
-            >
-                <ActionIcon forceCompact action={action} />
-            </button>
-            <TextField {...rest} />
-        </div>
+        <label data-testid="jkl-text-field" className={componentClassName}>
+            <Label variant={variant} forceCompact={forceCompact}>
+                {label}
+            </Label>
+            <div className="jkl-text-field__input-wrapper">
+                <input
+                    type={type}
+                    aria-invalid={!!errorLabel}
+                    className="jkl-text-field__input"
+                    id={id}
+                    placeholder={placeholder}
+                    readOnly={readOnly}
+                    value={value}
+                    maxLength={maxLength}
+                    style={style}
+                    {...rest}
+                />
+                <button
+                    type="button"
+                    className="jkl-text-field__action-button"
+                    onClick={onClick}
+                    aria-label={action}
+                    title={action}
+                >
+                    <ActionIcon className="jkl-text-field__action-icon" action={action} />
+                </button>
+            </div>
+            <SupportLabel helpLabel={helpLabel} errorLabel={errorLabel} forceCompact={forceCompact} />
+        </label>
     );
 };
