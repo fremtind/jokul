5aa02cb35b844cbaf2ea12a30994090219700546
Pio Rasch-Halvorsen <pio.rasch-halvorsen@fremtind.no>
2019-10-31 13:44:19 +0100

    refactor(select-react): remove unused helper function and tests
    affects: @fremtind/jkl-select-react

--- packages/select-react/src/SelectValuePair.ts
+++
@@ -1,8 +0,0 @@
-export interface SelectValuePair {
-    value: string;
-    label: string;
-}
-
-export function getSelectValuePair(item: string | SelectValuePair): SelectValuePair {
-    return typeof item === "string" ? { value: item, label: item } : item;
-}
