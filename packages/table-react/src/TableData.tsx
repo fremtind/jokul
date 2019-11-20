4da6b09a9eb877a70de6b850956d0f27d2e31f58
Pio Rasch-Halvorsen <pio.rasch-halvorsen@fremtind.no>
2019-11-20 13:07:28 +0100

    test(table-react): add tests for Table component
    affects: @fremtind/jkl-table-react

--- packages/table-react/src/TableData.tsx
+++ packages/table-react/src/TableData.tsx
@@ -19,8 +19,13 @@ export function TableData({ data, row, isFirstCell }: Props) {
             {/* Only create an anchor in the first cell, so that screen readers doesn't read each cell as a link */}
             {/* The downside of this is that only the first cell can be right clicked and get the anchor context menu */}
             {isFirstCell && isAnchorRowData(row) ? (
-                <a className="jkl-sr-only" href={row.href} onClick={onAnchorClick}>
-                    {row.hrefLabel}{" "}
+                <a
+                    data-testid="jkl-table__screenreader-link"
+                    className="jkl-sr-only"
+                    href={row.href}
+                    onClick={onAnchorClick}
+                >
+                    {row.hrefLabel}
                 </a>
             ) : (
                 undefined
