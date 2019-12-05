2341c33519b43df1e669cb3ebc61ed0f89bca4ca
Frida Thorkildsen Solhaug <a504567@hty60090.sb1a.sparebank1.no>
2019-12-05 10:01:06 +0100

    fix(select): increase padding on value-items in select
    affects: @fremtind/jkl-select-react, @fremtind/jkl-select

--- packages/select-react/example/index.tsx
+++ packages/select-react/example/index.tsx
@@ -9,7 +9,12 @@ import "./index.scss";
 
 initTabListener();
 const SelectDemo = () => {
-    const items = ["The flower shop", "I have cancer", "Throwing the football", "ChirpChirpChirp"];
+    const items = [
+        "The flower shop is very far away from here",
+        "I have cancer",
+        "Throwing the football",
+        "ChirpChirpChirp",
+    ];
     const valuePairs = [{ value: "firstvalue", label: "Value 1" }, { value: "secondvalue", label: "Value 2" }];
     const years = [...Array(120)].map((_, i) => (i + 1900).toString()); // 1900 - 2019
 
