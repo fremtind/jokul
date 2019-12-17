f0a3a17fdf4f7d435c25a887ea2d8f8fe5073945
Pio Rasch-Halvorsen <pio.rasch-halvorsen@fremtind.no>
2019-12-17 09:32:30 +0100

    fix(portal): remove export of deleted Changelog component
    affects: @fremtind/portal

--- portal/src/components/index.ts
+++ portal/src/components/index.ts
@@ -1,4 +1,3 @@
-import { Changelog } from "./Changelog/Changelog";
 import { Footer } from "./Footer/Footer";
 import { Header } from "./Header/Header";
 import { Layout } from "./Layout/Layout";
@@ -6,4 +5,4 @@ import { ComponentLayout } from "./Layout/ComponentLayout";
 import { Menu } from "./Menu/Menu";
 import { Sandbox } from "./Sandbox/Sandbox";
 
-export { Changelog, Footer, Header, Layout, Menu, ComponentLayout, Sandbox };
+export { Footer, Header, Layout, Menu, ComponentLayout, Sandbox };
