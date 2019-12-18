4046fac8141db0a9fe40d7eb531965cad755de21
Pio Rasch-Halvorsen <pio.rasch-halvorsen@fremtind.no>
2019-12-18 10:51:49 +0100

    perf(portal): use dart-sass as scss compiler in the portal
    affects: @fremtind/portal

--- portal/gatsby-config.js
+++ portal/gatsby-config.js
@@ -8,7 +8,10 @@ module.exports = {
     },
     plugins: [
         "gatsby-plugin-typescript",
-        "gatsby-plugin-sass",
+        {
+            resolve: "gatsby-plugin-sass",
+            options: { implementation: require("sass") },
+        },
         {
             resolve: `gatsby-source-filesystem`,
             options: {
