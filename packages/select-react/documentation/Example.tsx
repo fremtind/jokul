89e83cd0f6bf8ae8a5e82e8636d5c6c329798d33
Pio Rasch-Halvorsen <pio.rasch-halvorsen@fremtind.no>
2020-01-03 10:23:26 +0100

    docs: tweak some component examples for portal
    affects: @fremtind/jkl-button-react, @fremtind/jkl-card-react, @fremtind/jkl-message-box-react,
    @fremtind/jkl-progress-bar-react, @fremtind/jkl-select-react, @fremtind/jkl-text-input-react

--- packages/select-react/documentation/Example.tsx
+++ packages/select-react/documentation/Example.tsx
@@ -1,84 +1,101 @@
 import React, { useState } from "react";
 import { Select, NativeSelect } from "../src";
 import { ToggleSwitch } from "@fremtind/jkl-toggle-switch-react";
+import { LabelVariant } from "@fremtind/jkl-core";
 import "@fremtind/jkl-core/core.min.css";
 import "@fremtind/jkl-select/select.min.css";
 import "@fremtind/jkl-toggle-switch/toggle-switch.min.css";
 import "./index.scss";
 
 const Example = () => {
-    const items = [
-        "The flower shop is very far away from here",
-        "I have cancer",
-        "Throwing the football",
-        "ChirpChirpChirp",
-    ];
     const valuePairs = [
         { value: "firstvalue", label: "Value 1" },
         { value: "secondvalue", label: "Value 2" },
     ];
     const years = [...Array(120)].map((_, i) => (i + 1900).toString()); // 1900 - 2019
 
-    const [favoriteScene, setFavoriteScene] = useState<string>();
     const [valuePair, setValuePair] = useState<string>();
-    const [darkMode, isDarkMode] = useState(false);
+    const [darkMode, setDarkMode] = useState(false);
+    const [hasError, setHasError] = useState(false);
+    const [isCompact, setIsCompact] = useState(false);
+    const [variant, setVariant] = useState<LabelVariant | undefined>("medium");
+    const typecheckAndSetVariant = (val: string) => {
+        if (val === "large" || val === "medium" || val === "small") {
+            setVariant(val);
+        } else {
+            setVariant(undefined);
+        }
+    };
 
     return (
         <section className={"example-page " + (!darkMode ? "example-page--light-mode" : "example-page--dark-mode")}>
-            <ToggleSwitch className={"toggle-switch"} inverted={darkMode} onChange={() => isDarkMode(!darkMode)}>
-                Dark Mode
-            </ToggleSwitch>
+            <fieldset className="example-page__controls">
+                <ToggleSwitch className={"toggle-switch"} inverted={darkMode} onChange={() => setDarkMode(!darkMode)}>
+                    Dark Mode
+                </ToggleSwitch>
+                <ToggleSwitch className={"toggle-switch"} inverted={darkMode} onChange={() => setHasError(!hasError)}>
+                    Vis feilmelding
+                </ToggleSwitch>
+                <ToggleSwitch className={"toggle-switch"} inverted={darkMode} onChange={() => setIsCompact(!isCompact)}>
+                    Kompakt variant
+                </ToggleSwitch>
+                <Select
+                    forceCompact
+                    variant="small"
+                    label="Etikettvariant"
+                    items={["large", "medium", "small"]}
+                    onChange={typecheckAndSetVariant}
+                    value={variant}
+                />
+            </fieldset>
+
             <Select
-                forceCompact
                 inline
-                className="jkl-spacing--top-3"
-                label="The Room scene"
-                defaultPrompt="Choose your favorite"
-                items={items}
-                onChange={setFavoriteScene}
-                value={favoriteScene}
-                helpLabel="The room is the greatest movie"
-                errorLabel={favoriteScene ? "You can't pick, they are all the best" : undefined}
-                variant="large"
+                forceCompact={isCompact}
+                variant={variant}
+                label="Select"
+                items={years}
+                value="1986"
+                helpLabel="Med strengverdier"
+                errorLabel={hasError ? "Beskrivende feilmelding" : undefined}
+                className="jkl-spacing--all-2"
             />
             <Select
                 inline
-                className="jkl-spacing--top-3"
-                label="Fødselsår"
-                items={years}
-                value="1986"
-                variant="small"
-                forceCompact
+                forceCompact={isCompact}
+                variant={variant}
+                label="Select"
+                defaultPrompt="Velg et alternativ"
+                items={valuePairs}
+                onChange={setValuePair}
+                value={valuePair}
+                helpLabel="Med verdipar"
+                errorLabel={hasError ? "Beskrivende feilmelding" : undefined}
+                className="jkl-spacing--all-2"
             />
 
             <NativeSelect
-                className="jkl-spacing--top-5"
-                label="Native select"
+                inline
+                forceCompact={isCompact}
+                variant={variant}
+                label="NativeSelect"
+                items={years}
+                value="1986"
+                helpLabel="Med strengverdier"
+                errorLabel={hasError ? "Beskrivende feilmelding" : undefined}
+                className="jkl-spacing--all-2"
+            />
+            <NativeSelect
+                inline
+                forceCompact={isCompact}
+                variant={variant}
+                label="NativeSelect"
                 items={valuePairs}
                 onChange={(e) => setValuePair(e.target.value)}
                 value={valuePair}
-                helpLabel="This uses value/label pairs"
-            />
-            <NativeSelect
-                className="jkl-spacing--top-3"
-                helpLabel="The room is the greatest movie"
-                errorLabel={favoriteScene ? "You can't pick, they are all the best" : undefined}
-                label="Native select"
-                items={items}
-                onChange={(e) => setFavoriteScene(e.target.value)}
-                placeholder="Choose your favorite"
-                variant="large"
-                value={favoriteScene}
-                forceCompact
-            />
-
-            <Select className="jkl-spacing--top-5" label="Favorite The Room scene" items={items} />
-            <Select
-                className="jkl-spacing--top-3"
-                label="Value pairs"
-                items={valuePairs}
-                onChange={(v) => console.log(v)}
-                helpLabel="This uses value/label pairs"
+                helpLabel="Med verdipar"
+                errorLabel={hasError ? "Beskrivende feilmelding" : undefined}
+                className="jkl-spacing--all-2"
             />
         </section>
     );
--- packages/text-input-react/documentation/Example.tsx
+++ packages/text-input-react/documentation/Example.tsx
@@ -1,28 +1,53 @@
 import React, { ChangeEvent, useState } from "react";
 import { TextArea, TextField, ActionTextField, InlineTextField } from "../src";
+import { ToggleSwitch } from "@fremtind/jkl-toggle-switch-react";
+import { Select } from "@fremtind/jkl-select-react";
+import { LabelVariant } from "@fremtind/jkl-core";
 import "@fremtind/jkl-core/core.min.css";
 import "@fremtind/jkl-text-input/text-input.min.css";
 import "./index.scss";
 
 const Example = () => {
     const [value, setValue] = useState("");
+    const [darkMode, setDarkMode] = useState(false);
+    const [hasError, setHasError] = useState(false);
+    const [isCompact, setIsCompact] = useState(false);
+    const [variant, setVariant] = useState<LabelVariant | undefined>("medium");
+    const typecheckAndSetVariant = (val: string) => {
+        if (val === "large" || val === "medium" || val === "small") {
+            setVariant(val);
+        } else {
+            setVariant(undefined);
+        }
+    };
     function handleChange(event: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) {
         setValue(event.target.value);
     }
 
     return (
-        <section className="jkl-spacing--top-3 jkl-spacing--bottom-3">
-            <div className="side-by-side jkl-spacing--bottom-2">
-                <pre>
-                    <code>{`forceCompact={true}`}</code>
-                </pre>
-                <pre>
-                    <code>{`forceCompact={false}`}</code>
-                </pre>
-            </div>
+        <section className={"example-page " + (!darkMode ? "example-page--light-mode" : "example-page--dark-mode")}>
+            <fieldset className="example-page__controls">
+                <ToggleSwitch className={"toggle-switch"} inverted={darkMode} onChange={() => setDarkMode(!darkMode)}>
+                    Dark Mode
+                </ToggleSwitch>
+                <ToggleSwitch className={"toggle-switch"} inverted={darkMode} onChange={() => setHasError(!hasError)}>
+                    Vis feilmelding
+                </ToggleSwitch>
+                <ToggleSwitch className={"toggle-switch"} inverted={darkMode} onChange={() => setIsCompact(!isCompact)}>
+                    Kompakt variant
+                </ToggleSwitch>
+                <Select
+                    forceCompact
+                    variant="small"
+                    label="Etikettvariant"
+                    items={["large", "medium", "small"]}
+                    onChange={typecheckAndSetVariant}
+                    value={variant}
+                />
+            </fieldset>
             <div className="side-by-side jkl-spacing--bottom-2">
                 <ActionTextField
-                    forceCompact
+                    forceCompact={isCompact}
                     action={{
                         icon: "clear",
                         label: "Utfør nullstilling",
@@ -33,6 +58,7 @@ const Example = () => {
                     onChange={(e) => setValue(e.target.value)}
                 />
                 <ActionTextField
+                    forceCompact={isCompact}
                     action={{
                         icon: "search",
                         label: "Skriv til konsoll",
@@ -45,12 +71,14 @@ const Example = () => {
             </div>
             <div className="side-by-side jkl-spacing--bottom-2">
                 <p className="jkl-small">
-                    Jeg tjener <InlineTextField type="number" maxLength={5} label="kronebeløp" forceCompact /> kroner i
+                    Jeg tjener{" "}
+                    <InlineTextField type="number" maxLength={5} label="kronebeløp" forceCompact={isCompact} /> kroner i
                     måneden.
                 </p>
                 <p className="jkl-p">
-                    Jeg tjener <InlineTextField invalid type="number" maxLength={5} label="kronebeløp" /> kroner i
-                    måneden.
+                    Jeg tjener{" "}
+                    <InlineTextField invalid type="number" maxLength={5} label="kronebeløp" forceCompact={isCompact} />{" "}
+                    kroner i måneden.
                 </p>
             </div>
             <div className="side-by-side jkl-spacing--bottom-2">
@@ -61,41 +89,56 @@ const Example = () => {
                     placeholder={"f.eks. Ola"}
                     variant="small"
                     helpLabel="La oss se..."
-                    forceCompact
+                    forceCompact={isCompact}
                     maxLength={10}
                 />
-                <TextField label="Fornavn" value={"Per"} onChange={handleChange} readOnly variant="small" />
+                <TextField
+                    forceCompact={isCompact}
+                    label="Fornavn"
+                    value={"Per"}
+                    onChange={handleChange}
+                    readOnly
+                    variant="small"
+                />
             </div>
 
             <div className="side-by-side jkl-spacing--bottom-2">
                 <TextField
-                    forceCompact
+                    forceCompact={isCompact}
                     label="Telefon"
                     type="tel"
                     value={value}
                     onChange={handleChange}
                     placeholder="999 99 999"
                 />
-                <TextField label="Telefon" type="tel" value={value} onChange={handleChange} placeholder="999 99 999" />
+                <TextField
+                    forceCompact={isCompact}
+                    label="Telefon"
+                    type="tel"
+                    value={value}
+                    onChange={handleChange}
+                    placeholder="999 99 999"
+                />
             </div>
 
             <div className="side-by-side jkl-spacing--bottom-5">
                 <TextField
-                    forceCompact
+                    forceCompact={isCompact}
                     label="Passord"
                     type="password"
                     value={value}
                     onChange={handleChange}
                     helpLabel="Passord er en vanlig form for autentisering"
-                    variant="large"
+                    variant={variant}
                 />
                 <TextField
+                    forceCompact={isCompact}
                     label="Passord"
                     type="password"
                     value={value}
                     onChange={handleChange}
                     helpLabel="Passord er en vanlig form for autentisering"
-                    variant="large"
+                    variant={variant}
                 />
             </div>
 
@@ -105,7 +148,7 @@ const Example = () => {
                     label="Svar med en kommentar"
                     value={value}
                     onChange={handleChange}
-                    forceCompact
+                    forceCompact={isCompact}
                     placeholder="Begrens deg til tre linjer"
                 />
                 <TextArea
@@ -113,12 +156,25 @@ const Example = () => {
                     label="Svar med en kommentar"
                     value={value}
                     onChange={handleChange}
+                    forceCompact={isCompact}
                     placeholder="Begrens deg til tre linjer"
                 />
             </div>
             <div className="side-by-side jkl-spacing--bottom-2">
-                <TextArea label="Din livshistorie" value={value} onChange={handleChange} variant="large" forceCompact />
-                <TextArea label="Din livshistorie" value={value} onChange={handleChange} variant="large" />
+                <TextArea
+                    label="Din livshistorie"
+                    value={value}
+                    onChange={handleChange}
+                    variant={variant}
+                    forceCompact={isCompact}
+                />
+                <TextArea
+                    label="Din livshistorie"
+                    value={value}
+                    onChange={handleChange}
+                    variant={variant}
+                    forceCompact={isCompact}
+                />
             </div>
         </section>
     );
