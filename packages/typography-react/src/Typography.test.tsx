d88c5818a89b3d249943180cb7350a4669534621
Vegard Sørlie <vegard.sorlie@fremtind.no>
2020-01-10 10:18:55 +0100

    test: add missing typography tests
    affects: @fremtind/jkl-typography-react

--- packages/typography-react/src/Typography.test.tsx
+++ packages/typography-react/src/Typography.test.tsx
@@ -6,19 +6,43 @@ describe("typography", () => {
     afterEach(cleanup);
     const text = "Hello Mr Universe";
 
-    [H1, H2, H3, H4, H5, Lead, Body, Small, Micro].forEach((element) => {
-        test(`should not explode`, () => {
-            const C = element;
+    const elements = [
+        { type: "heading 1", component: H1, className: "jkl-h1" },
+        { type: "heading 2", component: H2, className: "jkl-h2" },
+        { type: "heading 3", component: H3, className: "jkl-h3" },
+        { type: "heading 4", component: H4, className: "jkl-h4" },
+        { type: "heading 5", component: H5, className: "jkl-h5" },
+        { type: "lead paragraph", component: Lead, className: "jkl-lead" },
+        { type: "body paragraph", component: Body, className: "jkl-body" },
+        { type: "small paragraph", component: Small, className: "jkl-small" },
+        { type: "micro paragraph", component: Micro, className: "jkl-micro" },
+    ];
+
+    elements.forEach((element) => {
+        test(`should render ${element.type} with correct text`, () => {
+            const C = element.component;
             const { getByText } = render(<C>{text}</C>);
-            getByText(text);
+            const res = getByText(text);
+            expect(res).toHaveClass(element.className);
         });
     });
 
-    [H1, H2, H3, H4, H5].forEach((element) => {
-        test(`should not explode`, () => {
-            const C = element;
-            const { getByText } = render(<C styledAs="h1">{text}</C>);
-            getByText(text);
+    const styleElements = [
+        { element: "h1", className: "jkl-h1" },
+        { element: "h2", className: "jkl-h2" },
+        { element: "h3", className: "jkl-h3" },
+        { element: "h4", className: "jkl-h4" },
+        { element: "h5", className: "jkl-h5" },
+    ];
+
+    elements.slice(0, 5).forEach((element) => {
+        styleElements.forEach((styleElement) => {
+            test(`should render ${element.type} styled as ${styleElement.element}`, () => {
+                const C = element.component;
+                const { getByText } = render(<C styledAs={styleElement.element}>{text}</C>);
+                const res = getByText(text);
+                expect(res).toHaveClass(styleElement.className);
+            });
         });
     });
 });
