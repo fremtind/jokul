4d983b6dec70efd8ff60093f9e103a1344867f86
Leiv Fredrik Berge <leiv.fredrik.berge@sparebank1.no>
2020-01-16 08:56:32 +0100

    fix(dividerline): use intersection hook in dividerline
    affects: @fremtind/jkl-divider-line-react, @fremtind/jkl-react-hooks

--- packages/react-hooks/src/index.ts
+++ packages/react-hooks/src/index.ts
@@ -3,3 +3,4 @@ export { useClickOutside } from "./useClickOutside";
 export { useFocusOutside } from "./useFocusOutside";
 export { useKeyListener } from "./useKeyListener";
 export { useMutationObserver } from "./useMutationObserver/useMutationObserver";
+export { useIntersectionObserver } from "./useIntersectionObserver/useIntersectionObserver";
