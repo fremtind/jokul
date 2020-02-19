8f98177f875dbdb98586f5d25c551dc500d8237f
Pio Rasch-Halvorsen <pio.rasch-halvorsen@fremtind.no>
2020-02-19 09:41:45 +0100

    refactor(portal): simplify header structure and style
    affects: @fremtind/portal

--- portal/src/components/Header/header-react/src/header.tsx
+++ portal/src/components/Header/header-react/src/header.tsx
@@ -1,53 +1,15 @@
-import React, { ReactNode, MouseEventHandler } from "react";
-import classNames from "classnames";
+import React from "react";
+import { Link } from "gatsby";
 import { Logo } from "@fremtind/jkl-logo-react";
 import "../../header/header.scss";
 
-type validButtons = "primary" | "secondary" | "tertiary";
-
-type Action = {
-    type: validButtons;
-    name: string;
-    onClick: MouseEventHandler<HTMLButtonElement>;
-};
-
-type Clickable = {
-    href?: string;
-    onClick?: MouseEventHandler<HTMLAnchorElement>;
-};
-
-interface Props {
-    title?: string;
-    brand?: string;
-    children?: ReactNode;
-    className?: string;
-    action?: Action;
-    dark?: boolean;
-    clickable?: Clickable;
-}
-
-export const Header = ({ title, brand, children, className, dark, clickable }: Props) => {
-    const componentClassName = classNames("portal-header", className, { "portal-header--dark": dark });
-
-    return (
-        <header data-testid="portal-header" className={componentClassName}>
-            {title && (
-                <div data-testid="portal-header__title" className="portal-header__title">
-                    {clickable ? (
-                        <a className="portal-header__link" href={clickable.href} onClick={clickable.onClick}>
-                            {brand && <span className="portal-header__link--brand">{brand}</span>} {title}
-                        </a>
-                    ) : (
-                        <span>
-                            {brand && <span className="portal-header__link--brand">{brand}</span>} {title}
-                        </span>
-                    )}
-                </div>
-            )}
-            {children}
-            <div className="portal-header__logo">
-                <Logo isSymbol centered={false} />
-            </div>
-        </header>
-    );
-};
+export const Header = () => (
+    <header className="portal-header">
+        <Link to="/" className="portal-header__title">
+            <strong>Jøkul</strong> Designsystem
+        </Link>
+        <div className="portal-header__logo">
+            <Logo isSymbol centered={false} />
+        </div>
+    </header>
+);
