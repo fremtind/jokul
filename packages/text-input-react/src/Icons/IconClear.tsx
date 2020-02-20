19761c44fdea534fdc177a3753db10e8d0b53870
Pio Rasch-Halvorsen <pio.rasch-halvorsen@fremtind.no>
2020-02-20 15:33:47 +0100

    fix(text-input-react): adjust size of "clear" action icon
    affects: @fremtind/jkl-text-input-react

--- packages/text-input-react/src/Icons/IconClear.tsx
+++ packages/text-input-react/src/Icons/IconClear.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export const IconClear = () => (
     <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-        <path d="M1 23L23 1" stroke="currentColor" strokeWidth="1.5" />
-        <path d="M1 1L23 23" stroke="currentColor" strokeWidth="1.5" />
+        <path d="M3 21L21 3" stroke="currentColor" strokeWidth="1.5" />
+        <path d="M3 3L21 21" stroke="currentColor" strokeWidth="1.5" />
     </svg>
 );
