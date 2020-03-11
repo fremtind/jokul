41d18c22cf4e6fc3026dc65cc6558814ff5033c1
Pio Rasch-Halvorsen <pio.rasch-halvorsen@fremtind.no>
2020-03-11 19:52:50 +0100

    feat(logo-react): remove LogoStamp components
    affects: @fremtind/jkl-logo-react
    
    BREAKING CHANGE:
    LogoStamp components are no longer available for use

--- packages/logo-react/src/index.ts
+++ packages/logo-react/src/index.ts
@@ -1,7 +1,1 @@
-import { Logo } from "./Logo";
-import { LogoStampTechnology } from "./stamp/LogoStampTechnology";
-import { LogoStampDistributor } from "./stamp/LogoStampDistributor";
-import { LogoStampInnovation } from "./stamp/LogoStampInnovation";
-import { LogoStampInsurance } from "./stamp/LogoStampInsurance";
-
-export { Logo, LogoStampTechnology, LogoStampDistributor, LogoStampInnovation, LogoStampInsurance };
+export { Logo } from "./Logo";
