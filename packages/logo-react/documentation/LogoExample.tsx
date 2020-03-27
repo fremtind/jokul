e067d18f385724ad34ab71f7f94cdeafbfb527a9
Sercan Leylek <leylek.sercan@gmail.com>
2020-03-27 14:40:25 +0100

    fix(logo-react): use .logo-example on LogoExample
    affects: @fremtind/jkl-logo-react, @fremtind/jkl-logo

--- packages/logo-react/documentation/LogoExample.tsx
+++ packages/logo-react/documentation/LogoExample.tsx
@@ -1,9 +1,11 @@
 import React from "react";
 import { ExampleComponentProps } from "@fremtind/jkl-portal-components";
 import { Logo } from "../src";
+import "./LogoExample.scss";
 
 export const LogoExample = ({ boolValues }: ExampleComponentProps) => (
     <Logo
+        className="logo-example"
         animated={boolValues && boolValues["Animert"]}
         isSymbol={boolValues && boolValues["Kun symbol"]}
         centered={boolValues && boolValues["Sentrert"]}
