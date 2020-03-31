2253fe873975028d74c615661a9edd4d8e0bd47b
Eduardo Ibacache Rodriguez <a504503@hty60081.sb1a.sparebank1.no>
2020-03-31 13:51:35 +0200

    feat: new layout, with icons and darkmode
    affects: @fremtind/jkl-message-box-react, @fremtind/jkl-message-box
    
    added dynamic color on icons

--- packages/message-box-react/documentation/Example.tsx
+++ packages/message-box-react/documentation/Example.tsx
@@ -2,9 +2,7 @@ import React from "react";
 import { ExampleComponentProps } from "@fremtind/jkl-portal-components";
 import { ErrorMessage, InfoMessage, SuccessMessage, WarningMessage } from "../src";
 
-
 export const Example: React.FC<ExampleComponentProps> = ({ boolValues, choiceValues }) => {
-//export function Example({ boolValues, choiceValues }: ExampleComponentProps) {
     const types = {
         Infomelding: InfoMessage,
         Suksessmelding: SuccessMessage,
@@ -15,10 +13,14 @@ export const Example: React.FC<ExampleComponentProps> = ({ boolValues, choiceVal
     //@ts-ignore
     const C = types[choiceValues["Type"]] || InfoMessage;
     return (
-        <C fullWidth={boolValues && boolValues["Full bredde"]} title={choiceValues && choiceValues["Type"]} inverted={boolValues && boolValues["Invertert"]}>
+        <C
+            fullWidth={boolValues && boolValues["Full bredde"]}
+            title={choiceValues && choiceValues["Type"]}
+            inverted={boolValues && boolValues["Invertert"]}
+        >
             Cupidatat Lorem incididunt incididunt in non mollit cillum Lorem eiusmod sunt magna.
         </C>
     );
-}
+};
 
 export default Example;
