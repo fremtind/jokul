d44985ffadcc1149a98c83579c6c4812dd4f8dad
Pio Rasch-Halvorsen <pio.rasch-halvorsen@fremtind.no>
2020-04-15 09:33:27 +0200

    feat: add inverted mode to error label
    affects: @fremtind/jkl-core, @fremtind/jkl-typography-react

--- packages/typography-react/src/SupportLabel.tsx
+++ packages/typography-react/src/SupportLabel.tsx
@@ -8,14 +8,16 @@ interface Props {
     forceCompact?: boolean;
     className?: string;
     srOnly?: boolean;
+    inverted?: boolean;
 }
 
-export const SupportLabel = ({ id, helpLabel, errorLabel, forceCompact, className, srOnly }: Props) => {
+export const SupportLabel = ({ id, helpLabel, errorLabel, forceCompact, className, srOnly, inverted }: Props) => {
     const componentClassName = classNames("jkl-form-support-label", className, {
         "jkl-form-support-label--compact": forceCompact,
         "jkl-form-support-label--error": errorLabel,
         "jkl-form-support-label--help": !errorLabel,
         "jkl-form-support-label--sr-only": srOnly,
+        "jkl-form-support-label--inverted": inverted,
     });
 
     if (errorLabel || helpLabel) {
