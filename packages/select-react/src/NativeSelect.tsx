00d2c0f46b390688ddb0198d1e894cf82a147168
Pio Rasch-Halvorsen <pio.rasch-halvorsen@fremtind.no>
2020-04-30 10:34:22 +0200

    feat(native select): add support for refs
    affects: @fremtind/jkl-select-react

--- packages/select-react/src/NativeSelect.tsx
+++ packages/select-react/src/NativeSelect.tsx
@@ -1,6 +1,6 @@
 /* eslint "jsx-a11y/no-onchange": 0 */
 
-import React, { FocusEventHandler, ChangeEventHandler, useState } from "react";
+import React, { FocusEventHandler, ChangeEventHandler, useState, forwardRef } from "react";
 import nanoid from "nanoid";
 import { LabelVariant, ValuePair, getValuePair } from "@fremtind/jkl-core";
 import { Label, SupportLabel } from "@fremtind/jkl-typography-react";
@@ -26,75 +26,81 @@ interface Props {
     onBlur?: FocusEventHandler<HTMLSelectElement>;
 }
 
-export function NativeSelect({
-    id,
-    label,
-    items,
-    className = "",
-    inline = false,
-    helpLabel,
-    errorLabel,
-    variant,
-    placeholder,
-    value,
-    forceCompact,
-    inverted,
-    onChange,
-    onBlur,
-    onFocus,
-}: Props) {
-    // If no value is given, set it to first item, or to empty string if there is a placeholder
-    if (!value) {
-        if (!placeholder && items.length) {
-            value = getValuePair(items[0]).value;
+export const NativeSelect = forwardRef<HTMLSelectElement, Props>(
+    (
+        {
+            id,
+            label,
+            items,
+            className = "",
+            inline = false,
+            helpLabel,
+            errorLabel,
+            variant,
+            placeholder,
+            value,
+            forceCompact,
+            inverted,
+            onChange,
+            onBlur,
+            onFocus,
+        },
+        ref,
+    ) => {
+        // If no value is given, set it to first item, or to empty string if there is a placeholder
+        if (!value) {
+            if (!placeholder && items.length) {
+                value = getValuePair(items[0]).value;
+            }
         }
-    }
 
-    const componentClassName = classNames("jkl-select", className, {
-        "jkl-select--inline": inline,
-        "jkl-select--compact": forceCompact,
-        "jkl-select--inverted": inverted,
-        "jkl-select--invalid": !!errorLabel,
-    });
+        const componentClassName = classNames("jkl-select", className, {
+            "jkl-select--inline": inline,
+            "jkl-select--compact": forceCompact,
+            "jkl-select--inverted": inverted,
+            "jkl-select--invalid": !!errorLabel,
+        });
 
-    const defaultValue = value ? undefined : "";
+        const defaultValue = value ? undefined : "";
 
-    const [uid] = useState(id || `jkl-select-${nanoid(8)}`);
+        const [uid] = useState(id || `jkl-select-${nanoid(8)}`);
 
-    return (
-        <div data-testid="jkl-select" className={componentClassName}>
-            <Label standAlone htmlFor={uid} variant={variant} forceCompact={forceCompact}>
-                {label}
-            </Label>
-            <div className="jkl-select__outer-wrapper">
-                <select
-                    id={uid}
-                    value={value}
-                    defaultValue={defaultValue}
-                    className="jkl-select__button"
-                    onChange={onChange}
-                    onBlur={onBlur || onChange}
-                    onFocus={onFocus}
-                >
-                    {placeholder && !value && (
-                        <option disabled value="">
-                            {placeholder}
-                        </option>
-                    )}
-                    {items.map(getValuePair).map((item) => (
-                        <option
-                            data-testid="jkl-select__option"
-                            className="jkl-select__option"
-                            key={item.value}
-                            value={item.value}
-                        >
-                            {item.label}
-                        </option>
-                    ))}
-                </select>
-                <ExpandArrow className="jkl-select__arrow" />
+        return (
+            <div data-testid="jkl-select" className={componentClassName}>
+                <Label standAlone htmlFor={uid} variant={variant} forceCompact={forceCompact}>
+                    {label}
+                </Label>
+                <div className="jkl-select__outer-wrapper">
+                    <select
+                        ref={ref}
+                        id={uid}
+                        value={value}
+                        defaultValue={defaultValue}
+                        className="jkl-select__button"
+                        onChange={onChange}
+                        onBlur={onBlur || onChange}
+                        onFocus={onFocus}
+                    >
+                        {placeholder && !value && (
+                            <option disabled value="">
+                                {placeholder}
+                            </option>
+                        )}
+                        {items.map(getValuePair).map((item) => (
+                            <option
+                                data-testid="jkl-select__option"
+                                className="jkl-select__option"
+                                key={item.value}
+                                value={item.value}
+                            >
+                                {item.label}
+                            </option>
+                        ))}
+                    </select>
+                    <ExpandArrow className="jkl-select__arrow" />
+                </div>
+                <SupportLabel helpLabel={helpLabel} errorLabel={errorLabel} forceCompact={forceCompact} />
             </div>
-            <SupportLabel helpLabel={helpLabel} errorLabel={errorLabel} forceCompact={forceCompact} />
-        </div>
-    );
-}
+        );
+    },
+);
