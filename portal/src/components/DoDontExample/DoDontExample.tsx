122474c618ffaafde8e1117df2e985df09166cda
Jo Emil Holen <jo.emil.holen@fremtind.no>
2020-05-14 13:13:20 +0200

    feat(portal): lagt til do's and dont's
    affects: @fremtind/jkl-alert-message-react, @fremtind/portal
    
    to support the design, I've added a fullWidth prop to the DoDontExample
    https://www.figma.com/file/dWhCsnlMAmJEBPyGnRrse5RM/J%C3%B8kul-portalen?node-id=2001%3A8092

--- portal/src/components/DoDontExample/DoDontExample.tsx
+++ portal/src/components/DoDontExample/DoDontExample.tsx
@@ -8,9 +8,10 @@ interface Props {
     type: "do" | "dont";
     image: string;
     description?: string;
+    fullWidth?: boolean;
 }
 
-export function DoDontExample({ type, image, description }: Props) {
+export function DoDontExample({ type, image, description, fullWidth }: Props) {
     const heading = type === "do" ? "Riktig" : "Feil";
     const altText = description ? `${heading}: ${description}` : `${heading} bruk`;
     const headingClass = classNames({
@@ -18,8 +19,12 @@ export function DoDontExample({ type, image, description }: Props) {
         "jkl-portal-do-dont-example__heading--do": type === "do",
         "jkl-portal-do-dont-example__heading--dont": type === "dont",
     });
+    const containerClass = classNames("jkl-portal-do-dont-example", {
+        "jkl-portal-do-dont-example--fullwidth": fullWidth,
+    });
+
     return (
-        <aside className="jkl-portal-do-dont-example">
+        <aside className={containerClass}>
             <img className="jkl-portal-do-dont-example__image" src={withPrefix(image)} alt={altText} />
             <p className={headingClass}>{heading}</p>
             <p className="jkl-portal-do-dont-example__description">{description}</p>
