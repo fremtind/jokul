928222b619e5964c45d9fc33a4c980ba8d68750c
Jo Emil Holen <jo-emil@outlook.com>
2020-05-20 09:22:16 +0200

    fix: export interface in an actual functioning manner
    affects: @fremtind/jkl-button-react
    
    Co-authored-by: Pio Rasch-Halvorsen <pio.rasch-halvorsen@fremtind.no>

--- packages/text-input-react/src/index.ts
+++ packages/text-input-react/src/index.ts
@@ -1,3 +1,9 @@
-export { TextArea, Props as TextAreaPropsInterface } from "./TextArea";
-export { TextInput, Props as TextInputPropsInterface } from "./TextInput";
-export { BaseInputField, Props as BaseInputPropsInterface } from "./BaseInputField";
+import { TextArea, Props as TextAreaPropsInterface } from "./TextArea";
+import { TextInput, Props as TextInputPropsInterface } from "./TextInput";
+import { BaseInputField, Props as BaseInputPropsInterface } from "./BaseInputField";
+
+interface BaseInputProps extends BaseInputPropsInterface {}
+interface TextInputProps extends TextInputPropsInterface {}
+interface TextAreaProps extends TextAreaPropsInterface {}
+
+export { TextArea, TextAreaProps, TextInput, TextInputProps, BaseInputField, BaseInputProps };
