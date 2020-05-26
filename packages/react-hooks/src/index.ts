dcde26a888833ecf1ba6646ea9c7e6086d13e57c
Leiv Fredrik Berge <leiv.fredrik.berge@sparebank1.no>
2020-05-26 15:05:23 +0200

    feat: add hook to wrap scrollintoview
    affects: @fremtind/jkl-portal-components, @fremtind/jkl-react-hooks
    
    ISSUES CLOSED: #967
    
    fix: allow all htmlelements to be scrolledto
    
    affects: @fremtind/jkl-react-hooks
    
    test: add useScrollIntoView test
    
    affects: @fremtind/jkl-react-hooks
    
    test: clean up test
    
    affects: @fremtind/jkl-react-hooks

--- packages/react-hooks/src/index.ts
+++ packages/react-hooks/src/index.ts
@@ -5,4 +5,5 @@ export { useKeyListener } from "./useKeyListener";
 export { useMutationObserver } from "./useMutationObserver/useMutationObserver";
 export { useIntersectionObserver } from "./useIntersectionObserver/useIntersectionObserver";
 export { useScreen } from "./useScreen/useScreen";
+export { useScrollIntoView } from "./useScrollIntoView/useScrollIntoView";
 export { initialState as initialScreenState } from "./useScreen/state";
---
+++ packages/react-hooks/src/useScrollIntoView/useScrollIntoView.tsx
@@ -0,0 +1,27 @@
+import { useEffect, RefObject } from "react";
+
+type ValueOf<T> = T[keyof T];
+
+interface Props {
+    ref: RefObject<ValueOf<HTMLElementTagNameMap>>;
+    timeout?: number;
+    autoScroll?: boolean;
+    options?: ScrollIntoViewOptions;
+}
+
+export const useScrollIntoView = ({ ref, timeout = 0, autoScroll = true, options = { behavior: "smooth" } }: Props) => {
+    const scrollIntoView = () => {
+        if (ref && ref.current) {
+            ref.current.scrollIntoView(options);
+        }
+    };
+    useEffect(() => {
+        if (!autoScroll) {
+            return;
+        }
+        const scrollFn = setTimeout(scrollIntoView, timeout);
+        return () => clearTimeout(scrollFn);
+    }, [ref, timeout, autoScroll]);
+
+    return [scrollIntoView];
+};
