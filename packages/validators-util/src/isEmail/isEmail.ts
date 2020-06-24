c738269e64d3e43eed41cd354f98afa811a0695c
Pio Rasch-Halvorsen <pio.rasch-halvorsen@fremtind.no>
2020-06-24 10:55:31 +0200

    chore(validators): remove extraneous files
    affects: @fremtind/jkl-validators-util

--- packages/validators-util/src/isEmail/isEmail.ts
+++
@@ -1,3 +0,0 @@
-export const EMAIL_REGEX = /^[a-zA-ZæøåÆØÅ0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-ZæøåÆØÅ0-9-]+(?:\.[a-zA-Z0-9-]+)+$/;
-
-export const isEmail = (value: string) => EMAIL_REGEX.test(value);
--- packages/validators-util/src/isRegistrationNumber/isRegistrationNumber.ts
+++
@@ -1,3 +0,0 @@
-export const REGNR_REGEX = /^[a-zA-Z]{2}\d{5}/;
-
-export const isRegistrationNumber = (value: string) => REGNR_REGEX.test(value);
