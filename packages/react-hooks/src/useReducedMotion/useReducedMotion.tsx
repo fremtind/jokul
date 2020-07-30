fffcfbeff482f36fe9d4f8922478b4ed71303c9b
Leiv Fredrik Berge <leiv.fredrik.berge@sparebank1.no>
2020-07-30 15:16:22 +0200

    chore: stronger window check
    affects: @fremtind/jkl-react-hooks

--- packages/react-hooks/src/useReducedMotion/useReducedMotion.tsx
+++ packages/react-hooks/src/useReducedMotion/useReducedMotion.tsx
@@ -2,7 +2,7 @@ import { useState, useEffect } from "react";
 
 const PREFERS_REDUCED_MOTION = "(prefers-reduced-motion: no-preference)";
 const getInitialState = () => {
-    if (typeof window !== "undefined") {
+    if (typeof window !== "undefined" && window.matchMedia) {
         return !window.matchMedia(PREFERS_REDUCED_MOTION).matches;
     }
     return false;
@@ -12,10 +12,14 @@ export const useReducedMotion = (): boolean => {
     const [prefersReducedMotion, setPrefersReducedMotion] = useState(getInitialState());
 
     useEffect(() => {
-        const mediaQueryList = window?.matchMedia(PREFERS_REDUCED_MOTION);
-        const listener = (event: MediaQueryListEvent) => setPrefersReducedMotion(!event.matches);
+        let mediaQueryList: MediaQueryList;
+        let listener: (event: MediaQueryListEvent) => void;
+        if (typeof window !== "undefined" && window.matchMedia) {
+            mediaQueryList = window.matchMedia(PREFERS_REDUCED_MOTION);
+            listener = (event: MediaQueryListEvent) => setPrefersReducedMotion(!event.matches);
+            mediaQueryList?.addListener(listener);
+        }
 
-        mediaQueryList?.addListener(listener);
         return () => {
             mediaQueryList?.removeListener(listener);
         };
