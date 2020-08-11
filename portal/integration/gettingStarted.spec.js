9bd291b01bcb83add440d60073b029f5c9aa4c72
eduardodir <56429532+eduardodir@users.noreply.github.com>
2020-08-11 12:45:15 +0200

    Update portal/integration/gettingStarted.spec.js
    Co-authored-by: Leiv Fredrik Berge <46530368+lfbergee@users.noreply.github.com>

--- portal/integration/gettingStarted.spec.js
+++ portal/integration/gettingStarted.spec.js
@@ -7,7 +7,7 @@ context("Profile page", () => {
     it("Profile page should work", () => {
         cy.checkPortalPage("Designsystemprosessen", "komigang/prosessen", "Designsystemprosessen");
         cy.checkPortalPage("For-designere", "komigang/design", "Kom i gang som designer");
-        cy.checkPortalPage("For-utviklere", "komigang/utvikling", "Slik bruker du Jøkul");
+        cy.checkPortalPage("For-utviklere", "komigang/utvikling", "Kom i gang som utviklere");
         cy.checkPortalPage("Tester", "komigang/tester", "Kom i gang med tester");
         cy.checkPortalPage("Pull-requests", "komigang/pullrequests", "Bidra til Jøkul på GitHub");
         cy.checkPortalPage("Portalen", "komigang/portalen", "Dokumentasjon til portalen");
