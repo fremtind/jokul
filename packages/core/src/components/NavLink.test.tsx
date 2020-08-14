03247ebd6cb29402f50f3006161839138a167943
Pio Rasch-Halvorsen <pio.rasch-halvorsen@fremtind.no>
2020-08-14 10:08:01 +0200

    test(links): add tests for NavLink
    affects: @fremtind/jkl-core

---
+++ packages/core/src/components/NavLink.test.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import { NavLink } from "./NavLink";
+
+describe("NavLink", () => {
+    it("should render with the correct text and style", () => {
+        render(<NavLink>Some link</NavLink>);
+
+        expect(screen.getByText("Some link")).toBeInTheDocument();
+        expect(screen.getByText("Some link")).toHaveClass("jkl-nav-link");
+    });
+
+    it("should have the correct href attribute", () => {
+        render(<NavLink href="somepage.com">Some link</NavLink>);
+
+        expect(screen.getByText("Some link")).toHaveAttribute("href", "somepage.com");
+    });
+
+    it("should get the correct style when active", () => {
+        render(<NavLink active>Some link</NavLink>);
+
+        expect(screen.getByText("Some link")).toHaveClass("jkl-nav-link--active");
+    });
+
+    it("should get the correct style when inverted", () => {
+        render(<NavLink inverted>Some link</NavLink>);
+
+        expect(screen.getByText("Some link")).toHaveClass("jkl-nav-link--inverted");
+    });
+
+    it("should get the supplied className", () => {
+        render(<NavLink className="my-class">Some link</NavLink>);
+
+        expect(screen.getByText("Some link")).toHaveClass("my-class");
+    });
+});
