97858dc72c556b2162743ce2d92ad764506df674
Sercan Leylek <leylek.sercan@gmail.com>
2020-09-22 15:35:44 +0200

    feat(button-react): enabling forwardRef on button components
    affects: @fremtind/jkl-button-react
    
    BREAKING CHANGE:
    Ref now points to button element
    
    ISSUES CLOSED: #1362

--- packages/button-react/src/Button.tsx
+++ packages/button-react/src/Button.tsx
@@ -1,4 +1,4 @@
-import React, { HTMLAttributes } from "react";
+import React, { forwardRef, HTMLAttributes } from "react";
 import classNames from "classnames";
 
 export interface Props extends Exclude<HTMLAttributes<HTMLButtonElement>, "disabled"> {
@@ -8,25 +8,28 @@ export interface Props extends Exclude<HTMLAttributes<HTMLButtonElement>, "disab
 
 type ValidButtons = "primary" | "secondary" | "tertiary";
 
-function makeButtonComponent(buttonType: ValidButtons) {
-    return function button({ children, className, forceCompact, inverted, ...rest }: Props) {
-        const componentClassName = classNames(
-            "jkl-button",
-            "jkl-button--" + buttonType,
-            {
-                "jkl-button--compact": forceCompact,
-                "jkl-button--inverted": inverted,
-            },
-            className,
-        );
+const makeButtonComponent = (buttonType: ValidButtons) => {
+    const button = forwardRef<HTMLButtonElement, Props>(
+        ({ children, className, forceCompact, inverted, ...rest }, ref) => {
+            const componentClassName = classNames(
+                "jkl-button",
+                "jkl-button--" + buttonType,
+                {
+                    "jkl-button--compact": forceCompact,
+                    "jkl-button--inverted": inverted,
+                },
+                className,
+            );
 
-        return (
-            <button className={componentClassName} {...rest}>
-                {children}
-            </button>
-        );
-    };
-}
+            return (
+                <button className={componentClassName} {...rest} ref={ref}>
+                    {children}
+                </button>
+            );
+        },
+    );
+    return button;
+};
 
 export const PrimaryButton = makeButtonComponent("primary");
 export const SecondaryButton = makeButtonComponent("secondary");
