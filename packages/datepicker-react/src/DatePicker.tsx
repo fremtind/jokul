27aeb145715fabb024139d582d200a18efb6c2de
Sercan Leylek <leylek.sercan@gmail.com>
2020-09-25 13:58:00 +0200

    feat(datepicker-react): return event when onChange, onBlur, onFocus triggered
    affects: @fremtind/jkl-datepicker-react
    
    ISSUES CLOSED: #1386

--- packages/datepicker-react/src/DatePicker.tsx
+++ packages/datepicker-react/src/DatePicker.tsx
@@ -14,7 +14,9 @@ export interface ChangeDate {
     date: Date;
 }
 
-type DateEventHandler = (date?: Date) => void;
+type onChangeEventHandler = (date?: Date, e?: ChangeEvent) => void;
+type onBlurEventHandler = (date?: Date, e?: FocusEvent) => void;
+type onFocusEventHandler = (date?: Date, e?: FocusEvent) => void;
 
 interface Props {
     label?: string;
@@ -37,9 +39,9 @@ interface Props {
     inverted?: boolean;
     disableBeforeDate?: Date;
     disableAfterDate?: Date;
-    onChange?: DateEventHandler;
-    onFocus?: DateEventHandler;
-    onBlur?: DateEventHandler;
+    onChange?: onChangeEventHandler;
+    onFocus?: onFocusEventHandler;
+    onBlur?: onBlurEventHandler;
 }
 
 export function DatePicker({
@@ -138,37 +140,37 @@ export function DatePicker({
     const handleBlur = (e: FocusEvent) => {
         const nextFocusIsInside = componentRef.current && componentRef.current.contains(e.relatedTarget as Node);
         if (onBlur && !nextFocusIsInside) {
-            onBlur(date);
+            onBlur(date, e);
         }
     };
 
     const handleFocus = (e: FocusEvent) => {
         const prevFocusIsInside = componentRef.current && componentRef.current.contains(e.relatedTarget as Node);
         if (onFocus && !prevFocusIsInside) {
-            onFocus(date);
+            onFocus(date, e);
         }
     };
 
-    function dateHasChanged(date: Date | undefined, newDate: Date) {
-        return !date || !isSameDay(date, newDate);
-    }
-
-    function handleChange(event: ChangeEvent<HTMLInputElement>) {
+    const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
         const newDateString = event.target.value;
         const newDate = parseDateString(newDateString);
         // Only set the date if it is a valid date
         if (newDate && dateHasChanged(date, newDate)) {
             setDate(newDate);
             if (onChange) {
-                onChange(newDate);
+                onChange(newDate, event);
             }
         } else if (newDateString === "") {
             setDate(undefined);
             if (onChange) {
-                onChange(undefined);
+                onChange(undefined, event);
             }
         }
         setDateString(newDateString);
+    };
+
+    function dateHasChanged(date: Date | undefined, newDate: Date) {
+        return !date || !isSameDay(date, newDate);
     }
 
     function onClickCalendarDay(event: ChangeEvent<ChangeDate>) {
