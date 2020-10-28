5e43a3ca1fc3ad25bbb522deb763821f52462f88
Leiv Fredrik Berge <leiv.fredrik.berge@sparebank1.no>
2020-10-28 15:00:31 +0100

    fix: update related to match sorter
    affects: @fremtind/jkl-text-input-react

--- packages/text-input-react/src/autosuggest/utils.ts
+++ packages/text-input-react/src/autosuggest/utils.ts
@@ -1,4 +1,4 @@
-import matchSorter from "match-sorter";
+import { matchSorter } from "match-sorter";
 
 export function filter<T>(allItems: T[], inputValue: string | null, keys?: string[]) {
     const options = keys ? { keys } : {};
