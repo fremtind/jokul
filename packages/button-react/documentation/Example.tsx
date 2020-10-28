39dd4419f8372ad698fbab897b0527c8fc3d5de7
Leiv Fredrik Berge <leiv.fredrik.berge@sparebank1.no>
2020-10-28 17:41:46 +0100

    feat(button): add action button
    affects: @fremtind/jkl-button-react, @fremtind/jkl-button
    
    split documentation to make visual regression testing easier, refactor test
    
    ISSUES CLOSED: #1485

--- packages/button-react/documentation/Example.tsx
+++ packages/button-react/documentation/Example.tsx
@@ -1,37 +1,60 @@
 import React from "react";
 import { ExampleComponentProps } from "@fremtind/jkl-portal-components";
-import { PrimaryButton, SecondaryButton, TertiaryButton } from "../src";
+import { PrimaryButton, SecondaryButton, TertiaryButton, ActionButton } from "../src";
 
 function onClick() {
     console.log("Hello!");
 }
 
+export const Primary: React.FC<ExampleComponentProps> = ({ boolValues }) => (
+    <PrimaryButton
+        inverted={boolValues && boolValues["Invertert"]}
+        forceCompact={boolValues && boolValues["Kompakt"]}
+        onClick={onClick}
+        className="jkl-spacing--right-1"
+    >
+        Send
+    </PrimaryButton>
+);
+
+export const Secondary: React.FC<ExampleComponentProps> = ({ boolValues }) => (
+    <SecondaryButton
+        inverted={boolValues && boolValues["Invertert"]}
+        forceCompact={boolValues && boolValues["Kompakt"]}
+        onClick={onClick}
+        className="jkl-spacing--right-1"
+    >
+        Gå videre
+    </SecondaryButton>
+);
+
+export const Tertiary: React.FC<ExampleComponentProps> = ({ boolValues }) => (
+    <TertiaryButton
+        inverted={boolValues && boolValues["Invertert"]}
+        forceCompact={boolValues && boolValues["Kompakt"]}
+        onClick={onClick}
+        className="jkl-spacing--right-1"
+    >
+        Avbryt
+    </TertiaryButton>
+);
+export const Action: React.FC<ExampleComponentProps> = ({ boolValues }) => (
+    <ActionButton
+        inverted={boolValues && boolValues["Invertert"]}
+        forceCompact={boolValues && boolValues["Kompakt"]}
+        onClick={onClick}
+        className="jkl-spacing--right-1"
+    >
+        Kjøp
+    </ActionButton>
+);
 export const Example: React.FC<ExampleComponentProps> = ({ boolValues }) => {
     return (
         <>
-            <PrimaryButton
-                inverted={boolValues && boolValues["Invertert"]}
-                forceCompact={boolValues && boolValues["Kompakt"]}
-                onClick={onClick}
-                className="jkl-spacing--right-1"
-            >
-                Send
-            </PrimaryButton>
-            <SecondaryButton
-                inverted={boolValues && boolValues["Invertert"]}
-                forceCompact={boolValues && boolValues["Kompakt"]}
-                onClick={onClick}
-                className="jkl-spacing--right-1"
-            >
-                Gå videre
-            </SecondaryButton>
-            <TertiaryButton
-                inverted={boolValues && boolValues["Invertert"]}
-                forceCompact={boolValues && boolValues["Kompakt"]}
-                onClick={onClick}
-            >
-                Avbryt
-            </TertiaryButton>
+            <Primary boolValues={boolValues} />
+            <Secondary boolValues={boolValues} />
+            <Tertiary boolValues={boolValues} />
+            <Action boolValues={boolValues} />
         </>
     );
 };
