306a34c2e25831494c24169b950c21513d512ae8
Leiv Fredrik Berge <leiv.fredrik.berge@sparebank1.no>
2020-10-30 10:02:40 +0100

    test(flipper): improve a11y test
    affects: @fremtind/jkl-flipper-react

--- packages/flipper-react/src/Flipper.test.tsx
+++ packages/flipper-react/src/Flipper.test.tsx
@@ -6,8 +6,20 @@ import { axe } from "jest-axe";
 test("should render to the DOM", () => {
     render(<Flipper firstBorn="Hello" lastBorn="World" showFirst />);
 
-    expect(screen.getByText("Hello")).toBeInTheDocument();
-    expect(screen.getByText("World")).toBeInTheDocument();
+    const firstChild = screen.getByText("Hello");
+    const secondChild = screen.getByText("World");
+    expect(firstChild).toBeInTheDocument();
+    expect(firstChild).toHaveAttribute("aria-hidden", "false");
+
+    expect(secondChild).toBeInTheDocument();
+    expect(secondChild).toHaveAttribute("aria-hidden", "true");
+});
+
+test("should render to the DOM", () => {
+    render(<Flipper firstBorn="Hello" lastBorn="World" showFirst={false} />);
+
+    expect(screen.getByText("Hello")).toHaveAttribute("aria-hidden", "true");
+    expect(screen.getByText("World")).toHaveAttribute("aria-hidden", "false");
 });
 
 test("button should be a11y compliant", async () => {
@@ -16,4 +28,3 @@ test("button should be a11y compliant", async () => {
 
     expect(results).toHaveNoViolations();
 });
-//TODO: test aria hidden
