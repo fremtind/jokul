7fd330c9b5af7730f1e735a9062c069e91e975bb
Jo Emil Holen <jo.emil.holen@fremtind.no>
2020-11-25 11:48:51 +0100

    remove bloat

--- portal/gatsby-node.js
+++ portal/gatsby-node.js
@@ -30,22 +30,13 @@ exports.onCreatePage = async ({ page, actions }) => {
     }
 };
 
-exports.onCreateNode = ({ node, actions, getNode }) => {
+// https://www.gatsbyjs.com/docs/mdx/programmatically-creating-pages/
+// Create indexable source for UU articles
+exports.onCreateNode = ({ node, actions }) => {
     const { createNodeField } = actions;
-    // you only want to operate on `Mdx` nodes. If you had content from a
-    // remote CMS you could also check to see if the parent node was a
-    // `File` node here
     if (node.internal.type === "Mdx") {
-        const value = createFilePath({ node, getNode });
         createNodeField({
-            // Name of the field you are adding
-            name: "waduuu",
-            // Individual MDX node
             node,
-            // Generated value based on filepath with "blog" prefix. you
-            // don't need a separating "/" before the value because
-            // createFilePath returns a path with the leading "/".
-            value: `/blog${value}`,
         });
     }
 };
