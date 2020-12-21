8acb119b416f14f3f1df1ae5b394a9b72a132533
Leiv Fredrik Berge <leiv.fredrik.berge@sparebank1.no>
2020-12-21 11:48:29 +0100

    fix(toggleslider): improve screenreader experience
    affects: @fremtind/jkl-toggle-switch-react

--- packages/toggle-switch-react/src/ToggleSlider.tsx
+++ packages/toggle-switch-react/src/ToggleSlider.tsx
@@ -1,6 +1,7 @@
 import React, { useState, FC } from "react";
 import classNames from "classnames";
 import { nanoid } from "nanoid";
+import { ScreenReaderOnly } from "@fremtind/jkl-core";
 
 interface Props {
     labels: [string, string];
@@ -8,21 +9,38 @@ interface Props {
     defaultValue?: string;
     className?: string;
     inverted?: boolean;
+    screenReaderLabel?: string;
+    screenReaderSelected?: string;
 }
 
-export const ToggleSlider: FC<Props> = ({ labels, className = "", inverted, onToggle, defaultValue, children }) => {
+export const ToggleSlider: FC<Props> = ({
+    labels,
+    className = "",
+    inverted,
+    onToggle,
+    defaultValue,
+    children,
+    screenReaderLabel,
+    screenReaderSelected,
+}) => {
     const [checked, setChecked] = useState(defaultValue !== labels[0]);
+    const [currentLabel, setCurrentLabel] = useState(defaultValue || labels[0]);
     const [id] = useState(nanoid(8));
+
+    const ariaLabel = screenReaderLabel || `${labels[0]} eller ${labels[1]}`;
+    const selectedAriaLabel = screenReaderSelected || `${currentLabel} valgt`;
 
     const handleChange = () => {
         const nextValue = !checked;
+        const nextLabel = nextValue ? labels[1] : labels[0];
         setChecked(nextValue);
-        return onToggle(nextValue ? labels[1] : labels[0]);
+        setCurrentLabel(nextLabel);
+        return onToggle(nextLabel);
     };
 
     return (
         <div className={`jkl-toggle-slider__wrapper ${className}`}>
-            <label htmlFor={id} className="jkl-micro jkl-component-spacing--medium-right">
+            <label htmlFor={id} aria-label={ariaLabel} className="jkl-micro jkl-component-spacing--medium-right">
                 {children}
             </label>
             <button
@@ -38,6 +56,9 @@ export const ToggleSlider: FC<Props> = ({ labels, className = "", inverted, onTo
                     "jkl-toggle-slider--not-checked": !checked,
                 })}
             >
+                <div role="region" aria-live="polite">
+                    <ScreenReaderOnly>{selectedAriaLabel}</ScreenReaderOnly>
+                </div>
                 <span className="jkl-toggle-slider__pill" aria-hidden />
                 <span className="jkl-micro jkl-toggle-slider--left">{labels[0]}</span>
                 <span className="jkl-micro jkl-toggle-slider--right">{labels[1]}</span>
