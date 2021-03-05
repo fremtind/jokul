71e6438b3a4532eac238fc6f207bbf75f26cd467
Pio Rasch-Halvorsen <pio.rasch-halvorsen@fremtind.no>
2021-03-05 10:22:24 +0100

    feat: add close button to alert messages
    affects: @fremtind/jkl-alert-message-react, @fremtind/jkl-alert-message

--- packages/alert-message-react/src/AlertMessage.tsx
+++ packages/alert-message-react/src/AlertMessage.tsx
@@ -1,6 +1,7 @@
 import React, { ReactNode } from "react";
 import classNames from "classnames";
 import { MessageIcon } from "./common/MessageIcon";
+import { IconButton } from "@fremtind/jkl-icon-button-react";
 
 type messageTypes = "info" | "error" | "success" | "warning";
 
@@ -11,6 +12,11 @@ interface Props {
     maxContentWidth?: string;
     paddingLeft?: string;
     role?: string;
+    dismissed?: boolean;
+    dismissAction?: {
+        handleDismiss: () => void;
+        buttonTitle?: string;
+    };
 }
 
 function alertFactory(messageType: messageTypes) {
@@ -20,10 +26,13 @@ function alertFactory(messageType: messageTypes) {
         maxContentWidth,
         paddingLeft,
         role = "status",
+        dismissed,
+        dismissAction,
         children,
     }: Props) {
         const componentClassName = classNames("jkl-alert-message", "jkl-alert-message--" + messageType, className, {
             "jkl-alert-message--dark": inverted,
+            "jkl-alert-message--dismissed": dismissed,
         });
 
         const styles = {
@@ -34,10 +43,18 @@ function alertFactory(messageType: messageTypes) {
         return (
             <div className={componentClassName} role={role}>
                 <div className="jkl-alert-message__content" data-testid="alert-message-content" style={{ ...styles }}>
-                    <div className="jkl-alert-message__icon">
+                    <div aria-hidden className="jkl-alert-message__icon">
                         <MessageIcon messageType={messageType} />
                     </div>
                     <p className="jkl-alert-message__message jkl-body">{children}</p>
+                    {dismissAction?.handleDismiss && (
+                        <IconButton
+                            className="jkl-alert-message__dismiss-button"
+                            iconType="clear"
+                            buttonTitle={dismissAction.buttonTitle || "Lukk"}
+                            onClick={dismissAction.handleDismiss}
+                        />
+                    )}
                 </div>
             </div>
         );
