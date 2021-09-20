4f4287c6fb191cd30df0c13dee23efb74800c501
William Killerud <william@killerud.com>
2021-09-20 14:36:38 +0200

    refactor: skriv om 404-siden i portalen til å følge Figma
    affects: @fremtind/portal
    
    Det er ønsket eksempler på feilsider. Start like godt med vår egen.

--- portal/src/pages/404.tsx
+++ portal/src/pages/404.tsx
@@ -4,21 +4,33 @@ import { Link } from "@fremtind/jkl-core";
 import { Link as InternalLink } from "gatsby";
 
 const NotFoundPage = () => (
-    <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }} exit={{ opacity: 0 }} transition={{ duration: 0.35 }}>
-        <h1 className="jkl-title jkl-spacing--bottom-3 jkl-spacing--top-3">Oj, dette ble feil</h1>
-        <p className="jkl-heading-2 jkl-spacing--bottom-2">Minst en av oss har rota dette til.</p>
+    <motion.main
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        exit={{ opacity: 0 }}
+        transition={{ duration: 0.35 }}
+        className="jkl-portal__main"
+    >
+        <h1 className="jkl-title jkl-spacing--bottom-3 jkl-spacing--top-3">Oj sann, der fikk du en blank side!</h1>
         <p className="jkl-body">
-            Kanskje du kan hjelpe oss å rette opp i dette? Rapporter en feil eller endringsønske på vår{" "}
-            <Link external href="https://github.com/fremtind/jokul/">
-                Github
-            </Link>
-            . Og har du lyst, så kan du rette feilen selv, sjekk ut{" "}
-            <InternalLink to="/komigang/utvikling" className="jkl-link">
-                kom i gang guiden
-            </InternalLink>{" "}
-            her i portalen!
+            Det beklager vi. Du kan prøve å:
+            <ul>
+                <li>
+                    rapportere en feil eller endringsønske på vår{" "}
+                    <Link external href="https://github.com/fremtind/jokul/">
+                        Github
+                    </Link>
+                </li>
+                <li>
+                    rette feilen selv? Sjekk ut{" "}
+                    <InternalLink to="/komigang/utvikling" className="jkl-link">
+                        kom i gang guiden
+                    </InternalLink>{" "}
+                    her i portalen!
+                </li>
+            </ul>
         </p>
-    </motion.div>
+    </motion.main>
 );
 
 export default NotFoundPage;
