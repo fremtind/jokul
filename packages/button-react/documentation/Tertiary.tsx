69e6f9fc9e708bc644ec24197987151d34127404
William Killerud <william@killerud.com>
2021-10-06 09:09:34 +0200

    docs: strukturer om dokumentasjonen av lenkeknapper
    affects: @fremtind/jkl-button-react

--- packages/button-react/documentation/Tertiary.tsx
+++ packages/button-react/documentation/Tertiary.tsx
@@ -6,18 +6,20 @@ export const Tertiary: React.FC<ExampleComponentProps> = ({ boolValues }) => {
     const [showLoader, setShowLoader] = useState(false);
     const loader = { showLoader: showLoader || !!boolValues?.["isLoading"], textDescription: "Laster innhold" };
 
+    const onClick = () => {
+        console.log("Hello!");
+        setShowLoader(true);
+        setTimeout(() => {
+            setShowLoader(false);
+        }, 2200);
+    };
+
     return (
         <TertiaryButton
             inverted={boolValues && boolValues["Invertert"]}
             forceCompact={boolValues && boolValues["Kompakt"]}
             loader={showLoader || !!boolValues?.["withLoader"] ? loader : undefined}
-            onClick={() => {
-                console.log("Hello!");
-                setShowLoader(true);
-                setTimeout(() => {
-                    setShowLoader(false);
-                }, 2200);
-            }}
+            onClick={onClick}
             className="jkl-spacing--right-1"
         >
             Avbryt
@@ -29,11 +31,7 @@ export const TertiaryCode = `
 <TertiaryButton
     inverted={boolValues && boolValues["Invertert"]}
     forceCompact={boolValues && boolValues["Kompakt"]}
-    loader={
-        !!boolValues?.["withLoader"]
-            ? { showLoader: !!boolValues?.["isLoading"], textDescription: "Laster innhold" }
-            : undefined
-    }
+    loader={showLoader || !!boolValues?.["withLoader"] ? loader : undefined}
     onClick={onClick}
     className="jkl-spacing--right-1"
 >
