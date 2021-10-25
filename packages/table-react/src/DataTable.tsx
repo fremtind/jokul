d4cf5bf22d70c034ff7a13ea5f72c56bba142093
William Killerud <william@killerud.com>
2021-10-25 13:12:14 +0200

    feat: variant av table-cell med toppjustering
    affects: @fremtind/jkl-table-react, @fremtind/jkl-table
    
    Om tabellen har enkeltkolonner som gjør at sentrering av innhold fungerer dårlig kan du velge å
    toppjustere i stedet. Antas at DataTable er spesielt utsatt for dette, så åpne for samme prop.

--- packages/table-react/src/DataTable.tsx
+++ packages/table-react/src/DataTable.tsx
@@ -15,9 +15,11 @@ interface Props extends React.DetailedHTMLProps<React.HTMLAttributes<HTMLTableEl
     rows: React.ReactNode[][];
     columns: string[];
     emptyTableText?: string;
+    /** Velg mellom vertikal sentrering av innholdet eller toppjustering. Typisk skal innholdet være sentrert, men dersom én eller flere kolonner typisk har mye mer innhold enn naboene kan toppjustering være bedre. */
+    verticalAlign?: "center" | "top";
 }
 
-export const DataTable: VFC<Props> = ({ caption, columns, emptyTableText, rows, ...rest }) => {
+export const DataTable: VFC<Props> = ({ caption, columns, emptyTableText, rows, verticalAlign, ...rest }) => {
     return (
         <Table fullWidth={true} {...rest}>
             {caption && <TableCaption srOnly>{caption}</TableCaption>}
@@ -39,7 +41,7 @@ export const DataTable: VFC<Props> = ({ caption, columns, emptyTableText, rows, 
                 {rows.map((row, rowIndex) => (
                     <TableRow key={rowIndex}>
                         {row.map((cell, cellIndex) => (
-                            <TableCell key={cellIndex} data-th={columns[cellIndex]}>
+                            <TableCell key={cellIndex} data-th={columns[cellIndex]} verticalAlign={verticalAlign}>
                                 {cell}
                             </TableCell>
                         ))}
