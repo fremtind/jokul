b99687f7a198fef123fbdd3862151c2b6bcaf7a1
William Killerud <william@killerud.com>
2021-11-03 16:16:52 +0100

    feat: eksporter propstype, legg til labelProps
    affects: @fremtind/jkl-field-group-react
    
    Spre ...rest på fieldset også

--- packages/field-group-react/src/FieldGroup.tsx
+++ packages/field-group-react/src/FieldGroup.tsx
@@ -1,21 +1,26 @@
-import React, { FC, ReactNode } from "react";
+import React, { FC, ReactNode, HTMLAttributes } from "react";
+import type { LabelProps } from "@fremtind/jkl-core";
 import { Label, SupportLabel, LabelVariant, DataTestAutoId } from "@fremtind/jkl-core";
 import classNames from "classnames";
 
-interface Props extends DataTestAutoId {
+export interface FieldGroupProps extends DataTestAutoId, HTMLAttributes<HTMLFieldSetElement> {
     legend: string;
+    labelProps?: LabelProps;
     children?: ReactNode;
     className?: string;
     helpLabel?: string;
     errorLabel?: string;
+    /** @deprecated Foretrekk `labelProps.variant` */
     variant?: LabelVariant;
+    /** @deprecated Foretrekk `labelProps.forceCompact` */
     forceCompact?: boolean;
     /** @deprecated */
     inverted?: boolean;
 }
 
-export const FieldGroup: FC<Props> = ({
+export const FieldGroup: FC<FieldGroupProps> = ({
     legend,
+    labelProps,
     className,
     children,
     helpLabel,
@@ -24,12 +29,13 @@ export const FieldGroup: FC<Props> = ({
     forceCompact,
     "data-testautoid": testAutoId,
     inverted,
+    ...rest
 }) => {
     const componentClassName = classNames("jkl-field-group", className);
     return (
-        <fieldset className={componentClassName} data-testautoid={testAutoId}>
+        <fieldset className={componentClassName} data-testautoid={testAutoId} {...rest}>
             <legend className="jkl-field-group__legend">
-                <Label variant={variant} forceCompact={forceCompact}>
+                <Label variant={variant} forceCompact={forceCompact} {...labelProps}>
                     {legend}
                 </Label>
             </legend>
