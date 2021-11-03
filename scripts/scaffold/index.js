80d7a6b237f939cf6050429f232f1977a66f5c6f
Tom Erland Fevang <tom.erland.fevang@fremtind.no>
2021-11-03 17:56:29 +0100

    refactor: scaffold script

--- scripts/scaffold/index.js
+++ scripts/scaffold/index.js
@@ -1,9 +1,11 @@
 #!/usr/bin/node
 /* eslint-disable @typescript-eslint/no-var-requires */
 
-const fs = require("fs");
+const fs = require("fs-extra");
 const path = require("path");
 const prompt = require("prompt");
+const replace = require("replace-in-file");
+const glob = require("glob");
 
 const main = async () => {
     try {
@@ -22,101 +24,45 @@ const main = async () => {
 
         const { componentName } = await prompt.get(promtSchema);
 
-        if (!componentName) {
-            console.error("You have to supply a component name");
-            process.exit(1);
-        }
-
         const toPascalCase = (string) =>
             string
                 .split("-")
                 .map((string) => `${string.slice(0, 1).toUpperCase()}${string.slice(1)}`)
                 .join("");
 
-        const asArray = (val) => (typeof val === "string" ? [val] : val);
-
-        const createCpFile = (templatePath, packagePath) => (src, dest, mutator) => {
-            let copied = fs.readFileSync(path.resolve(templatePath, ...asArray(src)));
-
-            if (mutator) {
-                copied = mutator(copied);
-            }
-
-            fs.writeFile(path.resolve(packagePath, ...asArray(dest || src)), copied, () => undefined);
-        };
-
-        const scaffoldName = "scaffold";
-        const scaffoldNamePascalCased = toPascalCase(scaffoldName);
-        const scaffoldRegExp = new RegExp(scaffoldName, "g");
-        const scaffoldRegExpPascalCased = new RegExp(scaffoldNamePascalCased, "g");
+        const scaffoldRegExp = new RegExp("scaffold", "g");
+        const scaffoldRegExpPascalCased = new RegExp("Scaffold", "g");
 
         const componentNamePascalCased = toPascalCase(componentName);
 
         const replaceComponentNames = (string) =>
             string.replace(scaffoldRegExp, componentName).replace(scaffoldRegExpPascalCased, componentNamePascalCased);
 
-        const mapFilenames = (filename) => {
-            const comparator = Array.isArray(filename) ? filename[filename.length - 1] : filename;
+        const copyAndReplace = (templatePath, packagePath) => {
+            fs.copySync(templatePath, packagePath);
 
-            const withDirs = (filename, componentName) => [...filename.slice(0, filename.length - 1), componentName];
+            const files = glob.sync(path.join(packagePath, "**", "*"));
 
-            return [
-                filename,
-                Array.isArray(filename)
-                    ? withDirs(filename, replaceComponentNames(comparator))
-                    : replaceComponentNames(comparator),
-            ];
+            replace.sync({
+                files,
+                from: [scaffoldRegExp, scaffoldRegExpPascalCased],
+                to: [componentName, componentNamePascalCased],
+            });
 
-            return [filename, null];
+            files.forEach((path) => fs.renameSync(path, replaceComponentNames(path)));
         };
 
-        // Creates files for the sass component
-        const createComponentFiles = () => {
-            const packagePath = path.resolve(process.cwd(), "packages", componentName);
-            const templatePath = path.resolve(__dirname, "templates", "component");
+        copyAndReplace(
+            path.resolve(__dirname, "templates", "component"),
+            path.resolve(process.cwd(), "packages", componentName),
+        );
 
-            const dir = fs.readdirSync(templatePath);
-
-            const filenames = dir.map(mapFilenames);
-
-            const cpFile = createCpFile(templatePath, packagePath);
-
-            fs.mkdirSync(packagePath);
-
-            filenames.forEach((filename) => cpFile(...filename, (file) => replaceComponentNames(file.toString())));
-        };
-
-        createComponentFiles();
-
-        // Creates files for the react component
-        const createReactComponentFiles = () => {
-            const packagePath = path.resolve(process.cwd(), "packages", `${componentName}-react`);
-            const templatePath = path.resolve(__dirname, "templates", "component-react");
-
-            const rootDir = fs.readdirSync(templatePath).filter((path) => path.includes("."));
-            const srcDir = fs.readdirSync(path.resolve(templatePath, "src"));
-            const intDir = fs.readdirSync(path.resolve(templatePath, "integration"));
-            const docDir = fs.readdirSync(path.resolve(templatePath, "documentation"));
-
-            const cpFile = createCpFile(templatePath, packagePath);
-
-            fs.mkdirSync(packagePath);
-            fs.mkdirSync(path.resolve(packagePath, "src"));
-            fs.mkdirSync(path.resolve(packagePath, "integration"));
-            fs.mkdirSync(path.resolve(packagePath, "documentation"));
-
-            [
-                rootDir.map(mapFilenames),
-                srcDir.map((filename) => ["src", filename]).map(mapFilenames),
-                intDir.map((filename) => ["integration", filename]).map(mapFilenames),
-                docDir.map((filename) => ["documentation", filename]).map(mapFilenames),
-            ].forEach((dir) =>
-                dir.forEach((filename) => cpFile(...filename, (file) => replaceComponentNames(file.toString()))),
-            );
-        };
-
-        createReactComponentFiles();
+        copyAndReplace(
+            path.resolve(__dirname, "templates", "component-react"),
+            path.resolve(process.cwd(), "packages", `${componentName}-react`),
+        );
     } catch (error) {
+        console.error(error);
         process.exit(1);
     }
 };
