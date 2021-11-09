b10ea7fb43bfb6ad6a4d554971fd67eecb08994b
William Killerud <william@killerud.com>
2021-11-09 12:31:45 +0100

    docs: fjern duplikat fargedokumentasjon

--- packages/core/documentation/index.tsx
+++ packages/core/documentation/index.tsx
@@ -1,10 +1,6 @@
 import React from "react";
-
-// Import utils for showing example
-import { renderExample, DevExample } from "../../../doc-utils";
+import ReactDOM from "react-dom";
+import { CoreExample } from "./CoreExample";
 import "@fremtind/jkl-icon-button/icon-button.min.css";
 
-// Import actual example and component stylesheet (specific for this component):
-import { CoreExample } from "./CoreExample";
-
-renderExample(<DevExample component={CoreExample} />, document.getElementById("app"));
+ReactDOM.render(<CoreExample />, document.getElementById("app"));
