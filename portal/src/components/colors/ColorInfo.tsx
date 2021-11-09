a506ca6d6c68cdeab65c49387093c9ca22e2c1c9
William Killerud <william@killerud.com>
2021-11-09 15:51:44 +0100

    docs: oppdater fargedokumentasjonen
    affects: @fremtind/portal
    
    ISSUES CLOSED: #2417

---
+++ portal/src/components/colors/ColorInfo.tsx
@@ -0,0 +1,40 @@
+import React, { VFC, useState } from "react";
+import cx from "classnames";
+import { ColorSwatch, rgbToHex } from "./";
+import "./color-info.scss";
+
+export interface Color {
+    colorVariable: string;
+    cmyk?: string;
+    pantone?: string;
+}
+
+interface ColorInfoProps extends Color {
+    vertical?: boolean;
+    className?: string;
+}
+
+export const ColorInfo: VFC<ColorInfoProps> = ({ colorVariable, cmyk, pantone, vertical, className }) => {
+    const [color, setColor] = useState<number[]>();
+    return (
+        <article
+            className={cx("jkl-portal-color-info", className, {
+                "jkl-portal-color-info--vertical": vertical,
+            })}
+        >
+            <ColorSwatch setColor={setColor} className="jkl-portal-color-info__swatch" colorVariable={colorVariable} />
+            <p className="jkl-portal-color-info__values jkl-small">
+                {color && color.length === 3 && (
+                    <>
+                        {`RGB: ${color[0]} ${color[1]} ${color[2]}`} <br />
+                        {`HEX: ${rgbToHex(color)}`}
+                    </>
+                )}
+                {cmyk && <br />}
+                {cmyk && `CMYK: ${cmyk}`}
+                {pantone && <br />}
+                {pantone}
+            </p>
+        </article>
+    );
+};
---
+++ portal/src/components/colors/PrimaryColor.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import cx from "classnames";
+import { ColorInfo, Color } from "./";
+import "./primary-color.scss";
+
+interface PrimaryColorProps extends Color {
+    className?: string;
+    title: string;
+}
+
+export const PrimaryColor: React.FC<PrimaryColorProps> = ({ title, className, children, ...colorInfo }) => (
+    <article className="jkl-portal-primary-color">
+        <h3 className="jkl-portal-primary-color__title jkl-sub-heading-large">{title}</h3>
+        <ColorInfo className={cx("jkl-portal-primary-color__swatch", className)} {...colorInfo} />
+        <p className="jkl-portal-primary-color__text jkl-body jkl-portal-paragraph">{children}</p>
+    </article>
+);
