b2389765fe0d9e642fa5f968b3d16b044c066e5c
William Killerud <william@killerud.com>
2021-11-22 10:39:22 +0100

    docs: utvid komponentdokumentasjon for Message Box
    affects: @fremtind/jkl-alert-message-react, @fremtind/jkl-message-box-react
    
    ISSUES CLOSED: #2524

--- packages/message-box-react/documentation/MessageBoxExample.tsx
+++ packages/message-box-react/documentation/MessageBoxExample.tsx
@@ -20,23 +20,43 @@ const getTypeOfBox = (typeofBox?: string) => {
 
 export const MessageBoxExample: React.FC<ExampleComponentProps> = ({ boolValues, choiceValues }) => {
     const [dismissed, setDismissed] = useState(false);
-    const C = getTypeOfBox(choiceValues ? choiceValues["Type"] : "");
-    const dismissAction =
-        boolValues && boolValues["Avvisbar"]
-            ? {
-                  handleDismiss: () => setDismissed(true),
-                  buttonTitle: "Merk som lest",
-              }
-            : undefined;
+    const C = getTypeOfBox(choiceValues?.["Type"]);
+    const dismissAction = boolValues?.["Avvisbar"]
+        ? {
+              handleDismiss: () => {
+                  setDismissed(true);
+                  setTimeout(() => setDismissed(false), 2600);
+              },
+              buttonTitle: "Merk som lest",
+          }
+        : undefined;
     return (
         <C
-            fullWidth={boolValues && boolValues["Full bredde"]}
-            title={choiceValues && choiceValues["Type"]}
+            fullWidth={boolValues?.["Full bredde"]}
+            title={choiceValues?.["Type"]}
             dismissed={dismissed}
             dismissAction={dismissAction}
         >
-            Hei, jeg er en melding av typen {choiceValues ? choiceValues["Type"] : "ʕ•ᴥ•ʔ"} med{" "}
-            <NavLink href="/">en navlink</NavLink>
+            Hei, jeg er en melding av typen {choiceValues?.["Type"]} med <NavLink href="/">en navlink</NavLink>
         </C>
     );
+};
+
+export const messageBoxExampleCode = ({ boolValues, choiceValues }: ExampleComponentProps): string => {
+    const C = getTypeOfBox(choiceValues ? choiceValues["Type"] : "");
+    return `
+<${C.displayName}
+    title="${choiceValues?.["Type"]}"
+    fullWidth={${boolValues?.["Full bredde"]}}
+    dismissed={false}
+    dismissAction={${
+        boolValues?.["Avvisbar"]
+            ? `{
+        handleDismiss: () => setDismissed(true),
+        buttonTitle: "Merk som lest",
+    }`
+            : undefined
+    }}
+/>
+`;
 };
