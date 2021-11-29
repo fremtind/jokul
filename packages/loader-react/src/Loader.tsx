3527f84422f90eaa6aae201965d55d6ee3c9ece7
William Killerud <william@killerud.com>
2021-11-29 12:28:38 +0100

    feat: legg til inline og delay-props
    affects: @fremtind/jkl-loader-react
    
    Inline lar deg vise en loader – du gjettet det - inline. Delay-propen lar deg sette et antall
    millisekunder før loaderen vises.

--- packages/loader-react/src/Loader.tsx
+++ packages/loader-react/src/Loader.tsx
@@ -1,26 +1,53 @@
-import React, { VFC } from "react";
+import React, { useEffect, useState } from "react";
 import classNames from "classnames";
 
 export type LoaderVariant = "small" | "medium" | "large";
 
-interface Props {
+export interface LoaderProps {
     variant?: LoaderVariant;
     textDescription: string;
     className?: string;
     dataTestAutoId?: string;
     politeness?: "polite" | "assertive";
+    inline?: boolean;
+    /**
+     * Antall millisekunder komponenten vil vente før den rendrer
+     * @default 0
+     */
+    delay?: number;
 }
-export const Loader: VFC<Props> = ({
+
+function useDelayedRender(delayMilliseconds: number) {
+    const [renderComponent, setRenderComponent] = useState(false);
+
+    useEffect(() => {
+        const timeout = setTimeout(() => setRenderComponent(true), delayMilliseconds);
+        return () => clearTimeout(timeout);
+    }, [delayMilliseconds, setRenderComponent]);
+
+    return [renderComponent];
+}
+
+export const Loader = ({
+    delay = 0,
     variant = "large",
     textDescription,
     className,
     dataTestAutoId = "jkl-loader",
     politeness = "polite",
+    inline = false,
     ...rest
-}) => {
+}: LoaderProps): JSX.Element | null => {
+    const [renderComponent] = useDelayedRender(delay);
+
+    if (delay && !renderComponent) {
+        return null;
+    }
+
     const componentClassName = classNames("jkl-loader", className, {
         "jkl-loader--medium": variant === "medium",
         "jkl-loader--small": variant === "small",
+        "jkl-loader--inline": inline,
     });
 
     return (
