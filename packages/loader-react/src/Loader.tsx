00e3f36b7916a51f0ab5166388499021e5358b4b
William Killerud <william@killerud.com>
2021-12-01 13:03:14 +0100

    refactor: effektiviser dersom delay er 0
    affects: @fremtind/jkl-loader-react

--- packages/loader-react/src/Loader.tsx
+++ packages/loader-react/src/Loader.tsx
@@ -18,9 +18,13 @@ export interface LoaderProps {
 }
 
 function useDelayedRender(delayMilliseconds: number) {
-    const [renderComponent, setRenderComponent] = useState(false);
+    const [renderComponent, setRenderComponent] = useState(delayMilliseconds === 0 ? true : false);
 
     useEffect(() => {
+        if (delayMilliseconds === 0) {
+            return;
+        }
+
         const timeout = setTimeout(() => setRenderComponent(true), delayMilliseconds);
         return () => clearTimeout(timeout);
     }, [delayMilliseconds, setRenderComponent]);
