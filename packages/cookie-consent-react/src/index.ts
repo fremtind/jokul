7f0e50ef491fc0ad9c3234cc12d992ebc6c8bce2
William Killerud <william@killerud.com>
2021-12-01 15:50:27 +0100

    feat: eksporter typer
    affects: @fremtind/jkl-cookie-consent-react

--- packages/cookie-consent-react/src/index.ts
+++ packages/cookie-consent-react/src/index.ts
@@ -1,2 +1,4 @@
+export type { AcceptConsentCallback, Consent, ConsentState } from "./types";
+export type { CookieConsentProps } from "./CookieConsent";
 export { CookieConsent } from "./CookieConsent";
 export { CookieConsentProvider, useCookieConsent } from "./CookieConsentContext";
