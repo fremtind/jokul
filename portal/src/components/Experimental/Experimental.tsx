de7333fdc3af68ec170ec4fe007748877c5d4ef9
William Killerud <william@killerud.com>
2021-12-06 15:03:11 +0100

    docs: dokumenter Autosuggest med kodeeksempel, bruk
    affects: @fremtind/jkl-text-input-react
    
    ISSUES CLOSED: #2575

--- portal/src/components/Experimental/Experimental.tsx
+++ portal/src/components/Experimental/Experimental.tsx
@@ -3,7 +3,10 @@ import { WarningMessageBox } from "@fremtind/jkl-message-box-react";
 
 export const Experimental: React.FC = ({ children }) => {
     return (
-        <WarningMessageBox className="jkl-spacing-xl--top jkl-spacing-xl--bottom" title="Eksperimentell komponent">
+        <WarningMessageBox
+            className="jkl-portal-paragraph jkl-spacing-xl--top jkl-spacing-xl--bottom"
+            title="Eksperimentell komponent"
+        >
             {children}
         </WarningMessageBox>
     );
