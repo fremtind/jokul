721db9f6632f523e2b38badbc0b54eddecf868bb
William Killerud <william@killerud.com>
2021-12-16 16:43:20 +0100

    fix: rett a11y-problemer i Hamburger
    affects: @fremtind/jkl-hamburger-react, @fremtind/portal
    
    Rollen `menu` har en betydning som ikke blir riktig for hamburgermenyer.
    Den er tiltenkt menyer som er mer native-liknende (tenk menu bar på macOS,
    eller Fil-menyen fra Windows). Den legger føringer for forventninger om
    tastaturnavigasjon og liknende som ikke stemmer for hamburgermenyer.
    
    BREAKING CHANGE:
    - Fjernet `description`-propen. Den var en renamet `aria-label`. Bruk
    heller `aria-label`.
    - Hamburger krever nå at du gir den en ID og setter `aria-controls` med IDen
    til menyen hamburgeren styrer. Dette er for å gjøre det enklere å bygge en
    tilgjengelig hamburgermeny.
    
    ISSUES CLOSED: #2606

--- packages/hamburger-react/src/Hamburger.test.tsx
+++ packages/hamburger-react/src/Hamburger.test.tsx
@@ -1,15 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import { render, screen, fireEvent } from "@testing-library/react";
 import { Hamburger } from ".";
 import { axe } from "jest-axe";
 
 describe("Hamburger", () => {
     it("should render to document", () => {
-        render(<Hamburger isOpen={false} onClick={() => {}} />);
+        render(<Hamburger id="jkl-hamburger" aria-controls="nothing" isOpen={false} onClick={() => {}} />);
     });
 
     it("should have attribute aria-expanded when isOpen is true", () => {
-        render(<Hamburger isOpen onClick={() => {}} />);
+        render(<Hamburger id="jkl-hamburger" aria-controls="nothing" isOpen onClick={() => {}} />);
 
         const burger = screen.getByTestId("jkl-hamburger");
 
@@ -17,7 +17,7 @@ describe("Hamburger", () => {
     });
 
     it("should get class jkl-hamburger--inverted when specified", () => {
-        render(<Hamburger inverted isOpen={false} onClick={() => {}} />);
+        render(<Hamburger id="jkl-hamburger" aria-controls="nothing" inverted isOpen={false} onClick={() => {}} />);
 
         const component = screen.getByTestId("jkl-hamburger");
         expect(component).toHaveClass("jkl-hamburger--inverted");
@@ -25,7 +25,7 @@ describe("Hamburger", () => {
 
     it("sets correct value for aria-controls", () => {
         const menuid = "hgsf8w4kjDH";
-        render(<Hamburger isOpen={false} aria-controls={menuid} onClick={() => {}} />);
+        render(<Hamburger id="jkl-hamburger" isOpen={false} aria-controls={menuid} onClick={() => {}} />);
 
         const component = screen.getByTestId("jkl-hamburger");
         expect(component).toHaveAttribute("aria-controls", menuid);
@@ -33,7 +33,7 @@ describe("Hamburger", () => {
 
     it("should render call onClick", () => {
         const fn = jest.fn();
-        render(<Hamburger isOpen={false} onClick={fn} />);
+        render(<Hamburger id="jkl-hamburger" aria-controls="nothing" isOpen={false} onClick={fn} />);
         const burger = screen.getByTestId("jkl-hamburger");
         fireEvent.click(burger);
 
@@ -41,38 +41,100 @@ describe("Hamburger", () => {
     });
 
     it("should set attribute aria-expanded when changing isOpen state", () => {
-        const { rerender } = render(<Hamburger isOpen={false} onClick={() => {}} />);
+        const { rerender } = render(
+            <Hamburger id="jkl-hamburger" aria-controls="nothing" isOpen={false} onClick={() => {}} />,
+        );
         const burger = screen.getByTestId("jkl-hamburger");
 
-        expect(burger).not.toHaveAttribute("aria-expanded");
+        expect(burger).toHaveAttribute("aria-expanded", "false");
 
-        rerender(<Hamburger isOpen={true} onClick={() => {}} />);
-        expect(burger).toHaveAttribute("aria-expanded");
+        rerender(<Hamburger id="jkl-hamburger" aria-controls="nothing" isOpen={true} onClick={() => {}} />);
+        expect(burger).toHaveAttribute("aria-expanded", "true");
     });
 
     it("should render have correct description", () => {
-        render(<Hamburger isOpen={false} onClick={() => {}} description="max is better than micky d" />);
+        render(
+            <Hamburger
+                id="jkl-hamburger"
+                aria-controls="nothing"
+                aria-label="Testburger"
+                isOpen={false}
+                onClick={() => {}}
+            />,
+        );
         const burger = screen.getByTestId("jkl-hamburger");
 
-        expect(burger).toHaveAttribute("aria-label", "max is better than micky d");
+        expect(burger).toHaveAttribute("aria-label", "Testburger");
     });
 
     it("should show open text when closed", () => {
-        render(<Hamburger isOpen={false} onClick={() => {}} actionLabel={{ open: "Open me", close: "Close me" }} />);
+        render(
+            <Hamburger
+                id="jkl-hamburger"
+                aria-label=""
+                aria-controls="nothing"
+                isOpen={false}
+                onClick={() => {}}
+                actionLabel={{ open: "Open me", close: "Close me" }}
+            />,
+        );
         expect(screen.getByText("Open me")).toBeInTheDocument();
+
+        // Vi må sette `aria-label=""` selv, men greit å bekrefte at det funker
+        const burger = screen.getByTestId("jkl-hamburger");
+        expect(burger).not.toHaveAttribute("aria-label");
     });
 
     it("should show close text when the hamburger is open", () => {
-        render(<Hamburger isOpen onClick={() => {}} actionLabel={{ open: "Open me", close: "Close me" }} />);
+        render(
+            <Hamburger
+                id="jkl-hamburger"
+                aria-controls="nothing"
+                isOpen
+                onClick={() => {}}
+                actionLabel={{ open: "Open me", close: "Close me" }}
+            />,
+        );
         expect(screen.getByText("Close me")).toBeInTheDocument();
     });
 });
 
 describe("a11y", () => {
+    const HamburgerExample = () => {
+        const [isOpen, setIsOpen] = useState(false);
+        return (
+            <div data-testid="jkl-hamburger-example">
+                <div>
+                    <Hamburger
+                        aria-controls="jkl-example-menu-placeholder"
+                        aria-label="Testburger"
+                        id="jkl-example-hamburger"
+                        isOpen={isOpen}
+                        onClick={() => setIsOpen(!isOpen)}
+                    />
+                </div>
+                <div
+                    id="jkl-example-menu-placeholder"
+                    aria-labelledby="jkl-example-hamburger"
+                    role="group"
+                    hidden={!isOpen}
+                >
+                    Menyinnholdet ville vært inni et element med disse attributtene
+                </div>
+            </div>
+        );
+    };
     it("hamburger should be a11y compliant", async () => {
-        const { container } = render(<Hamburger isOpen={false} onClick={() => {}} />);
-        const results = await axe(container);
+        render(<HamburgerExample />);
 
+        let results = await axe(screen.getByTestId("jkl-hamburger-example"));
+
+        expect(results).toHaveNoViolations();
+
+        const burger = screen.getByTestId("jkl-hamburger");
+        fireEvent.click(burger);
+
+        results = await axe(screen.getByTestId("jkl-hamburger-example"));
         expect(results).toHaveNoViolations();
     });
 });
