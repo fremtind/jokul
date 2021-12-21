0a72180129922e92788d0a24274586f09dd6b1e8
Jo Emil Holen <jo.emil.holen@fremtind.no>
2021-12-21 10:03:56 +0100

    fix: fix correct prop destructuring and propagation
    affects: @fremtind/jkl-tabs-react

--- packages/tabs-react/src/Tabs.tsx
+++ packages/tabs-react/src/Tabs.tsx
@@ -14,13 +14,13 @@ interface Props {
  *
  * Docs: https://jokul.fremtind.no/komponenter/tabs
  */
-export const Tabs = (props: Props) => {
+export const Tabs = ({ onChange, ...props }: Props) => {
     const [activeIndex, setActiveIndex] = useState(0);
 
     const [tabIDs, setTabIDs] = useState<string[]>([]);
     const [tabPanelIDs, setTabPanelIds] = useState<string[]>([]);
 
-    useEffect(() => props.onChange?.(activeIndex), [activeIndex, props]);
+    useEffect(() => onChange?.(activeIndex), [activeIndex, onChange]);
 
     const resolveIDs = useCallback(() => {
         const tabList = React.Children.toArray(props.children)[0];
