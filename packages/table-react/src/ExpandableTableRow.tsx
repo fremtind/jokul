27747295310b99113d2426a77cce20e5da954eee
Jo Emil Holen <jo.emil.holen@fremtind.no>
2022-01-06 16:12:24 +0100

    feat: properly compose component from TableRow
    affects: @fremtind/jkl-table-react

--- packages/table-react/src/ExpandableTableRow.tsx
+++ packages/table-react/src/ExpandableTableRow.tsx
@@ -1,8 +1,7 @@
-import { useAnimatedHeight } from "@fremtind/jkl-react-hooks";
-import cx from "classnames";
 import React, { FC, useState } from "react";
+import cx from "classnames";
+import { useAnimatedHeight } from "@fremtind/jkl-react-hooks";
 import { ExpandableTableRowCell } from "./ExpandableTableRowCell";
-import { useTableSectionContext } from "./tableSectionContext";
 import { TableRowProps, TableRow } from "./TableRow";
 export interface ExpandableTableRowProps extends TableRowProps {
     expandedChildren: React.ReactNode;
@@ -15,62 +14,25 @@ export const ExpandableTableRow: FC<ExpandableTableRowProps> = ({
     expandedChildren,
     ...rest
 }) => {
-    const { isTableBody } = useTableSectionContext();
-
     const [isOpen, setIsOpen] = useState(false);
     const [animationRef] = useAnimatedHeight<HTMLDivElement>(isOpen);
 
-    if (isTableBody && clickable) {
-        return (
-            <>
-                <TableRow
-                    className={cx("jkl-table-row--expandable", className, {
-                        ["jkl-table-row--expanded"]: isOpen,
-                    })}
-                    {...rest}
-                    clickable={clickable}
-                >
-                    {React.Children.map(children, (child) => {
-                        if (React.isValidElement(child) && child.type == ExpandableTableRowCell) {
-                            return React.cloneElement(child, {
-                                isOpen,
-                                onClick: () => setIsOpen(!isOpen),
-                            });
-                        } else {
-                            return child;
-                        }
-                    })}
-                </TableRow>
-                <tr>
-                    <td colSpan={100}>
-                        <div
-                            ref={animationRef}
-                            className={cx("jkl-expandable-table-row__expanded-row", {
-                                ["jkl-expandable-table-row__expanded-row--expanded"]: isOpen,
-                            })}
-                        >
-                            {React.Children.map(expandedChildren, (child) => {
-                                if (React.isValidElement(child)) {
-                                    return React.cloneElement(child, {});
-                                }
-                                return child;
-                            })}
-                        </div>
-                    </td>
-                </tr>
-            </>
-        );
-    }
+    const tableRowClassName = cx("jkl-table-row--expandable", className, {
+        ["jkl-table-row--expanded"]: isOpen,
+    });
+    const childWrapperClassName = cx("jkl-expandable-table-row__expanded-row", {
+        ["jkl-expandable-table-row__expanded-row--expanded"]: isOpen,
+    });
 
     return (
         <>
             <TableRow
-                className={cx("jkl-table-row--expandable", className, {
-                    ["jkl-table-row--expanded"]: isOpen,
-                })}
-                clickable={{
-                    onClick: () => setIsOpen(!isOpen),
-                }}
+                className={tableRowClassName}
+                clickable={
+                    clickable ?? {
+                        onClick: () => setIsOpen(!isOpen),
+                    }
+                }
                 {...rest}
             >
                 {React.Children.map(children, (child) => {
@@ -86,12 +48,7 @@ export const ExpandableTableRow: FC<ExpandableTableRowProps> = ({
             </TableRow>
             <tr>
                 <td colSpan={100}>
-                    <div
-                        ref={animationRef}
-                        className={cx("jkl-expandable-table-row__expanded-row", {
-                            ["jkl-expandable-table-row__expanded-row--expanded"]: isOpen,
-                        })}
-                    >
+                    <div ref={animationRef} className={childWrapperClassName}>
                         {React.Children.map(expandedChildren, (child) => {
                             if (React.isValidElement(child)) {
                                 return React.cloneElement(child, {});
