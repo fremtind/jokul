68184f7fae92256c4c30e7a0be0a23d722bb325e
William Killerud <william@killerud.com>
2022-01-07 12:31:44 +0100

    fix: skru av no-descending-specificity
    affects: @fremtind/stylelint-config-jkl
    
    Den fungerer ikke godt nok for måten vi har strukturert Sass på. Vi
    har samlet stiler som har med modifiers og elementer nøstet under
    samme selector, eller på samme sted i stilarket. Det gjør at nøstede
    selectors gjerne har høyere spesifisitet enn "rot" for neste modifier
    eller element. Heller enn å bryte opp etablerte prinsipper for en
    teoretisk forenkling av lesbarhet (og det kan diskuteres om det blir
    mer lesbart) så velger jeg å skru av denne regelen.

--- packages/stylelint-config-jkl/rules/sheet.js
+++ packages/stylelint-config-jkl/rules/sheet.js
@@ -1,5 +1,6 @@
 module.exports = {
     "max-nesting-depth": 5,
     "media-feature-name-no-vendor-prefix": true,
+    "no-descending-specificity": null,
     "no-invalid-double-slash-comments": true,
 };
