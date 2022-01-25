9f93b49cf1cc29e365365e36ef8e393cba615cba
William Killerud <william@killerud.com>
2022-01-25 16:54:51 +0100

    fix: sync useScreen med Sass-mixins
    Var en feil i hooken akkurat i brytningene

--- packages/react-hooks/src/useScreen/state.ts
+++ packages/react-hooks/src/useScreen/state.ts
@@ -28,10 +28,10 @@ export const actionTypes = {
 };
 
 const setDeviceSize = (width: number, height: number): State => ({
-    isSmallDevice: width <= breakpoints.medium,
-    isMediumDevice: width > breakpoints.medium && width < breakpoints.large,
-    isLargeDevice: width > breakpoints.large && width < breakpoints.xl,
-    isXlDevice: width > breakpoints.xl,
+    isSmallDevice: width < breakpoints.medium,
+    isMediumDevice: width >= breakpoints.medium && width < breakpoints.large,
+    isLargeDevice: width >= breakpoints.large && width < breakpoints.xl,
+    isXlDevice: width >= breakpoints.xl,
     isPortrait: height >= width,
     isLandscape: height < width,
     inner: {
