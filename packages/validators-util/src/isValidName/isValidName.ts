a38d2cc1cbbe0daa15780890a315a0cf5eabbb60
Kenneth Apeland <kenneth.apeland@fremtind.no>
2022-02-01 09:18:30 +0100

    fix(isValidName): added support for minus symbol in names
    affects: @fremtind/jkl-validators-util

--- packages/validators-util/src/isValidName/isValidName.ts
+++ packages/validators-util/src/isValidName/isValidName.ts
@@ -1,5 +1,5 @@
 // https://en.wikipedia.org/wiki/List_of_Unicode_characters
-// Checks for all Punctuation, Symbols or Digits until Unicode position 191, but allows Apostrophe
-export const INVALID_NAME_REGEX = /[!-&(-@\[-`{-¿]+/;
+// Checks for all Punctuation, Symbols or Digits until Unicode position 191, but allows Apostrophe and Minus
+export const INVALID_NAME_REGEX = /[!-&(-,.-@\[-`{-¿]+/;
 
 export const isValidName = (value: string): boolean => !INVALID_NAME_REGEX.test(value);
