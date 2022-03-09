436f4f3d61346cae53851e8d1b0fd37e3b0feead
William Killerud <william@killerud.com>
2022-03-09 10:24:33 +0100

    fix: annonser endring av kalenderåret i datovelger
    ISSUES CLOSED: #2750

--- packages/datepicker-react/src/Calendar.tsx
+++ packages/datepicker-react/src/Calendar.tsx
@@ -1,4 +1,4 @@
-import React, { ChangeEvent, forwardRef } from "react";
+import React, { ChangeEvent, forwardRef, useCallback, useState } from "react";
 import cn from "classnames";
 // @ts-ignore: wait for nrk to supply types
 import CoreDatepicker from "@nrk/core-datepicker/jsx";
@@ -36,66 +36,79 @@ export const Calendar = forwardRef<HTMLElement, Props>(
         },
         ref,
     ) => {
+        const value = currentDate || new Date();
+
+        const [date, setDate] = useState(value);
+        const onChange = useCallback((e: ChangeEvent<ChangeDate>) => {
+            setDate(e.target.date);
+        }, []);
+
         const componentClassName = cn("jkl-calendar", {
             "jkl-calendar--hidden": hidden,
             "jkl-calendar--extended": extended,
         });
 
         return (
-            <CoreDatepicker
-                date={currentDate || new Date()}
-                months={months}
-                days={days}
-                onDatepickerClickDay={onClickDate}
-                disabled={disableDate}
-                forwardRef={ref}
-                className={componentClassName}
-                data-testid="jkl-calendar__core-datepicker"
-            >
-                <div className="jkl-calendar__padding">
-                    {extended && (
-                        <div className="jkl-calendar__navigation">
-                            <TextInput
-                                label={yearLabel}
-                                type="year"
-                                className="jkl-calendar__year-selector"
-                                width="5rem"
-                                variant="small"
-                                forceCompact={forceCompact}
-                            />
-                            <NativeSelect
-                                className="jkl-calendar__month-selector"
-                                label={monthLabel}
-                                items={[]}
-                                variant="small"
-                                forceCompact={forceCompact}
-                                width="auto"
-                            />
-                        </div>
-                    )}
-                    {!extended && (
-                        <fieldset className="jkl-calendar__month-navigation">
-                            <button
-                                title="forrige måned"
-                                aria-label="forrige måned"
-                                className="jkl-calendar__month-button"
-                                value="- 1 month"
-                            >
-                                ←
-                            </button>
-                            <button
-                                title="neste måned"
-                                aria-label="neste måned"
-                                className="jkl-calendar__month-button jkl-calendar__month-button--right"
-                                value="+ 1 month"
-                            >
-                                →
-                            </button>
-                        </fieldset>
-                    )}
-                    <table data-testid="jkl-datepicker-calendar" />
-                </div>
-            </CoreDatepicker>
+            <>
+                <CoreDatepicker
+                    date={date}
+                    months={months}
+                    days={days}
+                    onDatepickerClickDay={onClickDate}
+                    onDatepickerChange={onChange}
+                    disabled={disableDate}
+                    forwardRef={ref}
+                    className={componentClassName}
+                    data-testid="jkl-calendar__core-datepicker"
+                >
+                    <div className="jkl-calendar__padding">
+                        {extended && (
+                            <div className="jkl-calendar__navigation">
+                                <TextInput
+                                    label={yearLabel}
+                                    type="year"
+                                    className="jkl-calendar__year-selector"
+                                    width="5rem"
+                                    variant="small"
+                                    forceCompact={forceCompact}
+                                />
+                                <NativeSelect
+                                    className="jkl-calendar__month-selector"
+                                    label={monthLabel}
+                                    items={[]}
+                                    variant="small"
+                                    forceCompact={forceCompact}
+                                    width="auto"
+                                />
+                            </div>
+                        )}
+                        {!extended && (
+                            <fieldset className="jkl-calendar__month-navigation">
+                                <button
+                                    title="forrige måned"
+                                    aria-label="forrige måned"
+                                    className="jkl-calendar__month-button"
+                                    value="- 1 month"
+                                >
+                                    ←
+                                </button>
+                                <button
+                                    title="neste måned"
+                                    aria-label="neste måned"
+                                    className="jkl-calendar__month-button jkl-calendar__month-button--right"
+                                    value="+ 1 month"
+                                >
+                                    →
+                                </button>
+                            </fieldset>
+                        )}
+                        <table data-testid="jkl-datepicker-calendar" />
+                    </div>
+                </CoreDatepicker>
+                <span className="jkl-sr-only" aria-live="polite" aria-atomic="true">
+                    {date.getFullYear()}
+                </span>
+            </>
         );
     },
 );
