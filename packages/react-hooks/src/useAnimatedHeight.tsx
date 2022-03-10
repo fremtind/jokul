3a304a848f79d45bdcf287d293ac5eb95843f9c5
William Killerud <william@killerud.com>
2022-03-10 09:56:29 +0100

    feat: useAnimatedHeight trenger ikke lenger hjelp fra CSS
    Det var ikke alltid like lett å forstå at man måtte inn med CSS i tillegg for at ting skulle
    fungere. Nå tar hooken seg av å sette display none osv. Du kan med andre ord fjerne egne CSS
    klasser om du bare gjorde `display: none`.

--- packages/react-hooks/src/useAnimatedHeight.tsx
+++ packages/react-hooks/src/useAnimatedHeight.tsx
@@ -1,4 +1,6 @@
 import { MutableRefObject, useEffect, useRef, RefObject, useCallback, useLayoutEffect } from "react";
+import type { Easing, Timing } from "@fremtind/jkl-core";
+import { easings, timings } from "@fremtind/jkl-core";
 import { useBrowserPreferences } from "./useBrowserPreferences/useBrowserPreferences";
 
 interface HTMLElementOrCoreToggleElement<T extends HTMLElementOrCoreToggleElement<T>> extends HTMLElement {
@@ -6,32 +8,60 @@ interface HTMLElementOrCoreToggleElement<T extends HTMLElementOrCoreToggleElemen
 }
 
 export interface UseAnimatedHeightOptions {
+    /**
+     * Overstyr standard easingfunksjon
+     * @default "standard"
+     */
+    easing?: Easing;
+    /**
+     * Overstyr standard timing
+     * @default "productive"
+     */
+    timing?: Timing;
     onTransitionStart?: (isOpening: boolean) => void;
     onTransitionEnd?: (isOpen: boolean) => void;
 }
+
+const defaultEasing = easings.standard;
+const defaultTiming = timings.productive;
 
 export function useAnimatedHeight<T extends HTMLElement>(
     isOpen: boolean,
     options?: UseAnimatedHeightOptions,
 ): [RefObject<T>, () => void] {
+    const easing = options?.easing || defaultEasing;
+    const timing = options?.timing || defaultTiming;
+    const transition = `${timing} height ${easing}`;
+
+    const { prefersReducedMotion } = useBrowserPreferences();
+
     const raf1 = useRef<number>();
     const raf2 = useRef<number>();
     const elementRef = useRef<T>(null);
     const firstRender = useRef<boolean>(true);
-    const { prefersReducedMotion } = useBrowserPreferences();
 
     function handleTransitionEnd(event: TransitionEvent) {
         const element = getElement(elementRef);
 
         // Ignore bubbling transitions from within container
         if (element && event.target === element) {
-            element.removeAttribute("style");
+            if (isOpen) {
+                element.removeAttribute("style");
+            } else {
+                element.removeAttribute("style");
+                element.style.display = "none";
+            }
             options?.onTransitionEnd?.(isOpen);
         }
     }
 
     const runAnimation = useCallback(() => {
         if (firstRender.current) {
+            const element = getElement(elementRef);
+            if (element && !isOpen) {
+                element.style.display = "none";
+            }
+
             return; // Do not play animation on first render
         }
 
@@ -44,6 +74,7 @@ export function useAnimatedHeight<T extends HTMLElement>(
 
         const element = getElement(elementRef);
         if (element) {
+            element.style.transition = transition;
             element.style.display = "block";
             element.style.overflow = "hidden";
             if (isOpen) {
@@ -67,7 +98,7 @@ export function useAnimatedHeight<T extends HTMLElement>(
                 });
             }
         }
-    }, [isOpen, options, prefersReducedMotion]);
+    }, [isOpen, options, transition, prefersReducedMotion]);
 
     useLayoutEffect(() => {
         runAnimation();
