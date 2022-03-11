9571a419a76111a90c7c75b1d57df95e62a904a2
William Killerud <william@killerud.com>
2022-03-11 15:22:18 +0100

    docs: bruk og dokumenter footer i portalen

--- portal/src/components/Layout/Layout.tsx
+++ portal/src/components/Layout/Layout.tsx
@@ -3,12 +3,13 @@ import { graphql, useStaticQuery } from "gatsby";
 import React, { useEffect } from "react";
 import { AnimatePresence } from "framer-motion";
 import { useScreen } from "@fremtind/jkl-react-hooks";
-
+import { Footer } from "@fremtind/jkl-footer-react";
+import { Feedback } from "@fremtind/jkl-feedback-react";
+import { LogoStamp } from "@fremtind/jkl-logo-react";
 import { FormatProvider } from "../Typography";
-import { Header, Sidebar, Footer } from "..";
+import { Header, Sidebar } from "..";
 import { ThemeBG } from "./components";
 import { useLocation } from "../../contexts/locationContext";
-
 import "./Layout.scss";
 
 interface Props {
@@ -95,7 +96,68 @@ export const Layout: React.FC<Props> = ({ children, location, pageContext }) => 
             <FormatProvider>
                 <AnimatePresence exitBeforeEnter>{children}</AnimatePresence>
             </FormatProvider>
-            <Footer className="jkl-portal__footer" />
+            <div className="jkl-portal-footer">
+                <div className="jkl-portal-footer__feedback">
+                    <Feedback
+                        type="radio"
+                        label="Fant du det du lette etter?"
+                        options={[
+                            {
+                                label: "Ja",
+                                value: "ja",
+                                textAreaLabel: "Så bra! Har du noen tilbakemeldinger kan du skrive dem her.",
+                            },
+                            {
+                                label: "Nei",
+                                value: "nei",
+                                textAreaLabel:
+                                    "Det var leit! Fortell oss gjerne hva du savner, så kan vi gjøre sidene våre bedre.",
+                            },
+                        ]}
+                        addOnQuestion={{
+                            label: "Vil du legge til noe mer?",
+                            helpLabel:
+                                "Når du trykker på send vil en e-post genereres for deg. Ikke del personlige opplysninger. Vi behandler dine data i henhold til vår personvernserklæring.",
+                        }}
+                        successMessage={{
+                            title: "Takk! Jøkul blir enda bedre med dine tilbakemeldinger.",
+                            children: (
+                                <>
+                                    <p className="jkl-spacing-s--bottom">
+                                        Spill gjerne inn på GitHub, eller delta på forum (hver tirsdag{" "}
+                                        <time dateTime="14:00">kl. 14</time>)!
+                                    </p>
+                                    <a
+                                        href="https://github.com/fremtind/jokul/discussions"
+                                        className="jkl-button jkl-button--secondary"
+                                    >
+                                        Diskuter på GitHub
+                                    </a>
+                                </>
+                            ),
+                        }}
+                        onSubmit={(feedback) => {
+                            const message = `${feedback.message}
+Fant du det du trengte? ${feedback.feedbackValue}
+Side: ${window.location.href}`;
+                            const to = "fremtind.designsystem";
+                            const domain = "fremtind";
+                            const mailto = `mailto:${to}@${domain}.no?subject=Tilbakemelding%20fra%20portalen&body=${encodeURIComponent(
+                                message,
+                            )}`;
+                            window.location.href = mailto;
+                        }}
+                    />
+                    <LogoStamp className="jkl-portal-footer__stamp" animated />
+                </div>
+                <Footer
+                    links={[
+                        { external: true, href: "https://github.com/fremtind/jokul", title: "Jøkul på GitHub" },
+                        { external: true, href: "https://www.fremtind.no/personvern/", title: "Personvernserklæring" },
+                        { external: true, href: "https://jobb.fremtind.no", title: "Jobb i Fremtind" },
+                    ]}
+                />
+            </div>
         </div>
     );
 };
--- portal/src/components/index.ts
+++ portal/src/components/index.ts
@@ -1,7 +1,6 @@
 import { APIDocumentation } from "./APIDocumentation";
 import { Header } from "./Header";
-import { Footer } from "./Footer";
 import { Layout, ThemeBG } from "./Layout";
 import { Sidebar } from "./Sidebar";
 
-export { APIDocumentation, Header, Footer, Layout, ThemeBG, Sidebar };
+export { APIDocumentation, Header, Layout, ThemeBG, Sidebar };
