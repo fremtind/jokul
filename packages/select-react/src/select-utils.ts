3d7acc459f64a491c8786ac1f76785971c9d0e1a
William Killerud <william@killerud.com>
2022-03-30 16:40:44 +0200

    fix: fjern core-toggle fra Select
    Så enda en feilende CI fordi noe inni core-toggle ikke var synlig.
    Fjerner core-toggle fra Select og håndterer ting selv.
    
    Legger til støtte for å lukke med Escape og åpne med Space a la native.

---
+++ packages/select-react/src/select-utils.ts
@@ -0,0 +1,28 @@
+import type { ValuePair } from "@fremtind/jkl-core";
+
+export function toLower(str = "") {
+    return str.toLowerCase().replace(/[\W_]+/g, ""); // strip all non-alphanumeric chars
+}
+
+export function toItemLabel(value: string | undefined, items: Array<string | ValuePair>): string {
+    if (!value) {
+        return "";
+    }
+    const item = items.find((i) => (typeof i === "string" ? i === value : i.value === value));
+    if (!item) {
+        return value;
+    }
+    return typeof item === "string" ? item : item.label;
+}
+
+export function focusSelected(listEl: HTMLElement, listId: string, selected: string | undefined) {
+    let focusedItem: HTMLElement | null;
+    if (selected) {
+        // move focus to selected option
+        focusedItem = listEl.querySelector(`#${listId}__${toLower(selected)}`);
+    } else {
+        // move focus to first option
+        focusedItem = listEl.querySelector('[role="option"]');
+    }
+    focusedItem?.focus();
+}
