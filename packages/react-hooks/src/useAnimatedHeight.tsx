1941cd8eae3e180915e7371755610c34098414e1
William Killerud <william@killerud.com>
2022-04-01 10:42:34 +0200

    fix: ikke baser animasjon på antall renders
    Med Concurrency i React har vi en annen rendering å forholde oss til.
    StrictMode kan hjelpe med å avdekke feil, og her avdekker den at
    useAnimatedHeight ikke er idempotent - noe som ikke er overaskende
    når vi ser på isFirstRender :D
    
    Skriv om så vi beholder samme intensjon - at elementer som starter
    med isOpen false ikke får en "animert collapse" ved første render –
    men på en idempotent måte.

--- packages/react-hooks/src/useAnimatedHeight.tsx
+++ packages/react-hooks/src/useAnimatedHeight.tsx
@@ -2,6 +2,7 @@ import { MutableRefObject, useEffect, useRef, RefObject, useCallback, useLayoutE
 import type { Easing, Timing } from "@fremtind/jkl-core";
 import { easings, timings } from "@fremtind/jkl-core";
 import { useBrowserPreferences } from "./useBrowserPreferences/useBrowserPreferences";
+import { usePreviousValue } from "./usePreviousValue/usePreviousValue";
 
 interface HTMLElementOrCoreToggleElement<T extends HTMLElementOrCoreToggleElement<T>> extends HTMLElement {
     el?: T; // Hack and workaround until https://github.com/nrkno/custom-element-to-react/pull/17 has landed
@@ -29,6 +30,7 @@ export function useAnimatedHeight<T extends HTMLElement>(
     isOpen: boolean,
     options?: UseAnimatedHeightOptions,
 ): [RefObject<T>, () => void] {
+    const wasOpen = usePreviousValue(isOpen);
     const easing = options?.easing || defaultEasing;
     const timing = options?.timing || defaultTiming;
     const transition = `${timing} height ${easing}`;
@@ -38,7 +40,6 @@ export function useAnimatedHeight<T extends HTMLElement>(
     const raf1 = useRef<number>();
     const raf2 = useRef<number>();
     const elementRef = useRef<T>(null);
-    const firstRender = useRef<boolean>(true);
 
     function handleTransitionEnd(event: TransitionEvent) {
         const element = getElement(elementRef);
@@ -56,13 +57,17 @@ export function useAnimatedHeight<T extends HTMLElement>(
     }
 
     const runAnimation = useCallback(() => {
-        if (firstRender.current) {
-            const element = getElement(elementRef);
-            if (element && !isOpen) {
-                element.style.display = "none";
-            }
+        const element = getElement(elementRef);
+        if (!element) {
+            return;
+        }
 
-            return; // Do not play animation on first render
+        if (!isOpen) {
+            element.style.display = "none";
+            if (!wasOpen) {
+                // Første render eller rerender med isOpen false
+                return;
+            }
         }
 
         options?.onTransitionStart?.(isOpen);
@@ -76,33 +81,30 @@ export function useAnimatedHeight<T extends HTMLElement>(
             return;
         }
 
-        const element = getElement(elementRef);
-        if (element) {
-            element.style.transition = transition;
-            element.style.display = "block";
-            element.style.overflow = "hidden";
-            if (isOpen) {
-                element.style.height = "0";
-                element.style.height = `${element.scrollHeight}px`;
-            } else {
-                // If the scrollHeight is 0 it means that we are transitioning from height 0 -> 0.
-                // This causes the "transitionend"-event to never fire and the element gets stuck with
-                // style: height: 0; display: block; overflow:hidden
-                if (element.scrollHeight === 0) {
-                    element.removeAttribute("style");
-                    return;
-                }
-
-                element.style.height = `${element.scrollHeight}px`;
-
-                raf1.current = requestAnimationFrame(() => {
-                    raf2.current = requestAnimationFrame(() => {
-                        element.style.height = `${0}px`;
-                    });
-                });
+        element.style.transition = transition;
+        element.style.display = "block";
+        element.style.overflow = "hidden";
+        if (isOpen) {
+            element.style.height = "0";
+            element.style.height = `${element.scrollHeight}px`;
+        } else {
+            // If the scrollHeight is 0 it means that we are transitioning from height 0 -> 0.
+            // This causes the "transitionend"-event to never fire and the element gets stuck with
+            // style: height: 0; display: block; overflow:hidden
+            if (element.scrollHeight === 0) {
+                element.removeAttribute("style");
+                return;
             }
+
+            element.style.height = `${element.scrollHeight}px`;
+
+            raf1.current = requestAnimationFrame(() => {
+                raf2.current = requestAnimationFrame(() => {
+                    element.style.height = `${0}px`;
+                });
+            });
         }
-    }, [isOpen, options, transition, prefersReducedMotion]);
+    }, [isOpen, options, wasOpen, transition, prefersReducedMotion]);
 
     useLayoutEffect(() => {
         runAnimation();
@@ -125,7 +127,6 @@ export function useAnimatedHeight<T extends HTMLElement>(
     useEffect(() => {
         const r1 = raf1.current;
         const r2 = raf2.current;
-        firstRender.current = false;
         return () => {
             r1 && cancelAnimationFrame(r1);
             r2 && cancelAnimationFrame(r2);
