27f38fadfecc0f736c9cc406ebb9493b1b1af10c
William Killerud <william@killerud.com>
2022-04-08 15:28:47 +0200

    chore: bruk egenkomponert action for endrede snapshots
    Gidder ikke mer av å slette hele mapper for å få lagt til nye snapshots

---
+++ actions/changed-files/src/input-validation.ts
@@ -0,0 +1,28 @@
+export function validatePatternsInput(input: string): string {
+    if (!input) {
+        throw new Error(
+            `The action must be given an input named "patterns" with either a single pattern as a string or a multiline string list of patterns`,
+        );
+    }
+    return input;
+}
+
+export function toList(input: string): string[] {
+    if (Array.isArray(input)) {
+        return input;
+    }
+    return input.split(/\n/);
+}
+
+export function validateOutputInput(input: string, defaultOutput = "string"): "string" | "array" {
+    if (!input) {
+        return defaultOutput as "string";
+    }
+
+    const validValues: string[] = ["string", "array"];
+    if (!validValues.includes(input)) {
+        throw new Error(`Variable "output" must be one of ${validValues}`);
+    }
+
+    return input as "string" | "array";
+}
