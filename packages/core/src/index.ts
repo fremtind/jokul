f21359802590123bf2ac5b135949f19a3e03360b
William Killerud <william@killerud.com>
2022-04-19 16:54:30 +0200

    feat: core eksporterer to interfaces for children
    React 18 fjerner children som implicit prop. Om komponenten din bare trenger children, eller du vil
    slippe å deklarere den overalt, extend disse to typene.

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -2,14 +2,4 @@ export type { Easing, Timing } from "./utils";
 export { initTabListener, getValuePair, breakpoints, timings, easings } from "./utils";
 export type { LabelProps, LinkProps, NavLinkProps, ScreenReaderOnlyProps, SupportLabelProps } from "./components";
 export { Label, Link, NavLink, ScreenReaderOnly, SupportLabel } from "./components";
-
-export type LabelVariant = "small" | "medium" | "large";
-export type ValuePair = {
-    value: string;
-    label: string;
-};
-
-// Brukes til å extende props for komponenter som skal støtte data-testautoid ifbm. Test Complete
-export interface DataTestAutoId {
-    "data-testautoid"?: string;
-}
+export type { DataTestAutoId, LabelVariant, ValuePair, WithChildren, WithOptionalChildren } from "./types";
