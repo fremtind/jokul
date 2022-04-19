90444a652b762d85c9bdbcf58b5e861ce9f07933
William Killerud <william@killerud.com>
2022-04-19 09:25:14 +0200

    chore: oppdater avhengigheter
    Fjern testing-library/react-hooks nå som renderHook er flyttet til
    hovedpakka.

--- packages/react-hooks/src/useScrollIntoView/useScrollIntoView.test.tsx
+++ packages/react-hooks/src/useScrollIntoView/useScrollIntoView.test.tsx
@@ -1,4 +1,4 @@
-import { renderHook } from "@testing-library/react-hooks";
+import { renderHook } from "@testing-library/react";
 import { useScrollIntoView } from "./useScrollIntoView";
 import { RefObject } from "react";
 
