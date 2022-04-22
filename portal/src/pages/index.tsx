f1e32c419bb57b5098fdbf104b16974a1133b5f0
William Killerud <william@killerud.com>
2022-04-22 16:18:13 +0200

    docs: oppdater innganger på forsiden :cowboy:

--- portal/src/pages/index.tsx
+++ portal/src/pages/index.tsx
@@ -1,13 +1,14 @@
 import React, { useState, useMemo } from "react";
-import { withPrefix } from "gatsby";
+import { Link, withPrefix } from "gatsby";
 import { motion } from "framer-motion";
 import { useScreen, useBrowserPreferences } from "@fremtind/jkl-react-hooks";
 import { DelayText } from "../components/Delaytext";
-import { CardList, Card } from "../components/Card";
+import { CardList } from "../components/Card";
 import { PrincipleDiamond } from "../components/PrincipleDiamond";
 import { VisibleDetector } from "../components/VisibleDetector";
 import "./style.scss";
 import "./index.scss";
+import { NavCard } from "@fremtind/jkl-card-react";
 
 // TODO Try to use scrollY in stead of time when lerping diamonds
 // TODO Remove DelayText when it's done
@@ -77,18 +78,16 @@ const IndexPage: React.FC = () => {
                             />
                         </div>
                         <CardList vertical>
-                            <Card heading="Universell utforming" link="/universell-utforming/guide">
-                                Få hjelp til å komme i gang med Universell utforming.
-                            </Card>
-                            <Card heading="For utviklere" link="/kom-i-gang/utvikling">
-                                Her får du vite det mest grunnleggende før du setter i gang å bruke Jøkul.
-                            </Card>
-                            <Card heading="For designere" link="/kom-i-gang/design">
-                                Her får du vite det mest grunnleggende før du setter i gang å bruke Jøkul.
-                            </Card>
-                            <Card heading="Endre" link="/guider/hvordan-endre-jokul">
-                                Hvordan du kan endre designsystemet.
-                            </Card>
+                            <NavCard title="Introduksjon" to="/kom-i-gang/introduksjon" component={Link}>
+                                Start her om Jøkul er nytt for deg.
+                            </NavCard>
+                            <NavCard title="For designere" to="/kom-i-gang/design" component={Link}>
+                                Branching, Variants, Auto-Layout, breakpoints, praktiske plugins, “uskrevne regler” og
+                                mer.
+                            </NavCard>
+                            <NavCard title="For utviklere" to="/kom-i-gang/utvikling" component={Link}>
+                                Om pakker, steder å finne kodeeksempler og dokumentasjon, nyttige verktøy og mer.
+                            </NavCard>
                         </CardList>
                     </div>
                 </section>
