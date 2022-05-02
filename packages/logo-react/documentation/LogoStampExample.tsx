271fc88f657aaaa5f6d9cb72768b44349080b299
Pio Rasch-Halvorsen <pio.rasch-halvorsen@fremtind.no>
2022-05-02 16:42:07 +0200

    feat: legg til flere varianter av logostemplet

--- packages/logo-react/documentation/LogoStampExample.tsx
+++ packages/logo-react/documentation/LogoStampExample.tsx
@@ -1,12 +1,37 @@
-import React, { VFC } from "react";
-import { CodeExample, ExampleComponentProps } from "../../../doc-utils";
+import React from "react";
+import { CodeExample, ExampleComponentProps, ExampleKnobsProps } from "../../../doc-utils";
 import { LogoStamp } from "../src";
+import { logoStampTextPaths } from "../src/logoStampTextPaths";
 import "./LogoExample.scss";
 
-export const LogoStampExample: VFC<ExampleComponentProps> = ({ boolValues }) => (
-    <LogoStamp className="logo-example" animated={boolValues?.["Animert"]} />
+export const LogoStampExample = ({ boolValues, choiceValues }: ExampleComponentProps) => (
+    <LogoStamp
+        className="logo-example"
+        animated={boolValues?.["Animert"]}
+        variant={choiceValues?.["Variant"] as keyof typeof logoStampTextPaths}
+    />
 );
 
-export const logoStampExampleCode: CodeExample = ({ boolValues }) => `
-<LogoStamp animated={${boolValues?.["Animert"]}} />
-`;
+export const logoStampExampleCode: CodeExample = ({ boolValues, choiceValues }) => `
+<LogoStamp
+    animated={${boolValues?.["Animert"]}}
+    variant="${choiceValues?.["Variant"]}"
+/>`;
+
+export const logoStampExampleKnobs: ExampleKnobsProps = {
+    boolProps: ["Animert"],
+    choiceProps: [
+        {
+            name: "Variant",
+            values: [
+                "fraSB1ogDNB",
+                "forsikringLevertAvFremtind",
+                "vartForsikringsselskap",
+                "innovasjonFraFremtind",
+                "teknologiFraFremtind",
+                "vartEgetForsikringsselskap",
+            ],
+            defaultValue: 0,
+        },
+    ],
+};
