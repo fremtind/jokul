383f330ebca3a4f69ae87eb12a1c7a27e2527670
Pio Rasch-Halvorsen <pio.rasch-halvorsen@fremtind.no>
2022-05-09 14:05:39 +0200

    feat: refaktorer LogoStamp til å være composite
    Eksporter teksten rundt logostemplene som separate komponenter for å redusere importstørrelsen til
    logostemplet. Refaktorer komponenten for å legge til rette for dette.
    
    BREAKING CHANGE:
    LogoStamp tar ikke lenger propen variant, og må nå ha children i form av en av tekstkomponentene.

--- packages/logo-react/src/LogoStamp.tsx
+++ packages/logo-react/src/LogoStamp.tsx
@@ -1,51 +1,32 @@
-import { useId, useIntersectionObserver } from "@fremtind/jkl-react-hooks";
+import { useId } from "@fremtind/jkl-react-hooks";
 import cn from "classnames";
-import React, { useRef, useState } from "react";
+import { WithChildren } from "@fremtind/jkl-core";
+import React, { useRef } from "react";
 
-import { logoStampTextPaths } from "./logoStampTextPaths";
+import { useTextSpinner } from "./useTextSpinner";
 
-export interface LogoStampProps {
+export interface LogoStampProps extends WithChildren {
     className?: string;
     id?: string;
     /**
      * Velger hva som står i teksten rundt logostemplet. Se portalen eller Figma for oversikt.
      * @default "fraSB1ogDNB"
      */
-    variant?: keyof typeof logoStampTextPaths;
+    // variant?: keyof typeof logoStampTextPaths;
     /**
      * Beskrivelse av logostempelet tilgjengelig for skjermlesere.
      * Teksten i logostempelet er en path, og ikke tilgjengelig for skjermlesere.
-     * @default Tekst tilsvarende teksten i stempelvarianten du har valgt
+     * @default "Forsikring levert av Fremtind"
      */
     title?: string;
     /** Roterer teksten rundt logostempelet første gangen stempelet kommer til syne */
     animated?: boolean;
 }
 
-export const LogoStamp = ({
-    className,
-    id,
-    animated = false,
-    title,
-    variant = "fraSB1ogDNB",
-}: LogoStampProps): JSX.Element => {
+export const LogoStamp = ({ children, className, id, animated = false, title }: LogoStampProps): JSX.Element => {
     const uniqueId = useId(id || "jkl-logo-stamp", { generateSuffix: !id });
-
     const stampRef = useRef<SVGSVGElement>(null);
-    const [hasAnimated, setHasAnimated] = useState(false);
-    const [visible, setVisible] = useState(false);
-
-    const onIntersect = (entries: IntersectionObserverEntry[]) => {
-        const intersecting = entries.some((entry) => entry.isIntersecting);
-        setVisible(intersecting);
-        if (intersecting) {
-            setHasAnimated(true);
-        }
-    };
-    useIntersectionObserver(stampRef, onIntersect, () => setVisible(true), {
-        rootMargin: "50% 0% 0% 0%",
-        threshold: 0.5,
-    });
+    const { hasAnimated, visible } = useTextSpinner(stampRef);
 
     return (
         <svg
@@ -59,16 +40,14 @@ export const LogoStamp = ({
             data-rotate={animated && (visible || hasAnimated)}
             role="img"
         >
-            <title id={uniqueId}>{title || logoStampTextPaths[variant].title}</title>
+            <title id={uniqueId}>{title || "Forsikring levert av Fremtind"}</title>
             <g className="jkl-logo-stamp__logo">
                 <path
                     fill="currentColor"
                     d="M256 177.2L146.4 286.8l8.5 8.4 51.4-51.4 51.4 51.4 8.2-8.1-51.5-51.5 41.9-41.8L357.5 295l8.1-8.2z"
                 />
             </g>
-            <g className="jkl-logo-stamp__text">
-                <path fill="currentColor" d={logoStampTextPaths[variant].path} />
-            </g>
+            <g className="jkl-logo-stamp__text">{children}</g>
         </svg>
     );
 };
