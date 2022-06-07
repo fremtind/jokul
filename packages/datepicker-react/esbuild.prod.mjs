246952ae75afe20bcf0d007a0a068b76b114f9a6
Pio Nizar Finn Rasch-Halvorsen <pio.rasch-halvorsen@fremtind.no>
2022-06-07 10:58:21 +0200

    perf: bruk tiny-glob for globbing av filer under bygg

--- packages/datepicker-react/esbuild.prod.mjs
+++ packages/datepicker-react/esbuild.prod.mjs
@@ -1,7 +1,7 @@
-import glob from "glob";
+import glob from "tiny-glob";
 import { build } from "../../esbuild.prod.mjs";
 
 await build({
-    entryPoints: glob.sync("src/**/*.@(ts|tsx)", { ignore: "src/**/*.test.@(ts|tsx)" }),
+    entryPoints: await glob("src/**/!(*.test).@(ts|tsx)"),
     outdir: "build",
 });
