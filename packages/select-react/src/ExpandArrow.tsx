bde149ed20466b9b28b2f57366be24248c5f9bfb
William Killerud <william@killerud.com>
2022-08-03 09:51:00 +0200

    chore: oppdater til å bruke nyeste klassenavn fra CSS-pakka

--- packages/select-react/src/ExpandArrow.tsx
+++ packages/select-react/src/ExpandArrow.tsx
@@ -9,20 +9,30 @@ interface Props {
 export const ExpandArrow: FC<Props> = ({ expanded = false, className }) => {
     const componentClassName = classNames(
         {
-            "jkl-expand-arrow": true,
-            "jkl-expand-arrow--expanded": expanded,
+            "jkl-select-expand-arrow": true,
+            "jkl-select-expand-arrow--expanded": expanded,
         },
         className,
     );
     return (
         <span className={componentClassName}>
-            <svg className="jkl-expand-arrow__down" viewBox="0 0 13 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <svg
+                className="jkl-select-expand-arrow__down"
+                viewBox="0 0 13 16"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+            >
                 <path
                     d="M6.90762 13.8828L11.9272 8.86326L12.7093 9.64537L6.35463 16L-1.68645e-06 9.64536L0.782101 8.86326L5.80156 13.8827L5.80156 1.01438e-06L6.90762 1.20777e-06L6.90762 13.8828Z"
                     fill="currentColor"
                 />
             </svg>
-            <svg className="jkl-expand-arrow__up" viewBox="0 0 13 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <svg
+                className="jkl-select-expand-arrow__up"
+                viewBox="0 0 13 16"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+            >
                 <path
                     d="M5.80163 2.1172L0.78209 7.13674L-1.29938e-05 6.35464L6.35462 5.14415e-08L12.7093 6.35464L11.9272 7.13674L6.90769 2.11728L6.9077 16L5.80163 16L5.80163 2.1172Z"
                     fill="currentColor"
