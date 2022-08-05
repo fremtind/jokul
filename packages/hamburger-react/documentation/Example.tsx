e046a1a889d41f9153eb8c9bff76226e2c57a4aa
William Killerud <william@killerud.com>
2022-08-05 13:39:42 +0200

    feat: kompaktversjon av hamburger

--- packages/hamburger-react/documentation/Example.tsx
+++ packages/hamburger-react/documentation/Example.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { DevExample } from "../../../doc-utils";
 import { HamburgerExample, hamburgerExampleCode, hamburgerExampleKnobs } from "./HamburgerExample";
 import "../../hamburger/hamburger.scss";
+import "../../content-toggle/content-toggle.scss";
 
 export default function Example() {
     return <DevExample component={HamburgerExample} knobs={hamburgerExampleKnobs} codeExample={hamburgerExampleCode} />;
