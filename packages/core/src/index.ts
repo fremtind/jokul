f10156cf40b14e6e696224c4ce10bc07fdd6a27c
William Killerud <william@killerud.com>
2022-08-23 12:20:47 +0200

    fix: eksporter getCompactValue fra rotnivå

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,5 +1,5 @@
 export type { Easing, Timing } from "./utils";
-export { initTabListener, getValuePair, breakpoints, timings, easings } from "./utils";
+export { initTabListener, getValuePair, breakpoints, timings, easings, getCompactValue } from "./utils";
 export type { LabelProps, LinkProps, NavLinkProps, ScreenReaderOnlyProps, SupportLabelProps } from "./components";
 export { Label, Link, NavLink, ScreenReaderOnly, SupportLabel } from "./components";
 export type { DataTestAutoId, LabelVariant, ValuePair, WithChildren, WithOptionalChildren } from "./types";
