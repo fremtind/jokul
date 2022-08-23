70f2aa3c9f769fcb27f5901897c7be99aa2a7261
William Killerud <william@killerud.com>
2022-08-23 10:20:02 +0200

    feat: legg til mulighet å skru av compact

--- packages/breadcrumb-react/src/Breadcrumb.tsx
+++ packages/breadcrumb-react/src/Breadcrumb.tsx
@@ -1,15 +1,25 @@
 import { WithChildren } from "@fremtind/jkl-core";
+import { getCompactValue } from "@fremtind/jkl-core/src/utils/compact";
 import cn from "classnames";
 import React from "react";
 
 export interface BreadcrumbProps extends WithChildren {
     className?: string;
+    /**
+     * Skal bare brukes i informasjonstette applikasjoner.
+     */
+    compact?: boolean;
 }
 
-export const Breadcrumb = ({ className, children, ...rest }: BreadcrumbProps): JSX.Element => {
+export const Breadcrumb = ({ className, children, compact, ...rest }: BreadcrumbProps): JSX.Element => {
     const numberOfChildren = React.Children.count(children);
     return (
-        <nav aria-label="Sti" className={cn("jkl-breadcrumb", className)} {...rest}>
+        <nav
+            aria-label="Sti"
+            className={cn("jkl-breadcrumb", className)}
+            data-compactlayout={getCompactValue(compact)}
+            {...rest}
+        >
             <ol className="jkl-breadcrumb__list">
                 {React.Children.map(children, (child, index) => {
                     const isLastElement = index + 1 === numberOfChildren;
