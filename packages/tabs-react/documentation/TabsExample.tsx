4a08783449c1e0fa5969a2193def4b4d78441cf5
William Killerud <william@killerud.com>
2022-08-29 10:11:29 +0200

    chore: resync med figma

--- packages/tabs-react/documentation/TabsExample.tsx
+++ packages/tabs-react/documentation/TabsExample.tsx
@@ -12,18 +12,24 @@ export const TabsExample: FC<ExampleComponentProps> = () => {
                 <Tab>Bedriftansvarlige</Tab>
                 <Tab>Rådgivere</Tab>
             </TabList>
-            <TabPanel>Innhold for Bedrifter</TabPanel>
-            <TabPanel>Innhold for Bedriftansvarlige</TabPanel>
             <TabPanel>
-                Innhold for Rådgivere
-                <Tabs>
-                    <TabList aria-label="subtabs">
-                        <Tab>Bedrift AS</Tab>
-                        <Tab>Alternativ AS</Tab>
-                    </TabList>
-                    <TabPanel className="jkl-spacing-m--all">Innhold for Bedrift AS</TabPanel>
-                    <TabPanel className="jkl-spacing-m--all">Innhold for Alternativ AS</TabPanel>
-                </Tabs>
+                <div className="jkl-spacing-m--all">Innhold for Bedrifter</div>
+            </TabPanel>
+            <TabPanel>
+                <div className="jkl-spacing-m--all">Innhold for Bedriftansvarlige</div>
+            </TabPanel>
+            <TabPanel>
+                <div className="jkl-spacing-m--all">
+                    Innhold for Rådgivere
+                    <Tabs>
+                        <TabList aria-label="subtabs">
+                            <Tab>Bedrift AS</Tab>
+                            <Tab>Alternativ AS</Tab>
+                        </TabList>
+                        <TabPanel className="jkl-spacing-m--all">Innhold for Bedrift AS</TabPanel>
+                        <TabPanel className="jkl-spacing-m--all">Innhold for Alternativ AS</TabPanel>
+                    </Tabs>
+                </div>
             </TabPanel>
         </Tabs>
     );
