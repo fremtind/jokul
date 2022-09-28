7027fd774d40a37c49c25a4bf73c6c9a02c4dc59
William Killerud <william@killerud.com>
2022-09-28 09:18:59 +0200

    test: utvid regresjonstestene med åpen tilstand

--- packages/tooltip-react/integration/tooltip.spec.js
+++ packages/tooltip-react/integration/tooltip.spec.js
@@ -8,5 +8,19 @@ describe("Tooltip", () => {
 
     it("renders correctly", () => {
         cy.takeSnapshots();
+
+        cy.takeSnapshots({
+            setup: () => {
+                cy.get(`button.jkl-tooltip__button`).first().click();
+                cy.get(`button.jkl-tooltip__button`).first().focus();
+                // eslint-disable-next-line cypress/no-unnecessary-waiting
+                cy.wait(120); // Vent på Framer Motion
+            },
+            teardown: () => {
+                cy.get(`button.jkl-tooltip__button`).first().click();
+                // eslint-disable-next-line cypress/no-unnecessary-waiting
+                cy.wait(120); // Vent på Framer Motion
+            },
+        });
     });
 });
