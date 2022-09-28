57d9a9b1dfdabdcd1ed2f0387411e058c50afa88
William Killerud <william@killerud.com>
2022-09-28 09:11:56 +0200

    fix: sett easeIn og duration 100 som speccet

--- packages/tooltip-react/src/Tooltip.tsx
+++ packages/tooltip-react/src/Tooltip.tsx
@@ -63,7 +63,7 @@ export const Tooltip = ({ content, initialPlacement = "top", className }: Toolti
                             initial={{ opacity: 0 }}
                             animate={{ opacity: 1 }}
                             exit={{ opacity: 0 }}
-                            transition={{ ease: "easeInOut", duration: 0.25 }}
+                            transition={{ ease: "easeIn", duration: 0.1 }}
                             data-placement={placement}
                             className="jkl-tooltip__content"
                             {...getFloatingProps({
