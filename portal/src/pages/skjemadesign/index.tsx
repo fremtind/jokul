c237c2fd92f73fbf335fa9d7668ddb8bcc81754b
William Killerud <william@killerud.com>
2022-09-29 08:42:11 +0200

    docs: nytt hjem for skjemadesign

---
+++ portal/src/pages/skjemadesign/index.tsx
@@ -0,0 +1,10 @@
+import React, { FC } from "react";
+import { MainContent } from "../../layout/MainContent";
+
+const Skjemadesign: FC = () => (
+    <MainContent>
+        <h1 className="jkl-title">Skjemadesign</h1>
+    </MainContent>
+);
+
+export default Skjemadesign;
