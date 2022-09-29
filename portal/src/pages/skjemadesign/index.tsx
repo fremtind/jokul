88c354ac902ddbf777c03d1e1f79ea0e748aff5a
William Killerud <william@killerud.com>
2022-09-29 08:51:08 +0200

    docs: legg opp til å bruke faner

--- portal/src/pages/skjemadesign/index.tsx
+++ portal/src/pages/skjemadesign/index.tsx
@@ -1,9 +1,27 @@
+import { Tab, TabList, TabPanel, Tabs } from "@fremtind/jkl-tabs-react";
 import React, { FC } from "react";
 import { MainContent } from "../../layout/MainContent";
 
 const Skjemadesign: FC = () => (
     <MainContent>
         <h1 className="jkl-title">Skjemadesign</h1>
+        <Tabs>
+            <TabList aria-label="Seksjoner">
+                <Tab>Eksempler</Tab>
+                <Tab>Kode</Tab>
+                <Tab>Bruk</Tab>
+                <Tab>Universell utforming</Tab>
+                <Tab>Tekster</Tab>
+                <Tab>Changelog</Tab>
+            </TabList>
+
+            <TabPanel>Hallo eksempler</TabPanel>
+            <TabPanel>Hallo kode</TabPanel>
+            <TabPanel>Hallo bruk</TabPanel>
+            <TabPanel>Hallo UU</TabPanel>
+            <TabPanel>Hallo tekster</TabPanel>
+            <TabPanel>Hallo changelog</TabPanel>
+        </Tabs>
     </MainContent>
 );
 
