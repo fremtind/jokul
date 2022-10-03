2f9c4da14df887e20725094110096d819f295b24
Pio Nizar Finn Rasch-Halvorsen <pio.rasch-halvorsen@fremtind.no>
2022-10-03 09:33:48 +0200

    feat: legg til støtte for inline og data-testautoid

--- packages/input-group-react/src/InputGroup.tsx
+++ packages/input-group-react/src/InputGroup.tsx
@@ -8,7 +8,7 @@ import {
     type DataTestAutoId,
 } from "@fremtind/jkl-core";
 import { useId } from "@fremtind/jkl-react-hooks";
-import React, { ReactNode } from "react";
+import React, { CSSProperties, ReactNode } from "react";
 
 export interface InputProps {
     density?: Density;
@@ -18,7 +18,9 @@ export interface InputProps {
 
 export interface InputGroupProps extends WithChildren, DataTestAutoId {
     id?: string;
+    "data-testid"?: string;
     density?: Density;
+    inline?: boolean;
     label: ReactNode;
     errorLabel?: string;
     helpLabel?: string;
@@ -27,12 +29,15 @@ export interface InputGroupProps extends WithChildren, DataTestAutoId {
 }
 
 export const InputGroup = (props: InputGroupProps) => {
-    const { children, density, errorLabel, helpLabel, label, labelProps, supportLabelProps, id, ...rest } = props;
+    const { children, density, errorLabel, helpLabel, inline, label, labelProps, supportLabelProps, id, ...rest } =
+        props;
 
     const uid = useId(id || "jkl-input", { generateSuffix: !id });
     const supportId = useId("jkl-support-label");
     const hasSupportText = helpLabel || errorLabel;
     const describedBy = hasSupportText ? supportId : undefined;
+
+    const inlineStyle: CSSProperties = { display: "inline-block", margin: "0 8px" };
 
     const renderInput = () => {
         const input = React.Children.toArray(props.children)[0];
@@ -46,12 +51,18 @@ export const InputGroup = (props: InputGroupProps) => {
     };
 
     return (
-        <div className="jkl-input-group" data-density={density} {...rest}>
-            <Label standAlone htmlFor={uid} srOnly={false} {...labelProps}>
+        <div className="jkl-input-group" data-density={density} style={inline ? inlineStyle : undefined} {...rest}>
+            <Label standAlone htmlFor={uid} srOnly={inline} {...labelProps}>
                 {label}
             </Label>
             {renderInput()}
-            <SupportLabel id={supportId} errorLabel={errorLabel} helpLabel={helpLabel} {...supportLabelProps} />
+            <SupportLabel
+                id={supportId}
+                errorLabel={errorLabel}
+                helpLabel={helpLabel}
+                srOnly={inline}
+                {...supportLabelProps}
+            />
         </div>
     );
 };
