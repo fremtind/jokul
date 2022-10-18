696c92b212a0c7d5f8551ae10bcb443ae350c8d0
Pio Nizar Finn Rasch-Halvorsen <pio.rasch-halvorsen@fremtind.no>
2022-10-18 09:45:31 +0200

    refactor: legg til mer eksplisitt eksport av komponenter i tabs-react

--- packages/tabs-react/src/index.ts
+++ packages/tabs-react/src/index.ts
@@ -1,5 +1,5 @@
-export * from "./Tabs";
-export * from "./TabList";
-export * from "./Tab";
-export * from "./TabPanel";
-export * from "./useTabNavigation";
+export { Tabs, type TabsProps } from "./Tabs";
+export { TabList, type TabListProps } from "./TabList";
+export { Tab, type TabProps } from "./Tab";
+export { TabPanel, type TabPanelProps } from "./TabPanel";
+export { type TabNavigationOptions, useTabNavigation } from "./useTabNavigation";
