1ffbc34280556811c89693e0730118646f681f9c
William Killerud <william@killerud.com>
2022-10-26 10:25:48 +0200

    docs: push eksperiment med avspilling ved hover, fokus
    Eksperimenterte her som del av portal 3.0. Syntes det fungerte såpass godt at vi pusher.

--- portal/src/components/ExampleVideo/ExampleVideo.tsx
+++ portal/src/components/ExampleVideo/ExampleVideo.tsx
@@ -1,5 +1,5 @@
-import classNames from "classnames";
-import React, { FC, useRef, useState } from "react";
+import { Loader } from "@fremtind/jkl-loader-react";
+import React, { FC, useEffect, useRef, useState } from "react";
 import "./ExampleVideo.scss";
 
 interface Props {
@@ -8,27 +8,87 @@ interface Props {
 
 export const ExampleVideo: FC<Props> = ({ videoUrl }) => {
     const urlWithParams = `${videoUrl}?background=1&autoplay=0&loop=1`;
-    const [playing, togglePlayState] = useState(false);
+    const [play, setPlay] = useState(false);
+    const [isActive, setIsActive] = useState(false);
+    const [playing, setPlaying] = useState(false);
     const ref = useRef<HTMLIFrameElement>(null);
 
-    const togglePlay = () => {
+    const updatePlayerState = () => {
         if (ref.current) {
-            const method = playing ? "pause" : "play";
+            const method = play ? "pause" : "play";
             const playerData = { method };
             ref.current.contentWindow?.postMessage(JSON.stringify(playerData), "*");
-            togglePlayState(!playing);
         }
     };
 
-    const buttonClass = classNames("example-video__button", {
-        "example-video__button--play": !playing,
-        "example-video__button--pause": playing,
+    useEffect(() => {
+        const onMessage = (message: MessageEvent) => {
+            if (message.source !== ref.current?.contentWindow) {
+                return;
+            }
+            const data: { method: "play" | "pause" } = JSON.parse(message.data);
+            setPlaying(data.method === "play");
+        };
+
+        if (window) {
+            window.addEventListener("message", onMessage);
+        }
+
+        return () => {
+            if (window) {
+                window.removeEventListener("message", onMessage);
+            }
+        };
     });
 
+    const onEnter = () => {
+        setPlay(true);
+        setIsActive(true);
+        updatePlayerState();
+    };
+
+    const onExit = () => {
+        setPlay(false);
+        setIsActive(false);
+        updatePlayerState();
+    };
+
+    const togglePlay = () => {
+        setPlay(!play);
+        updatePlayerState();
+    };
+
+    const onKeyDown = (e: React.KeyboardEvent<HTMLDivElement>) => {
+        if (e.key === " " || e.key === "Enter") {
+            e.preventDefault();
+            togglePlay();
+        }
+    };
+
     return (
-        <div className="example-video">
-            <button aria-label={playing ? "Pause" : "Play"} className={buttonClass} onClick={togglePlay} />
-            <iframe ref={ref} title="Eksempelvideo" src={urlWithParams} width="100%" height="250" frameBorder="0" />
+        <div
+            className="example-video"
+            onMouseEnter={onEnter}
+            onMouseLeave={onExit}
+            onFocus={onEnter}
+            onBlur={onExit}
+            onKeyDown={onKeyDown}
+            aria-label={play ? "Pause" : "Play"}
+            role="button"
+            tabIndex={0}
+        >
+            {isActive && !playing && (
+                <Loader className="example-video__loader" variant="small" textDescription="Venter på video" />
+            )}
+            <iframe
+                tabIndex={-1}
+                ref={ref}
+                title="Eksempelvideo"
+                src={urlWithParams}
+                width="100%"
+                height="250"
+                frameBorder="0"
+            />
         </div>
     );
 };
