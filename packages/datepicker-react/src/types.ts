19c174725bad5476847a2f2ebb44cd0563979ffe
Jo Emil Holen <jo.emil.holen@fremtind.no>
2022-12-22 11:56:37 +0100

    fix: gjør typen for event handlere gjenbrukbar
    union types kan ikke brukes til å definere funksjoner, så med
    definisjonen under, vil a og b bli implisitt `any` i stedet for typene
    definert av DatePickerChangeEventHandler.
    
    `const handleChange: DatePickerChangeEventHandler => (a, b) => {}`

--- packages/datepicker-react/src/types.ts
+++ packages/datepicker-react/src/types.ts
@@ -3,22 +3,25 @@ import { ChangeEvent, KeyboardEvent, FocusEvent, ButtonHTMLAttributes } from "re
 
 export type DateValidationError = "WRONG_FORMAT" | "OUTSIDE_LOWER_BOUND" | "OUTSIDE_UPPER_BOUND";
 
-export type DatePickerChangeEventHandler =
-    | ((e: ChangeEvent<HTMLInputElement>) => void)
-    | ((e: ChangeEvent<HTMLInputElement>, date: Date | null) => void)
-    | ((e: ChangeEvent<HTMLInputElement>, date: Date | null, meta: DatePickerMetadata) => void);
+export type DatePickerChangeEventHandler = (
+    e: ChangeEvent<HTMLInputElement>,
+    date: Date | null,
+    meta: DatePickerMetadata,
+) => void;
 
-export type DatePickerBlurEventHandler =
-    | ((e: FocusEvent<HTMLInputElement>) => void)
-    | ((e: FocusEvent<HTMLInputElement>, date: Date | null) => void)
-    | ((e: FocusEvent<HTMLInputElement>, date: Date | null, meta: DatePickerMetadata) => void);
+export type DatePickerBlurEventHandler = (
+    e: FocusEvent<HTMLInputElement>,
+    date: Date | null,
+    meta: DatePickerMetadata,
+) => void;
 
 export type DatePickerFocusEventHandler = DatePickerBlurEventHandler;
 
-export type DatePickerKeyDownEventHandler =
-    | ((e: KeyboardEvent<HTMLInputElement>) => void)
-    | ((e: KeyboardEvent<HTMLInputElement>, date: Date | null) => void)
-    | ((e: KeyboardEvent<HTMLInputElement>, date: Date | null, meta: DatePickerMetadata) => void);
+export type DatePickerKeyDownEventHandler = (
+    e: KeyboardEvent<HTMLInputElement>,
+    date: Date | null,
+    meta: DatePickerMetadata,
+) => void;
 
 export type DatePickerMetadata = {
     error: DateValidationError | null;
