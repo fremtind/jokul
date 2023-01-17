ce7b66681eb7c42a61565c6f0115790927d3dc22
Pio Nizar Finn Rasch-Halvorsen <pio.rasch-halvorsen@fremtind.no>
2023-01-17 10:07:29 +0100

    feat: legg til Label og SupportLabel i InputGroup

--- packages/input-group-react/src/index.ts
+++ packages/input-group-react/src/index.ts
@@ -1,1 +1,3 @@
 export { InputGroup, type InputGroupProps, type InputProps } from "./InputGroup";
+export { Label, type LabelProps, type LabelVariant } from "./Label";
+export { SupportLabel, type SupportLabelProps } from "./SupportLabel";
