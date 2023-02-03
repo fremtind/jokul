11ba934abc6953b834ebaa2b838318327b5daaee
Maciej Ostrowski <maciej.ostrowski@fremtind.no>
2023-02-03 12:39:46 +0100

    feat: fjern ubrukt komponent

--- packages/new-datepicker-react/src/NewDatepicker.tsx
+++
@@ -1,7 +0,0 @@
-import React, { HTMLAttributes } from "react";
-
-interface Props extends HTMLAttributes<HTMLDivElement> {}
-
-export const NewDatepicker = (props: Props) => {
-    return <div>{props.children}</div>;
-};
