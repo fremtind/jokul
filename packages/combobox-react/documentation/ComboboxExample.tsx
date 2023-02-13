2882f03bc058dd07eec310c6a6d73f710ffd7727
fremtindelise <elise.brenna@fremtind.no>
2023-02-13 10:24:06 +0100

    feat: oppdaterte combobox-eksempel og endret noe styling

--- packages/combobox-react/documentation/ComboboxExample.tsx
+++ packages/combobox-react/documentation/ComboboxExample.tsx
@@ -1,11 +1,21 @@
+import type { LabelVariant } from "@fremtind/jkl-input-group-react";
 import React, { FC, useState } from "react";
-import { ExampleComponentProps, ExampleKnobsProps, CodeExample } from "../../../doc-utils";
+import { ExampleComponentProps, ExampleKnobsProps } from "../../../doc-utils";
 import { Combobox } from "../src";
 
-export const comboboxExampleKnobs: ExampleKnobsProps = {};
+export const comboboxExampleKnobs: ExampleKnobsProps = {
+    choiceProps: [
+        {
+            name: "Variant",
+            values: ["small", "medium", "large"],
+            defaultValue: 0,
+        },
+    ],
+};
 
-export const ComboboxExample: FC<ExampleComponentProps> = () => {
+export const ComboboxExample: FC<ExampleComponentProps> = ({ choiceValues }) => {
     const [selectedValue, setSelectedValue] = useState<any>([] || null);
+    const variant = choiceValues && (choiceValues["Variant"] as LabelVariant);
 
     const items = [
         { value: "google pixel", label: "Google Pixel" },
@@ -20,40 +30,40 @@ export const ComboboxExample: FC<ExampleComponentProps> = () => {
     ];
 
     return (
-        <>
-            <Combobox
-                placeholder="Søk"
-                label="Velg leverandører"
-                items={items}
-                value={selectedValue}
-                onChange={(value: any) => {
-                    setSelectedValue(value);
-                    console.log("Change: ", value);
-                }}
-            />
-        </>
+        <Combobox
+            placeholder="Søk"
+            labelProps={{ variant }}
+            label="Velg leverandører"
+            items={items}
+            value={selectedValue}
+            onChange={(value: any) => {
+                setSelectedValue(value);
+                console.log("Change: ", value);
+            }}
+        />
     );
 };
 
 export default ComboboxExample;
 
-export const comboboxExampleCode: CodeExample = () => `
-const items = [
-    { value: "google pixel", label: "Google Pixel" },
-    { value: "apple", label: "Apple" },
-    { value: "samsung", label: "Samsung" },
-    { value: "lg", label: "LG" },
-    { value: "huawei", label: "Huawei" },
-    { value: "oneplus", label: "OnePlus" },
-    { value: "nokia", label: "Nokia" },
-    { value: "sony", label: "Sony" },
-    { value: "doro", label: "Doro" },
-];
+export const comboboxExampleCode = ({ choiceValues }: ExampleComponentProps): string => `
 
 return (
     <Combobox
     placeholder="Søk"
-    items={items}
+    variant="${choiceValues?.["Variant"]}"
+    label="Velg leverandører
+    items={[
+        { value: "google pixel", label: "Google Pixel" },
+        { value: "apple", label: "Apple" },
+        { value: "samsung", label: "Samsung" },
+        { value: "lg", label: "LG" },
+        { value: "huawei", label: "Huawei" },
+        { value: "oneplus", label: "OnePlus" },
+        { value: "nokia", label: "Nokia" },
+        { value: "sony", label: "Sony" },
+        { value: "doro", label: "Doro" },
+    ]}
     value={selectedValue}
     onChange={(value: any) => {
         setSelectedValue(value);
