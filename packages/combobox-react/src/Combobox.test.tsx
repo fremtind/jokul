9d920a20c79b73b09a714d25b7eaf8ce26bf6449
fremtindelise <elise.brenna@fremtind.no>
2023-02-27 13:47:04 +0100

    fix: test

--- packages/combobox-react/src/Combobox.test.tsx
+++ packages/combobox-react/src/Combobox.test.tsx
@@ -1,11 +1,34 @@
 import { render, screen } from "@testing-library/react";
 import { axe } from "jest-axe";
 import React from "react";
-import { Combobox } from ".";
+import { Combobox, type ComboboxValuePair } from ".";
+
+const items: ComboboxValuePair[] = [
+    { value: "google pixel", label: "Google Pixel", tagLabel: "GP" },
+    { value: "apple", label: "Apple" },
+    { value: "samsung", label: "Samsung" },
+    { value: "lg", label: "LG" },
+    { value: "huawei", label: "Huawei" },
+    { value: "oneplus", label: "OnePlus" },
+    { value: "nokia", label: "Nokia" },
+    { value: "sony", label: "Sony" },
+    { value: "doro", label: "Doro" },
+];
 
 describe("Combobox", () => {
     it("should render", () => {
-        render(<Combobox>Edit me!</Combobox>);
+        render(
+            <Combobox
+                label="Test"
+                name="test"
+                onChange={() => {
+                    return;
+                }}
+                items={items}
+            >
+                Edit me!
+            </Combobox>,
+        );
 
         screen.getByText("Edit me!");
     });
@@ -13,7 +36,18 @@ describe("Combobox", () => {
 
 describe("a11y", () => {
     test("Combobox should be a11y compliant", async () => {
-        const { container } = render(<Combobox>I am special</Combobox>);
+        const { container } = render(
+            <Combobox
+                label="Test"
+                name="test"
+                onChange={() => {
+                    return;
+                }}
+                items={items}
+            >
+                I am special
+            </Combobox>,
+        );
         const results = await axe(container);
 
         expect(results).toHaveNoViolations();
