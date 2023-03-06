97e8878903c2708d5bbfc023f051ccc5f34a18ab
William Killerud <william.killerud@fremtind.no>
2023-03-06 10:25:29 +0100

    test: skriv tester som sjekker fravær av menyelementer

--- packages/contextual-menu-react/src/ContextualMenu.test.tsx
+++ packages/contextual-menu-react/src/ContextualMenu.test.tsx
@@ -1,69 +1,95 @@
-import { CopyIcon, DotsIcon, InfoIcon } from "@fremtind/jkl-icons-react";
-import { render, screen } from "@testing-library/react";
+import { DotsIcon } from "@fremtind/jkl-icons-react";
+import { render, RenderOptions } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import React from "react";
 import { ContextualMenu } from "./ContextualMenu";
-import { ContextualMenuDivider } from "./ContextualMenuDivider";
 import { ContextualMenuItem } from "./ContextualMenuItem";
 import { ContextualMenuTriggerButton } from "./ContextualMenuTriggerButton";
 
-// Framer motion is referencing ResizeObserver..
-ResizeObserver = jest.fn().mockImplementation(() => ({
-    observe: jest.fn(),
-    unobserve: jest.fn(),
-    disconnect: jest.fn(),
-}));
+function setup(jsx: JSX.Element, renderOptions?: RenderOptions) {
+    return {
+        user: userEvent.setup(),
+        ...render(jsx, renderOptions),
+    };
+}
 
 describe("ContextualMenu", () => {
+    let defaultResizeObserver = globalThis.ResizeObserver;
+
+    beforeAll(() => {
+        // Framer motion is referencing ResizeObserver..
+        globalThis.ResizeObserver = jest.fn().mockImplementation(() => ({
+            observe: jest.fn(),
+            unobserve: jest.fn(),
+            disconnect: jest.fn(),
+        }));
+    });
+
+    afterAll(() => {
+        globalThis.ResizeObserver = defaultResizeObserver;
+    });
+
     test("should render menu trigger", () => {
-        renderContextualMenu();
-        const contextualMenuTrigger = screen.getByText("En kontekstuell meny");
-        expect(contextualMenuTrigger).toBeInTheDocument();
+        const { getByRole, queryByRole } = setup(
+            <ContextualMenu
+                initialPlacement="bottom-start"
+                triggerElement={
+                    <ContextualMenuTriggerButton description="En kontekstuell meny" icon={<DotsIcon bold />} />
+                }
+            >
+                <ContextualMenuItem>Menyvalg</ContextualMenuItem>
+            </ContextualMenu>,
+        );
+
+        expect(getByRole("button", { name: "En kontekstuell meny" })).toBeInTheDocument();
+        // Start med menyen skjult
+        expect(queryByRole("menuitem")).not.toBeInTheDocument();
     });
 
     test("should open menu options when clicking on trigger element", async () => {
-        renderContextualMenu();
-        const user = userEvent.setup();
-        await user.click(screen.getByText("En kontekstuell meny"));
-        expect(screen.getByText(/Menyvalg 2/)).toBeInTheDocument();
-    });
-
-    test("should open menu expandable options when hover on ContextualMenuItem used as trigger element", async () => {
-        renderContextualMenu();
-        const user = userEvent.setup();
-        await user.click(screen.getByText("En kontekstuell meny"));
-        await user.hover(screen.getByText(/Ekspanderende menyvalg med veldig lang tekst/));
-        const expandedMenuItem = await screen.findByText(/Ekspandert menyvalg med mer tekst/);
-        expect(expandedMenuItem).toBeInTheDocument();
-    });
-});
-
-function renderContextualMenu() {
-    render(
-        <ContextualMenu
-            initialPlacement="bottom-start"
-            triggerElement={<ContextualMenuTriggerButton description="En kontekstuell meny" icon={<DotsIcon bold />} />}
-        >
-            <ContextualMenuItem icon={<InfoIcon />}>Menyvalg 1</ContextualMenuItem>
-            <ContextualMenuItem onClick={() => console.log("Hei fra Menyvalg 2")}>Menyvalg 2</ContextualMenuItem>
-            <ContextualMenuItem disabled>Menyvalg med en lengre beskrivende tekst</ContextualMenuItem>
-            <ContextualMenuDivider />
+        const { getByRole, user } = setup(
             <ContextualMenu
-                openOnHover
+                initialPlacement="bottom-start"
                 triggerElement={
-                    <ContextualMenuItem expandable>Ekspanderende menyvalg med veldig lang tekst</ContextualMenuItem>
+                    <ContextualMenuTriggerButton description="En kontekstuell meny" icon={<DotsIcon bold />} />
                 }
             >
-                <ContextualMenuItem icon={<InfoIcon />}>Ekspandert menyvalg</ContextualMenuItem>
-                <ContextualMenuItem>Ekspandert menyvalg med mer tekst</ContextualMenuItem>
-            </ContextualMenu>
+                <ContextualMenuItem>Menyvalg</ContextualMenuItem>
+            </ContextualMenu>,
+        );
+
+        await user.click(getByRole("button", { name: "En kontekstuell meny" }));
+
+        expect(getByRole("menuitem", { name: "Menyvalg" })).toBeInTheDocument();
+    });
+
+    test("when open on hover is set on submenu, opens on hover", async () => {
+        const { findByRole, getByRole, queryByRole, user } = setup(
             <ContextualMenu
-                openOnHover
-                triggerElement={<ContextualMenuItem expandable>Ekspanderende menyvalg</ContextualMenuItem>}
+                initialPlacement="bottom-start"
+                triggerElement={
+                    <ContextualMenuTriggerButton description="En kontekstuell meny" icon={<DotsIcon bold />} />
+                }
             >
-                <ContextualMenuItem icon={<CopyIcon />}>Ekspandert menyvalg</ContextualMenuItem>
-                <ContextualMenuItem>Ekspandert menyvalg med mer tekst</ContextualMenuItem>
-            </ContextualMenu>
-        </ContextualMenu>,
-    );
-}
+                <ContextualMenu
+                    openOnHover
+                    triggerElement={<ContextualMenuItem expandable>Ekspanderende</ContextualMenuItem>}
+                >
+                    <ContextualMenuItem>Ekspandert</ContextualMenuItem>
+                </ContextualMenu>
+            </ContextualMenu>,
+        );
+
+        expect(queryByRole("menuitem", { name: "Ekspanderende" })).not.toBeInTheDocument();
+
+        await user.click(getByRole("button", { name: "En kontekstuell meny" }));
+
+        expect(getByRole("menuitem", { name: "Ekspanderende" })).toBeInTheDocument();
+        expect(queryByRole("menuitem", { name: "Ekspandert" })).not.toBeInTheDocument();
+
+        await user.hover(getByRole("menuitem", { name: "Ekspanderende" }));
+
+        const ekspandert = await findByRole("menuitem", { name: "Ekspandert" });
+        expect(ekspandert).toBeInTheDocument();
+    });
+});
