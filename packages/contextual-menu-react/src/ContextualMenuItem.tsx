6f2ade88524d518f09d46d2b714d38c6b97dc67c
William Killerud <william.killerud@fremtind.no>
2023-03-06 09:59:08 +0100

    fix: legg til støtte for klassenavn uten å miste Jøkul-klassen

--- packages/contextual-menu-react/src/ContextualMenuItem.tsx
+++ packages/contextual-menu-react/src/ContextualMenuItem.tsx
@@ -1,4 +1,5 @@
 import { ChevronRightIcon } from "@fremtind/jkl-icons-react";
+import cn from "classnames";
 import React, { ButtonHTMLAttributes, forwardRef, ReactNode } from "react";
 
 export interface ContextualMenuItemProps extends ButtonHTMLAttributes<HTMLButtonElement> {
@@ -14,10 +15,10 @@ export interface ContextualMenuItemProps extends ButtonHTMLAttributes<HTMLButton
 }
 
 export const ContextualMenuItem = forwardRef<HTMLButtonElement, ContextualMenuItemProps>((props, ref) => {
-    const { children, icon, expandable = false, ...buttonProps } = props;
+    const { className, children, icon, expandable = false, ...rest } = props;
 
     return (
-        <button ref={ref} type="button" role="menuitem" className="jkl-contextual-menu-item" {...buttonProps}>
+        <button ref={ref} type="button" role="menuitem" className={cn("jkl-contextual-menu-item", className)} {...rest}>
             {icon && <span className="jkl-contextual-menu-item__icon">{icon}</span>}
             <div className="jkl-contextual-menu-item__content">{children}</div>
             {expandable && <ChevronRightIcon className="jkl-contextual-menu-item__arrow" bold />}
