82d51b2ad2c3f3b03275664cf3ca58042d7dd87e
William Killerud <william.killerud@fremtind.no>
2023-03-07 09:52:58 +0100

    test: rett opp URL i Cypress-test

--- packages/file-uploader-react/integration/file-uploader.spec.js
+++ packages/file-uploader-react/integration/file-uploader.spec.js
@@ -3,20 +3,10 @@
 
 describe("FileUploader", () => {
     beforeEach(() => {
-        cy.testComponent("file-uploader");
+        cy.testComponent("fileuploader");
     });
 
     it("renders correctly", () => {
-        cy.takeSnapshots({
-            setup: () => {
-                // Her kan du velge å klikke rundt for å gjøre klart eksempelet for snapshot
-            },
-            teardown: () => {
-                // Her kan du eventuelt resette ting du gjorde i setup, dersom snapshoten for dark mode blir feil.
-                // Om du har brukt f. eks. `cy.setMedFeil()` i setup må du
-                // kalle `cy.resetMedFeil()` her.
-            },
-            // Se for øvrig typedefinisjonen for propertyene `eq` og `variants`.
-        });
+        cy.takeSnapshots();
     });
 });
