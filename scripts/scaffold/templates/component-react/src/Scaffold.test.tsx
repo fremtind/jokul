cb2e2c2aad17c4b2be210e6efb765f4e9fad8cca
William Killerud <william.killerud@fremtind.no>
2023-03-08 13:34:38 +0100

    chore: legg inn vanlig setupfunksjon med userEvents

--- scripts/scaffold/templates/component-react/src/Scaffold.test.tsx
+++ scripts/scaffold/templates/component-react/src/Scaffold.test.tsx
@@ -1,17 +1,25 @@
-import { render } from "@testing-library/react";
+import { render, RenderOptions } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import { axe } from "jest-axe";
 import React from "react";
 import { Scaffold } from ".";
 
+function setup(jsx: JSX.Element, renderOptions?: RenderOptions) {
+    return {
+        user: userEvent.setup(),
+        ...render(jsx, renderOptions),
+    };
+}
+
 describe("Scaffold", () => {
     it("should render", () => {
-        const { getByText } = render(<Scaffold>Edit me!</Scaffold>);
+        const { getByText } = setup(<Scaffold>Edit me!</Scaffold>);
 
         getByText("Edit me!");
     });
 
     it("should pass jest-axe tests in default state", async () => {
-        const { container } = render(<Scaffold>Edit me!</Scaffold>);
+        const { container } = setup(<Scaffold>Edit me!</Scaffold>);
 
         const results = await axe(container);
 
