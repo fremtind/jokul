49b65388a922c957acadaf3f76847d5239ce1882
William Killerud <william.killerud@fremtind.no>
2023-03-21 15:51:08 +0100

    fix: rett opp i duplikat ID
    Nå som vi rendrer alle variantene ender vi opp med flere #a

--- packages/icons-react/src/icons/error/ErrorMedium.tsx
+++ packages/icons-react/src/icons/error/ErrorMedium.tsx
@@ -1,19 +1,23 @@
+import { useId } from "@fremtind/jkl-react-hooks";
 import React, { FC, SVGAttributes } from "react";
 
-export const ErrorMedium: FC<SVGAttributes<SVGElement>> = (props) => (
-    <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" {...props}>
-        <g clipPath="url(#a)">
-            <path
-                fillRule="evenodd"
-                clipRule="evenodd"
-                d="M12 22c5.523 0 10-4.477 10-10S17.523 2 12 2 2 6.477 2 12s4.477 10 10 10zm-1-8V7h2v7h-2zm2.25 2.75a1.25 1.25 0 11-2.5 0 1.25 1.25 0 012.5 0z"
-                fill="currentColor"
-            />
-        </g>
-        <defs>
-            <clipPath id="a">
-                <path fill="#fff" d="M0 0h24v24H0z" />
-            </clipPath>
-        </defs>
-    </svg>
-);
+export const ErrorMedium: FC<SVGAttributes<SVGElement>> = (props) => {
+    const id = useId("em");
+    return (
+        <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" {...props}>
+            <g clipPath={`url(#${id})`}>
+                <path
+                    fillRule="evenodd"
+                    clipRule="evenodd"
+                    d="M12 22c5.523 0 10-4.477 10-10S17.523 2 12 2 2 6.477 2 12s4.477 10 10 10zm-1-8V7h2v7h-2zm2.25 2.75a1.25 1.25 0 11-2.5 0 1.25 1.25 0 012.5 0z"
+                    fill="currentColor"
+                />
+            </g>
+            <defs>
+                <clipPath id={id}>
+                    <path fill="#fff" d="M0 0h24v24H0z" />
+                </clipPath>
+            </defs>
+        </svg>
+    );
+};
--- packages/icons-react/src/icons/error/ErrorSmall.tsx
+++ packages/icons-react/src/icons/error/ErrorSmall.tsx
@@ -1,19 +1,23 @@
+import { useId } from "@fremtind/jkl-react-hooks";
 import React, { FC, SVGAttributes } from "react";
 
-export const ErrorSmall: FC<SVGAttributes<SVGElement>> = (props) => (
-    <svg viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg" {...props}>
-        <g clipPath="url(#a)">
-            <path
-                fillRule="evenodd"
-                clipRule="evenodd"
-                d="M10 18a8 8 0 100-16 8 8 0 000 16zM9.25 6v5h1.5V6h-1.5zm.75 8.5a1 1 0 100-2 1 1 0 000 2z"
-                fill="currentColor"
-            />
-        </g>
-        <defs>
-            <clipPath id="a">
-                <path fill="#fff" d="M0 0h20v20H0z" />
-            </clipPath>
-        </defs>
-    </svg>
-);
+export const ErrorSmall: FC<SVGAttributes<SVGElement>> = (props) => {
+    const id = useId("es");
+    return (
+        <svg viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg" {...props}>
+            <g clipPath={`url(#${id})`}>
+                <path
+                    fillRule="evenodd"
+                    clipRule="evenodd"
+                    d="M10 18a8 8 0 100-16 8 8 0 000 16zM9.25 6v5h1.5V6h-1.5zm.75 8.5a1 1 0 100-2 1 1 0 000 2z"
+                    fill="currentColor"
+                />
+            </g>
+            <defs>
+                <clipPath id={id}>
+                    <path fill="#fff" d="M0 0h20v20H0z" />
+                </clipPath>
+            </defs>
+        </svg>
+    );
+};
