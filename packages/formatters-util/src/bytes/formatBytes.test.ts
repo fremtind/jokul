747e49a24cab8e712b2e117a24c7069fce24338a
William Killerud <william.killerud@fremtind.no>
2023-04-28 11:44:56 +0200

    test: fiks forventet output fra formatBytes

--- packages/formatters-util/src/bytes/formatBytes.test.ts
+++ packages/formatters-util/src/bytes/formatBytes.test.ts
@@ -4,27 +4,27 @@ import { formatBytes } from "./formatBytes";
 const { nbsp } = unicode;
 
 describe("formatBytes", () => {
-    it("correctly formats 8 Kb", () => {
-        expect(formatBytes(8_000)).toEqual(`8${nbsp}Kb`);
+    it("correctly formats 8 KB", () => {
+        expect(formatBytes(8_000)).toEqual(`8${nbsp}KB`);
     });
 
-    it("correctly formats 88 Kb", () => {
-        expect(formatBytes(88_000)).toEqual(`88${nbsp}Kb`);
+    it("correctly formats 88 KB", () => {
+        expect(formatBytes(88_000)).toEqual(`88${nbsp}KB`);
     });
 
-    it("correctly formats 8 Mb", () => {
-        expect(formatBytes(8_000_000)).toEqual(`8${nbsp}Mb`);
+    it("correctly formats 8 MB", () => {
+        expect(formatBytes(8_000_000)).toEqual(`8${nbsp}MB`);
     });
 
-    it("correctly formats 88 Mb", () => {
-        expect(formatBytes(88_000_000)).toEqual(`88${nbsp}Mb`);
+    it("correctly formats 88 MB", () => {
+        expect(formatBytes(88_000_000)).toEqual(`88${nbsp}MB`);
     });
 
-    it("correctly formats 888 Mb", () => {
-        expect(formatBytes(888_000_000)).toEqual(`888${nbsp}Mb`);
+    it("correctly formats 888 MB", () => {
+        expect(formatBytes(888_000_000)).toEqual(`888${nbsp}MB`);
     });
 
-    it("correctly formats 8 888 Mb", () => {
-        expect(formatBytes(8_888_000_000)).toEqual(`8${nbsp}888${nbsp}Mb`);
+    it("correctly formats 8 888 MB", () => {
+        expect(formatBytes(8_888_000_000)).toEqual(`8${nbsp}888${nbsp}MB`);
     });
 });
