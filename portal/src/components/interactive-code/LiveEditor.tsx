b37dae6ce621755e4c3d1a9b074983035b1592f5
William Killerud <william.killerud@fremtind.no>
2023-05-02 13:18:10 +0200

    chore: oppdater portalen

--- portal/src/components/interactive-code/LiveEditor.tsx
+++ portal/src/components/interactive-code/LiveEditor.tsx
@@ -1,6 +1,3 @@
-import { useBrowserPreferences } from "@fremtind/jkl-react-hooks";
-import themeDark from "prism-react-renderer/themes/vsDark";
-import themeLight from "prism-react-renderer/themes/vsLight";
 import React from "react";
 import { LiveEditor as ReactLiveEditor } from "react-live";
 
@@ -9,11 +6,8 @@ type LiveEditorProps = {
 };
 
 export const LiveEditor = ({ onChange, ...props }: LiveEditorProps) => {
-    const { prefersColorScheme } = useBrowserPreferences();
-
     return (
         <ReactLiveEditor
-            theme={prefersColorScheme === "light" ? themeLight : themeDark}
             style={{ overflow: "scroll", height: "100%", maxHeight: "400px" }}
             {...(onChange ? { onChange } : {})}
         />
