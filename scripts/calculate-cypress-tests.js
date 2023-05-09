4402a321bd18b3b1ff2432224cc20540726268fb
William Killerud <william.killerud@fremtind.no>
2023-05-09 08:47:28 +0200

    chore: legg til logging

--- scripts/calculate-cypress-tests.js
+++ scripts/calculate-cypress-tests.js
@@ -1,19 +1,25 @@
 const { execSync } = require("child_process");
 
 // Bruker lerna changed for å få testet dependencytreet ved endringer
-const cypressTests = execSync("pnpm --silent ci:changed")
-    .toString()
-    .split("\n")
-    .filter((row) => {
-        if (row === "") {
-            return false;
-        }
-        // Bare pakker med -react postfiks har visuelle regresjonstester.
-        if (!row.includes("-react")) {
-            return false;
-        }
-        return true;
-    })
-    .map((row) => `${row}/**/*.spec.js`);
+try {
+    const cypressTests = execSync("pnpm --silent ci:changed")
+        .toString()
+        .split("\n")
+        .filter((row) => {
+            if (row === "") {
+                return false;
+            }
+            // Bare pakker med -react postfiks har visuelle regresjonstester.
+            if (!row.includes("-react")) {
+                return false;
+            }
+            return true;
+        })
+        .map((row) => `${row}/**/*.spec.js`);
 
-console.log(cypressTests.join("\n"));
+    console.log(cypressTests.join("\n"));
+} catch (error) {
+    console.error(JSON.stringify(error));
+    console.error(error.message);
+    process.exit(1);
+}
