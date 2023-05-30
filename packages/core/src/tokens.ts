603b24460d925c28778e284d1842af0eb8545678
William Killerud <william.killerud@fremtind.no>
2023-05-30 12:30:37 +0200

    chore: commit tokens-filer
    CI er kranglete

---
+++ packages/core/src/tokens.ts
@@ -0,0 +1,180 @@
+/**
+ * Do not edit directly
+ * Generated on Tue, 30 May 2023 10:30:31 GMT
+ */
+
+export default {
+    color: {
+        svart: "#000",
+        granitt: "#1b1917",
+        skifer: "#313030",
+        fjellgra: "#444141",
+        stein: "#636060",
+        svaberg: "#c8c5c3",
+        varde: "#e0dbd4",
+        dis: "#ece9e5",
+        sand: "#f4f2ef",
+        snohvit: "#f9f9f9",
+        hvit: "#fff",
+        suksess: "#c8ecd2",
+        feil: "#ffc9b2",
+        info: "#d9ddff",
+        advarsel: "#fdeab9",
+    },
+    spacing: {
+        0: "0",
+        2: "0.125rem",
+        4: "0.25rem",
+        8: "0.5rem",
+        12: "0.75rem",
+        16: "1rem",
+        24: "1.5rem",
+        32: "2rem",
+        40: "2.5rem",
+        64: "4rem",
+        104: "6.5rem",
+        168: "10.5rem",
+    },
+    typography: {
+        weight: {
+            normal: "400",
+            bold: "700",
+        },
+        font: {
+            size: {
+                16: "1rem",
+                18: "1.125rem",
+                20: "1.25rem",
+                21: "1.3125rem",
+                23: "1.4375rem",
+                25: "1.5625rem",
+                26: "1.625rem",
+                28: "1.75rem",
+                30: "1.875rem",
+                36: "2.25rem",
+                44: "2.75rem",
+                56: "3.5rem",
+            },
+        },
+        line: {
+            height: {
+                24: "1.5rem",
+                28: "1.75rem",
+                32: "2rem",
+                36: "2.25rem",
+                40: "2.5rem",
+                44: "2.75rem",
+                52: "3.25rem",
+                64: "4rem",
+            },
+        },
+        title: {
+            small: {
+                fontSize: "2.25rem",
+                lineHeight: "2.75rem",
+                fontWeight: "400",
+            },
+            base: {
+                fontSize: "3.5rem",
+                lineHeight: "4rem",
+                fontWeight: "400",
+            },
+        },
+        titleSmall: {
+            small: {
+                fontSize: "1.875rem",
+                lineHeight: "2.25rem",
+                fontWeight: "400",
+            },
+            base: {
+                fontSize: "2.75rem",
+                lineHeight: "3.25rem",
+                fontWeight: "400",
+            },
+        },
+        heading_1: {
+            small: {
+                fontSize: "1.625rem",
+                lineHeight: "2rem",
+                fontWeight: "400",
+            },
+            base: {
+                fontSize: "2.25rem",
+                lineHeight: "2.75rem",
+                fontWeight: "400",
+            },
+        },
+        heading_2: {
+            small: {
+                fontSize: "1.4375rem",
+                lineHeight: "2rem",
+                fontWeight: "400",
+            },
+            base: {
+                fontSize: "1.75rem",
+                lineHeight: "2.5rem",
+                fontWeight: "400",
+            },
+        },
+        heading_3: {
+            small: {
+                fontSize: "1.3125rem",
+                lineHeight: "1.75rem",
+                fontWeight: "700",
+            },
+            base: {
+                fontSize: "1.5625rem",
+                lineHeight: "2rem",
+                fontWeight: "700",
+            },
+        },
+        heading_4: {
+            small: {
+                fontSize: "1.125rem",
+                lineHeight: "1.5rem",
+                fontWeight: "700",
+            },
+            base: {
+                fontSize: "1.3125rem",
+                lineHeight: "1.75rem",
+                fontWeight: "700",
+            },
+        },
+        heading_5: {
+            small: {
+                fontSize: "1rem",
+                lineHeight: "1.5rem",
+                fontWeight: "700",
+            },
+            base: {
+                fontSize: "1rem",
+                lineHeight: "1.5rem",
+                fontWeight: "700",
+            },
+        },
+        body: {
+            small: {
+                fontSize: "1.125rem",
+                lineHeight: "1.75rem",
+                fontWeight: "400",
+            },
+            base: {
+                fontSize: "1.25rem",
+                lineHeight: "2rem",
+                fontWeight: "400",
+            },
+        },
+        small: {
+            small: {
+                fontSize: "1rem",
+                lineHeight: "1.5rem",
+                fontWeight: "400",
+            },
+            base: {
+                fontSize: "1rem",
+                lineHeight: "1.5rem",
+                fontWeight: "400",
+            },
+        },
+    },
+};
