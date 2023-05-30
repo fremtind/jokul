61fe367d3f0939b56742fcd6bb0c0e2a22142e67
Pio Nizar Finn Rasch-Halvorsen <pio.rasch-halvorsen@fremtind.no>
2023-05-30 14:29:38 +0200

    test: oppdater tester for ny Tooltip-komponent

--- packages/tooltip-react/src/Tooltip.test.tsx
+++ packages/tooltip-react/src/Tooltip.test.tsx
@@ -1,46 +1,120 @@
-import { fireEvent, render, screen } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import React from "react";
 import { Tooltip } from "./Tooltip";
+import { TooltipContent } from "./TooltipContent";
+import { TooltipTrigger } from "./TooltipTrigger";
+
+// Polyfill for ResizeObserver, siden JSDom ikke støtter det
+global.ResizeObserver = require("resize-observer-polyfill");
 
 describe("Tooltip", () => {
-    test("should render button for showing tooltip", () => {
-        render(<Tooltip content={<p className="jkl-small">Dette er en tooltip</p>} />);
+    it("should show tooltip when hovering over trigger", async () => {
+        render(
+            <Tooltip>
+                <TooltipTrigger>Tekst som skal forklares</TooltipTrigger>
+                <TooltipContent>Forklarende tekst</TooltipContent>
+            </Tooltip>,
+        );
 
-        const tooltipButton = screen.getByRole("button", { name: /Vis hjelpetekst/ });
-        expect(tooltipButton).toBeInTheDocument();
-    });
-
-    test("should not render tooltip text when tooltip is not activated", () => {
-        render(<Tooltip content={<p className="jkl-small">Dette er en tooltip</p>} />);
-
-        const tooltipText = screen.queryByText(/Dette er en tooltip/);
-        expect(tooltipText).not.toBeInTheDocument();
-    });
-
-    test("should not render tooltip text when tooltip is HOVERED", async () => {
-        render(<Tooltip content={<p className="jkl-small">Dette er en tooltip</p>} />);
         const user = userEvent.setup();
+        const tooltipTrigger = screen.queryByText(/Tekst som skal forklares/);
+        expect(screen.queryByText(/Forklarende tekst/)).not.toBeInTheDocument();
 
-        const tooltipButton = screen.getByRole("button", { name: /Vis hjelpetekst/ });
-        await user.hover(tooltipButton);
-        expect(screen.queryByText(/Dette er en tooltip/)).not.toBeInTheDocument();
+        await user.hover(tooltipTrigger as HTMLElement);
+        expect(screen.queryByText(/Forklarende tekst/)).toBeInTheDocument();
     });
 
-    test("should not render tooltip text when tooltip is FOCUSED", () => {
-        render(<Tooltip content={<p className="jkl-small">Dette er en tooltip</p>} />);
+    it("should NOT show tooltip on hover when triggerOn is 'click'", async () => {
+        render(
+            <Tooltip triggerOn="click">
+                <TooltipTrigger>Tekst som skal forklares</TooltipTrigger>
+                <TooltipContent>Forklarende tekst</TooltipContent>
+            </Tooltip>,
+        );
 
-        const tooltipButton = screen.getByRole("button", { name: /Vis hjelpetekst/ });
-        fireEvent.focus(tooltipButton);
-        expect(screen.queryByText(/Dette er en tooltip/)).not.toBeInTheDocument();
-    });
-
-    test("should render tooltip text when tooltip is CLICKED", async () => {
-        render(<Tooltip content={<p className="jkl-small">Dette er en tooltip</p>} />);
         const user = userEvent.setup();
+        const tooltipTrigger = screen.queryByText(/Tekst som skal forklares/);
+        expect(screen.queryByText(/Forklarende tekst/)).not.toBeInTheDocument();
 
-        const tooltipButton = screen.getByRole("button", { name: /Vis hjelpetekst/ });
-        await user.click(tooltipButton);
-        expect(screen.getByText(/Dette er en tooltip/)).toBeInTheDocument();
+        await user.hover(tooltipTrigger as HTMLElement);
+        expect(screen.queryByText(/Forklarende tekst/)).not.toBeInTheDocument();
+    });
+
+    it("should show tooltip on click when triggerOn is 'click'", async () => {
+        render(
+            <Tooltip triggerOn="click">
+                <TooltipTrigger>Tekst som skal forklares</TooltipTrigger>
+                <TooltipContent>Forklarende tekst</TooltipContent>
+            </Tooltip>,
+        );
+
+        const user = userEvent.setup();
+        const tooltipTrigger = screen.queryByText(/Tekst som skal forklares/);
+        expect(screen.queryByText(/Forklarende tekst/)).not.toBeInTheDocument();
+
+        await user.click(tooltipTrigger as HTMLElement);
+        expect(screen.queryByText(/Forklarende tekst/)).toBeInTheDocument();
+    });
+
+    describe("Trigger", () => {
+        it("should render the provided content", () => {
+            render(
+                <Tooltip>
+                    <TooltipTrigger>Tekst som skal forklares</TooltipTrigger>
+                </Tooltip>,
+            );
+
+            const tooltipTrigger = screen.queryByText(/Tekst som skal forklares/);
+            expect(tooltipTrigger).toBeInTheDocument();
+        });
+
+        it("should render as a span element by default", () => {
+            render(
+                <Tooltip>
+                    <TooltipTrigger>Tekst som skal forklares</TooltipTrigger>
+                </Tooltip>,
+            );
+
+            const tooltipTrigger = screen.queryByText(/Tekst som skal forklares/);
+            expect(tooltipTrigger?.tagName).toBe("SPAN");
+        });
+
+        it("should render as a button element when onTrigger is 'click'", () => {
+            render(
+                <Tooltip triggerOn="click">
+                    <TooltipTrigger>Tekst som skal forklares</TooltipTrigger>
+                </Tooltip>,
+            );
+
+            const tooltipTrigger = screen.queryByText(/Tekst som skal forklares/);
+            expect(tooltipTrigger?.tagName).toBe("BUTTON");
+        });
+
+        it("should render as the provided content element", () => {
+            render(
+                <Tooltip triggerOn="click">
+                    <TooltipTrigger>
+                        <p>Tekst som skal forklares</p>
+                    </TooltipTrigger>
+                </Tooltip>,
+            );
+
+            const tooltipTrigger = screen.queryByText(/Tekst som skal forklares/);
+            expect(tooltipTrigger?.tagName).toBe("P");
+        });
+    });
+
+    describe("Content", () => {
+        it("should render the provided content", () => {
+            render(
+                <Tooltip initialOpen>
+                    <TooltipContent>Forklarende tekst</TooltipContent>
+                </Tooltip>,
+            );
+
+            const tooltipTrigger = screen.queryByText(/Forklarende tekst/);
+            expect(tooltipTrigger).toBeInTheDocument();
+        });
     });
 });
