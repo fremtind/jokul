96ea868142d140806d4e6ecc4e285b683ff160a7
William Killerud <william.killerud@fremtind.no>
2023-06-29 11:48:41 +0200

    docs: dokumenter med jsdoc så de dukker opp i API-seksjonen
    Det kan bli litt smør på flesk, men må til for at React Docgen skal plukke de opp.

--- packages/validators-util/src/hasNoIllegalCharacters/hasNoIllegalCharacters.ts
+++ packages/validators-util/src/hasNoIllegalCharacters/hasNoIllegalCharacters.ts
@@ -1,4 +1,9 @@
 const ILLEGAL_CHARS_REGEX = /[[\]{}%¤/()$@£€^~*?_;:"]+/;
 export const ILLEGAL_CHARS_STRING = '[]{}%¤/()$@£€^~*?_;:"';
 
+/**
+ * Sjekker at verdien ikke har spesialtegn som ikke er tillatt.
+ * @param value Verdien som skal valideres
+ * @returns Om verdien er fri for spesialtegn som ikke er tillatt
+ */
 export const hasNoIllegalCharacters = (value: string): boolean => !ILLEGAL_CHARS_REGEX.test(value);
--- packages/validators-util/src/isValidRegistreringsnummer/isValidRegistreringsnummer.ts
+++ packages/validators-util/src/isValidRegistreringsnummer/isValidRegistreringsnummer.ts
@@ -4,6 +4,13 @@ export const MC_MOPED_REGNR_REGEX = /^[a-zA-Z]{2}\s?\d{4}$/;
 export interface IsValidRegistreringsnummerOptions {
     vehicle?: "bil" | "mc" | "moped";
 }
+
+/**
+ * Validerer om en kandidat er et gyldig registreringsnummer, bestående av to bokstaver, et valgfritt mellomrom, og fem siffer for bil, fire siffer for MC/moped.
+ * @param value Kandidat for registreringsnummer
+ * @param options Type kjøretøy, default "bil". Kan også være "mc" eller "moped".
+ * @returns Om kandidaten er et gyldig registreringsnummer
+ */
 export const isValidRegistreringsnummer = (
     value: string,
     options: IsValidRegistreringsnummerOptions = { vehicle: "bil" },
