b8985319b59a8b3f1f36972a1aa4cfebf0a0bd61
Ivar Nilsen <ivar.nilsen@fremtind.no>
2023-12-03 15:29:38 +0100

    feat: accordion uses details/summary elements
    In order to work better with server side rendered applications where
    the first render will happen before the JS has mounted, Accordion now
    uses the summary/details elements.
    
    ISSUES CLOSED: #3767

--- packages/accordion-react/documentation/AccordionExample.tsx
+++ packages/accordion-react/documentation/AccordionExample.tsx
@@ -1,61 +1,97 @@
 import { UnorderedList, ListItem } from "@fremtind/jkl-list-react";
-import React from "react";
+import React, { useId } from "react";
 import { CodeExample, ExampleComponentProps, ExampleKnobsProps } from "../../../doc-utils";
 import { Accordion, AccordionItem } from "../src";
 
 export const accordionExampleKnobs: ExampleKnobsProps = {};
 
-export const AccordionExample: React.FC<ExampleComponentProps> = () => (
-    <Accordion>
+export const AccordionExample: React.FC<ExampleComponentProps> = () => {
+    const id = useId();
+    return (
+        <>
+            <h2
+                className="jkl-portal-heading-medium"
+                id={id}
+                style={{ textAlign: "center", marginBottom: 24, width: "100%" }}
+            >
+                Noen spørsmål
+            </h2>
+            <Accordion aria-labelledby={id}>
+                <AccordionItem
+                    title="Hvem kan kjøpe livsforsikring?"
+                    onClick={(e, isOpen) => {
+                        isOpen
+                            ? console.log("Opening first accordion item")
+                            : console.log("Closing first accordion item");
+                    }}
+                >
+                    <UnorderedList>
+                        <ListItem>Personer med et norsk fødselsnummer.</ListItem>
+                        <ListItem>
+                            Personer som har vært bosatt i Norge i minst de fem siste årene før avtalen inngås.
+                        </ListItem>
+                        <ListItem>Livsforsikring kan kjøpes på nett av personer mellom 18 og 79 år.</ListItem>
+                    </UnorderedList>
+                    Ønsker du en forsikringssum på mer enn 3 millioner trenger vi ekstra økonomiske opplysninger. Da må
+                    du ta kontakt med banken din.
+                </AccordionItem>
+                <AccordionItem title="Hvem kan kjøpe kritisk sykdom?">
+                    Personer med et norsk fødselsnummer. Personer som har vært bosatt i Norge i minst de fem siste årene
+                    før avtalen inngås. Kritisk sykdom kan kjøpes på nett av personer mellom 18 og 58 år. Ønsker du en
+                    forsikringssum på mer enn 1,1 millioner trenger vi ekstra økonomiske opplysninger. Da må du ta
+                    kontakt med banken din.
+                </AccordionItem>
+                <AccordionItem title="Hvem kan kjøpe uførepensjon?">
+                    Personer med et norsk fødselsnummer. Personer som har vært bosatt i Norge i minst de fem siste årene
+                    før avtalen inngås. Uførepensjon kan kjøpes på nett av personer mellom 18 og 60 år. Ønsker du en
+                    forsikringssum på mer enn 96 000 trenger vi ekstra økonomiske opplysninger. Da må du ta kontakt med
+                    banken din.
+                </AccordionItem>
+            </Accordion>
+        </>
+    );
+};
+
+export const accordionExampleCode: CodeExample = () => `
+<>
+    <h2
+        className="jkl-portal-heading-medium"
+        id={id}
+        style={{ textAlign: "center", marginBottom: 24, width: "100%" }}
+    >
+        Noen spørsmål
+    </h2>
+    <Accordion aria-labelledby={id}>
         <AccordionItem
             title="Hvem kan kjøpe livsforsikring?"
             onClick={(e, isOpen) => {
-                isOpen ? console.log("Opening first accordion item") : console.log("Closing first accordion item");
+                isOpen
+                    ? console.log("Opening first accordion item")
+                    : console.log("Closing first accordion item");
             }}
         >
             <UnorderedList>
                 <ListItem>Personer med et norsk fødselsnummer.</ListItem>
-                <ListItem>Personer som har vært bosatt i Norge i minst de fem siste årene før avtalen inngås.</ListItem>
+                <ListItem>
+                    Personer som har vært bosatt i Norge i minst de fem siste årene før avtalen inngås.
+                </ListItem>
                 <ListItem>Livsforsikring kan kjøpes på nett av personer mellom 18 og 79 år.</ListItem>
             </UnorderedList>
-            Ønsker du en forsikringssum på mer enn 3 millioner trenger vi ekstra økonomiske opplysninger. Da må du ta
-            kontakt med banken din.
+            Ønsker du en forsikringssum på mer enn 3 millioner trenger vi ekstra økonomiske opplysninger. Da må
+            du ta kontakt med banken din.
         </AccordionItem>
         <AccordionItem title="Hvem kan kjøpe kritisk sykdom?">
-            Personer med et norsk fødselsnummer. Personer som har vært bosatt i Norge i minst de fem siste årene før
-            avtalen inngås. Kritisk sykdom kan kjøpes på nett av personer mellom 18 og 58 år. Ønsker du en
-            forsikringssum på mer enn 1,1 millioner trenger vi ekstra økonomiske opplysninger. Da må du ta kontakt med
-            banken din.
+            Personer med et norsk fødselsnummer. Personer som har vært bosatt i Norge i minst de fem siste årene
+            før avtalen inngås. Kritisk sykdom kan kjøpes på nett av personer mellom 18 og 58 år. Ønsker du en
+            forsikringssum på mer enn 1,1 millioner trenger vi ekstra økonomiske opplysninger. Da må du ta
+            kontakt med banken din.
         </AccordionItem>
         <AccordionItem title="Hvem kan kjøpe uførepensjon?">
-            Personer med et norsk fødselsnummer. Personer som har vært bosatt i Norge i minst de fem siste årene før
-            avtalen inngås. Uførepensjon kan kjøpes på nett av personer mellom 18 og 60 år. Ønsker du en forsikringssum
-            på mer enn 96 000 trenger vi ekstra økonomiske opplysninger. Da må du ta kontakt med banken din.
+            Personer med et norsk fødselsnummer. Personer som har vært bosatt i Norge i minst de fem siste årene
+            før avtalen inngås. Uførepensjon kan kjøpes på nett av personer mellom 18 og 60 år. Ønsker du en
+            forsikringssum på mer enn 96 000 trenger vi ekstra økonomiske opplysninger. Da må du ta kontakt med
+            banken din.
         </AccordionItem>
     </Accordion>
-);
-
-export const accordionExampleCode: CodeExample = () => `
-<Accordion>
-    <AccordionItem title="Hvem kan kjøpe livsforsikring?">
-        <UnorderedList>
-            <ListItem>Personer med et norsk fødselsnummer.</ListItem>
-            <ListItem>Personer som har vært bosatt i Norge i minst de fem siste årene før avtalen inngås.</ListItem>
-            <ListItem>Livsforsikring kan kjøpes på nett av personer mellom 18 og 79 år.</ListItem>
-        </UnorderedList>
-        Ønsker du en forsikringssum på mer enn 3 millioner trenger vi ekstra økonomiske opplysninger. Da må du ta
-        kontakt med banken din.
-    </AccordionItem>
-    <AccordionItem title="Hvem kan kjøpe kritisk sykdom?">
-        Personer med et norsk fødselsnummer. Personer som har vært bosatt i Norge i minst de fem siste årene før
-        avtalen inngås. Kritisk sykdom kan kjøpes på nett av personer mellom 18 og 58 år. Ønsker du en
-        forsikringssum på mer enn 1,1 millioner trenger vi ekstra økonomiske opplysninger. Da må du ta kontakt med
-        banken din.
-    </AccordionItem>
-    <AccordionItem title="Hvem kan kjøpe uførepensjon?">
-        Personer med et norsk fødselsnummer. Personer som har vært bosatt i Norge i minst de fem siste årene før
-        avtalen inngås. Uførepensjon kan kjøpes på nett av personer mellom 18 og 60 år. Ønsker du en forsikringssum
-        på mer enn 96 000 trenger vi ekstra økonomiske opplysninger. Da må du ta kontakt med banken din.
-    </AccordionItem>
-</Accordion>
+</>
 `;
