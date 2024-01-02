8fcf14740b4b94a8cd64caa9411dc1cb0a99f8a2
Ivar Nilsen <ivar.nilsen@fremtind.no>
2024-01-02 07:04:50 +0100

    fix: pressing Enter key after opening searchable select does not submit form
    ISSUES CLOSED: #3783

--- packages/select-react/src/Select.tsx
+++ packages/select-react/src/Select.tsx
@@ -362,9 +362,17 @@ export const Select = forwardRef<HTMLSelectElement, SelectProps>((props, forward
                     e.stopPropagation();
                     focusSelected(listElement, selectedValue);
                 }
+            } else if (e.key === "Enter" && dropdownIsShown) {
+                // This component already deviates from how a native select would react
+                // to Enter keypresses (a native <select> would submit the form, not open itself)
+                // and  to create some symmetry with how Enter works make sure a second event
+                // closes the dropdown instead of suddenly triggering a submit.
+                e.preventDefault();
+                e.stopPropagation();
+                setShown(false);
             }
         },
-        [setShown, dropdownRef, selectedValue, isSearchable],
+        [setShown, dropdownRef, selectedValue, isSearchable, dropdownIsShown],
     );
 
     // onKeyDown so this Tab listener isn't triggered by tabbing from search field to option
