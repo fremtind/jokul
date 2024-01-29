5973df556acae73b407e2e039bae69422d623975
Ivar Nilsen <ivar.nilsen@fremtind.no>
2024-01-29 11:32:23 +0100

    feat: toggleSwitch now supports the disabled prop
    ISSUES CLOSED: #3803

--- packages/toggle-switch-react/documentation/ToggleSwitchExample.tsx
+++ packages/toggle-switch-react/documentation/ToggleSwitchExample.tsx
@@ -4,14 +4,20 @@ import { PrimaryButton } from "../../button-react/src";
 import { type ToggleChangeHandler, ToggleSwitch } from "../src";
 
 export const toggleSwitchExampleKnobs: ExampleKnobsProps = {
-    boolProps: ["Deaktivert", "Med hjelpetekst"],
+    boolProps: ["Deaktivert"],
 };
 
-export const ToggleSwitchExample: React.FC<ExampleComponentProps> = () => {
+export const ToggleSwitchExample: React.FC<ExampleComponentProps> = ({ boolValues }) => {
+    const isDisabled = boolValues?.["Deaktivert"];
+
     const handleToggle: ToggleChangeHandler<HTMLButtonElement> = (event, pressed) =>
         console.log("Mørk modus satt til: ", pressed, event);
 
-    return <ToggleSwitch onChange={handleToggle}>Mørk modus</ToggleSwitch>;
+    return (
+        <ToggleSwitch disabled={isDisabled} onChange={handleToggle}>
+            Mørk modus
+        </ToggleSwitch>
+    );
 };
 
 export const toggleSwitchCodeExample: CodeExample = () => `
