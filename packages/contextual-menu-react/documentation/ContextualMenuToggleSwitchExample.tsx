c8218b0aab927d4d774f282a1530727d4641a56d
Pio Rasch-Halvorsen <pio.rasch-halvorsen@fremtind.no>
2024-06-09 20:56:18 +0200

    docs: vis eksempel på ikonknapp med Tooltip

--- packages/contextual-menu-react/documentation/ContextualMenuToggleSwitchExample.tsx
+++ packages/contextual-menu-react/documentation/ContextualMenuToggleSwitchExample.tsx
@@ -2,8 +2,9 @@ import { type ColorScheme } from "@fremtind/jkl-core";
 import { IconButton } from "@fremtind/jkl-icon-button-react";
 import { DotsIcon } from "@fremtind/jkl-icons-react";
 import { useBrowserPreferences } from "@fremtind/jkl-react-hooks";
-import React, { type FC, useCallback, useState, useRef } from "react";
-import { ExampleComponentProps, ExampleKnobsProps, CodeExample } from "../../../doc-utils";
+import React, { useCallback, useRef, useState, type FC } from "react";
+import { CodeExample, ExampleComponentProps, ExampleKnobsProps } from "../../../doc-utils";
+import { Tooltip, TooltipContent, TooltipTrigger } from "../../tooltip-react";
 import { ContextualMenu, ContextualMenuItemCheckbox } from "../src";
 
 export const contextualMenuToggleSwitchExampleKnobs: ExampleKnobsProps = {
@@ -51,29 +52,37 @@ export const ContextualMenuToggleSwitchExample: FC<ExampleComponentProps> = ({ c
             className="jkl"
             data-theme={colorScheme}
             style={{
-                height: "5rem",
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "center",
+                height: "100%",
                 width: "100%",
                 padding: "1rem",
             }}
         >
-            <ContextualMenu
-                initialPlacement="bottom-start"
-                isOpen={isOpen}
-                triggerElement={
-                    <IconButton className="jkl-portal-navigation__contextual-menu-trigger" title="Åpne innstillinger">
-                        <DotsIcon variant="medium" bold />
-                    </IconButton>
-                }
-            >
-                <ContextualMenuItemCheckbox
-                    aria-checked={colorScheme === "dark"}
-                    onChange={(_, pressed) => {
-                        handleSetColorScheme(pressed ? "dark" : "light");
-                    }}
+            <Tooltip>
+                <ContextualMenu
+                    initialPlacement="bottom-start"
+                    isOpen={isOpen}
+                    triggerElement={
+                        <TooltipTrigger>
+                            <IconButton className="jkl-portal-navigation__contextual-menu-trigger">
+                                <DotsIcon variant="medium" bold />
+                            </IconButton>
+                        </TooltipTrigger>
+                    }
                 >
-                    Mørkt tema
-                </ContextualMenuItemCheckbox>
-            </ContextualMenu>
+                    <ContextualMenuItemCheckbox
+                        aria-checked={colorScheme === "dark"}
+                        onChange={(_, pressed) => {
+                            handleSetColorScheme(pressed ? "dark" : "light");
+                        }}
+                    >
+                        Mørkt tema
+                    </ContextualMenuItemCheckbox>
+                </ContextualMenu>
+                <TooltipContent>Innstillinger</TooltipContent>
+            </Tooltip>
         </div>
     );
 };
@@ -81,51 +90,30 @@ export const ContextualMenuToggleSwitchExample: FC<ExampleComponentProps> = ({ c
 export default ContextualMenuToggleSwitchExample;
 
 export const contextualMenuToggleSwitchExampleCode: CodeExample = () => `
-const pref = useBrowserPreferences();
-const [colorScheme, setColorScheme] = useState(pref.prefersColorScheme);
-
-const bodyRef = useRef<HTMLDivElement>(null);
-const handleSetColorScheme = useCallback(
-    (scheme: ColorScheme) => {
-        if (!bodyRef.current) {
-            return;
-        }
-        setColorScheme(scheme);
-        // @ts-ignore Ikke stabilt i alle nettlesere ennå: https://caniuse.com/view-transitions
-        if (!document.startViewTransition) {
-            bodyRef.current.dataset.theme = scheme;
-            return;
-        }
-        // @ts-ignore Ikke stabilt i alle nettlesere ennå: https://caniuse.com/view-transitions
-        document.startViewTransition(() => {
-            if (!bodyRef.current) {
-                return;
-            }
-            bodyRef.current.dataset.theme = scheme;
-        });
-    },
-    [setColorScheme],
-);
-
-return (
-    <div ref={bodyRef} className="jkl" data-theme={colorScheme}>
-        <ContextualMenu
-            initialPlacement="bottom-start"
-            triggerElement={
-                <IconButton className="jkl-portal-navigation__contextual-menu-trigger" title="Åpne innstillinger">
+// Vi bruker et Tooltip siden knappen ikke har tekst. For å sørge for
+// at knappen fortsatt fungerer som trigger for menyen må vi legge Tooltip
+// rundt hele menyen, og kun sende inn TooltipTrigger sammen med knappen
+// som trigger for menyen.
+<Tooltip>
+    <ContextualMenu
+        initialPlacement="bottom-start"
+        triggerElement={
+            <TooltipTrigger>
+                <IconButton className="jkl-portal-navigation__contextual-menu-trigger">
                     <DotsIcon variant="medium" bold />
                 </IconButton>
-            }
+            </TooltipTrigger>
+        }
+    >
+        <ContextualMenuItemCheckbox
+            aria-checked={colorScheme === "dark"}
+            onChange={(_, pressed) => {
+                setColorScheme(pressed ? "dark" : "light");
+            }}
         >
-            <ContextualMenuItemCheckbox
-                aria-checked={colorScheme === "dark"}
-                onChange={(_, pressed) => {
-                    handleSetColorScheme(pressed ? "dark" : "light");
-                }}
-            >
-                Mørkt tema
-            </ContextualMenuItemCheckbox>
-        </ContextualMenu>
-    </div>
-);
+            Mørkt tema
+        </ContextualMenuItemCheckbox>
+    </ContextualMenu>
+    <TooltipContent>Innstillinger</TooltipContent>
+</Tooltip>
 `;
