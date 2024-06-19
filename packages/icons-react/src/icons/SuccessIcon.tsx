e4ffac8e2c7636033b078a8b3b0c663b474cfbc9
Pio Rasch-Halvorsen <pio.rasch-halvorsen@fremtind.no>
2024-06-19 09:29:35 +0200

    fix: riktig klasse på ikoner og bold versjon av varselsikonene

--- packages/icons-react/src/icons/SuccessIcon.tsx
+++ packages/icons-react/src/icons/SuccessIcon.tsx
@@ -3,7 +3,7 @@ import { Icon } from "../Icon";
 import { type IconProps } from "../types";
 
 export const SuccessIcon = (props: IconProps) => (
-    <Icon {...props} filled>
+    <Icon {...props} bold filled>
         check_circle
     </Icon>
 );
