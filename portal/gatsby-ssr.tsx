02315033a9140af3e4fb0d0a4ce3d2df2ca05b4c
Pio Rasch-Halvorsen <pio.rasch-halvorsen@fremtind.no>
2024-06-19 14:04:10 +0200

    fix: preload ikonfont i portalen

--- portal/gatsby-ssr.tsx
+++ portal/gatsby-ssr.tsx
@@ -1,9 +1,12 @@
 import { GatsbySSR } from "gatsby";
 import React from "react";
 import { SharedProviders } from "./gatsby-shared-providers";
+// @ts-ignore joda, fontfilen finnes og kan importeres
+import font from "./static/fonts/Fremtind_Material_Symbols.woff2";
 
-export const onRenderBody: GatsbySSR["onRenderBody"] = ({ setHtmlAttributes }) => {
+export const onRenderBody: GatsbySSR["onRenderBody"] = ({ setHtmlAttributes, setHeadComponents }) => {
     setHtmlAttributes({ lang: "no" });
+    setHeadComponents([<link key="iconfont" rel="preload" href={font} as="font" type="font/woff2" crossOrigin="" />]);
 };
 
 export const wrapRootElement: GatsbySSR["wrapRootElement"] = ({ element }) => {
