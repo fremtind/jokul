74ef8c967da5df8b5165be00f92eaf1a032ae86b
Pio Rasch-Halvorsen <pio.rasch-halvorsen@fremtind.no>
2024-07-02 10:11:58 +0200

    feat: legg til mulighet for å sette headers på uploads

--- packages/file-input-react/src/utils.ts
+++ packages/file-input-react/src/utils.ts
@@ -4,9 +4,16 @@
  * @param {string} url Endepunktet som skal lastes opp til
  * @param {FormData} data FormData med filen som skal lastes opp
  * @param progress Callback som blir kalt med oppdatert progresjon
+ * @param {Record<string, string>} headers Eventuelle headers som skal sendes
+ * med opplasingen. Her kan du f.eks. sende med CSRF-verdier.
  * @returns {T} Svaret fra endepunktet
  */
-export async function upload<T>(url: string, data: FormData, progress: (progress: number) => void): Promise<T> {
+export async function upload<T>(
+    url: string,
+    data: FormData,
+    progress: (progress: number) => void,
+    headers?: Record<string, string>,
+): Promise<T> {
     // I skrivende stund er det ikke mulig å hente progress med fetch. Derfor bruker vi XMLHttpRequest.
     const xhr = new XMLHttpRequest();
     const request = new Promise<ProgressEvent<XMLHttpRequestEventTarget>>((resolve, reject) => {
@@ -32,6 +39,11 @@ export async function upload<T>(url: string, data: FormData, progress: (progress
         });
 
         xhr.open("POST", url);
+
+        Object.entries(headers || {}).forEach(([header, value]) => {
+            xhr.setRequestHeader(header, value);
+        });
+
         xhr.send(data);
     }).then(
         (e) => {
