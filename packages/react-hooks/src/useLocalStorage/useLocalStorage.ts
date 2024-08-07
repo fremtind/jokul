baf6bb874691d905670d7dec17d60daffa2e5f0a
Ivar Nilsen <ivar.nilsen@fremtind.no>
2024-08-07 10:23:28 +0200

    feat: added useLocalStorage hook
    ISSUES CLOSED: #3923

---
+++ packages/react-hooks/src/useLocalStorage/useLocalStorage.ts
@@ -0,0 +1,23 @@
+import { useCallback, useState } from "react";
+
+export const useLocalStorage = <T>(key: string, defaultValue: T): [T, (newValue: T) => void] => {
+    const [state, setState] = useState(() => {
+        if (typeof window === "undefined" || !localStorage) {
+            return defaultValue;
+        }
+        const storedValue = JSON.parse(localStorage.getItem(key) || "null");
+        return storedValue || defaultValue;
+    });
+
+    const updateState = useCallback(
+        (newValue: T) => {
+            setState(newValue);
+            if (typeof window !== "undefined" && localStorage) {
+                localStorage.setItem(key, JSON.stringify(newValue));
+            }
+        },
+        [key],
+    );
+
+    return [state, updateState];
+};
