505cff4b5d24f60c7f7b2d5771340c18c29e4029
kristian ulvund <kristian.ulvund@fremtind.no>
2024-08-08 09:15:40 +0200

    feat: refactor linklist component

---
+++ packages/list-react/src/LinkList.tsx
@@ -0,0 +1,45 @@
+import { ArrowRightIcon } from "@fremtind/jkl-icons-react";
+import cn from "classnames";
+import { PolymorphicPropsWithRef, PolymorphicRef } from "packages/core/build";
+import React, { ComponentProps, FC } from "react";
+
+const Item: FC<ComponentProps<"li">> = ({ className, ...rest }) => {
+    return <li className={cn("jkl-link-list-item", className)} {...rest}></li>;
+};
+
+type LinkComponent = <ElementType extends React.ElementType = "a">(
+    props: PolymorphicPropsWithRef<ElementType>,
+) => React.ReactElement | null;
+
+const Link = React.forwardRef(function LinkListLink<ElementType extends React.ElementType = "a">(
+    props: PolymorphicPropsWithRef<ElementType>,
+    ref: PolymorphicRef<ElementType>,
+) {
+    const { as: Component = "a", children, className, ...rest } = props;
+
+    return (
+        <Component className={cn("jkl-link-list-link", className)} ref={ref} {...rest}>
+            {children}
+            <ArrowRightIcon className="jkl-link-list-link__arrow" />
+        </Component>
+    );
+}) as LinkComponent;
+
+type UnorderedLinkListProps = React.HTMLAttributes<HTMLUListElement> & {
+    variant: "unordered";
+};
+
+type OrderedLinkListProps = React.HTMLAttributes<HTMLOListElement> & {
+    variant: "ordered";
+};
+
+type LinkListProps = UnorderedLinkListProps | OrderedLinkListProps;
+
+export const LinkList = ({ variant, className, ...rest }: LinkListProps): React.JSX.Element => {
+    const Component = variant === "ordered" ? "ol" : "ul";
+
+    return <Component className={cn(`jkl-${variant}-link-list`, className)} {...rest} />;
+};
+
+LinkList.Link = Link;
+LinkList.Item = Item;
--- packages/list-react/src/LinkListItem.tsx
+++
@@ -1,6 +0,0 @@
-import cn from "classnames";
-import React, { ComponentProps, FC } from "react";
-
-export const LinkListItem: FC<ComponentProps<"li">> = ({ className, ...rest }) => {
-    return <li className={cn("jkl-link-list-item", className)} {...rest}></li>;
-};
