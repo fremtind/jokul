ad48db47e6f02f2f95ebac22dca8ae4ea66488b7
kristian ulvund <kristian.ulvund@fremtind.no>
2024-08-22 12:54:19 +0200

    feat: add tabs styles to popover example component

--- packages/popover-react/documentation/Example.tsx
+++ packages/popover-react/documentation/Example.tsx
@@ -3,6 +3,7 @@ import { DevExample } from "../../../doc-utils";
 import { PopoverExample, popoverExampleCode, popoverExampleKnobs } from "./PopoverExample";
 import "../../popover/popover.scss";
 import "../../button/button.scss";
+import "../../tabs/tabs.scss";
 
 export default function Example() {
     return <DevExample component={PopoverExample} codeExample={popoverExampleCode} knobs={popoverExampleKnobs} />;
