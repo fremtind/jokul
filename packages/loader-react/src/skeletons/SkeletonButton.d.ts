bb17de13290c5ed1e3dd89a0bfd2165da0422888
Ivar Nilsen <ivar.nilsen@fremtind.no>
2024-09-09 15:35:57 +0200

    fix: remove d.ts files

--- packages/loader-react/src/skeletons/SkeletonButton.d.ts
+++
@@ -1,7 +0,0 @@
-import { Density } from "@fremtind/jkl-core";
-import React from "react";
-import { SkeletonElementProps } from "./SkeletonElement";
-export interface SkeletonButtonProps extends Omit<SkeletonElementProps, "height"> {
-    density?: Density;
-}
-export declare const SkeletonButton: ({ style, density, ...rest }: SkeletonButtonProps) => React.JSX.Element;
