33eca03d3222b7e40d5ff95f1cc6f9fb085adcb1
Pio Rasch-Halvorsen <pio.rasch-halvorsen@fremtind.no>
2024-09-11 12:52:25 +0200

    chore: la utils være en vanlig mappe og ikke en pakke

--- utils/interactive-code/LiveEditor.tsx
+++ utils/interactive-code/LiveEditor.tsx
@@ -1,6 +1,6 @@
-import { useBrowserPreferences } from "@fremtind/jkl-react-hooks/src";
 import React from "react";
 import { LiveEditor as ReactLiveEditor, type LiveEditorProps as ReactLiveEditorProps } from "react-live";
+import { useBrowserPreferences } from "../../packages/react-hooks/src";
 import jokulDark from "./themeDark";
 import jokulLight from "./themeLight";
 
--- utils/interactive-code/LiveError.tsx
+++ utils/interactive-code/LiveError.tsx
@@ -1,7 +1,7 @@
-import { ErrorMessage } from "@fremtind/jkl-message-react";
 import React, { Context, useContext } from "react";
 import type { ContextProps as LiveContextProps } from "react-live";
 import { LiveContext } from "react-live";
+import { ErrorMessage } from "../../packages/message-react/src";
 
 export const LiveError = () => {
     const { error } = useContext(LiveContext as Context<LiveContextProps>);
