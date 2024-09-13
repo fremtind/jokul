d1ca70fe9019d259d5489b38a883a0ee0f38780b
Pio Rasch-Halvorsen <pio.rasch-halvorsen@fremtind.no>
2024-09-13 19:35:59 +0200

    feat: legg til komponentene icon og link-list i jokul

---
+++ packages/jokul/src/components/icon/icons/CloseIcon.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+import { Icon, type IconComponent } from "../Icon";
+import { type IconProps } from "../types";
+
+export const CloseIcon: IconComponent = (props: IconProps) => <Icon {...props}>{"\ue5cd"}</Icon>;
+CloseIcon.displayName = "CloseIcon";
---
+++ packages/jokul/src/components/link-list/index.ts
@@ -0,0 +1,1 @@
+export { LinkList } from "./LinkList";
