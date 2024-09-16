e63fccb5a846f0938bff9966af845beaaef11e9d
Ivar Nilsen <ivar.nilsen@fremtind.no>
2024-09-16 15:02:10 +0200

    chore: remove deprecated sources for message-box
    ISSUES CLOSED: #4067

--- packages/message-box-react/src/index.ts
+++
@@ -1,4 +0,0 @@
-export type { MessageBoxProps } from "./MessageBox";
-export { InfoMessageBox, ErrorMessageBox, SuccessMessageBox, WarningMessageBox } from "./MessageBox";
-export type { FormErrorMessageBoxProps } from "./FormErrorMessageBox";
-export { FormErrorMessageBox } from "./FormErrorMessageBox";
