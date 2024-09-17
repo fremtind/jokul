0ec666c1aa453bac23fd95e0c8b39df47f2eba3c
Pio Rasch-Halvorsen <pio.rasch-halvorsen@fremtind.no>
2024-09-17 22:42:43 +0200

    fix: legg til eksporter av nye komponenter

--- packages/jokul/src/components/index.ts
+++ packages/jokul/src/components/index.ts
@@ -1,6 +1,9 @@
+export * from "./card";
 export * from "./icon";
+export * from "./icon-button";
 export * from "./link";
 export * from "./link-list";
 export * from "./loader";
+export * from "./tag";
 export { ScreenReaderOnly, type ScreenReaderOnlyProps } from "./ScreenReaderOnly";
 export { SlotComponent, type SlotComponentProps } from "./SlotComponent";
