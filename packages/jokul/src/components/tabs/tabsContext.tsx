b8fd3759c722d70dbb2bae570ea2232926757297
Ivar Nilsen <ivar.nilsen@fremtind.no>
2024-09-25 15:13:06 +0200

    feat: move tabs to jokul package

---
+++ packages/jokul/src/components/tabs/tabsContext.tsx
@@ -0,0 +1,20 @@
+import React, { createContext, useContext } from "react";
+import { Density, WithChildren } from "../..";
+
+type TabsContext = {
+    density?: Density;
+};
+
+const tabsContext = createContext<TabsContext>({
+    density: undefined,
+});
+
+export const useTabsContext = (): TabsContext => useContext(tabsContext);
+
+interface Props extends WithChildren {
+    state: TabsContext;
+}
+
+export const TabsContextProvider: React.FC<Props> = ({ state, children }) => (
+    <tabsContext.Provider value={state}>{children}</tabsContext.Provider>
+);
