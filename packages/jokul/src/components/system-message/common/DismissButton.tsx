bf70502870abbe736a96ff06c0310671dfc4df59
Ivar Nilsen <ivar.nilsen@fremtind.no>
2024-09-25 13:08:52 +0200

    feat: move system-message to jokul package

---
+++ packages/jokul/src/components/system-message/common/DismissButton.tsx
@@ -0,0 +1,17 @@
+import React, { ButtonHTMLAttributes, forwardRef } from "react";
+import { CloseIcon } from "../../icon";
+
+export interface DismissButtonProps extends Exclude<ButtonHTMLAttributes<HTMLButtonElement>, "disabled"> {
+    label?: string;
+}
+
+export const DismissButton = forwardRef<HTMLButtonElement, DismissButtonProps>(({ label = "Lukk", ...rest }, ref) => {
+    return (
+        <button ref={ref} type="button" title={label} {...rest}>
+            <CloseIcon variant="small" bold />
+            <span className="jkl-sr-only">{label}</span>
+        </button>
+    );
+});
+
+DismissButton.displayName = "DismissButton";
