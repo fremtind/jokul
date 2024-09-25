42c222f74e126f266ab5de105f4e1eb68a9a2cb1
Ivar Nilsen <ivar.nilsen@fremtind.no>
2024-09-25 15:25:00 +0200

    feat: move toggle-switch to jokul package

---
+++ packages/jokul/src/components/toggle-switch/ToggleSlider.tsx
@@ -0,0 +1,99 @@
+import clsx from "clsx";
+import React, { useState, Fragment, useRef, FC, MouseEventHandler } from "react";
+import { Density, WithChildren } from "../..";
+import { useId, useSwipeGesture } from "../../hooks";
+import { type ToggleChangeHandler } from "./ToggleSwitch";
+import { usePillStyles } from "./usePillStyles";
+
+interface Props extends WithChildren {
+    labels: [string, string];
+    onToggle: (value: string) => void;
+    defaultValue?: string;
+    className?: string;
+    hideLegend?: boolean;
+    density?: Density;
+}
+
+export const ToggleSlider: FC<Props> = ({
+    children,
+    labels,
+    className = "",
+    onToggle,
+    defaultValue,
+    hideLegend = false,
+    density,
+    ...rest
+}) => {
+    const [currentLabel, setCurrentLabel] = useState(defaultValue || labels[0]);
+    const id = useId("jkl-toggle-slider");
+    const legendId = useId("jkl-toggle-slider-legend");
+    const activeRef = useRef<HTMLLabelElement>(null);
+
+    const shouldTransform = currentLabel === labels[1];
+    const pillStyles = usePillStyles(activeRef, shouldTransform, [density]);
+
+    const handleChange: ToggleChangeHandler<HTMLElement> = (_, pressed) => {
+        setCurrentLabel(pressed ? labels[1] : labels[0]);
+        onToggle(pressed ? labels[1] : labels[0]);
+    };
+
+    const handleClick: MouseEventHandler<HTMLInputElement> = (event) => {
+        const { value } = event.currentTarget;
+        setCurrentLabel(value);
+        onToggle(value);
+    };
+
+    const { gestureHandlers } = useSwipeGesture<HTMLDivElement>({ onClick: handleClick, onChange: handleChange });
+    const { onClick, ...swipeHandlers } = gestureHandlers;
+
+    if (process.env.NODE_ENV !== "production") {
+        console.error(
+            "WARNING: ToggleSlider component has been deprecated and will be removed in some future version of the library",
+        );
+    }
+
+    return (
+        <fieldset
+            {...rest}
+            className={clsx("jkl-toggle-slider", className)}
+            aria-labelledby={legendId}
+            data-testid="jkl-toggle-slider"
+            data-density={density}
+        >
+            <div
+                id={legendId}
+                className={clsx("jkl-toggle-slider__legend", {
+                    "jkl-toggle-slider__legend--sr-only": hideLegend,
+                })}
+            >
+                {children}
+            </div>
+            <div className="jkl-toggle-slider__inputs" {...swipeHandlers}>
+                {labels.map((label) => (
+                    <Fragment key={label}>
+                        <input
+                            className="jkl-toggle-slider__input"
+                            type="radio"
+                            value={label}
+                            checked={label === currentLabel}
+                            name={id}
+                            id={`${label}-${id}`}
+                            onClick={onClick}
+                            onChange={() => {}}
+                        />
+                        <label
+                            className={clsx("jkl-toggle-slider__label", {
+                                "jkl-toggle-slider__label--selected": label === currentLabel,
+                            })}
+                            ref={label === currentLabel ? activeRef : undefined}
+                            htmlFor={`${label}-${id}`}
+                        >
+                            {label}
+                        </label>
+                    </Fragment>
+                ))}
+                <span className="jkl-toggle-slider__pill" style={pillStyles} aria-hidden />
+            </div>
+        </fieldset>
+    );
+};
---
+++ packages/jokul/src/components/toggle-switch/ToggleSwitch.tsx
@@ -0,0 +1,77 @@
+import clsx from "clsx";
+import React, { type ButtonHTMLAttributes, type MouseEventHandler, forwardRef } from "react";
+import { Density } from "../..";
+import { useId, useSwipeGesture, type SwipeChangeHandler } from "../../hooks";
+import { CheckIcon } from "../icon";
+
+export type ToggleChangeHandler<T extends HTMLElement> = SwipeChangeHandler<T>;
+
+export type ToggleProps = Omit<ButtonHTMLAttributes<HTMLButtonElement>, "onChange"> & {
+    density?: Density;
+    /**
+     * Handler for å håndtere toggling av knappen. Tar inn en boolean som indikerer om knappen er er togglet på
+     * eller ikke, samt en MouseEvent eller en PointerEvent avhengig av om togglingen skjedde via klikk eller swipe.
+     * @example
+     * ```js
+     * function handleChange(event, pressed) {
+     *    console.log(`ToggleSwitch er ${pressed ? "på" : "av"}`);
+     * }
+     * ```
+     */
+    onChange?: ToggleChangeHandler<HTMLButtonElement>;
+};
+
+export const ToggleSwitch = forwardRef<HTMLButtonElement, ToggleProps>(
+    ({ "aria-pressed": ariaPressed = false, children, className, density, id, onChange, ...rest }, ref) => {
+        const uid = useId(id || "jkl-toggle-switch", { generateSuffix: !id });
+        const [pressed, setPressed] = React.useState(ariaPressed);
+        React.useEffect(() => {
+            setPressed(ariaPressed);
+        }, [ariaPressed]);
+
+        const { onClick, onPointerCancel, onPointerDown, onPointerMove, onPointerUp, ...buttonProps } = rest;
+
+        const handleClick: MouseEventHandler<HTMLButtonElement> = (event) => {
+            setPressed(!pressed);
+            onChange?.(event, !pressed);
+            onClick?.(event);
+        };
+
+        const handleChange: ToggleChangeHandler<HTMLButtonElement> = (event, toggleTo) => {
+            if (toggleTo !== pressed) {
+                setPressed(toggleTo);
+                onChange?.(event, toggleTo);
+            }
+        };
+
+        const { gestureHandlers } = useSwipeGesture({
+            onClick: handleClick,
+            onChange: handleChange,
+            onPointerCancel,
+            onPointerDown,
+            onPointerMove,
+            onPointerUp,
+        });
+
+        return (
+            <button
+                className={clsx("jkl-toggle-switch", className)}
+                id={uid}
+                ref={ref}
+                aria-pressed={pressed}
+                data-density={density}
+                {...buttonProps}
+                {...(buttonProps.disabled ? {} : gestureHandlers)}
+            >
+                {children}
+                <div aria-hidden className="jkl-toggle-switch-widget">
+                    <div className="jkl-toggle-switch-widget__slider">
+                        <div className="jkl-toggle-switch-widget__knob" />
+                        <CheckIcon variant="small" bold className="jkl-toggle-switch-widget__indicator" />
+                    </div>
+                </div>
+            </button>
+        );
+    },
+);
+ToggleSwitch.displayName = "ToggleSwitch";
