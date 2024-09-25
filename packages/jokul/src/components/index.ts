130d00fe083be8807f2429fc0de2d408a3e17c9c
Ivar Nilsen <ivar.nilsen@fremtind.no>
2024-09-25 14:53:04 +0200

    feat: move logo into jokul package

--- packages/jokul/src/components/index.ts
+++ packages/jokul/src/components/index.ts
@@ -17,6 +17,7 @@ export * from "./link";
 export * from "./link-list";
 export * from "./list";
 export * from "./loader";
+export * from "./logo";
 export * from "./message";
 export * from "./modal";
 export * from "./popover";
