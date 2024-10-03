070f4db3712505c93212878ac646427b58bedebc
Ivar Nilsen <ivar.nilsen@fremtind.no>
2024-10-03 12:21:52 +0200

    feat: move Menu component to jokul package

---
+++ packages/jokul/src/components/menu/MenuItem.tsx
@@ -0,0 +1,59 @@
+import clsx from "clsx";
+import React, { forwardRef, ReactNode } from "react";
+import { PolymorphicPropsWithRef, PolymorphicRef } from "../../utilities/polymorphism/polymorphism";
+import { ArrowNorthEastIcon, ChevronRightIcon } from "../icon";
+
+export type MenuItemProps<ElementType extends React.ElementType> = PolymorphicPropsWithRef<
+    ElementType,
+    {
+        /**
+         * Et ikon som vises før innholdet i menypunktet
+         */
+        icon?: ReactNode;
+    } & (
+        | {
+              expandable?: never;
+              /**
+               * Indikerer at menypunktet åpner noe i et nytt vindu eller tab
+               * @default false
+               */
+              external?: boolean;
+          }
+        | {
+              external?: never;
+              /**
+               * Indikerer at menypunktet er utvidbart ved å vise en chevron
+               * @default false
+               */
+              expandable?: boolean;
+          }
+    )
+>;
+
+type MenuItemComponent = <ElementType extends React.ElementType = "button">(
+    props: MenuItemProps<ElementType>,
+) => React.ReactElement | null;
+
+export const MenuItem = forwardRef(function MenuItem<ElementType extends React.ElementType = "button">(
+    props: MenuItemProps<ElementType>,
+    ref: PolymorphicRef<ElementType>,
+) {
+    const { as = "button", className, children, icon, expandable = false, external = false, ...rest } = props;
+    const Component = as;
+    const type = Component === "button" ? "button" : undefined;
+
+    return (
+        <Component ref={ref} type={type} role="menuitem" className={clsx("jkl-menu-item", className)} {...rest}>
+            {icon && <span className="jkl-menu-item__icon">{icon}</span>}
+            <div className="jkl-menu-item__content">
+                {children}
+                {external && (
+                    <div className={"jkl-menu-item__arrow"}>
+                        <ArrowNorthEastIcon />
+                    </div>
+                )}
+            </div>
+            {expandable && <ChevronRightIcon className="jkl-menu-item__chevron" bold />}
+        </Component>
+    );
+}) as MenuItemComponent;
---
+++ packages/jokul/src/components/menu/index.ts
@@ -0,0 +1,4 @@
+export { Menu, type MenuProps } from "./Menu";
+export { MenuItem, type MenuItemProps } from "./MenuItem";
+export { MenuItemCheckbox, type MenuItemCheckboxProps } from "./MenuItemCheckbox";
+export { MenuDivider } from "./MenuDivider";
