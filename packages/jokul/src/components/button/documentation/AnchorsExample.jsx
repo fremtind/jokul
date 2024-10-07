8d08149ab631a6452ca2c1e96410d4216c57bd8d
Pio Rasch-Halvorsen <pio.rasch-halvorsen@fremtind.no>
2024-10-07 12:27:18 +0200

    docs: legg til dokumentasjon for button i jokul-pakka

---
+++ packages/jokul/src/components/button/documentation/AnchorsExample.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { Button } from "../Button";
+import "./style.scss";
+
+export const AnchorsExample = () => {
+    /* -- EXAMPLE CODE START -- */
+    return (
+        <div className="flex flex-column gap-32 justify-between items-center">
+            <Button as="a" variant="primary" href="/komponenter/buttons#knapper-rendret-som-andre-elementer">
+                Send inn
+            </Button>
+            <Button variant="secondary" as="a" href="/komponenter/buttons#knapper-rendret-som-andre-elementer">
+                Lagre
+            </Button>
+            <Button variant="tertiary" as="a" href="/komponenter/buttons#knapper-rendret-som-andre-elementer">
+                Avbryt
+            </Button>
+            <Button variant="ghost" as="a" href="/komponenter/buttons#knapper-rendret-som-andre-elementer">
+                Vis mer
+            </Button>
+        </div>
+    );
+    /* -- EXAMPLE CODE END -- */
+};
---
+++ packages/jokul/src/components/button/documentation/Example.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+import { formatCode, InteractiveCode, useInteractiveCodeControls } from "../../../../../../utils/interactive-code";
+import AnchorsExample from "./AnchorsExample?raw";
+// @ts-ignore
+import { knobs } from "./ButtonExample";
+import ButtonExample from "./ButtonExample?raw";
+
+// TODO: Flytt denne importen inn i InteractiveCode så den kan brukes i alle eksempler
+import "../../../core/styles/vind.scss";
+import "../styles/_index.scss";
+
+const code = formatCode(ButtonExample);
+const anchorsCode = formatCode(AnchorsExample);
+
+export default function Example() {
+    const controls = useInteractiveCodeControls(code, knobs);
+    const anchorsControls = useInteractiveCodeControls(anchorsCode, []);
+    return (
+        <div className={"jkl"}>
+            <InteractiveCode
+                title={"Button"}
+                defaultCode={code}
+                defaultShowEditor={true}
+                noInline={true}
+                controls={controls}
+            />
+            <InteractiveCode
+                title={"Button som lenke"}
+                defaultCode={anchorsCode}
+                defaultShowEditor={true}
+                noInline={true}
+                controls={anchorsControls}
+            />
+        </div>
+    );
+}
