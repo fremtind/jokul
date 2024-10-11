a50a73f3bacc08f48dc7848058bfb0e9cde79585
Ivar Nilsen <ivar.nilsen@fremtind.no>
2024-10-11 13:19:20 +0200

    chore: delete scaffolding files

--- packages/jokul/src/components/flex/documentation/main.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { initTabListener } from "../../..";
-import Example from "./Example";
-
-initTabListener();
-
-ReactDOM.createRoot(document.getElementById("root")!).render(<Example />);
