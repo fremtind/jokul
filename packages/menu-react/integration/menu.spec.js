778c85321510b8da70a70f69a76289417d6641b5
Ivar Nilsen <ivar.nilsen@fremtind.no>
2024-10-17 12:14:22 +0200

    fix: finished renaming contextual-menu => menu

--- packages/menu-react/integration/menu.spec.js
+++ packages/menu-react/integration/menu.spec.js
@@ -3,15 +3,15 @@
 
 describe("ContextualMenu", () => {
     beforeEach(() => {
-        cy.testComponent("contextualmenu");
+        cy.testComponent("menu");
     });
 
     it("renders menu when trigger is clicked", () => {
         cy.takeSnapshots({
             eq: 0,
-            customSelector: () => cy.get(".jkl-contextual-menu"),
+            customSelector: () => cy.get(".jkl-menu"),
             setup: () => {
-                cy.getByTestid("trigger-contextual-menu").first().click();
+                cy.getByTestid("trigger-menu").first().click();
             },
         });
     });
