567c415b36eddf391b01175968b91ee35588d69f
Pio Rasch-Halvorsen <pio.rasch-halvorsen@fremtind.no>
2024-10-22 13:16:20 +0200

    feat: lag ny byggmappe for stilark for enklere import

--- packages/jokul/build-styles.mjs
+++ packages/jokul/build-styles.mjs
@@ -2,8 +2,8 @@ import autoprefixer from "autoprefixer";
 import cssnano from "cssnano";
 import litePreset from "cssnano-preset-lite";
 import { glob } from "glob";
-import { writeFile } from "node:fs/promises";
-import { extname } from "node:path";
+import { writeFile, cp } from "node:fs/promises";
+import { mkdirSync } from "node:fs";
 import { fileURLToPath } from "node:url";
 import postcss from "postcss";
 import * as sass from "sass-embedded";
@@ -11,29 +11,53 @@ import * as sass from "sass-embedded";
 (async function build() {
     try {
         const sources = await glob("./src/**/[!_]*.scss", { ignore: ["node_modules/**", "**/documentation/**"] });
+        const unfilteredSources = await glob("./src/**/*.scss", { ignore: ["node_modules/**", "**/documentation/**"] });
         await Promise.all(
-            sources.flatMap((source) => {
-                const sourcePath = fileURLToPath(new URL(source, import.meta.url));
-                const cssPath = `${sourcePath.slice(0, sourcePath.length - extname(sourcePath).length)}.css`;
-                const cssMinPath = `${sourcePath.slice(0, sourcePath.length - extname(sourcePath).length)}.min.css`;
+            sources
+                .flatMap((source) => {
+                    const fileName = source.slice(source.lastIndexOf("/") + 1);
+                    const sourcePath = fileURLToPath(new URL(source, import.meta.url));
+                    const outDirName = sourcePath
+                        .substring(0, sourcePath.lastIndexOf("/styles/"))
+                        .replace("/src", "/styles");
 
-                const content = sass.compile(sourcePath);
+                    const content = sass.compile(sourcePath);
+                    mkdirSync(outDirName, { recursive: true });
 
-                return [
-                    writeFile(cssPath, content.css).then(() => console.log(`wrote file:${cssPath}`)),
-                    new Promise((resolve, reject) => {
-                        postcss([autoprefixer(), cssnano(litePreset)])
-                            .process(content.css)
-                            .then((result) =>
-                                writeFile(cssMinPath, result.css).then(() => {
-                                    console.log(`wrote file:${cssMinPath}`);
-                                    resolve();
-                                }),
-                            )
-                            .catch((error) => reject(error));
+                    return [
+                        writeFile(`${outDirName}/${fileName.replace(".scss", ".css")}`, content.css).then(() =>
+                            console.log("wrote file:${cssPath}"),
+                        ),
+                        new Promise((resolve, reject) => {
+                            postcss([autoprefixer(), cssnano(litePreset)])
+                                .process(content.css)
+                                .then((result) =>
+                                    writeFile(
+                                        `${outDirName}/${fileName.replace(".scss", ".min.css")}`,
+                                        result.css,
+                                    ).then(() => {
+                                        console.log("wrote file:${cssMinPath}");
+                                        resolve();
+                                    }),
+                                )
+                                .catch((error) => reject(error));
+                        }),
+                    ];
+                })
+                .concat(
+                    unfilteredSources.flatMap((source) => {
+                        const fileName = source.slice(source.lastIndexOf("/") + 1);
+                        const sourcePath = fileURLToPath(new URL(source, import.meta.url));
+                        const cutPoint = sourcePath.lastIndexOf("/styles/");
+                        const outDirName = sourcePath
+                            .substring(0, cutPoint > 0 ? cutPoint : sourcePath.lastIndexOf("/"))
+                            .replace("/src", "/styles");
+
+                        mkdirSync(outDirName, { recursive: true });
+
+                        return [cp(sourcePath, `${outDirName}/${fileName}`)];
                     }),
-                ];
-            }),
+                ),
         );
     } catch (error) {
         console.error(error);
