ec63d0d40d97001c84efbe3d6d379f56cdffe3e2
mariaeilertsen <maria.eilertsen@fremtind.no>
2024-11-14 09:37:32 +0100

    fix: fjerne duplicates

--- packages/jokul/src/utilities/polymorphism/polymorphism.ts
+++ packages/jokul/src/utilities/polymorphism/polymorphism.ts
@@ -7,12 +7,6 @@ type ElementTypeProp<ElementType extends React.ElementType> = {
     as?: ElementType;
 };
 
-type PropsToOmit<
-    ElementType extends React.ElementType,
-    Props,
-> = keyof (ElementTypeProp<ElementType> & Props);
-// type PropsToOmit<ElementType extends React.ElementType, Props> = keyof (ElementTypeProp<ElementType> & Props);
-
 export type PolymorphicProps<
     ElementType extends React.ElementType,
     Props = {},
@@ -25,13 +19,11 @@ export type PolymorphicProps<
 
 export type PolymorphicRef<As extends React.ElementType> = React.ComponentPropsWithRef<As>["ref"];
 
-// old utility with ref
-export type PolymorphicPropsWithRef<ElementType extends React.ElementType, Props = {}> = PolymorphicProps<
-    ElementType,
+export type PolymorphicComponentPropWithRef<As extends React.ElementType, Props = object> = PolymorphicComponentProp<
+    As,
     Props
-> & { ref?: PolymorphicRef<ElementType> };
+> & { ref?: PolymorphicRef<As> };
 
-// This is a new type utitlity with ref
 // Implementation of reusable polymorphic types
 // Explaination: https://blog.logrocket.com/build-strongly-typed-polymorphic-components-react-typescript/
 type AsProp<As extends React.ElementType> = { as?: As };
@@ -40,11 +32,6 @@ type PolymorphicComponentProp<As extends React.ElementType, Props = object> = Re
     Props & AsProp<As>
 > &
     Omit<React.ComponentPropsWithoutRef<As>, PropsToOmit<As, Props>>;
-
-export type PolymorphicComponentPropWithRef<As extends React.ElementType, Props = object> = PolymorphicComponentProp<
-    As,
-    Props
-> & { ref?: PolymorphicRef<As> };
 
 // Les https://stackoverflow.com/q/57683303
 export type Expand<T> = T extends (...args: infer A) => infer R
