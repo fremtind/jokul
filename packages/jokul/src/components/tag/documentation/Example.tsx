c6f22d847c80d3e41929217573dc084d40221e68
kristian ulvund <kristian.ulvund@fremtind.no>
2024-11-20 10:02:58 +0100

    refactor: fjern dismissableAction property
    fjerner dismissableAction fra tag da chip erstatter den funksjonaliteten
    
    BREAKING CHANGE:
    Fjernet den påkrevde dismissableAction-propen fra Tag-komponenten.
    
    ISSUES CLOSED: #4288

--- packages/jokul/src/components/tag/documentation/Example.tsx
+++ packages/jokul/src/components/tag/documentation/Example.tsx
@@ -1,13 +1,7 @@
 import { DevExample } from "doc-utils/DevExample.js";
 import React from "react";
-import { TagExample, tagExampleKnobs } from "./TagExample.js";
+import { TagExample } from "./TagExample.js";
 
 export default function Example() {
-    return (
-        <DevExample
-            title="Tag"
-            component={TagExample}
-            knobs={tagExampleKnobs}
-        />
-    );
+    return <DevExample title="Tag" component={TagExample} />;
 }
