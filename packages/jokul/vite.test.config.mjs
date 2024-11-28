1152a11b653c045a8dd3d602981e76a234cd196b
Pio Rasch-Halvorsen <pio.rasch-halvorsen@fremtind.no>
2024-11-28 16:26:55 +0100

    chore: kjør enhetstester i jokul raskere

--- packages/jokul/vite.test.config.mjs
+++ packages/jokul/vite.test.config.mjs
@@ -6,6 +6,7 @@ import { defineConfig } from "vite";
 export default defineConfig({
     plugins: [react()],
     test: {
+        pool: "threads",
         environment: "jsdom",
         setupFiles: ["vitest-setup.js"],
         include: ["src/**/*.test.?(c|m)ts?(x)"],
