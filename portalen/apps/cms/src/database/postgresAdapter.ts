a967327ad01fba84dabbb7fe8cdf96dada14a005
kristian ulvund <kristian.ulvund@fremtind.no>
2024-11-29 13:52:40 +0100

    fix: fjern password i postgres db con-string

--- portalen/apps/cms/src/database/postgresAdapter.ts
+++ portalen/apps/cms/src/database/postgresAdapter.ts
@@ -5,7 +5,7 @@ dotenv.config();
 
 const POSTGRES_DB_URL =
     process.env.PLATFORM === "aws"
-        ? `postgres://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`
+        ? `postgres://${process.env.DB_USER}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`
         : "postgres://localhost/jokul-portal";
 
 const getPostgresAdapter = () => {
