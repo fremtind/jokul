f5ca7cdee8fbed6872bb2f7e95b4b8ecf36bd914
Pio Rasch-Halvorsen <pio.rasch-halvorsen@fremtind.no>
2024-12-03 13:06:35 +0100

    chore: forsøk å koble til AWS-database med token

--- portalen/apps/cms/src/database/postgresAdapter.ts
+++ portalen/apps/cms/src/database/postgresAdapter.ts
@@ -1,17 +1,29 @@
+import { readFileSync } from "node:fs";
+import path from "node:path";
+import { Signer } from "@aws-sdk/rds-signer";
 import { postgresAdapter } from "@payloadcms/db-postgres";
 import dotenv from "dotenv";
 
 dotenv.config();
 
-const POSTGRES_DB_URL =
-    process.env.PLATFORM === "aws"
-        ? `postgres://${process.env.DB_USER}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`
-        : "postgres://localhost/jokul-portal";
+const ca = readFileSync(path.join(__dirname, "cacert.pem"), "utf-8");
+
+const signer = new Signer({
+    hostname: process.env.DB_HOST || "",
+    port: Number(process.env.DB_PORT) || 5432,
+    username: process.env.DB_USER || "",
+    region: process.env.AWS_REGION || "eu-north-1",
+});
 
 const getPostgresAdapter = () => {
     return postgresAdapter({
         pool: {
-            connectionString: POSTGRES_DB_URL,
+            ssl: { ca },
+            host: process.env.DB_HOST,
+            port: Number(process.env.DB_PORT),
+            user: process.env.DB_USER,
+            database: process.env.DB_NAME,
+            password: () => signer.getAuthToken(),
         },
     });
 };
