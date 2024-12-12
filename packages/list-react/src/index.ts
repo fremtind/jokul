f64693f386c21f78de2e58e27f819b68be5b98b2
kristian ulvund <kristian.ulvund@fremtind.no>
2024-12-12 10:29:01 +0100

    fix: eksporter listprops

--- packages/list-react/src/index.ts
+++ packages/list-react/src/index.ts
@@ -1,4 +1,4 @@
-export type { ListProps } from "./List";
 export { UnorderedList, OrderedList, List } from "./List";
 export { ListItem, CheckListItem, CrossListItem } from "./ListItem";
 export { LinkList } from "./LinkList";
+export type { ListProps } from "./types";
