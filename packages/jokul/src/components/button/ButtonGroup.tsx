963f5753400ad2b67299fdc6110a75fcb3bf8174
Pio Rasch-Halvorsen <pio.rasch-halvorsen@fremtind.no>
2025-01-29 10:01:20 +0100

    feat: legg til mulighet for å tvinge vertikal layout i ButtonGroup

--- packages/jokul/src/components/button/ButtonGroup.tsx
+++ packages/jokul/src/components/button/ButtonGroup.tsx
@@ -1,13 +1,26 @@
 import { clsx } from "clsx";
-import type { ComponentPropsWithRef } from "react";
+import React, { type ComponentPropsWithRef } from "react";
+
+export type ButtonGroupProps = {
+    /**
+     * Tvinger knappene til å vises over hverandre, i full bredde
+     * @default false
+     */
+    stacked?: boolean;
+} & ComponentPropsWithRef<"div">;
 
 // TODO: Denne burde kanskje være polymorf, og i hvert fall types bedre
 export const ButtonGroup = ({
     className,
     children,
+    stacked = false,
     ...props
-}: ComponentPropsWithRef<"div">): JSX.Element => (
-    <div className={clsx("jkl-button-group", className)} {...props}>
+}: ButtonGroupProps): JSX.Element => (
+    <div
+        data-stacked={stacked}
+        className={clsx("jkl-button-group", className)}
+        {...props}
+    >
         {children}
     </div>
 );
