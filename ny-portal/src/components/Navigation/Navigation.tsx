da46ba50bc9d262c55c15163283e40dc5c78ab3c
kristian ulvund <kristian.ulvund@fremtind.no>
2025-02-04 09:17:18 +0100

    refactor: opprydning etter pr

--- ny-portal/src/components/Navigation/Navigation.tsx
+++ ny-portal/src/components/Navigation/Navigation.tsx
@@ -2,29 +2,27 @@ import { Link } from "@fremtind/jokul";
 import { clsx } from "clsx";
 import styles from "./navigation.module.scss";
 import { NavigationMenu } from "./NavigationMenu";
-import { Search } from "./NavigationSearch";
+import { NavigationSearch } from "./NavigationSearch";
 
 export async function Navigation() {
     return (
-        <div className={clsx(styles.navigation)}>
-            <nav>
-                <header className={styles["navigation__header"]}>
-                    <div className={styles["navigation__headerColumn"]}>
-                        <Link className={styles["navigation__home"]} href="/">
-                            Jøkul
-                        </Link>
-                    </div>
-                </header>
+        <header className={styles["navigation__header"]}>
+            <nav className={clsx(styles.navigation)}>
+                <div className={styles["navigation__headerColumn"]}>
+                    <Link className={styles["navigation__home"]} href="/">
+                        Jøkul
+                    </Link>
+                </div>
                 <div
                     className={clsx(
                         styles.navigation__menu,
                         styles["navigation-menu"],
                     )}
                 >
-                    <Search />
+                    <NavigationSearch />
                     <NavigationMenu />
                 </div>
             </nav>
-        </div>
+        </header>
     );
 }
