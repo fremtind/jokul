de1d1c621e0386644760e9e0a10e8ed848f73807
Ivar Nilsen <ivar.nilsen@fremtind.no>
2025-02-12 15:08:45 +0100

    chore: sort stories

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -5,6 +5,24 @@ import "../packages/jokul/src/components/card/styles/_index.scss";
 
 const preview: Preview = {
     parameters: {
+        options: {
+            storySort: (a, b) => {
+                let compareBy = "name";
+                if (a.type === "docs" && b.type === "story") {
+                    return -1;
+                } else if (a.type === "story" && b.type === "docs") {
+                    return 1;
+                } else if (a.type === "docs" && b.type === "docs") {
+                    compareBy = "id";
+                }
+
+                return a[compareBy] === b[compareBy]
+                    ? 0
+                    : a[compareBy].localeCompare(b[compareBy], undefined, {
+                          numeric: true,
+                      });
+            },
+        },
         layout: "centered",
         controls: {
             matchers: {
