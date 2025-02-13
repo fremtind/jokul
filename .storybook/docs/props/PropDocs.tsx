32a8893a8cecf550cf8da723b8905d9983d2222c
Ivar Nilsen <ivar.nilsen@fremtind.no>
2025-02-13 06:47:26 +0100

    docs: documents component props inside storybook

---
+++ .storybook/docs/props/PropDocs.tsx
@@ -0,0 +1,38 @@
+import { useOf } from "@storybook/blocks";
+import React, { FC } from "react";
+import { Prop } from "./Prop";
+import clsx from "clsx";
+
+import styles from "./prop-docs.module.scss";
+
+type Props = {
+    of?: any;
+};
+
+export const PropDocs: FC<Props> = ({ of }) => {
+    const resolvedOf = useOf(of || "story", ["story", "meta"]);
+
+    if (resolvedOf.type !== "story") {
+        return null;
+    }
+
+    return (
+        <div className={clsx("sb-unstyled", styles["prop-list"])}>
+            <div className={styles["prop-list__header"]} aria-hidden={true}>
+                <div>Navn</div>
+                <div>Beskrivelse</div>
+                <div>Påkrevd</div>
+                <div>Type</div>
+            </div>
+            <ul className={styles["prop-list__subgrid"]}>
+                {Object.entries(resolvedOf.story.argTypes)
+                    .filter(([name]) => name !== "children")
+                    .map(([name, details], index) => {
+                        return (
+                            <Prop name={name} details={details} key={index} />
+                        );
+                    })}
+            </ul>
+        </div>
+    );
+};
