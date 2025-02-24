1f91a0e51076764ffd6cbcaacb10234477730d24
kristian ulvund <kristian.ulvund@fremtind.no>
2025-02-24 19:48:41 +0100

    refactor: fjern token da den ikke støttes i defineconfig

--- ny-portal/sanity.config.ts
+++ ny-portal/sanity.config.ts
@@ -4,12 +4,11 @@ import { structureTool } from "sanity/structure";
 import { schemaTypes } from "./sanity/schemas";
 
 export default defineConfig({
-    name: "portal-sanity-studio",
+    name: "default",
     basePath: "/studio",
-    title: "portal-sanity-studio",
+    title: "Jøkul Portal Studio",
     projectId: process.env.NEXT_PUBLIC_SANITY_STUDIO_PROJECT_ID || "",
     dataset: process.env.NEXT_PUBLIC_SANITY_STUDIO_DATASET || "test",
-    token: process.env.SANITY_API_READ_TOKEN || "",
     plugins: [structureTool(), visionTool()],
     schema: {
         types: schemaTypes,
