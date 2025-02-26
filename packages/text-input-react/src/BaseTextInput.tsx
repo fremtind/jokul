db005a47d500a8f286af920a82c89abe23a02dfc
Ole Jørgen <lmfaole@icloud.com>
2025-02-26 13:16:40 +0100

    feat: disabled/readOnly styling for TextInput
    ISSUES CLOSED: #4605

--- packages/text-input-react/src/BaseTextInput.tsx
+++ packages/text-input-react/src/BaseTextInput.tsx
@@ -1,6 +1,11 @@
 import { Density } from "@fremtind/jkl-core";
 import { IconButton } from "@fremtind/jkl-icon-button-react";
 import type { IconProps } from "@fremtind/jkl-icons-react";
+import {
+    Tooltip,
+    TooltipContent,
+    TooltipTrigger,
+} from "@fremtind/jkl-tooltip-react";
 import cn from "classnames";
 import React, {
     type CSSProperties,
@@ -48,8 +53,10 @@ export interface ActionSubmit extends ActionBaseProps {
 
 export type Action = ActionButton | ActionSubmit;
 
-export interface BaseTextInputProps
-    extends Omit<InputHTMLAttributes<HTMLInputElement>, "children"> {
+export type BaseTextInputProps = Omit<
+    InputHTMLAttributes<HTMLInputElement>,
+    "children" | "maxLength"
+> & {
     /**
      * Brukes til inputfelter hvor det brukes maskering, for formatering av store tall. Brukes typisk bare til valuta.
      * @default "left"
@@ -71,7 +78,33 @@ export interface BaseTextInputProps
      * Element som vises til høyre for inputfeltet. Brukes typisk til å trigge en handling som f.eks. å vise/skjule passord.
      */
     actionButton?: React.ReactElement;
-}
+    /**
+     * Setter maxlength attributtet og justerer bredden på feltet til å passe det tallet som settes
+     *
+     * Merk: I noen Android-browsere vil dette ikke fungere som forventet. Det er gjort sånn
+     * pga begrensninger med hvordan software-tastaturet fungerer og er ikke en bug. Dersom
+     * man er veldig avhengig av at maxLength håndteres på alle plattformer anbefales det
+     * å bruke input-feltet som en controlled input og selv begrense lengden på verdien.
+     */
+    maxLength?: number | undefined;
+    /**
+     * Lager en tooltip rundt elementet med en forklaring til bruker om hvorfor feltet er utilgjengelig.
+     */
+    disabledReason?: string;
+};
+
+const DisabledTooltip = ({
+    children,
+    description,
+}: {
+    children: React.ReactNode;
+    description?: string;
+}) => (
+    <Tooltip>
+        <TooltipContent>{description}</TooltipContent>
+        <TooltipTrigger>{children}</TooltipTrigger>
+    </Tooltip>
+);
 
 export const BaseTextInput = forwardRef<HTMLInputElement, BaseTextInputProps>(
     (props, ref) => {
@@ -87,54 +120,64 @@ export const BaseTextInput = forwardRef<HTMLInputElement, BaseTextInputProps>(
             unit,
             width,
             actionButton,
+            disabled,
+            disabledReason = "Dette feltet er utilgjengelig",
             ...rest
         } = props;
 
+        const Wrapper = disabled ? DisabledTooltip : React.Fragment;
+
         return (
-            <div
-                className="jkl-text-input-wrapper"
-                data-invalid={ariaInvalid}
-                style={{ ...style, ...getWidthAsStyle(width, maxLength) }}
-            >
-                <input
-                    aria-invalid={ariaInvalid}
-                    ref={ref}
-                    className={cn("jkl-text-input__input", className, {
-                        "jkl-text-input__input--align-right": align === "right",
-                    })}
-                    maxLength={maxLength}
-                    type={type}
-                    {...rest}
-                />
-                {unit && <span className="jkl-text-input__unit">{unit}</span>}
-                {!action &&
-                    actionButton &&
-                    React.cloneElement(actionButton, {
-                        className: cn(
-                            "jkl-text-input-action-button",
-                            actionButton.props.className,
-                        ),
-                        "data-theme": ariaInvalid ? "light" : undefined,
-                    })}
-                {action && !actionButton && (
-                    <IconButton
-                        data-theme={ariaInvalid ? "light" : undefined}
-                        density={density}
-                        className={cn(
-                            "jkl-text-input-action-button",
-                            action.className,
-                        )}
-                        title={action.label}
-                        onClick={action.onClick}
-                        onFocus={action.onFocus}
-                        onBlur={action.onBlur}
-                        ref={action.buttonRef}
-                        type={action.type || "button"}
-                    >
-                        {action.icon}
-                    </IconButton>
-                )}
-            </div>
+            <Wrapper description={disabledReason}>
+                <div
+                    className="jkl-text-input-wrapper"
+                    data-invalid={ariaInvalid}
+                    style={{ ...style, ...getWidthAsStyle(width, maxLength) }}
+                >
+                    <input
+                        aria-invalid={ariaInvalid}
+                        ref={ref}
+                        className={cn("jkl-text-input__input", className, {
+                            "jkl-text-input__input--align-right":
+                                align === "right",
+                        })}
+                        maxLength={maxLength}
+                        type={type}
+                        disabled={disabled}
+                        {...rest}
+                    />
+                    {unit && (
+                        <span className="jkl-text-input__unit">{unit}</span>
+                    )}
+                    {!action &&
+                        actionButton &&
+                        React.cloneElement(actionButton, {
+                            className: cn(
+                                "jkl-text-input-action-button",
+                                actionButton.props.className,
+                            ),
+                            "data-theme": ariaInvalid ? "light" : undefined,
+                        })}
+                    {action && !actionButton && (
+                        <IconButton
+                            data-theme={ariaInvalid ? "light" : undefined}
+                            density={density}
+                            className={cn(
+                                "jkl-text-input-action-button",
+                                action.className,
+                            )}
+                            title={action.label}
+                            onClick={action.onClick}
+                            onFocus={action.onFocus}
+                            onBlur={action.onBlur}
+                            ref={action.buttonRef}
+                            type={action.type || "button"}
+                        >
+                            {action.icon}
+                        </IconButton>
+                    )}
+                </div>
+            </Wrapper>
         );
     },
 );
