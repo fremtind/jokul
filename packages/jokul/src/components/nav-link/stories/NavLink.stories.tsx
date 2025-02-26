9e37b73f011aa4e0faae082f71bf5c4af130a1eb
fremtindelise <elise.brenna@fremtind.no>
2025-02-26 14:59:04 +0100

    feat: lagde story for NavLink

---
+++ packages/jokul/src/components/nav-link/stories/NavLink.stories.tsx
@@ -0,0 +1,30 @@
+import { Meta, StoryObj } from "@storybook/react";
+import React from "react";
+import { NavLink as NavLinkComponent } from "../NavLink.js";
+import "../styles/_index.scss";
+
+const meta: Meta = {
+    title: "Komponenter/NavLink",
+    component: NavLinkComponent,
+    parameters: {
+        layout: "centered",
+    },
+    tags: ["autodocs"],
+    argTypes: {
+        href: {
+            control: "text",
+            defaultValue: "https://www.fremtind.no",
+        },
+    },
+} satisfies Meta<typeof NavLinkComponent>;
+
+export default meta;
+
+type Story = StoryObj<typeof NavLinkComponent>;
+
+export const NavLink: Story = {
+    args: {
+        children: "NavLink",
+        href: "https://www.fremtind.no",
+    },
+};
