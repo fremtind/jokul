a9c12fdef6e790b5afaea64afdecfd01e59fc3f9
kristian ulvund <kristian.ulvund@fremtind.no>
2025-02-28 14:05:15 +0100

    feat: add portable-text renderer logic
    Added portable-text related logic
    
    ISSUES CLOSED: #4631

--- ny-portal/src/components/portable-text/props-documentation/PropsDocumentation.tsx
+++ ny-portal/src/components/portable-text/props-documentation/PropsDocumentation.tsx
@@ -1,12 +1,17 @@
 import { readFile } from "node:fs/promises";
-import { EOL } from "node:os";
 import { resolve } from "node:path";
+import { PortableTextTypeComponentProps } from "next-sanity";
+import { FC } from "react";
 import MissingTypesMessage from "./missing-types.mdx";
 import { Props } from "./Props";
 
-type Props = { component: string };
+type PropsDocumentationSchema = {
+    component_folder: string;
+};
 
-export async function PropDocumentation({ component }: Props) {
+export const PropsDocumentation: FC<
+    PortableTextTypeComponentProps<PropsDocumentationSchema>
+> = async ({ value }) => {
     const exampleFilePath = resolve(
         process.cwd(),
         "..",
@@ -14,24 +19,15 @@ export async function PropDocumentation({ component }: Props) {
         "jokul",
         "src",
         "components",
-        component,
+        value.component_folder,
         "types.ts",
     );
 
     try {
         const file = await readFile(exampleFilePath);
 
-        return (
-            <Props
-                props={
-                    file.toString()
-                    // .split(EOL)
-                    // .filter((line) => !line.startsWith("import"))
-                    // .join(EOL)
-                }
-            />
-        );
+        return <Props props={file.toString()} />;
     } catch (e) {
         return <MissingTypesMessage />;
     }
-}
+};
