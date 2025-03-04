30c425cf05a5d9cd6dfe852e818d46e75cccc9bf
Pio Rasch-Halvorsen <pio.rasch-halvorsen@fremtind.no>
2025-03-04 16:40:15 +0100

    chore: bytt tilbake til port 3000 for server i Dockerfile
    Dette vil gjøre at den felles serveren ikke fungerer lokalt, men
    lar oss teste publisering av portalen uten å gjøre for mye
    endringer i configrepoet. Vi bør finne en god måte å teste fullt
    oppsett lokalt etter hvert.

--- server.js
+++ server.js
@@ -2,12 +2,12 @@ const express = require("express");
 const proxy = require("express-http-proxy");
 
 const app = express();
-const PORT = 3333;
+const PORT = 3000;
 
 // Server bygd Storybook under /storybook
 app.use("/storybook", express.static("storybook-static"));
 // Omdiriger alle andre paths til kjørende Next.js
-app.use("/", proxy("localhost:3000/"));
+app.use("/", proxy("localhost:3333/"));
 
 app.listen(PORT, () => {
     console.log(`Express server listening on port ${PORT}`);
