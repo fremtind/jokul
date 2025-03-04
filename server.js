7cf437bbc204a47b1e956df99c44ee6891df7d3d
Pio Rasch-Halvorsen <pio.rasch-halvorsen@fremtind.no>
2025-03-04 12:56:22 +0100

    chore: oppdater Dockerfil for å kjøre opp ny portal og storybook

--- server.js
+++ server.js
@@ -2,12 +2,12 @@ const express = require("express");
 const proxy = require("express-http-proxy");
 
 const app = express();
-const PORT = 3000;
+const PORT = 3333;
 
 // Server bygd Storybook under /storybook
 app.use("/storybook", express.static("storybook-static"));
 // Omdiriger alle andre paths til kjørende Next.js
-app.use("/", proxy("localhost:3333/"));
+app.use("/", proxy("localhost:3000/"));
 
 app.listen(PORT, () => {
     console.log(`Express server listening on port ${PORT}`);
