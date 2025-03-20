21fdcbbae01150ae89d26b6785fe6a24d6b4e387
Ole Jørgen <lmfaole@icloud.com>
2025-03-20 14:07:03 +0100

    docs: fiks header, start med navigasjon

--- ny-portal/src/components/Navigation/Navigation.tsx
+++ ny-portal/src/components/Navigation/Navigation.tsx
@@ -5,24 +5,5 @@ import { NavigationMenu } from "./NavigationMenu";
 import { NavigationSearch } from "./NavigationSearch";
 
 export async function Navigation() {
-    return (
-        <header className={styles["navigation__header"]}>
-            <nav className={clsx(styles.navigation)}>
-                <div className={styles["navigation__headerColumn"]}>
-                    <Link className={styles["navigation__home"]} href="/">
-                        Jøkul
-                    </Link>
-                </div>
-                <div
-                    className={clsx(
-                        styles.navigation__menu,
-                        styles["navigation-menu"],
-                    )}
-                >
-                    <NavigationSearch />
-                    <NavigationMenu />
-                </div>
-            </nav>
-        </header>
-    );
+    return <NavigationMenu />;
 }
