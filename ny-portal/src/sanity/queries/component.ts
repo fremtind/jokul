878135050a213e3a0ba9580ecce658c4f6835e72
Ole Jørgen <lmfaole@icloud.com>
2025-03-20 12:51:21 +0100

    docs: sett opp oversiktsiden for komponenter

--- ny-portal/src/sanity/queries/component.ts
+++ ny-portal/src/sanity/queries/component.ts
@@ -1,7 +1,11 @@
 import { defineQuery } from "next-sanity";
 
 export const componentsQuery = defineQuery(
-    `*[_type == "jokul_component"]{name, slug}`,
+    `*[_type == "jokul_component"]{
+    name,
+    slug,
+    "imageUrl": image.asset->url
+    } | order(name)`,
 );
 
 export const componentBySlugQuery = defineQuery(
