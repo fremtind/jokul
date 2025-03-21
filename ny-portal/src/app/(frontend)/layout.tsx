220bc51595867ae39d1b7a3b4055aaf13604a501
Ole Jørgen <lmfaole@icloud.com>
2025-03-21 15:26:21 +0100

    docs: fiks navigasjon

--- ny-portal/src/app/(frontend)/layout.tsx
+++ ny-portal/src/app/(frontend)/layout.tsx
@@ -1,8 +1,9 @@
 import { Flex } from "@fremtind/jokul/components/flex";
-import { TabListener } from "@/components/TabListener";
 import Link from "next/link";
 import "./global.scss";
+import { NavigationMenuButton } from "./komponenter/navigation/NavigationMenuButton";
 import styles from "./layout.module.scss";
+import { TabListener } from "@/components/TabListener";
 
 interface Props {
     children: React.ReactNode;
@@ -22,11 +23,14 @@ export default function PortalLayout({ children }: Props) {
                 </Link>
                 <div className={styles.content}>
                     <Flex
+                        as="header"
                         gap={32}
+                        alignItems="center"
                         justifyContent="space-between"
                         className={styles.header}
                     >
                         <p className={styles.logo}>Jøkul</p>
+                        <NavigationMenuButton menuId="global-menu" />
                     </Flex>
                     <main>{children}</main>
                 </div>
