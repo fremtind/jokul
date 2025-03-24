4c24f9e1fdf3bc43cba26a62c78eb039dc731bf0
Kristian Ulvund <kristian.ulvund@fremtind.no>
2025-03-24 15:33:12 +0100

    feat: lag header i komponent side
    Lag header i komponent siden

--- ny-portal/src/app/(frontend)/komponenter/[slug]/page.tsx
+++ ny-portal/src/app/(frontend)/komponenter/[slug]/page.tsx
@@ -1,6 +1,6 @@
-import { PortableText } from "@/components/portable-text/PortableText";
 import { client } from "@/sanity/client";
 import { componentBySlugQuery } from "@/sanity/queries/component";
+import Component from "./component";
 
 type Props = {
     params: Promise<{ slug: string }>;
@@ -19,23 +19,5 @@ export default async function Page({ params }: Props) {
 
     const component = await client.fetch(componentBySlugQuery, { slug });
 
-    if (!component) {
-        return <h1>Ikke i databasen</h1>;
-    }
-
-    return (
-        <div>
-            <h1 className="jkl-title">
-                {component?.name || "ikke i databasen"}
-            </h1>
-
-            {component.short_description && (
-                <p>{component?.short_description}</p>
-            )}
-
-            {component.documentation_article && (
-                <PortableText blocks={component.documentation_article} />
-            )}
-        </div>
-    );
+    return <Component component={component} />;
 }
