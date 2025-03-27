a142c0864a4981ff4466ac5daa9fe4909ff8222b
mariaeilertsen <maria.eilertsen@fremtind.no>
2025-03-27 09:25:21 +0100

    fix: polymorphism test

--- packages/jokul/src/utilities/polymorphism/polymorphism.ts
+++ packages/jokul/src/utilities/polymorphism/polymorphism.ts
@@ -17,20 +17,29 @@ export type PolymorphicProps<
     > &
     ElementTypeProp<ElementType>;
 
-export type PolymorphicRef<As extends React.ElementType> = React.ComponentPropsWithRef<As>["ref"];
+export type PolymorphicRef<As extends React.ElementType> =
+    React.ComponentPropsWithRef<As>["ref"];
 
-export type PolymorphicComponentPropWithRef<As extends React.ElementType, Props = object> = PolymorphicComponentProp<
-    As,
-    Props
-> & { ref?: PolymorphicRef<As> };
+export type PolymorphicPropsWithRef<
+    ElementType extends React.ElementType,
+    Props = {},
+> = PolymorphicProps<ElementType, Props> & {
+    ref?: PolymorphicRef<ElementType>;
+} & ElementTypeProp<ElementType>;
+
+export type PolymorphicComponentPropWithRef<
+    As extends React.ElementType,
+    Props = object,
+> = PolymorphicComponentProp<As, Props> & { ref?: PolymorphicRef<As> };
 
 // Implementation of reusable polymorphic types
 // Explaination: https://blog.logrocket.com/build-strongly-typed-polymorphic-components-react-typescript/
 type AsProp<As extends React.ElementType> = { as?: As };
 type PropsToOmit<As extends React.ElementType, P> = keyof (AsProp<As> & P);
-type PolymorphicComponentProp<As extends React.ElementType, Props = object> = React.PropsWithChildren<
-    Props & AsProp<As>
-> &
+type PolymorphicComponentProp<
+    As extends React.ElementType,
+    Props = object,
+> = React.PropsWithChildren<Props & AsProp<As>> &
     Omit<React.ComponentPropsWithoutRef<As>, PropsToOmit<As, Props>>;
 
 // Les https://stackoverflow.com/q/57683303
