345c8617b7d7da60f8c36315847cd6fd03fa1cb1
Ole Jørgen <lmfaole@icloud.com>
2025-04-02 10:26:33 +0200

    refactor: server side component siden

--- ny-portal/src/app/(frontend)/komponenter/[slug]/page.tsx
+++ ny-portal/src/app/(frontend)/komponenter/[slug]/page.tsx
@@ -1,4 +1,9 @@
-import Component from "./component";
+import clsx from "clsx";
+import styles from "./component.module.scss";
+import { ComponentEmptyState } from "./components/ComponentEmptyState";
+import { ComponentFooter } from "./components/ComponentFooter";
+import { ComponentHeader } from "./components/ComponentHeader";
+import { PortableText } from "@/components/portable-text/PortableText";
 import { client } from "@/sanity/client";
 import { componentBySlugQuery } from "@/sanity/queries/component";
 
@@ -19,5 +24,34 @@ export default async function Page({ params }: Props) {
 
     const component = await client.fetch(componentBySlugQuery, { slug });
 
-    return <Component component={component} />;
+    if (!component) return null;
+
+    return (
+        <article>
+            <ComponentHeader
+                name={component?.name}
+                description={component?.short_description}
+                links={{
+                    figma: component?.external_links?.figma_link,
+                    storybook: component?.external_links?.storybook_link,
+                    github: component?.external_links?.github_link,
+                }}
+            />
+
+            {component.name && (
+                <div className={clsx("prose", styles.wrapper)}>
+                    {component.documentation_article ? (
+                        <>
+                            <PortableText
+                                blocks={component.documentation_article}
+                            />
+                            <ComponentFooter name={component.name} />
+                        </>
+                    ) : (
+                        <ComponentEmptyState name={component.name} />
+                    )}
+                </div>
+            )}
+        </article>
+    );
 }
