f63e1df6b8433c3d5cedb6528500e47c883f8e47
Pio Rasch-Halvorsen <pio.rasch-halvorsen@fremtind.no>
2025-04-03 13:21:35 +0200

    fix: la storybook-eksemplet hente url fra env-variabel

--- ny-portal/src/components/portable-text/storybook-story/Storybook.tsx
+++ ny-portal/src/components/portable-text/storybook-story/Storybook.tsx
@@ -1,12 +1,7 @@
 import "@fremtind/jokul/styles/components/link/_index.scss";
-import { backgroundOptions } from "@storybook-config/backgrounds";
-import { themeGlobal } from "@storybook-config/theme";
-import DOMPurify from "dompurify";
 import { PortableTextTypeComponentProps } from "next-sanity";
 import { type FC } from "react";
 import styles from "./storybook.module.scss";
-
-const STORYBOOK_BASE_URL = process.env.NEXT_PUBLIC_STORYBOOK_BASE_URL;
 
 type StorybookSchema = {
     stories: Array<{
@@ -15,27 +10,17 @@ type StorybookSchema = {
     }>;
 };
 
-const backgrounds = Object.entries(backgroundOptions).map(([key, option]) => ({
-    label: option.name,
-    value: key,
-}));
-
-const themes = themeGlobal.toolbar.items.map((item) => ({
-    label: item.title,
-    value: item.value || "undefined",
-}));
-
 export const Storybook: FC<PortableTextTypeComponentProps<StorybookSchema>> = ({
     value,
 }) => {
+    const STORYBOOK_URL = process.env.NEXT_PUBLIC_STORYBOOK_BASE_URL;
+
     return (
         <div className={styles.storybook}>
             <iframe
                 title={value.stories[0].storyName}
                 className={styles.story}
-                src={DOMPurify.sanitize(
-                    `${STORYBOOK_BASE_URL}/iframe.html?viewMode=story&id=${value.stories[0].storyId}&globals=theme:${themes[0].value};backgrounds.value:${backgrounds[1].value}`,
-                )}
+                src={`${STORYBOOK_URL}/iframe.html?viewMode=story&id=${value.stories[0].storyId}`}
             />
         </div>
     );
