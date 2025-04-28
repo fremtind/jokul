b8dd777189926609ee00d840d7e4d2dbc9f163f2
Pio Rasch-Halvorsen <pio.rasch-halvorsen@fremtind.no>
2025-04-28 14:10:27 +0200

    fix: rett opp i typer for portable text-blokker

--- ny-portal/src/components/portable-text/typography/Typography.tsx
+++ ny-portal/src/components/portable-text/typography/Typography.tsx
@@ -1,24 +1,18 @@
 import {
-    PortableTextComponentProps,
     PortableTextMarkComponentProps,
+    PortableTextTypeComponentProps,
 } from "next-sanity";
 import type { FC } from "react";
 import { CodeBlock as FTCodeBlock } from "../code-block";
 import styles from "./typography.module.scss";
 import { Jokul_codeBlock } from "@/sanity/types";
 
-type InlineCode = PortableTextMarkComponentProps<any>;
+export const InlineCode: FC<PortableTextMarkComponentProps<any>> = ({
+    children,
+}) => <code className={styles.inlineCode}>{children}</code>;
 
-export const InlineCode: FC<InlineCode> = ({ children }) => (
-    <code className={styles.inlineCode}>{children}</code>
+export const CodeBlock: React.FC<
+    PortableTextTypeComponentProps<Jokul_codeBlock>
+> = ({ value }) => (
+    <FTCodeBlock language={value.language}>{value.code || ""}</FTCodeBlock>
 );
-
-type CodeBlockProps = PortableTextComponentProps<Jokul_codeBlock>;
-
-export const CodeBlock: React.FC<CodeBlockProps> = (props) => {
-    const { value } = props;
-
-    return (
-        <FTCodeBlock language={value.language}>{value.code || ""}</FTCodeBlock>
-    );
-};
