1b1cacd4cdb3c165730b1641072c2349aad5b17a
Ole Jørgen <lmfaole@icloud.com>
2025-04-30 14:36:50 +0200

    feat: ny generell document i sanity
    ISSUES CLOSED: #4778

---
+++ ny-portal/src/sanity/schemas/blogPost.ts
@@ -0,0 +1,35 @@
+import { defineField, defineType } from "sanity";
+
+const MAX_LENGTH = 70;
+
+export const blogPost = defineType({
+    name: "jokul_blog_post",
+    title: "Bloggartikkel",
+    type: "document",
+    fields: [
+        defineField({
+            name: "name",
+            title: "Navn",
+            type: "string",
+            validation: (Rule) => Rule.required(),
+        }),
+        defineField({
+            name: "slug",
+            title: "Slug",
+            type: "slug",
+            options: {
+                source: "name",
+                maxLength: MAX_LENGTH,
+                slugify: (input) =>
+                    input.toLowerCase().replace(/\s+/g, "-").slice(0, 200),
+            },
+            validation: (Rule) => Rule.required(),
+        }),
+        defineField({
+            name: "article",
+            title: "Artikkel",
+            type: "array",
+            of: [{ type: "block" }, { type: "image" }],
+        }),
+    ],
+});
