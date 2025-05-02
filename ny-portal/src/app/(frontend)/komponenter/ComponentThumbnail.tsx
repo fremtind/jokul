2713dafec50dffe46aa8f4fea6bd3f8d6e1c14b3
Pio Rasch-Halvorsen <pio.rasch-halvorsen@fremtind.no>
2025-05-02 12:19:32 +0200

    feat: bruk card for thumbnails i komponentoversikten
    closes #4781

---
+++ ny-portal/src/app/(frontend)/komponenter/ComponentThumbnail.tsx
@@ -0,0 +1,23 @@
+"use client";
+
+import { Card, CardImage } from "@fremtind/jokul";
+import styles from "./komponenter.module.scss";
+
+type Props = {
+    lightImage: string;
+    darkImage: string;
+};
+
+export function ComponentThumbnail({ darkImage, lightImage }: Props) {
+    return (
+        <Card variant="high" className={styles.image}>
+            <picture>
+                <source
+                    media="(prefers-color-scheme: dark)"
+                    srcSet={darkImage}
+                />
+                <CardImage as="img" placement="top" src={lightImage} alt="" />
+            </picture>
+        </Card>
+    );
+}
