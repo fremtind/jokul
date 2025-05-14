bffbfab0afd8388ab70ca7dca8f41dd268f79a92
OJ <olejorgenbakken@gmail.com>
2025-05-14 12:02:35 +0200

    feat: gjør det mulig å endre density i storybook
    ISSUES CLOSED: #4846

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -1,18 +1,21 @@
 import type { Preview } from "@storybook/react";
 import { backgroundOptions } from "./backgrounds.js";
 import { themeDecorator, themeGlobal, themes } from "./theme.js";
+import { densityDecorator, densityGlobal, densities } from "./density.js";
 import "../packages/jokul/src/components/card/styles/_index.scss";
 import "./global.scss";
 
 const preview: Preview = {
     globalTypes: {
         theme: themeGlobal,
+        density: densityGlobal,
     },
     initialGlobals: {
         theme: themes[0], // Automatisk dark/light
+        density: densities[0],
         backgrounds: { value: "pageVariant" },
     },
-    decorators: [themeDecorator],
+    decorators: [themeDecorator, densityDecorator],
     parameters: {
         actions: { argTypesRegex: "^on.*" },
         backgrounds: {
