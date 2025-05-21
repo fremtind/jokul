9e10be5420d9e5e79c627d2da8ae6e2f6d0cb200
Kristian Ulvund <kristian.ulvund@fremtind.no>
2025-05-21 14:25:32 +0200

    feat: slett gammel portal relaterte filer

--- packages/jokul/src/components/toast/development/ToastExample.tsx
+++ packages/jokul/src/components/toast/development/ToastExample.tsx
@@ -1,90 +1,96 @@
-import React, { FC, useState } from "react";
-import {
-    ExampleComponentProps,
-    ExampleKnobsProps,
+import React, { type FC, useState } from "react";
+import type {
+	ExampleComponentProps,
+	ExampleKnobsProps,
 } from "utils/dev-example/index.js";
 import { PrimaryButton, SecondaryButton } from "../../button/Button.js";
 import { Flex } from "../../flex/Flex.js";
-import { ToastContent, ToastProvider, useToast } from "../index.js";
-import { ToastOptions } from "../types.js";
+import { type ToastContent, ToastProvider, useToast } from "../index.js";
+import type { ToastOptions } from "../types.js";
 
 export const toastExampleKnobs: ExampleKnobsProps = {
-    boolProps: [],
-    choiceProps: [
-        {
-            name: "Plassering",
-            values: ["Sentrert", "Venstre"],
-            defaultValue: 0,
-        },
-    ],
+	boolProps: [],
+	choiceProps: [
+		{
+			name: "Plassering",
+			values: ["Sentrert", "Venstre"],
+			defaultValue: 0,
+		},
+	],
 };
 
 const examples: Array<[ToastContent, ToastOptions]> = [
-    [
-        {
-            title: "Nytt i Jøkul",
-            content: (
-                <p className="jkl-body">
-                    Les mer om{" "}
-                    <a className="jkl-link" href="/komponenter/toast/">
-                        Toast
-                    </a>{" "}
-                    på komponentsiden.
-                </p>
-            ),
-        },
-        {},
-    ],
-    [{ content: "Saken går til automatisk behandling" }, { variant: "info" }],
-    [
-        { content: "Navn Navnesen er fjernet som reell rettighetshaver" },
-        { variant: "success" },
-    ],
-    [
-        {
-            title: "Mistet internettforbindelsen",
-            content:
-                "Du kan fortsette å fylle ut skjemaet, men får ikke lagret før forbindelsen er tilbake.",
-        },
-        { variant: "warning" },
-    ],
-    [
-        {
-            title: "Ingen internettforbindelse",
-            content: "Du får ikke lagret før forbindelsen er tilbake.",
-        },
-        { variant: "error" },
-    ],
+	[
+		{
+			title: "Nytt i Jøkul",
+			content: (
+				<p className="jkl-body">
+					Les mer om{" "}
+					<a className="jkl-link" href="/komponenter/toast/">
+						Toast
+					</a>{" "}
+					på komponentsiden.
+				</p>
+			),
+		},
+		{},
+	],
+	[{ content: "Saken går til automatisk behandling" }, { variant: "info" }],
+	[
+		{ content: "Navn Navnesen er fjernet som reell rettighetshaver" },
+		{ variant: "success" },
+	],
+	[
+		{
+			title: "Mistet internettforbindelsen",
+			content:
+				"Du kan fortsette å fylle ut skjemaet, men får ikke lagret før forbindelsen er tilbake.",
+		},
+		{ variant: "warning" },
+	],
+	[
+		{
+			title: "Ingen internettforbindelse",
+			content: "Du får ikke lagret før forbindelsen er tilbake.",
+		},
+		{ variant: "error" },
+	],
 ];
 
 function ToastUsageExample() {
-    const { add, close } = useToast();
-    const [keys, setKeys] = useState<string[]>([]);
+	const { add, close } = useToast();
+	const [keys, setKeys] = useState<string[]>([]);
 
-    return (
-        <Flex direction="column" rowGap={24}>
-            <PrimaryButton
-                onClick={() => {
-                    const [content, options] =
-                        examples[Math.floor(Math.random() * examples.length)];
-                    setKeys([...keys, add(content, options)]);
-                }}
-            >
-                Vis toast i kontekst
-            </PrimaryButton>
-            <SecondaryButton onClick={() => keys.forEach((key) => close(key))}>
-                Lukk alle åpne toasts
-            </SecondaryButton>
-        </Flex>
-    );
+	return (
+		<Flex direction="column" rowGap={24}>
+			<PrimaryButton
+				onClick={() => {
+					const [content, options] =
+						examples[Math.floor(Math.random() * examples.length)];
+					setKeys([...keys, add(content, options)]);
+				}}
+			>
+				Vis toast i kontekst
+			</PrimaryButton>
+			<SecondaryButton
+				onClick={() => {
+					for (const key of keys) {
+						close(key);
+					}
+				}}
+			>
+				Lukk alle åpne toasts
+			</SecondaryButton>
+		</Flex>
+	);
 }
 
 export const ToastExample: FC<ExampleComponentProps> = ({ choiceValues }) => {
-    const placement =
-        choiceValues?.["Plassering"] === "Venstre" ? "left" : "center";
-    return (
-        <ToastProvider placement={placement}>
-            <ToastUsageExample />
-        </ToastProvider>
-    );
+	const placement =
+		choiceValues?.["Plassering"] === "Venstre" ? "left" : "center";
+	return (
+		<ToastProvider placement={placement}>
+			<ToastUsageExample />
+		</ToastProvider>
+	);
 };
