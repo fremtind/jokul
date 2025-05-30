170d5015181d6aace33c60823d6e5ad319bd58f2
Ole Jørgen <37940048+lmfaole@users.noreply.github.com>
2025-05-30 12:05:10 +0200

    Update packages/button/button.scss
    Co-authored-by: Pio Rasch-Halvorsen <pio.rasch-halvorsen@fremtind.no>

--- packages/button-react/src/Button.tsx
+++ packages/button-react/src/Button.tsx
@@ -6,114 +6,118 @@ import React, { ButtonHTMLAttributes } from "react";
 import type { ButtonComponent, ButtonProps } from "./types";
 
 export const Button = React.forwardRef(function Button<
-	ElementType extends React.ElementType = "button",
+    ElementType extends React.ElementType = "button",
 >(props: ButtonProps<ElementType>, ref?: PolymorphicRef<ElementType>) {
-	const {
-		as = "button",
-		children,
-		className,
-		density,
-		onTouchStart,
-		onAnimationEnd,
-		loader,
-		icon,
-		iconPosition = "left",
-		iconLeft,
-		iconRight,
-		variant = "secondary",
-		...rest
-	} = props;
+    const {
+        as = "button",
+        children,
+        className,
+        density,
+        onTouchStart,
+        onAnimationEnd,
+        loader,
+        icon,
+        iconPosition = "left",
+        iconLeft,
+        iconRight,
+        variant = "secondary",
+        ...rest
+    } = props;
 
-	const Component = as;
+    const Component = as;
 
-	if (
-		process.env.NODE_ENV !== "production" &&
-		React.Children.count(children) === 0 &&
-		!props["aria-label"] &&
-		!props["aria-labelledby"] &&
-		!props.title
-	) {
-		console.warn(
-			"Når du lager en Button uten synlig tekst må du huske å gi den et navn med enten aria-label, aria-labelledby eller title",
-		);
-	}
+    if (
+        process.env.NODE_ENV !== "production" &&
+        React.Children.count(children) === 0 &&
+        !props["aria-label"] &&
+        !props["aria-labelledby"] &&
+        !props.title
+    ) {
+        console.warn(
+            "Når du lager en Button uten synlig tekst må du huske å gi den et navn med enten aria-label, aria-labelledby eller title",
+        );
+    }
 
-	const ariaLive = useAriaLiveRegion(loader?.showLoader);
-	const showLoader = Boolean(children) && Boolean(loader?.showLoader);
+    const ariaLive = useAriaLiveRegion(loader?.showLoader);
+    const showLoader = Boolean(children) && Boolean(loader?.showLoader);
 
-	return (
-		<Component
-			{...ariaLive}
-			data-loading={showLoader}
-			data-density={density}
-			className={cn("jkl-button", "jkl-button--" + variant, className)}
-			disabled={as === "button" ? loader?.showLoader : undefined}
-			{...rest}
-			ref={ref}
-		>
-			<div className="jkl-button__label">
-				{iconLeft && iconLeft}
-				{icon && iconPosition === "left" && icon}
-				{children && <span className="jkl-button__text">{children}</span>}
-				{iconRight && iconRight}
-				{icon && iconPosition === "right" && icon}
-			</div>
+    return (
+        <Component
+            {...ariaLive}
+            data-loading={showLoader}
+            data-density={density}
+            className={cn("jkl-button", "jkl-button--" + variant, className)}
+            disabled={as === "button" ? loader?.showLoader : undefined}
+            {...rest}
+            ref={ref}
+        >
+            <div className="jkl-button__label">
+                {iconLeft && iconLeft}
+                {icon && iconPosition === "left" && icon}
+                {children && (
+                    <span className="jkl-button__text">{children}</span>
+                )}
+                {iconRight && iconRight}
+                {icon && iconPosition === "right" && icon}
+            </div>
 
-			{children && (
-				<Loader
-					className="jkl-button__loader"
-					variant="medium"
-					textDescription={loader?.textDescription || "Vennligst vent"}
-					aria-hidden={!loader?.showLoader}
-				/>
-			)}
-		</Component>
-	);
+            {children && (
+                <Loader
+                    className="jkl-button__loader"
+                    variant="medium"
+                    textDescription={
+                        loader?.textDescription || "Vennligst vent"
+                    }
+                    aria-hidden={!loader?.showLoader}
+                />
+            )}
+        </Component>
+    );
 }) as ButtonComponent;
 
 export function PrimaryButton<ElementType extends React.ElementType = "button">(
-	props: Omit<ButtonProps<ElementType>, "variant" | "onClick" | "as"> &
-		Pick<ButtonHTMLAttributes<HTMLButtonElement>, "onClick">,
+    props: Omit<ButtonProps<ElementType>, "variant" | "onClick" | "as"> &
+        Pick<ButtonHTMLAttributes<HTMLButtonElement>, "onClick">,
 ) {
-	const buttonProps = {
-		...props,
-		variant: "primary",
-	} as ButtonProps<ElementType>;
-	return <Button {...buttonProps} />;
+    const buttonProps = {
+        ...props,
+        variant: "primary",
+    } as ButtonProps<ElementType>;
+    return <Button {...buttonProps} />;
 }
 
 export function SecondaryButton<
-	ElementType extends React.ElementType = "button",
+    ElementType extends React.ElementType = "button",
 >(
-	props: Omit<ButtonProps<ElementType>, "variant" | "onClick" | "as"> &
-		Pick<ButtonHTMLAttributes<HTMLButtonElement>, "onClick">,
+    props: Omit<ButtonProps<ElementType>, "variant" | "onClick" | "as"> &
+        Pick<ButtonHTMLAttributes<HTMLButtonElement>, "onClick">,
 ) {
-	const buttonProps = {
-		...props,
-		variant: "secondary",
-	} as ButtonProps<ElementType>;
-	return <Button {...buttonProps} />;
+    const buttonProps = {
+        ...props,
+        variant: "secondary",
+    } as ButtonProps<ElementType>;
+    return <Button {...buttonProps} />;
 }
 
 export function TertiaryButton<
-	ElementType extends React.ElementType = "button",
+    ElementType extends React.ElementType = "button",
 >(
-	props: Omit<ButtonProps<ElementType>, "variant" | "onClick" | "as"> &
-		Pick<ButtonHTMLAttributes<HTMLButtonElement>, "onClick">,
+    props: Omit<ButtonProps<ElementType>, "variant" | "onClick" | "as"> &
+        Pick<ButtonHTMLAttributes<HTMLButtonElement>, "onClick">,
 ) {
-	const buttonProps = {
-		...props,
-		variant: "tertiary",
-	} as ButtonProps<ElementType>;
-	return <Button {...buttonProps} />;
+    const buttonProps = {
+        ...props,
+        variant: "tertiary",
+    } as ButtonProps<ElementType>;
+    return <Button {...buttonProps} />;
 }
 
 export function GhostButton<ElementType extends React.ElementType = "button">(
-	props: Omit<ButtonProps<ElementType>, "variant" | "loader">,
+    props: Omit<ButtonProps<ElementType>, "variant" | "loader">,
 ) {
-	const buttonProps = {
-		...props,
-		variant: "ghost",
-	} as ButtonProps<ElementType>;
-	return <Button {...buttonProps} />;
+    const buttonProps = {
+        ...props,
+        variant: "ghost",
+    } as ButtonProps<ElementType>;
+    return <Button {...buttonProps} />;
 }
