1ab13a89e27c6694e070db578a189704b04805fe
Pio Rasch-Halvorsen <pio.rasch-halvorsen@fremtind.no>
2025-06-22 21:43:22 +0200

    feat: legg til layout-komponent two-column

---
+++ packages/jokul/src/layout/two-column/TwoColumn.tsx
@@ -0,0 +1,61 @@
+import clsx from "clsx";
+import React from "react";
+import { tokens } from "../../core/index.js";
+import type {
+    PolymorphicPropsWithRef,
+    PolymorphicRef,
+} from "../../utilities/index.js";
+
+type Props = {
+    /**
+     * En CSS-lengde som definerer den minste bredden innholdet kan ha
+     * før layouten brekker ned til en kolonne. Som standard er den satt
+     * til 50 %, som vil si at sidebar-en aldri vil ta opp mer enn halve
+     * bredden av skjermen.
+     * @default "50%"
+     */
+    minContentWidth?: string;
+    /**
+     * Du kan velge å legge inn avstand mellom sidepanel og innholdet, men
+     * vi anbefaler å heller bruke andre layout-komponenter med padding
+     * når du bygger sidepanel og innhold for å oppnå ønsket avstand.
+     * @default undefined
+     */
+    gap?: keyof typeof tokens.unit;
+};
+
+export type TwoColumnProps<ElementType extends React.ElementType> =
+    PolymorphicPropsWithRef<ElementType, Props> & Props;
+
+type TwoColumnComponent = <ElementType extends React.ElementType = "div">(
+    props: TwoColumnProps<ElementType>,
+) => React.ReactElement | null;
+
+export const TwoColumn = React.forwardRef(function TwoColumn<
+    ElementType extends React.ElementType = "div",
+>(props: TwoColumnProps<ElementType>, ref?: PolymorphicRef<ElementType>) {
+    const {
+        as = "div",
+        className,
+        minContentWidth = "50%",
+        gap,
+        style,
+        ...componentProps
+    } = props;
+
+    const Component = as;
+
+    return (
+        <Component
+            data-testid="jkl-two-column"
+            className={clsx("jkl-two-column", className)}
+            style={{
+                ...style,
+                "--min-content-width": minContentWidth,
+                "--gap": gap ? tokens.unit[gap] : 0,
+            }}
+            {...componentProps}
+            ref={ref}
+        />
+    );
+}) as TwoColumnComponent;
---
+++ packages/jokul/src/layout/two-column/stories/TwoColumn.stories.tsx
@@ -0,0 +1,96 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import { Button } from "../../../components/button/Button.jsx";
+import { tokens } from "../../../core/index.js";
+import { Column } from "../../column/Column.jsx";
+import { Prose } from "../../prose/Prose.jsx";
+import { TwoColumn } from "../TwoColumn.jsx";
+
+import "../../../components/button/styles/_index.scss";
+import "../../prose/styles/_index.scss";
+import "../../column/styles/_index.scss";
+import "../styles/_index.scss";
+
+const meta = {
+    title: "Layout/Primitives/TwoColumn",
+    component: TwoColumn,
+    tags: ["autodocs"],
+    argTypes: {
+        minContentWidth: {
+            table: {
+                defaultValue: {
+                    summary: "50%",
+                },
+            },
+            options: ["50%", "60%", "70%", "80%", "90%"],
+            control: {
+                type: "select",
+            },
+        },
+        gap: {
+            table: {
+                defaultValue: { summary: "undefined" },
+            },
+            options: Object.keys(tokens.unit),
+            control: {
+                type: "select",
+            },
+        },
+        ref: {
+            table: { disable: true },
+        },
+        as: {
+            table: { disable: true },
+        },
+        className: {
+            table: { disable: true },
+        },
+        style: {
+            table: { disable: true },
+        },
+    },
+    args: {
+        minContentWidth: "50%",
+        gap: undefined,
+    },
+} satisfies Meta<typeof TwoColumn>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Demo: Story = {
+    parameters: {
+        layout: "fullscreen",
+    },
+    render: (props) => {
+        return (
+            <TwoColumn {...props}>
+                {/* TODO: Lag bedre innhold for Sidebar */}
+                <div
+                    style={{
+                        border: "2px dashed var(--jkl-color-border-subdued)",
+                        minWidth: "200px",
+                        paddingInline: tokens.unit[40],
+                    }}
+                >
+                    Sidebar
+                </div>
+                <Column as={Prose}>
+                    <h2 className="jkl-heading-2">Lag en commit</h2>
+                    <p>
+                        For å dokumentere endringer på en måte som automatiserer
+                        versjonering og generering av changelogs, bruker vi
+                        verktøyet Changesets.
+                    </p>
+
+                    <p>
+                        Changesets lar oss definere nøyaktig hvilke pakker som
+                        er påvirket av en endring og hva slags
+                        versjonsoppdatering (major, minor, eller patch)
+                        endringen medfører.
+                    </p>
+                    <Button>Gå videre</Button>
+                </Column>
+            </TwoColumn>
+        );
+    },
+};
