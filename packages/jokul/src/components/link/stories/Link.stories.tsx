d693f6cc33e763cc597d6aa98bbdc5253302cf90
OJ <olejorgenbakken@gmail.com>
2025-06-24 15:48:13 +0200

    feat: endret stil på lenker
    ISSUES CLOSED: #5005, #4977

--- packages/jokul/src/components/link/stories/Link.stories.tsx
+++ packages/jokul/src/components/link/stories/Link.stories.tsx
@@ -1,7 +1,8 @@
-import type { Meta, StoryObj } from "@storybook/react";
+import type {Meta, StoryObj} from "@storybook/react";
 import React from "react";
-import { Link } from "../Link.js";
+import {Link} from "../Link.js";
 import "../styles/_index.scss";
+import {Flex} from "../../flex/index.js";
 
 const meta: Meta = {
     title: "Komponenter/Link",
@@ -25,7 +26,7 @@ type Story = StoryObj<typeof Link>;
 export const LinkStory: Story = {
     name: "Link",
     args: {
-        children: "Example Link",
+        children: "Lenke",
         external: false,
         as: "a",
         target: "#",
@@ -34,5 +35,95 @@ export const LinkStory: Story = {
     render: (props) => (
         // Setter style.cursor til pointer fordi Storybook overskriver default styles.
         <Link style={{ cursor: "pointer" }} {...props} as={props.as || "a"} />
+    ),
+};
+
+export const LinkInParagraphStory: Story = {
+    name: "Mønster: Lenke i avsnitt",
+    args: {
+        href: "https://www.fremtind.no",
+    },
+    render: (args) => (
+        // Setter style.cursor til pointer fordi Storybook overskriver default styles.
+        <p style={{maxWidth: "45ch"}}>
+            Vi bruker lenker for å lede brukeren til{" "}
+            <Link {...args} external={false} style={{cursor: "pointer"}}>
+                andre nettsider
+            </Link>
+            , eller til andre steder på samme nettside.{" "}
+            <Link
+                {...args}
+                external={true}
+                target={"_blank"}
+                style={{cursor: "pointer"}}
+            >
+                Lenker til eksterne nettsider
+            </Link>{" "}
+            markeres med en pil opp og til høyre etter lenketeksten.
+        </p>
+    ),
+};
+
+export const LinkInOtherStory: Story = {
+    name: "Mønster: Lenke i andre tekststiler",
+    args: {
+        children: "lenke",
+        external: false,
+        as: "a",
+        target: "#",
+        href: "https://www.fremtind.no",
+    },
+    render: (args) => (
+        // Setter style.cursor til pointer fordi Storybook overskriver default styles.
+        <Flex direction={"column"} style={{gap: "1.5lh"}}>
+            <h1 className={"jkl-title"} style={{maxWidth: "55ch"}}>
+                Du står fritt til å bruke{" "}
+                <Link {...args} style={{cursor: "pointer"}}/> i titler
+            </h1>
+
+            <h2 className={"jkl-title-small"} style={{maxWidth: "55ch"}}>
+                Du står fritt til å bruke{" "}
+                <Link {...args} style={{cursor: "pointer"}}/> i små titler
+            </h2>
+
+            <hr style={{width: "100%", opacity: "0.1"}}/>
+
+            <h2 className={"jkl-heading-1"} style={{maxWidth: "55ch"}}>
+                Du står fritt til å bruke{" "}
+                <Link {...args} style={{cursor: "pointer"}}/> i overskrifter
+            </h2>
+
+            <h2 className={"jkl-heading-2"} style={{maxWidth: "55ch"}}>
+                Du står fritt til å bruke{" "}
+                <Link {...args} style={{cursor: "pointer"}}/> i overskrifter
+            </h2>
+
+            <h3 className={"jkl-heading-3"} style={{maxWidth: "55ch"}}>
+                Du står fritt til å bruke{" "}
+                <Link {...args} style={{cursor: "pointer"}}/> i overskrifter
+            </h3>
+
+            <h4 className={"jkl-heading-4"} style={{maxWidth: "55ch"}}>
+                Du står fritt til å bruke{" "}
+                <Link {...args} style={{cursor: "pointer"}}/> i overskrifter
+            </h4>
+
+            <h5 className={"jkl-heading-5"} style={{maxWidth: "55ch"}}>
+                Du står fritt til å bruke{" "}
+                <Link {...args} style={{cursor: "pointer"}}/> i overskrifter
+            </h5>
+
+            <hr style={{width: "100%", opacity: "0.1"}}/>
+
+            <p className={"jkl-body"} style={{maxWidth: "55ch"}}>
+                Du står fritt til å bruke{" "}
+                <Link {...args} style={{cursor: "pointer"}}/> i avsnitt
+            </p>
+
+            <small className={"jkl-small"} style={{maxWidth: "55ch"}}>
+                Du står fritt til å bruke{" "}
+                <Link {...args} style={{cursor: "pointer"}}/> i små avsnitt
+            </small>
+        </Flex>
     ),
 };
