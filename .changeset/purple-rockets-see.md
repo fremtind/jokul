ea63eb58dbad3e99d24a26588f3e703ef133f2d6
Pio Rasch-Halvorsen <pio.rasch-halvorsen@fremtind.no>
2025-06-26 12:05:56 +0200

    fix: litt bedre vinsing av artikler i portalen

---
+++ .changeset/purple-rockets-see.md
@@ -0,0 +1,5 @@
+---
+"portal": patch
+---
+
+Artiklene i portalen (under punktet "Artikler") vises nå uten omriss, og med litt bedre kontroll over størrelse og avstand mellom hver artikkel i listen.
--- portal/src/app/(frontend)/blog/ArticleCard.tsx
+++ portal/src/app/(frontend)/blog/ArticleCard.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import type { BlogPostsQueryResult } from "@/sanity/types";
-import { Card } from "@fremtind/jokul/components/card";
 import { Flex } from "@fremtind/jokul/components/flex";
 import { Link } from "@fremtind/jokul/components/link";
 import { formatDate } from "@fremtind/jokul/utilities";
@@ -11,28 +10,24 @@ import styles from "./blog.module.scss";
 
 export const ArticleCard = (post: BlogPostsQueryResult[number]) => {
     return (
-        <Card
-            asChild
-            variant="outlined"
-            padding="l"
+        <Flex
             className={styles.articleCard}
+            as="article"
+            gap={16}
+            direction="column"
         >
-            <Flex as="article" gap={16} direction="column">
-                <Flex direction="column" gap={12}>
-                    <h2 className="jkl-heading-1">
-                        <Link
-                            as={NextLink}
-                            href={`/blog/${post.slug?.current || ""}`}
-                        >
-                            {post.name}
-                        </Link>
-                    </h2>
-                    <p className={styles.date}>
-                        {formatDate(new Date(post.date))}
-                    </p>
-                </Flex>
-                <p className="jkl-body">{post.short_description}</p>
+            <Flex direction="column" gap={12}>
+                <h2 className="jkl-heading-1">
+                    <Link
+                        as={NextLink}
+                        href={`/blog/${post.slug?.current || ""}`}
+                    >
+                        {post.name}
+                    </Link>
+                </h2>
+                <p className={styles.date}>{formatDate(new Date(post.date))}</p>
             </Flex>
-        </Card>
+            <p className="jkl-body">{post.short_description}</p>
+        </Flex>
     );
 };
