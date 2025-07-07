86e0192285b2088c120a4ff02369b0486722cc6f
Pio Rasch-Halvorsen <pio.rasch-halvorsen@fremtind.no>
2025-07-07 12:18:34 +0200

    chore!: fjern accordion-komponenten
    Fjerner komponentene `Accordion` og `AccordionItem`, som har vært merket som
    deprecated ganske lenge. I stedet for disse komponentene kan man bruke
    `ExpandablePanel` og `ExpandablePanel.Content`.

--- packages/jokul/src/components/accordion/development/Example.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import { DevExample } from "utils/dev-example/DevExample.js";
-import { AccordionExample, knobs } from "./AccordionExample.js";
-
-export default function Example() {
-    return <DevExample component={AccordionExample} knobs={knobs} />;
-}
