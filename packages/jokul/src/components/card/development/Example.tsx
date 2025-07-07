0d38c23df8284cea95466dda8942c654143fd61c
Pio Rasch-Halvorsen <pio.rasch-halvorsen@fremtind.no>
2025-07-07 12:21:27 +0200

    chore!: fjern gamle kortkomponenter
    Fjerner komponentene `NavCard`, `InfoCard` og `TaskCard`, som har vært
    merket som deprecated ganske lenge. I stedet for disse komponentene kan
    man bygge opp tilsvarende kort ved hjelp av den generelle
    `Card`-komponenten, `CardImage`-komponenten, og andre Jøkul-komponenter.
    Se eksemplene i portalen/Storybook for hvordan dette kan gjøres.

--- packages/jokul/src/components/card/development/Example.tsx
+++ packages/jokul/src/components/card/development/Example.tsx
@@ -1,24 +1,12 @@
-import React from "react";
 import { DevExample } from "utils/dev-example/DevExample.js";
 import {
     FakturainfoExample,
     fakturainfoExampleProps,
 } from "./examples/FakturainfoExample.js";
-import { InfoCardExample } from "./examples/InfoCardExample.js";
-import { NavCardExample } from "./examples/NavCardExample.js";
 import {
     StatuskortExample,
     statuskortExampleProps,
 } from "./examples/StatuskortExample.js";
-import { TaskCardExample } from "./examples/TaskCardExample.js";
-import {
-    infoCardExampleProps,
-    navCardExampleProps,
-    taskCardExampleProps,
-} from "./examples/cardExampleProps.js";
-
-import "./info-card-example.scss";
-import "./task-card-example.scss";
 
 export default function Example() {
     return (
@@ -32,21 +20,6 @@ export default function Example() {
                 title="Informasjon om forfalt faktura"
                 knobs={fakturainfoExampleProps}
                 component={FakturainfoExample}
-            />
-            <DevExample
-                title="NavCard"
-                knobs={navCardExampleProps}
-                component={NavCardExample}
-            />
-            <DevExample
-                title="InfoCard"
-                knobs={infoCardExampleProps}
-                component={InfoCardExample}
-            />
-            <DevExample
-                title="TaskCard"
-                knobs={taskCardExampleProps}
-                component={TaskCardExample}
             />
         </>
     );
