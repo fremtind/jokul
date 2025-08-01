c967b01367a37ac794ca47c3f4d35d8d55b44538
OJ <olejorgenbakken@gmail.com>
2025-08-01 08:32:18 +0200

    stories for table

--- packages/jokul/src/components/table/stories/table.stories.tsx
+++ packages/jokul/src/components/table/stories/table.stories.tsx
@@ -1,17 +1,31 @@
 import type { Meta, StoryObj } from "@storybook/react";
-import React from "react";
+import React, { useState } from "react";
 import { Table } from "../Table.js";
 import { TableBody } from "../TableBody.js";
 import { TableCaption } from "../TableCaption.js";
 import { TableCell } from "../TableCell.js";
 import { TableHead } from "../TableHead.js";
-import { TableHeader } from "../TableHeader.js";
+import { TableHeader, type TableSortDirection } from "../TableHeader.js";
 import { TableRow } from "../TableRow.js";
+import { columns } from "../development/paginated-table-data.js";
+import { useSortableTableHeader } from "../utils.js";
+import { faktura, skadesaker } from "./data.js";
+
 import "../styles/_index.scss";
+import "../../description-list/styles/_index.scss";
+import "../../link/styles/_index.scss";
 
 const meta = {
     title: "Komponenter/Table",
     component: Table,
+    subcomponents: {
+        TableCaption,
+        TableHead,
+        TableHeader,
+        TableBody,
+        TableRow,
+        TableCell,
+    },
     parameters: {
         layout: "centered",
     },
@@ -21,52 +35,22 @@ const meta = {
 export default meta;
 type Story = StoryObj<typeof meta>;
 
-const columns = [
-    "Dato",
-    "Saksnummer",
-    "Kundenummer",
-    "Kundenavn",
-    "Milepæl",
-    "Følger saken",
-];
-
-const rows = [
-    [
-        "24.02.2020",
-        "20-1234567",
-        "010203 99887",
-        "Ola Nordmann",
-        "Opprettet",
-        "Siri Saksbehandler",
-    ],
-    [
-        "13.04.2019",
-        "20-8382811",
-        "010203 99887",
-        "Kari Nordkvinne",
-        "Opprettet",
-        "Siri Saksbehandler",
-    ],
-    [
-        "31.07.2017",
-        "20-1111",
-        "010203 99887",
-        "Kari Nordkvinne",
-        "Opprettet",
-        "Per Persen",
-    ],
-];
-
-export const TableComponent: Story = {
+export const TableStory: Story = {
+    name: "Table",
     args: {
+        collapseToList: true,
+        fullWidth: true,
         caption: (
             <TableCaption srOnly>Overskrift for skjermlesere</TableCaption>
         ),
-        children: (
-            <>
-                <TableHead>
+        children: "",
+    },
+    render: (args) => (
+        <div style={{ width: "90dvw" }}>
+            <Table {...args}>
+                <TableHead sticky={true}>
                     <TableRow>
-                        {columns.map((column, index) => (
+                        {skadesaker.columns.map((column, index) => (
                             <TableHeader key={index} bold>
                                 {column}
                             </TableHeader>
@@ -74,12 +58,12 @@ export const TableComponent: Story = {
                     </TableRow>
                 </TableHead>
                 <TableBody>
-                    {rows.map((row, rowIndex) => (
+                    {skadesaker.rows.map((row, rowIndex) => (
                         <TableRow key={rowIndex}>
                             {row.map((cell, cellIndex) => (
                                 <TableCell
                                     key={cellIndex}
-                                    data-th={columns[cellIndex]}
+                                    data-th={skadesaker.columns[cellIndex]}
                                     align={
                                         [1, 2].includes(cellIndex)
                                             ? "right"
@@ -92,12 +76,98 @@ export const TableComponent: Story = {
                         </TableRow>
                     ))}
                 </TableBody>
-            </>
-        ),
-    },
-    render: (args) => (
-        <Table caption={args.caption} fullWidth>
-            {args.children}
-        </Table>
+            </Table>
+        </div>
     ),
+};
+
+export const StortableTable: Story = {
+    name: "useSortableTableHeader",
+    args: {
+        caption: (
+            <TableCaption srOnly>Overskrift for skjermlesere</TableCaption>
+        ),
+        children: "",
+    },
+    render: (args) => {
+        const [sortBy, setSortBy] = useState(columns[0]);
+        const [direction, setDirection] = useState<TableSortDirection>("desc");
+
+        const handleSortChange = (
+            sortKey: string,
+            sortDirection: TableSortDirection,
+        ) => {
+            setSortBy(sortKey);
+            setDirection(sortDirection);
+        };
+
+        const { getSortProps } = useSortableTableHeader(
+            sortBy,
+            direction,
+            handleSortChange,
+        );
+
+        return (
+            <Table caption={args.caption} fullWidth>
+                <TableHead sticky={true}>
+                    <TableRow>
+                        {faktura.columns.slice(0, 4).map((column, index) => (
+                            <TableHeader
+                                key={index}
+                                bold
+                                {...getSortProps(column)}
+                            >
+                                {column}
+                            </TableHeader>
+                        ))}
+                    </TableRow>
+                </TableHead>
+                <TableBody>
+                    {faktura.rows
+                        .sort((a, b) => {
+                            if (sortBy === "Forfallsdato") {
+                                const timeA = new Date(a[0] as Date).getTime();
+                                const timeB = new Date(b[0] as Date).getTime();
+
+                                if (direction === "asc") {
+                                    return timeA - timeB;
+                                }
+
+                                return timeB - timeA;
+                            }
+
+                            if (sortBy === "Beløp") {
+                                const numberA = Number(a[2]);
+                                const numberB = Number(b[2]);
+
+                                if (direction === "asc") {
+                                    return numberA - numberB;
+                                }
+
+                                return numberB - numberA;
+                            }
+
+                            return 0;
+                        })
+                        .map((row, rowIndex) => (
+                            <TableRow key={rowIndex}>
+                                {row.slice(0, 4).map((cell, cellIndex) => (
+                                    <TableCell
+                                        key={cellIndex}
+                                        data-th={columns[cellIndex]}
+                                        align={
+                                            [1, 2].includes(cellIndex)
+                                                ? "right"
+                                                : "left"
+                                        }
+                                    >
+                                        {cell.toLocaleString()}
+                                    </TableCell>
+                                ))}
+                            </TableRow>
+                        ))}
+                </TableBody>
+            </Table>
+        );
+    },
 };
---
+++ packages/jokul/src/components/table/stories/tableComplex.stories.tsx
@@ -0,0 +1,333 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import React, { Fragment, useEffect, useRef, useState } from "react";
+import { Button } from "../../button/index.js";
+import {
+    DescriptionDetail,
+    DescriptionList,
+    DescriptionTerm,
+} from "../../description-list/index.js";
+import { Link } from "../../link/index.js";
+import { Select } from "../../select/index.js";
+import { TextInput } from "../../text-input/index.js";
+import { ExpandableTableRow } from "../ExpandableTableRow.js";
+import { ExpandableTableRowController } from "../ExpandableTableRowController.js";
+import { Table } from "../Table.js";
+import { TableBody } from "../TableBody.js";
+import { TableCaption } from "../TableCaption.js";
+import { TableCell } from "../TableCell.js";
+import { TableFooter } from "../TableFooter.js";
+import { TableHead } from "../TableHead.js";
+import { TableHeader, type TableSortDirection } from "../TableHeader.js";
+import { TablePagination } from "../TablePagination.js";
+import { TableRow } from "../TableRow.js";
+import { useSortableTableHeader } from "../utils.js";
+import { faktura } from "./data.js";
+
+import "../styles/_index.scss";
+import "../../description-list/styles/_index.scss";
+import "../../link/styles/_index.scss";
+import "../../button/styles/_index.scss";
+import "../../combobox/styles/_index.scss";
+import "../../text-input/styles/_index.scss";
+
+const meta = {
+    title: "Komponenter/Table",
+    component: Table,
+    subcomponents: {
+        TableCaption,
+        TableHead,
+        TableHeader,
+        TableBody,
+        TableRow,
+        TableCell,
+    },
+    parameters: {
+        layout: "centered",
+    },
+    tags: ["autodocs"],
+} satisfies Meta<typeof Table>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const TableComplex: Story = {
+    name: "Kompleks Table",
+    args: {
+        collapseToList: true,
+        fullWidth: true,
+        caption: (
+            <TableCaption srOnly>Overskrift for skjermlesere</TableCaption>
+        ),
+        children: "",
+    },
+    render: (args) => {
+        const ref = useRef<HTMLTableElement>(null);
+        const [activePage, setActivePage] = useState(0);
+        const [rowsPerPage, setRowsPerPage] = useState(6);
+        const [sortBy, setSortBy] = useState(faktura.columns[0]);
+        const [direction, setDirection] = useState<TableSortDirection>("desc");
+        const [search, setSearch] = useState("");
+        const [status, setStatus] = useState("");
+        const [filteredRows, setFilteredRows] = useState(faktura.rows);
+
+        const startIndex = activePage * rowsPerPage;
+
+        const handleSortChange = (
+            sortKey: string,
+            sortDirection: TableSortDirection,
+        ) => {
+            setSortBy(sortKey);
+            setDirection(sortDirection);
+        };
+
+        const { getSortProps } = useSortableTableHeader(
+            sortBy,
+            direction,
+            handleSortChange,
+        );
+
+        useEffect(() => {
+            setFilteredRows(
+                faktura.rows
+                    .filter((row) =>
+                        row
+                            .toString()
+                            .toLowerCase()
+                            .includes(search.toLowerCase()),
+                    )
+                    .filter((row) =>
+                        row[3]
+                            .toString()
+                            .toLowerCase()
+                            .includes(status.toLowerCase()),
+                    )
+                    .slice(startIndex, startIndex + rowsPerPage),
+            );
+        }, [startIndex, rowsPerPage, search, status]);
+
+        return (
+            <>
+                <aside
+                    style={{
+                        display: "flex",
+                        gap: "24px",
+                        alignItems: "end",
+                        marginBlockEnd: "24px",
+                    }}
+                >
+                    <TextInput
+                        label={"Søk"}
+                        value={search}
+                        placeholder={"Søk"}
+                        onChange={(e) => setSearch(e.target.value)}
+                    />
+                    <Select
+                        items={[
+                            { label: "Alle statuser", value: "" },
+                            "Faktura",
+                            "Påminnelse",
+                            "Inkasso",
+                        ]}
+                        label={"Status"}
+                        name={"status"}
+                        value={status}
+                        onChange={(e) => setStatus(e.target.value)}
+                    />
+
+                    <Button
+                        onClick={() => {
+                            setSearch("");
+                            setStatus("");
+                        }}
+                    >
+                        Fjern filter
+                    </Button>
+                </aside>
+                <div style={{ width: "90dvw" }}>
+                    <Table {...args}>
+                        <TableHead sticky={true}>
+                            <TableRow>
+                                {faktura.columns
+                                    .slice(0, 6)
+                                    .map((column, index) => (
+                                        <TableHeader
+                                            key={index}
+                                            bold
+                                            {...getSortProps(column)}
+                                        >
+                                            {column}
+                                        </TableHeader>
+                                    ))}
+                            </TableRow>
+                        </TableHead>
+                        <TableBody>
+                            {filteredRows.length !== 0 ? (
+                                filteredRows
+                                    .sort((a, b) => {
+                                        if (sortBy === "Forfallsdato") {
+                                            const timeA = new Date(
+                                                a[0] as Date,
+                                            ).getTime();
+                                            const timeB = new Date(
+                                                b[0] as Date,
+                                            ).getTime();
+
+                                            if (direction === "asc") {
+                                                return timeA - timeB;
+                                            }
+
+                                            return timeB - timeA;
+                                        }
+
+                                        if (sortBy === "Beløp") {
+                                            const numberA = Number(a[2]);
+                                            const numberB = Number(b[2]);
+
+                                            if (direction === "asc") {
+                                                return numberA - numberB;
+                                            }
+
+                                            return numberB - numberA;
+                                        }
+
+                                        return 0;
+                                    })
+                                    .map((row, rowIndex) => (
+                                        <ExpandableTableRow
+                                            // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+                                            // @ts-ignore
+                                            clickable={false}
+                                            key={rowIndex}
+                                            expandedChildren={
+                                                <DescriptionList>
+                                                    {faktura.columns.map(
+                                                        (column, columnIndex) =>
+                                                            row.map(
+                                                                (
+                                                                    cell,
+                                                                    rowIndex,
+                                                                ) =>
+                                                                    columnIndex ===
+                                                                        rowIndex && (
+                                                                        <Fragment
+                                                                            key={
+                                                                                rowIndex
+                                                                            }
+                                                                        >
+                                                                            <DescriptionTerm>
+                                                                                {
+                                                                                    column
+                                                                                }
+                                                                            </DescriptionTerm>
+                                                                            <DescriptionDetail>
+                                                                                {cell.toLocaleString()}
+                                                                            </DescriptionDetail>
+                                                                        </Fragment>
+                                                                    ),
+                                                            ),
+                                                    )}
+                                                    <DescriptionTerm>
+                                                        Faktura
+                                                    </DescriptionTerm>
+                                                    <DescriptionDetail>
+                                                        <Link
+                                                            external
+                                                            target={"_blank"}
+                                                            href={"#"}
+                                                        >
+                                                            Åpne i ny fane
+                                                        </Link>
+                                                    </DescriptionDetail>
+                                                    <DescriptionDetail>
+                                                        <Link
+                                                            download={`${row[3]} ${new Date(row[0] as Date).toLocaleDateString()}`}
+                                                            href={"#"}
+                                                        >
+                                                            Last ned
+                                                        </Link>
+                                                    </DescriptionDetail>
+                                                </DescriptionList>
+                                            }
+                                        >
+                                            <TableCell>
+                                                <Link
+                                                    download={`${row[3]} ${new Date(row[0] as Date).toLocaleDateString()}`}
+                                                    href={"#"}
+                                                >
+                                                    {new Date(
+                                                        row[0] as Date,
+                                                    ).toLocaleDateString()}
+                                                </Link>
+                                            </TableCell>
+                                            {row
+                                                .slice(1, 6)
+                                                .map((cell, cellIndex) => (
+                                                    <TableCell
+                                                        key={cellIndex}
+                                                        data-th={
+                                                            faktura.columns[
+                                                                cellIndex
+                                                            ]
+                                                        }
+                                                    >
+                                                        {cell.toLocaleString()}
+                                                    </TableCell>
+                                                ))}
+                                            <ExpandableTableRowController>
+                                                Vis detaljer
+                                            </ExpandableTableRowController>
+                                        </ExpandableTableRow>
+                                    ))
+                            ) : (
+                                <TableRow>
+                                    <TableCell colSpan={99}>
+                                        Fant ingen fakturaer.
+                                    </TableCell>
+                                </TableRow>
+                            )}
+                        </TableBody>
+                        <TableFooter>
+                            <TableRow>
+                                <TableCell colSpan={99}>
+                                    <TablePagination
+                                        activePage={activePage}
+                                        totalNumberOfRows={faktura.rows.length}
+                                        rowsPerPage={rowsPerPage}
+                                        rowsPerPageItems={[
+                                            6,
+                                            12,
+                                            24,
+                                            {
+                                                label: "Alle",
+                                                value: filteredRows.length,
+                                            },
+                                        ]}
+                                        onChangeRowsPerPage={(e) => {
+                                            const newRowsPerPage =
+                                                Number.parseInt(e.target.value);
+                                            setRowsPerPage(newRowsPerPage);
+                                            setActivePage(0);
+                                            if (ref.current) {
+                                                ref.current.scrollIntoView({
+                                                    behavior: "smooth",
+                                                });
+                                            }
+                                        }}
+                                        onChange={(_, toPage) => {
+                                            setActivePage(toPage);
+                                            if (ref.current) {
+                                                ref.current.scrollIntoView({
+                                                    behavior: "smooth",
+                                                });
+                                            }
+                                        }}
+                                    />
+                                </TableCell>
+                            </TableRow>
+                        </TableFooter>
+                    </Table>
+                </div>
+            </>
+        );
+    },
+};
