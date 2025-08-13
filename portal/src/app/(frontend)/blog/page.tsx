16bb5d3ea42a41f8f5a10afea44e9880e7a7280d
Pio Rasch-Halvorsen <pio.rasch-halvorsen@fremtind.no>
2025-08-13 13:51:51 +0200

    fix: tving dynamisk innlasting av artikkelside

--- portal/src/app/(frontend)/blog/page.tsx
+++ portal/src/app/(frontend)/blog/page.tsx
@@ -3,6 +3,8 @@ import { sanityFetch } from "@/sanity/lib/live";
 import { blogPostsQuery } from "@/sanity/queries/blog";
 import styles from "./blog.module.scss";
 
+export const dynamic = "force-dynamic";
+
 export default async function BlogPage() {
     const { data: posts } = await sanityFetch({
         query: blogPostsQuery,
