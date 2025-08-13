b4e22aeb7b67ab9fc3918efff7539a16801356b6
OJ <olejorgenbakken@gmail.com>
2025-08-13 15:11:36 +0200

    Bug: Vis Tooltip i Modal
    Signed-off-by: OJ <olejorgenbakken@gmail.com>

--- packages/jokul/src/components/modal/stories/Modal.stories.tsx
+++ packages/jokul/src/components/modal/stories/Modal.stories.tsx
@@ -1,10 +1,38 @@
 import type { Meta, StoryObj } from "@storybook/react";
+import React, { useEffect, useState } from "react";
+import { Button } from "../../button/Button.js";
+import { InputGroup } from "../../input-group/InputGroup.js";
+import { BaseTextInput } from "../../text-input/BaseTextInput.js";
+import { PopupTip } from "../../tooltip/PopupTip.js";
+import {
+    ModalActions,
+    ModalBody,
+    ModalCloseButton,
+    Modal as ModalComponent,
+    ModalContainer,
+    ModalHeader,
+    ModalOverlay,
+    ModalTitle,
+} from "../Modal.js";
+import { useModal } from "../useModal.js";
+
+import "../../button/styles/_index.scss";
 import "../styles/_index.scss";
-import { Modal as ModalComponent } from "../Modal.js";
+import "../../input-group/styles/_index.scss";
+import "../../text-input/styles/_index.scss";
 
 const meta = {
     title: "Komponenter/Modal",
     component: ModalComponent,
+    subcomponents: {
+        ModalActions,
+        ModalBody,
+        ModalCloseButton,
+        ModalContainer,
+        ModalHeader,
+        ModalOverlay,
+        ModalTitle,
+    },
     parameters: {
         layout: "centered",
     },
@@ -13,10 +41,75 @@ const meta = {
 
 export default meta;
 
-type Story = StoryObj<typeof meta>;
+type ModalStory = StoryObj<typeof meta>;
 
-export const Modal: Story = {
+export const Modal: ModalStory = {
     args: {
         role: "document",
+    },
+    render: (args) => {
+        const [showModal, setShowModal] = useState(false);
+        const heading = "Bekreft sletting";
+        const [instance, { title, overlay, container, modal, closeButton }] =
+            useModal({ title: heading });
+
+        useEffect(() => {
+            if (!instance) {
+                return;
+            }
+            instance.show();
+        }, [instance]);
+
+        return (
+            <>
+                <Button onClick={(e) => setShowModal(!showModal)}>
+                    Vis modal
+                </Button>
+                {showModal && (
+                    <ModalContainer {...container}>
+                        <ModalOverlay {...overlay} />
+                        <ModalComponent {...modal}>
+                            <ModalHeader>
+                                <ModalTitle {...title}>{heading}</ModalTitle>
+                                <ModalCloseButton {...closeButton} />
+                            </ModalHeader>
+                            <ModalBody>
+                                Er du sikker på at du vil slette Foo Bar Baz?
+                                <InputGroup
+                                    label="Fødselsnummer"
+                                    helpLabel={undefined}
+                                    errorLabel={undefined}
+                                    tooltip={
+                                        <PopupTip
+                                            content={
+                                                <>
+                                                    Du må fylle ut fødelsnummer
+                                                    eller D-nummer. Se hvordan
+                                                    du finner D-nummer.
+                                                </>
+                                            }
+                                        />
+                                    }
+                                >
+                                    <BaseTextInput />
+                                </InputGroup>
+                            </ModalBody>
+                            <ModalActions>
+                                <Button
+                                    onClick={() => {
+                                        instance?.hide();
+                                    }}
+                                >
+                                    Bekreft
+                                </Button>
+                                <Button onClick={() => instance?.hide()}>
+                                    Avbryt
+                                </Button>
+                            </ModalActions>
+                        </ModalComponent>
+                    </ModalContainer>
+                )}
+            </>
+        );
     },
 };
--- packages/jokul/src/components/tooltip/TooltipContent.tsx
+++ packages/jokul/src/components/tooltip/TooltipContent.tsx
@@ -76,8 +76,17 @@ export const TooltipContent = forwardRef<
         refs.reference.current as HTMLElement,
     );
 
+    const floatingPortalRef = React.useRef<HTMLElement | null>(null);
+
+    React.useEffect(() => {
+        floatingPortalRef.current =
+            context.elements.domReference?.closest<HTMLElement>(
+                "[data-portal]",
+            ) || null;
+    }, [context.elements.domReference]);
+
     return (
-        <FloatingPortal>
+        <FloatingPortal root={floatingPortalRef.current}>
             <FloatingFocusManager
                 initialFocus={triggerOn === "click" ? 0 : -1}
                 returnFocus={true}
