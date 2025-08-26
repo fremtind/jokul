6e64f4412f4a0e59b20b7060e38daaa9e8ad7b0a
Pio Rasch-Halvorsen <pio.rasch-halvorsen@fremtind.no>
2025-08-26 10:56:23 +0200

    fix: bruk tablistener i storybook for fokushåndtering
    Sørger for å initiere tablistener fra Jøkul, slik at fokusringer kun
    vises ved tastaturnavigasjon, ikke ved hvert klikk.

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -1,9 +1,14 @@
 import type { Preview } from "@storybook/react";
+import { initTabListener } from "../packages/jokul/src/utilities/tabListener.js";
 import { backgroundOptions } from "./backgrounds.js";
 import { densities, densityDecorator, densityGlobal } from "./density.js";
 import { themeDecorator, themeGlobal, themes } from "./theme.js";
+
+// Styles
 import "../packages/jokul/src/components/card/styles/_index.scss";
 import "./global.scss";
+
+initTabListener();
 
 const preview: Preview = {
     globalTypes: {
