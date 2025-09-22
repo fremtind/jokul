285cc6b821488e5ce56d409878a0ac74564c2676
OJ <olejorgenbakken@gmail.com>
2025-09-22 14:53:43 +0200

    legg til bedre story for tooltip
    Signed-off-by: OJ <olejorgenbakken@gmail.com>

--- packages/jokul/src/components/tooltip/stories/Tooltip.stories.tsx
+++ packages/jokul/src/components/tooltip/stories/Tooltip.stories.tsx
@@ -1,6 +1,9 @@
 import type { Meta, StoryObj } from "@storybook/react";
+
 import React from "react";
+import { Button } from "../../button/index.js";
 import "../styles/_index.scss";
+import { Icon } from "../../icon/index.js";
 import { Tooltip as TooltipComponent } from "../Tooltip.js";
 import { TooltipContent } from "../TooltipContent.js";
 import { TooltipTrigger } from "../TooltipTrigger.js";
@@ -26,21 +29,88 @@ type Story = StoryObj<typeof meta>;
 
 export const Tooltip: Story = {
     args: {
-        children: <p>Klikk for å kopiere til utklippstavlen</p>,
-        delay: 0,
-        initialOpen: false,
+        children: "Send til saksbehandler",
+        delay: 300,
+        initialOpen: true,
         placement: "top",
         triggerOn: "hover",
     },
     render: (args) => (
-        <p>
-            Kontonummer:{" "}
-            <TooltipComponent {...args}>
-                <TooltipTrigger>1602 44 54979</TooltipTrigger>
-                <TooltipContent>
-                    Klikk for å kopiere til utklippstavlen
-                </TooltipContent>
-            </TooltipComponent>
-        </p>
+        <TooltipComponent {...args}>
+            <TooltipTrigger>
+                <Button
+                    variant="ghost"
+                    icon={<Icon>send</Icon>}
+                    iconPosition="right"
+                />
+            </TooltipTrigger>
+            <TooltipContent>{args.children}</TooltipContent>
+        </TooltipComponent>
+    ),
+};
+
+export const TooltipInMenuBar: Story = {
+    args: {
+        children: "Send til saksbehandler",
+        delay: 300,
+        initialOpen: false,
+        placement: "top",
+        triggerOn: "hover",
+    },
+    parameters: {
+        layout: "fullscreen",
+    },
+    render: (args) => (
+        <menu
+            style={{
+                boxSizing: "border-box",
+                listStyle: "none",
+                margin: 0,
+                padding: "1ch",
+                width: "100%",
+                display: "flex",
+                border: "1px solid var(--jkl-color-border-separator)",
+                backgroundColor: "var(--jkl-color-background-container-high)",
+            }}
+        >
+            <li>
+                <TooltipComponent {...args}>
+                    <TooltipTrigger>
+                        <Button
+                            variant="ghost"
+                            icon={<Icon>Person_remove</Icon>}
+                            iconPosition="right"
+                        />
+                    </TooltipTrigger>
+                    <TooltipContent>Gi fra deg saken</TooltipContent>
+                </TooltipComponent>
+            </li>
+
+            <li>
+                <TooltipComponent {...args}>
+                    <TooltipTrigger>
+                        <Button
+                            variant="ghost"
+                            icon={<Icon>add_notes</Icon>}
+                            iconPosition="right"
+                        />
+                    </TooltipTrigger>
+                    <TooltipContent>Legg til dokument</TooltipContent>
+                </TooltipComponent>
+            </li>
+
+            <li style={{ marginLeft: "auto" }}>
+                <TooltipComponent {...args}>
+                    <TooltipTrigger>
+                        <Button
+                            variant="primary"
+                            icon={<Icon>Send</Icon>}
+                            iconPosition="right"
+                        />
+                    </TooltipTrigger>
+                    <TooltipContent>{args.children}</TooltipContent>
+                </TooltipComponent>
+            </li>
+        </menu>
     ),
 };
