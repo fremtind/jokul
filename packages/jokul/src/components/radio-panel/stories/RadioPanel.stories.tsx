8bfc4759b9219107d0606ecb68e874a4b20b86fd
Kristian Ulvund <kristian.ulvund@fremtind.no>
2025-09-22 10:03:12 +0200

    Oppdatert radio og checkbox-panel stories

--- packages/jokul/src/components/radio-panel/stories/RadioPanel.stories.tsx
+++ packages/jokul/src/components/radio-panel/stories/RadioPanel.stories.tsx
@@ -1,12 +1,20 @@
+import React from "react";
 import type { Meta, StoryObj } from "@storybook/react";
 import { RadioPanel as RadioPanelComponent } from "../RadioPanel.js";
+import { RadioPanelGroup } from "../RadioPanelGroup.jsx";
 import "../styles/_index.scss";
 
 const meta = {
     title: "Komponenter/RadioPanel",
     component: RadioPanelComponent,
     parameters: {
-        layout: "centered",
+        layout: "padded",
+    },
+    tags: ["autodocs"],
+    argTypes: {
+        extraLabel: {
+            control: "text",
+        },
     },
     args: {
         label: "Livsforsikring",
@@ -16,15 +24,51 @@ const meta = {
         extraLabel: "xxx kr/mnd",
         value: "Livsforsikring",
     },
-    argTypes: {
-        extraLabel: {
-            control: "text",
-        },
-    },
-    tags: ["autodocs"],
 } satisfies Meta<typeof RadioPanelComponent>;
 
 export default meta;
+
 type Story = StoryObj<typeof meta>;
 
 export const RadioPanel: Story = {};
+
+export const VelgForsikring: Story = {
+    name: "Velg forsikring",
+    render: () => {
+        return (
+            <RadioPanelGroup
+                legend="Velg en forsikring"
+                name="forsikring"
+                defaultValue="Livsforsikring"
+            >
+                <RadioPanelComponent
+                    name="forsikring"
+                    label="Ulykkesforsikring"
+                    extraLabel="50 kr/mnd"
+                    value="Ulykkesforsikring"
+                >
+                    Gir deg en engangsutbetaling hvis du skulle bli utsatt for
+                    en ulykke som fører til medisinsk invaliditet eller død.
+                </RadioPanelComponent>
+                <RadioPanelComponent
+                    name="forsikring"
+                    label="Innboforsikring"
+                    extraLabel="100 kr/mnd"
+                    value="Innboforsikring"
+                >
+                    Sikrer dine eiendeler mot brann, vannskader og tyveri.
+                </RadioPanelComponent>
+                <RadioPanelComponent
+                    name="forsikring"
+                    label="Livsforsikring"
+                    extraLabel="200 kr/mnd"
+                    value="Livsforsikring"
+                >
+                    Gir dine etterlatte en engangsutbetaling hvis du dør.
+                    Pengene kan de for eksempel bruke til å nedbetale lån og
+                    tilpasse seg en ny livssituasjon.
+                </RadioPanelComponent>
+            </RadioPanelGroup>
+        );
+    },
+};
