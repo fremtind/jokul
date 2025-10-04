00ac4345e1dd960580cb4f199a62ac1b9057a5c0
Pio Rasch-Halvorsen <pio.rasch-halvorsen@fremtind.no>
2025-10-04 22:01:30 +0200

    Legg til minimumsbredde for klapper med tekst

--- packages/jokul/src/components/button/stories/Button.stories.tsx
+++ packages/jokul/src/components/button/stories/Button.stories.tsx
@@ -1,10 +1,13 @@
 import type { Meta, StoryObj } from "@storybook/react";
-import { Button as ButtonComponent } from "../Button.js";
+import React from "react";
+import { Button } from "../Button.js";
 import "../styles/_index.scss";
+import { Flex } from "../../flex/Flex.jsx";
+import { CheckIcon, CloseIcon } from "../../icon/index.js";
 
 const meta = {
     title: "Komponenter/Button",
-    component: ButtonComponent,
+    component: Button,
     parameters: {
         layout: "centered",
         docs: {
@@ -21,12 +24,12 @@ const meta = {
         },
     },
     tags: ["autodocs", "forms"],
-} satisfies Meta<typeof ButtonComponent>;
+} satisfies Meta<typeof Button>;
 
 export default meta;
 type Story = StoryObj<typeof meta>;
 
-export const Button: Story = {};
+export const ButtonStory: Story = {};
 
 export const PrimaryButton: Story = {
     args: {
@@ -50,4 +53,33 @@ export const GhostButton: Story = {
     args: {
         variant: "ghost",
     },
+};
+
+export const ShortButtons: Story = {
+    name: "Knapper med kort tekst",
+    argTypes: {
+        as: {
+            table: { disable: true },
+        },
+        children: {
+            table: { disable: true },
+        },
+        iconLeft: {
+            table: { disable: true },
+        },
+        iconRight: {
+            table: { disable: true },
+        },
+        icon: {
+            control: "boolean",
+        },
+    },
+    render: ({ icon }) => (
+        <Flex gap={16}>
+            <Button icon={icon ? <CloseIcon /> : undefined}>Nei</Button>
+            <Button icon={icon ? <CheckIcon /> : undefined} variant="primary">
+                Ja
+            </Button>
+        </Flex>
+    ),
 };
