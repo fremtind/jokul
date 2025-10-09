dba064bd13aa8d2e2d84c86b723cbf96c81b080a
fremtindelise <elise.brenna@fremtind.no>
2025-10-09 09:22:11 +0200

    feat: Laget sjekkliste til komponentene i PortableText

--- portal/src/sanity/schemas/component.ts
+++ portal/src/sanity/schemas/component.ts
@@ -139,6 +139,7 @@ export const component = defineType({
                 { type: "jokul_codeExample" },
                 { type: "jokul_storybook" },
                 { type: "jokul_codeBlock" },
+                { type: "jokul_checkList" },
             ],
         }),
         defineField({
