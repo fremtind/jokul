9c9e1aea915eab3a09c659a4c7bafdc397200af7
Kristian Ulvund <kristian.ulvund@fremtind.no>
2025-10-14 14:45:49 +0200

    Gjør Image-komponenten om til en forwardRef

--- packages/jokul/src/components/image/Image.tsx
+++ packages/jokul/src/components/image/Image.tsx
@@ -1,43 +1,45 @@
 import clsx from "clsx";
-import React, { type FC } from "react";
+import { forwardRef } from "react";
 import { useElementDimensions } from "../../hooks/useElementDimensions/useElementDimensions.js";
-import type { ImageProps } from "./types.js";
 import { useImageLoadingStatus } from "./useImageLoadingStatus.js";
+import { mergeRefs } from "../../utilities/index.js";
+import type { ImageProps } from "./types.js";
 
-export const Image: FC<ImageProps> = ({
-    className,
-    placeholder,
-    alt,
-    ...imageProperties
-}) => {
-    const [imageRef, imageLoaded] = useImageLoadingStatus();
-    const [containerRef, dimensions] =
-        useElementDimensions<HTMLDivElement>(400);
+export const Image = forwardRef<HTMLImageElement, ImageProps>(
+    ({ className, placeholder, alt, ...imageProperties }, ref) => {
+        const [imageRef, imageLoaded] = useImageLoadingStatus();
+        const [containerRef, dimensions] =
+            useElementDimensions<HTMLDivElement>(400);
 
-    return (
-        <div
-            ref={containerRef}
-            className={clsx("jkl-image", className, {
-                "jkl-image--loading": !imageLoaded,
-            })}
-        >
-            {/* Placeholder er bevisst uten loading og decoding for å vises umiddelbart */}
-            <img
-                className="jkl-image__placeholder"
-                alt={imageLoaded ? "" : alt}
-                src={placeholder || imageProperties.src}
-            />
-            {/* biome-ignore lint/a11y/useAltText: Den har da vitterlig en alt-attributt? */}
-            <img
-                alt={alt}
-                className="jkl-image__image"
-                data-testid="jkl-image__image"
-                ref={imageRef}
-                sizes={`${dimensions.width}px`}
-                loading="lazy"
-                decoding="async"
-                {...imageProperties}
-            />
-        </div>
-    );
-};
+        const mergedImageRef = mergeRefs(ref, imageRef);
+
+        return (
+            <div
+                ref={containerRef}
+                className={clsx("jkl-image", className, {
+                    "jkl-image--loading": !imageLoaded,
+                })}
+            >
+                {/* Placeholder er bevisst uten loading og decoding for å vises umiddelbart */}
+                <img
+                    className="jkl-image__placeholder"
+                    alt={imageLoaded ? "" : alt}
+                    src={placeholder || imageProperties.src}
+                />
+                {/* biome-ignore lint/a11y/useAltText: Den har da vitterlig en alt-attributt? */}
+                <img
+                    alt={alt}
+                    className="jkl-image__image"
+                    data-testid="jkl-image__image"
+                    ref={mergedImageRef}
+                    sizes={`${dimensions.width}px`}
+                    decoding="async"
+                    loading="lazy"
+                    {...imageProperties}
+                />
+            </div>
+        );
+    },
+);
+
+Image.displayName = "Image";
