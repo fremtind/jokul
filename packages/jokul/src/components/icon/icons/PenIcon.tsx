98bdec6e39f532e7da6cb8507501edbda0d2f2e7
fremtindelise <elise.brenna@fremtind.no>
2025-10-15 12:07:21 +0200

    docs(changeset): Erstattet PenIcon med nytt ikon fra Material.

--- packages/jokul/src/components/icon/icons/PenIcon.tsx
+++ packages/jokul/src/components/icon/icons/PenIcon.tsx
@@ -3,6 +3,6 @@ import { Icon, type IconComponent } from "../Icon.js";
 import type { IconProps } from "../types.js";
 
 export const PenIcon: IconComponent = (props: IconProps) => (
-    <Icon {...props}>{"\uf097"}</Icon>
+    <Icon {...props}>{"\uf604"}</Icon>
 );
 PenIcon.displayName = "PenIcon";
