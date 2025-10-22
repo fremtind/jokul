e88ba356e34e9a06e208175ffdd7ad764f6dc3fa
fremtindelise <elise.brenna@fremtind.no>
2025-10-22 11:00:25 +0200

    feat: Satt opp do and dont block

---
+++ portal/src/components/portable-text/do-and-dont/DoAndDont.tsx
@@ -0,0 +1,62 @@
+import { client } from "@/sanity/lib/client";
+import type { Jokul_doAndDont } from "@/sanity/types";
+import { Card } from "@fremtind/jokul/card";
+import { Flex } from "@fremtind/jokul/flex";
+import { GreenCheckIcon, RedCrossIcon } from "@fremtind/jokul/icon";
+import type { PortableTextTypeComponentProps } from "@portabletext/react";
+import imageUrlBuilder from "@sanity/image-url";
+import type { SanityImageObject } from "@sanity/image-url/lib/types/types";
+import type { FC } from "react";
+
+import styles from "./doAndDont.module.scss";
+
+const builder = imageUrlBuilder(client);
+
+function isSanityImageObject(source: unknown): source is SanityImageObject {
+    return (
+        typeof source === "object" &&
+        source !== null &&
+        "_type" in source &&
+        (source as { _type?: string })._type === "image" &&
+        "asset" in source
+    );
+}
+
+function urlFor(source: unknown): string | undefined {
+    if (!isSanityImageObject(source)) return undefined;
+    if (!source.asset || !("_ref" in source.asset)) return undefined;
+    return builder.image(source).url();
+}
+
+export const DoAndDont: FC<PortableTextTypeComponentProps<Jokul_doAndDont>> = ({
+    value,
+}) => {
+    if (!value?.do && !value?.dont) return null;
+
+    return (
+        <Flex className={styles.container} gap={8} wrap={true}>
+            <Card padding="l">
+                <Flex
+                    justifyContent="space-between"
+                    style={{ paddingBottom: "16px" }}
+                >
+                    <h4>Riktig bruk</h4>
+                    <GreenCheckIcon />
+                </Flex>
+
+                <img className={styles.image} src={urlFor(value.do)} alt="" />
+            </Card>
+            <Card padding="l">
+                <Flex
+                    justifyContent="space-between"
+                    style={{ paddingBottom: "16px" }}
+                >
+                    <h4>Feil bruk</h4>
+                    <RedCrossIcon />
+                </Flex>
+
+                <img className={styles.image} src={urlFor(value.dont)} alt="" />
+            </Card>
+        </Flex>
+    );
+};
--- portal/src/sanity/schemas/component.ts
+++ portal/src/sanity/schemas/component.ts
@@ -156,6 +156,7 @@ export const component = defineType({
                 { type: "jokul_codeExample" },
                 { type: "jokul_storybook" },
                 { type: "jokul_codeBlock" },
+                { type: "jokul_doAndDont" },
             ],
         }),
         defineField({
