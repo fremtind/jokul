9f18961ab399f4ff51b654bbb6d515583e028eea
OJ <olejorgenbakken@gmail.com>
2025-11-13 08:31:56 +0100

    Legg til Description List og NavLink som beta-komponent
    Signed-off-by: OJ <olejorgenbakken@gmail.com>

---
+++ packages/jokul/src/components-beta/description-list/index.ts
@@ -0,0 +1,7 @@
+export { DescriptionList as BETA_DescriptionList } from "./DescriptionList.js";
+export { DescriptionListItem as BETA_DescriptionListItem } from "./DescriptionListItem.js";
+
+export type {
+    DescriptionListProps as BETA_DescriptionListProps,
+    DescriptionListItemProps as BETA_DescriptionListItemProps,
+} from "./types.js";
