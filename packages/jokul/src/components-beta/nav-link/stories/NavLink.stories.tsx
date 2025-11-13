9046363e967e7961b46128be84ecaf9c88cb0b59
OJ <olejorgenbakken@gmail.com>
2025-11-13 08:31:56 +0100

    Legg til Description List og NavLink som beta-komponent
    Signed-off-by: OJ <olejorgenbakken@gmail.com>

---
+++ packages/jokul/src/components-beta/nav-link/stories/NavLink.stories.tsx
@@ -0,0 +1,69 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import React from "react";
+import { BETA_NavLink } from "../index.js";
+
+import "../styles/_index.scss";
+import "../../../components/button/styles/_index.scss";
+
+const meta: Meta = {
+    title: "Beta/Nav Link",
+    component: BETA_NavLink,
+    parameters: {
+        layout: "centered",
+    },
+    args: {
+        as: "a",
+        title: "Vurderingen vår",
+        href: "#",
+        description: "",
+        to: "#",
+    },
+    argTypes: {
+        title: {
+            control: "text",
+            table: {
+                category: "Innhold",
+            },
+        },
+        description: {
+            table: {
+                category: "Innhold",
+            },
+        },
+        as: {
+            description: "Bytt ut med ditt rammeverks lenke-element",
+            control: "text",
+            table: {
+                category: "Teknisk",
+            },
+        },
+        href: {
+            if: { arg: "as", eq: "a" },
+            table: {
+                category: "Teknisk",
+            },
+        },
+        to: {
+            if: { arg: "as", neq: "a" },
+            control: "text",
+            table: {
+                category: "Teknisk",
+            },
+        },
+        ref: {
+            table: {
+                category: "Teknisk",
+            },
+        },
+    },
+    tags: ["autodocs", "grouping content"],
+} satisfies Meta<typeof BETA_NavLink>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const NavLinkStory: Story = {
+    name: "Nav Link",
+    render: (args) => <BETA_NavLink {...args} title={args.title} as="a" />,
+};
