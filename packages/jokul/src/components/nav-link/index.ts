1da97dab2dd9e8529d88882ebb3335c2bcdf5958
OJ <olejorgenbakken@gmail.com>
2025-11-13 08:31:56 +0100

    Legg til Description List og NavLink som beta-komponent
    Signed-off-by: OJ <olejorgenbakken@gmail.com>

--- packages/jokul/src/components/nav-link/index.ts
+++ packages/jokul/src/components/nav-link/index.ts
@@ -1,3 +1,7 @@
 export { NavLink } from "./NavLink.js";
 
 export type { NavLinkProps } from "./types.js";
+
+// BETA
+export { NavLink as BETA_NavLink } from "../../components-beta/nav-link/NavLink.js";
+export type { NavLinkProps as BETA_NavLinkProps } from "../../components-beta/nav-link/types.js";
