d2662de46459c38072b257a5c138828d9ae7e58f
OJ <olejorgenbakken@gmail.com>
2025-11-17 13:40:11 +0100

    Revert "Legg til Description List og NavLink som beta-komponent"
    This reverts commit 1e46e41aece2becadbf59bc43aa2378d483a30d3.

--- packages/jokul/src/components/description-list/index.ts
+++ packages/jokul/src/components/description-list/index.ts
@@ -9,8 +9,3 @@ export type {
     DescriptionTermProps,
     DescriptionDetailProps,
 } from "./types.js";
-
-// BETA
-export { DescriptionList as BETA_DescriptionList } from "../../components-beta/description-list/DescriptionList.js";
-export { DescriptionListItem as BETA_DescriptionListItem } from "../../components-beta/description-list/DescriptionListItem.js";
-export type { DescriptionListProps as BETA_DescriptionListProps } from "../../components-beta/description-list/types.js";
--- packages/jokul/src/components/nav-link/index.ts
+++ packages/jokul/src/components/nav-link/index.ts
@@ -1,7 +1,3 @@
 export { NavLink } from "./NavLink.js";
 
 export type { NavLinkProps } from "./types.js";
-
-// BETA
-export { NavLink as BETA_NavLink } from "../../components-beta/nav-link/NavLink.js";
-export type { NavLinkProps as BETA_NavLinkProps } from "../../components-beta/nav-link/types.js";
