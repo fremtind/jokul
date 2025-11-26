29ff5aeafe502dd46ed18e15455c94a763843736
Ole Jørgen Bakken <olejorgenbakken@gmail.com>
2025-11-26 15:03:08 +0100

    docs(changeset): Gi `Message` mulihget til å styre variant med prop

--- packages/jokul/src/components/message/Message.figma.tsx
+++ packages/jokul/src/components/message/Message.figma.tsx
@@ -1,17 +1,11 @@
 import { figma } from "@figma/code-connect";
 import React from "react";
-import {
-    ErrorMessage,
-    InfoMessage,
-    SuccessMessage,
-    WarningMessage,
-} from "./Message.js";
+import { Message } from "./Message.js";
 
 figma.connect(
-    InfoMessage,
+    Message,
     "https://www.figma.com/design/jd7QGZJIQ5ZU6AhAq31yuv?node-id=14268%3A9812",
     {
-        variant: { Type: "Info" },
         imports: [
             'import { InfoMessage } from "@fremtind/jokul/components/message";',
         ],
@@ -22,75 +16,15 @@ figma.connect(
                 true: figma.string("Title"),
                 false: undefined,
             }),
-        },
-        example: ({ children, ...props }) => (
-            <InfoMessage {...props}>{children}</InfoMessage>
-        ),
-    },
-);
-
-figma.connect(
-    ErrorMessage,
-    "https://www.figma.com/design/jd7QGZJIQ5ZU6AhAq31yuv?node-id=14268%3A9812",
-    {
-        variant: { Type: "Error" },
-        imports: [
-            'import { ErrorMessage } from "@fremtind/jokul/components/message";',
-        ],
-        props: {
-            children: figma.string("Text"),
-            dismissable: figma.boolean("Dismissable"),
-            title: figma.boolean("Show Title", {
-                true: figma.string("Title"),
-                false: undefined,
+            variant: figma.enum("Variant", {
+                Error: "error",
+                Warning: "warning",
+                Success: "success",
+                Info: "info",
             }),
         },
         example: ({ children, ...props }) => (
-            <ErrorMessage {...props}>{children}</ErrorMessage>
-        ),
-    },
-);
-
-figma.connect(
-    SuccessMessage,
-    "https://www.figma.com/design/jd7QGZJIQ5ZU6AhAq31yuv?node-id=14268%3A9812",
-    {
-        variant: { Type: "Success" },
-        imports: [
-            'import { SuccessMessage } from "@fremtind/jokul/components/message";',
-        ],
-        props: {
-            children: figma.string("Text"),
-            dismissable: figma.boolean("Dismissable"),
-            title: figma.boolean("Show Title", {
-                true: figma.string("Title"),
-                false: undefined,
-            }),
-        },
-        example: ({ children, ...props }) => (
-            <SuccessMessage {...props}>{children}</SuccessMessage>
-        ),
-    },
-);
-
-figma.connect(
-    WarningMessage,
-    "https://www.figma.com/design/jd7QGZJIQ5ZU6AhAq31yuv?node-id=14268%3A9812",
-    {
-        variant: { Type: "Warning" },
-        imports: [
-            'import { WarningMessage } from "@fremtind/jokul/components/message";',
-        ],
-        props: {
-            children: figma.string("Text"),
-            dismissable: figma.boolean("Dismissable"),
-            title: figma.boolean("Show Title", {
-                true: figma.string("Title"),
-                false: undefined,
-            }),
-        },
-        example: ({ children, ...props }) => (
-            <WarningMessage {...props}>{children}</WarningMessage>
+            <Message {...props}>{children}</Message>
         ),
     },
 );
--- packages/jokul/src/components/message/Message.tsx
+++ packages/jokul/src/components/message/Message.tsx
@@ -8,9 +8,7 @@ import { WarningIcon } from "../icon/icons/WarningIcon.js";
 import { DismissButton } from "./DismissButton.js";
 import type { MessageProps } from "./types.js";
 
-type messageTypes = "info" | "error" | "success" | "warning";
-
-const getIcon = (messageType: messageTypes) => {
+const getIcon = (messageType: MessageProps["variant"]) => {
     switch (messageType) {
         case "error":
             return <ErrorIcon className="jkl-message__icon" />;
@@ -25,7 +23,7 @@ const getIcon = (messageType: messageTypes) => {
     }
 };
 
-function messageFactory(messageType: messageTypes) {
+function messageFactory(messageType: MessageProps["variant"]) {
     const Message = forwardRef<HTMLDivElement, MessageProps>((props, ref) => {
         const {
             id,
@@ -88,11 +86,83 @@ function messageFactory(messageType: messageTypes) {
     return Message;
 }
 
+export const Message = forwardRef<HTMLDivElement, MessageProps>(
+    (props, ref) => {
+        const {
+            id,
+            title,
+            fullWidth,
+            density,
+            className = "",
+            dismissed,
+            dismissAction,
+            children,
+            variant = "info",
+            role,
+            ...rest
+        } = props;
+
+        const boxId = useId(id || "jkl-message", { generateSuffix: !id });
+
+        const hasStringChild = React.Children.map(
+            children,
+            (child) => typeof child === "string",
+        );
+        const newChildren = hasStringChild?.[0] ? <p>{children}</p> : children;
+
+        return (
+            <div
+                {...rest}
+                id={id}
+                ref={ref}
+                className={clsx(
+                    "jkl-message",
+                    `jkl-message--${variant}`,
+                    className,
+                    {
+                        "jkl-message--full": fullWidth,
+                        "jkl-message--dismissed": dismissed,
+                    },
+                )}
+                role={role}
+                data-density={density}
+            >
+                {getIcon(variant)}
+                <div className="jkl-message__content" data-theme="light">
+                    {title && <p className="jkl-message__title">{title}</p>}
+                    <div className="jkl-message__message">{newChildren}</div>
+                </div>
+                {dismissAction?.handleDismiss && (
+                    <DismissButton
+                        data-theme="light"
+                        aria-controls={boxId}
+                        className="jkl-message__dismiss-button"
+                        label={dismissAction.buttonTitle || "Lukk"}
+                        onClick={dismissAction.handleDismiss}
+                    />
+                )}
+            </div>
+        );
+    },
+);
+
+/**
+ * @deprecated bruk Message med variant="info"
+ */
 export const InfoMessage = messageFactory("info");
 InfoMessage.displayName = "InfoMessage";
+/**
+ * @deprecated bruk Message med variant="error"
+ */
 export const ErrorMessage = messageFactory("error");
 ErrorMessage.displayName = "ErrorMessage";
+/**
+ * @deprecated bruk Message med variant="warning"
+ */
 export const WarningMessage = messageFactory("warning");
 WarningMessage.displayName = "WarningMessage";
+/**
+ * @deprecated bruk Message med variant="success"
+ */
 export const SuccessMessage = messageFactory("success");
 SuccessMessage.displayName = "SuccessMessage";
