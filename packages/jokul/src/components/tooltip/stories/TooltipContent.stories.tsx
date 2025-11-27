d44aaa4b4a5144a8bdb2b02a95d0af4c29676c46
Ole Jørgen Bakken <olejorgenbakken@gmail.com>
2025-11-27 10:45:50 +0100

    Legg til flere stories for komponenter

---
+++ packages/jokul/src/components/tooltip/stories/TooltipContent.stories.tsx
@@ -0,0 +1,37 @@
+import type { Meta, StoryObj } from "@storybook/nextjs";
+import React from "react";
+import { Tooltip } from "../Tooltip.js";
+import { TooltipContent } from "../TooltipContent.js";
+import { TooltipTrigger } from "../TooltipTrigger.js";
+import { _TooltipTrigger as TriggerStory } from "./TooltipTrigger.stories.js";
+
+import "../styles/_index.scss";
+import { Button } from "../../button/index.js";
+import { Icon } from "../../icon/index.js";
+
+const meta = {
+    title: "Komponenter/Tooltip",
+    component: TooltipContent,
+    args: {
+        children: "Tooltip Content",
+    },
+    tags: ["autodocs"],
+} satisfies Meta<typeof TooltipContent>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const _TooltipContent: Story = {
+    render: (args) => (
+        <Tooltip initialOpen>
+            <TooltipTrigger {...TriggerStory.args}>
+                <Button
+                    variant="ghost"
+                    icon={<Icon>send</Icon>}
+                    iconPosition="right"
+                />
+            </TooltipTrigger>
+            <TooltipContent {...args} />
+        </Tooltip>
+    ),
+};
