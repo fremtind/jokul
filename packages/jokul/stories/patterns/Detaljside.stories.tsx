970c57b9c76d9863fbd57bbfe6fbc19457eecd87
Pio Rasch-Halvorsen <pio.rasch-halvorsen@fremtind.no>
2025-12-10 09:54:52 +0100

    opprett variabler for avrunding og test ut størrelser

---
+++ packages/jokul/stories/patterns/Detaljside.stories.tsx
@@ -0,0 +1,196 @@
+import type { Meta, StoryObj } from "@storybook/nextjs";
+import React, { type CSSProperties } from "react";
+import { Button } from "../../src/components/button/Button.jsx";
+import { Card, CardImage } from "../../src/components/card/index.js";
+import {
+    ExpandablePanel,
+    Expander,
+} from "../../src/components/expander/index.js";
+import { Flex } from "../../src/components/flex/Flex.jsx";
+import { CopyIcon, HamburgerIcon } from "../../src/components/icon/index.js";
+import { BETA_LinkList } from "../../src/components/link-list/index.js";
+import { NavLink } from "../../src/components/nav-link/NavLink.jsx";
+import { PopupTip } from "../../src/components/tooltip/PopupTip.jsx";
+
+import "../../src/components/button/styles/_index.scss";
+import "../../src/components/expander/styles/_index.scss";
+import "../../src/components/link-list/styles/_index.scss";
+import "../../src/components/tooltip/styles/_index.scss";
+
+const meta = {
+    title: "Mønstre/Detaljside",
+    component: () => null,
+    parameters: {
+        layout: "fullscreen",
+        docs: {
+            toc: true,
+        },
+    },
+    // biome-ignore lint/complexity/noBannedTypes: Her vil jeg faktisk ha et helt tomt objekt
+} satisfies Meta<{}>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+const poolside = "/images/poolside.jpg";
+
+const Header = () => (
+    <Flex
+        gap="l"
+        justifyContent="space-between"
+        alignItems="center"
+        style={{
+            marginBlockEnd: "var(--jkl-spacing-24)",
+        }}
+    >
+        <NavLink back>Tilbake</NavLink>
+        <Button variant="ghost" icon={<HamburgerIcon />} iconPosition="right">
+            Meny
+        </Button>
+    </Flex>
+);
+
+export const Detaljside: Story = {
+    render: () => {
+        return (
+            <Flex
+                direction="column"
+                gap="m"
+                style={{
+                    maxWidth: "calc(500px + var(--jkl-unit-10) * 40)",
+                    marginInline: "auto",
+                    marginBlock: "var(--jkl-spacing-64)",
+                }}
+            >
+                <Header />
+                <Flex gap="m">
+                    <Card padding="xl" asChild variant="high">
+                        <Flex
+                            direction="column"
+                            gap="xl"
+                            style={{ flexBasis: 999 }}
+                        >
+                            <Flex as="hgroup" direction="column" gap="m">
+                                <p className="jkl-small">Reiseforsikring for</p>
+                                <h1
+                                    className="jkl-heading-1"
+                                    style={
+                                        {
+                                            "text-box":
+                                                "trim-both cap alphabetic",
+                                        } as CSSProperties
+                                    }
+                                >
+                                    Per Andreas Hansen med familie
+                                </h1>
+                            </Flex>
+                            <p>
+                                248 kr/mnd.
+                                <PopupTip content="Beløpet vil variere hver måned" />
+                                <br />
+                                <time>24.10.24</time> &ndash;{" "}
+                                <time>24.09.25</time>
+                            </p>
+                            <Button variant="primary">Meld skade</Button>
+                        </Flex>
+                    </Card>
+                    <Card style={{ flexBasis: 999 }}>
+                        <CardImage
+                            height="110%"
+                            style={{
+                                minWidth: "auto",
+                            }}
+                            placement="full"
+                            src={poolside}
+                        />
+                    </Card>
+                </Flex>
+                <ExpandablePanel variant="fill">
+                    <Expander>Forsikringsdetaljer</Expander>
+                    <ExpandablePanel.Content>
+                        Her kommer nyttig informasjon om forsikringen
+                    </ExpandablePanel.Content>
+                </ExpandablePanel>
+                <BETA_LinkList label="Navigasjon">
+                    {[
+                        "Hva dekker forsikringen?",
+                        "Reisekort",
+                        "SmartDelay+",
+                        "Reise SMS",
+                        "Hjelp",
+                    ].map((title) => (
+                        <BETA_LinkList.Link key={title}>
+                            {title}
+                        </BETA_LinkList.Link>
+                    ))}
+                </BETA_LinkList>
+                <Button
+                    className="jkl-spacing-24--top"
+                    style={{ alignSelf: "center" }}
+                >
+                    Avslutt forsikring
+                </Button>
+            </Flex>
+        );
+    },
+};
+
+export const SmartDelay: Story = {
+    render: () => (
+        <Flex
+            direction="column"
+            gap="xl"
+            style={{
+                maxWidth: "500px",
+                marginInline: "auto",
+                marginBlock: "var(--jkl-spacing-64)",
+            }}
+        >
+            <Header />
+            <Flex direction="column" as="hgroup" gap="m">
+                <h1 className="jkl-heading-1">SmartDelay+</h1>
+                <p>
+                    Ved flyforsinkelse får du gratis tilgang til utvalgte
+                    flyplass-lounger verden over.{" "}
+                </p>
+            </Flex>
+            <Flex direction="column" gap="l">
+                <h2 className="jkl-heading-4">1. Kopier koden</h2>
+                <Flex
+                    as={Card}
+                    justifyContent="space-between"
+                    alignItems="center"
+                    gap="xl"
+                    variant="low"
+                    padding="m"
+                    style={{ alignSelf: "start" }}
+                >
+                    <p>FremtindSD</p>
+                    <Button icon={<CopyIcon />} variant="ghost" />
+                </Flex>
+                <h2 className="jkl-heading-4">2. Registrer flygning</h2>
+                <p>
+                    Du må registrere flyvningen senest to timer før planlagt
+                    reise hos SmartDelay+.
+                </p>
+                <h2 className="jkl-heading-4">Ofte stilte spørsmål</h2>
+                <Button variant="primary">Registrer SmartDelay+</Button>
+            </Flex>
+            <div>
+                {[
+                    "Hva er SmartDelay+?",
+                    "Hvem kan bruke tjenesten?",
+                    "Når kan jeg registrere?",
+                    "Hvilke flygninger kan jeg registrere?",
+                ].map((tittel) => (
+                    <ExpandablePanel variant="stroke" key={tittel}>
+                        <Expander>{tittel}</Expander>
+                        <ExpandablePanel.Content>
+                            En akkurat passe lang og meget god forklaring
+                        </ExpandablePanel.Content>
+                    </ExpandablePanel>
+                ))}
+            </div>
+        </Flex>
+    ),
+};
