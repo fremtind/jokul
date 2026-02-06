9fbf4bc39813067a9ea187a0109f4a84e5dc42ec
Kristian Ulvund <kristian.ulvund@fremtind.no>
2026-02-06 13:56:32 +0100

    Beta Select sender nå ref riktig i forwardRef.

--- packages/jokul/src/components-beta/select/Select.tsx
+++ packages/jokul/src/components-beta/select/Select.tsx
@@ -4,56 +4,59 @@ import { ArrowDownIcon } from "../../components/icon/index.js";
 import { InputGroup } from "../../components/input-group/InputGroup.js";
 import type { SelectProps } from "./types.js";
 
-export const Select = forwardRef<HTMLSelectElement, SelectProps>((props) => {
-    const {
-        label,
-        className,
-        density,
-        errorLabel,
-        helpLabel,
-        labelProps,
-        placeholder = "Velg",
-        supportLabelProps,
-        tooltip,
-        value,
-        description,
-        ...rest
-    } = props;
+export const Select = forwardRef<HTMLSelectElement, SelectProps>(
+    (props, ref) => {
+        const {
+            label,
+            className,
+            density,
+            errorLabel,
+            helpLabel,
+            labelProps,
+            placeholder = "Velg",
+            supportLabelProps,
+            tooltip,
+            value,
+            description,
+            ...rest
+        } = props;
 
-    const inputGroupProps = {
-        label,
-        density,
-        errorLabel,
-        helpLabel,
-        labelProps,
-        supportLabelProps,
-        tooltip,
-        description,
-    };
+        const inputGroupProps = {
+            label,
+            density,
+            errorLabel,
+            helpLabel,
+            labelProps,
+            supportLabelProps,
+            tooltip,
+            description,
+        };
 
-    return (
-        <InputGroup
-            {...inputGroupProps}
-            data-testid="jkl-select--beta"
-            className={clsx("jkl-select--beta", className)}
-            render={(inputProps) => (
-                <div className="jkl-select--beta-wrapper">
-                    <select
-                        defaultValue={value ? undefined : ""}
-                        value={value}
-                        {...inputProps}
-                        {...rest}
-                    >
-                        {placeholder && (
-                            <option disabled selected value="">
-                                {placeholder}
-                            </option>
-                        )}
-                        {props.children}
-                    </select>
-                    <ArrowDownIcon variant="medium" />
-                </div>
-            )}
-        />
-    );
-});
+        return (
+            <InputGroup
+                {...inputGroupProps}
+                data-testid="jkl-select--beta"
+                className={clsx("jkl-select--beta", className)}
+                render={(inputProps) => (
+                    <div className="jkl-select--beta-wrapper">
+                        <select
+                            ref={ref}
+                            defaultValue={value ? undefined : ""}
+                            value={value}
+                            {...inputProps}
+                            {...rest}
+                        >
+                            {placeholder && (
+                                <option disabled selected value="">
+                                    {placeholder}
+                                </option>
+                            )}
+                            {props.children}
+                        </select>
+                        <ArrowDownIcon variant="medium" />
+                    </div>
+                )}
+            />
+        );
+    },
+);
