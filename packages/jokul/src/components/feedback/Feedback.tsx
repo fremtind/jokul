294682a54e6f1380ca19a68608a0996f5b290e56
Maria Eilertsen <maria.turtles@outlook.com>
2026-02-10 11:54:10 +0100

    Update packages/jokul/src/components/feedback/Feedback.tsx
    Co-authored-by: Pio Rasch-Halvorsen <pio.rasch-halvorsen@fremtind.no>

--- packages/jokul/src/components/feedback/Feedback.tsx
+++ packages/jokul/src/components/feedback/Feedback.tsx
@@ -14,7 +14,6 @@ import type {
 /**
  * @deprecated denne komponenten er i ferd med å fases ut, og vil bli erstattet av mer fleksible og mindre styrende komponenter for innsamling av tilbakemeldinger. Ta kontakt med oss i Jøkul-teamet hvis du ønsker å bruke eller bidra til utviklingen av de nye komponentene.
  */
-
 export type FeedbackProps = {
     className?: string;
     /** Velg typen alternativer, Smiley eller RadioButtons. */
