0ffd3ecde7e4c87ccec8aa433f640766943d2b35
Kristian Ulvund <kristian.ulvund@fremtind.no>
2026-02-27 11:46:15 +0100

    Link: Stramme inn polymorfisme

--- packages/jokul/src/components/link/types.ts
+++ packages/jokul/src/components/link/types.ts
@@ -1,6 +1,13 @@
 import type { PolymorphicPropsWithRef } from "../../utilities/polymorphism/polymorphism.js";
 
-export type LinkProps<ElementType extends React.ElementType> =
+type LinkComponentElementType = Exclude<
+    React.ElementType,
+    keyof React.JSX.IntrinsicElements
+>;
+
+export type LinkElementType = "a" | LinkComponentElementType;
+
+export type LinkProps<ElementType extends LinkElementType = "a"> =
     PolymorphicPropsWithRef<
         ElementType,
         {
