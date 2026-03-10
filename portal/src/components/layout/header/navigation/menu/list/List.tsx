faab13c229bc276b76f9959ef76ed3fefd07ad8c
Ole Jørgen <olejorgenbakken@gmail.com>
2026-03-10 10:41:17 +0100

    docs(changeset): Implementert søk etter innhold
    feat: refactor header navigation

--- portal/src/components/layout/header/navigation/menu/list/List.tsx
+++ portal/src/components/layout/header/navigation/menu/list/List.tsx
@@ -1,22 +1,27 @@
+"use client";
+
 import { navigationLinks } from "@/components/layout/header/navigation/menu/data";
 import { Button } from "@fremtind/jokul/button";
 import { Flex } from "@fremtind/jokul/flex";
 import Link from "next/link";
 
+import styles from "../../navigation.module.scss";
+
 export const NavigationList = () => {
     return (
         <Flex
-            as="ul"
+            as="nav"
+            aria-label="Hovednavigasjon"
             alignItems="center"
+            justifyContent="center"
             gap="none"
             style={{ listStyleType: "none", padding: 0, margin: 0 }}
+            className={styles.list}
         >
             {navigationLinks.map((link) => (
-                <li key={link}>
-                    <Button as={Link} href={`/${link}`} variant="ghost">
-                        {link.charAt(0).toUpperCase() + link.slice(1)}
-                    </Button>
-                </li>
+                <Button as={Link} href={`/${link}`} variant="ghost" key={link}>
+                    {link.charAt(0).toUpperCase() + link.slice(1)}
+                </Button>
             ))}
         </Flex>
     );
