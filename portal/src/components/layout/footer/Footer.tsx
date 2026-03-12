22c40ba3dbb33fa4152606f7ce3dc5162bbcc1f9
Ole Jørgen <olejorgenbakken@gmail.com>
2026-03-12 15:31:03 +0100

    fix: fjern footer fetch

--- portal/src/components/layout/footer/Footer.tsx
+++ portal/src/components/layout/footer/Footer.tsx
@@ -22,26 +22,6 @@ export const Footer = async () => {
         return null;
     }
 
-    const packageInfo = await fetch(
-        "https://registry.npmjs.org/@fremtind/jokul",
-    ).then((res) => res.json());
-
-    const versionTime = packageInfo.time[jokul.version] ?? new Date();
-    const releaseDate = new Date(versionTime as string);
-
-    const daysSinceRelease = Math.round(
-        (Date.now() - releaseDate.getTime()) / (1000 * 60 * 60 * 24),
-    );
-
-    const relativeTimeFormat = new Intl.RelativeTimeFormat("no", {
-        numeric: "auto",
-    });
-
-    const timeString = ` ble utgitt ${relativeTimeFormat.format(
-        -daysSinceRelease,
-        "day",
-    )}`;
-
     return (
         <Flex as="footer" gap="l" direction="column" className={styles.footer}>
             <Flex as="ul" gap="l" wrap="wrap">
@@ -81,7 +61,7 @@ export const Footer = async () => {
                 >
                     versjon {jokul.version}
                 </Link>
-                {timeString}.
+                .
             </p>
             <p>{footer.text}</p>
         </Flex>
