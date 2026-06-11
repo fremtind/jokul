41e42349050820ad11e03d9e3706376a1d15463b
Ole Jørgen <olejorgenbakken@gmail.com>
2026-06-11 10:20:57 +0200

    storybook: endre oppsett i samsvar med Confluence

---
+++ packages/jokul/src/components/combobox/stories/shared.data.ts
@@ -0,0 +1,22 @@
+import type { ComboboxValuePair } from "../types.js";
+
+export const comboboxItems: ComboboxValuePair[] = [
+    {
+        value: "Valg 1",
+        tagLabel: "Valg 1",
+        label: "Valg 1: Ulik tekst i dropdownen dersom det trengs",
+        description: "En beskrivelse av valget",
+    },
+    {
+        value: "Valg 2",
+        tagLabel: "Valg 2",
+        label: "Valg 2: Ulik tekst i dropdownen dersom det trengs",
+        description: "En beskrivelse av valget",
+    },
+    {
+        value: "Valg 3",
+        tagLabel: "Valg 3",
+        label: "Valg 3: Ulik tekst i dropdownen dersom det trengs",
+        description: "En beskrivelse av valget",
+    },
+];
