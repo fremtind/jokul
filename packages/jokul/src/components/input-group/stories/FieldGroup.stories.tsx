f22e50f10020c2c88e27f729f2ba946eeb389661
Ole Jørgen <olejorgenbakken@gmail.com>
2026-06-25 11:12:27 +0200

    Legg til stories for *-Panels i FieldGroup-gruppa

--- packages/jokul/src/components/input-group/stories/FieldGroup.stories.tsx
+++ packages/jokul/src/components/input-group/stories/FieldGroup.stories.tsx
@@ -8,6 +8,10 @@ import HelpStories from "../../help/stories/Help.stories.js";
 import { RadioButton } from "../../radio-button/index.js";
 import RadioButtonStories from "../../radio-button/stories/RadioButton.stories.js";
 import { FieldGroup } from "../FieldGroup.js";
+import { CheckboxPanel } from "../../checkbox-panel/index.js";
+import { CheckboxPanelStory } from "../../checkbox-panel/stories/CheckboxPanel.stories.js";
+import { RadioPanel } from "../../radio-panel/index.js";
+import { RadioPanel as RadioPanelStory } from "../../radio-panel/stories/RadioPanel.stories.js";
 
 const meta: Meta = {
     title: "Komponenter/Field Group",
@@ -52,6 +56,40 @@ export const FieldGroupCheckboxGroup: Story = {
             >
                 {value}
             </Checkbox>
+        )),
+    },
+};
+
+export const FieldGroupCheckboxPanelGroup: Story = {
+    name: "Checkbox panel gruppe",
+    args: {
+        legend: "Velg kontaktmetoder",
+        children: contactChoices.map((value) => (
+            <CheckboxPanel
+                {...CheckboxPanelStory.args}
+                key={value}
+                value={value}
+                name="kontaktmetode"
+                label={value}
+            >
+                {value}
+            </CheckboxPanel>
+        )),
+    },
+};
+
+export const FieldGroupRadioPanelGroup: Story = {
+    name: "Radio panel gruppe",
+    args: {
+        legend: "Velg kontaktmetoder",
+        children: contactChoices.map((value) => (
+            <RadioPanel
+                {...RadioPanelStory.args}
+                key={value}
+                value={value}
+                name="kontaktmetode"
+                label={value}
+            />
         )),
     },
 };
