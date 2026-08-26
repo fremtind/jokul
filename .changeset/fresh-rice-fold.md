89e925876349b486dbf6cd5959517cc4ff48b5b4
Pio Rasch-Halvorsen <pio.rasch-halvorsen@fremtind.no>
2026-08-26 09:28:25 +0200

    Fjern data-theme fra innhold i message

---
+++ .changeset/fresh-rice-fold.md
@@ -0,0 +1,5 @@
+---
+"@fremtind/jokul": patch
+---
+
+Retter en feil som gjorde at innholdet i `Message` alltid ble vist med `data-theme="light"`.
--- packages/jokul/src/components/message/Message.tsx
+++ packages/jokul/src/components/message/Message.tsx
@@ -62,7 +62,7 @@ function messageFactory(messageType: MessageProps["variant"]) {
                 role={role}
             >
                 {getIcon(messageType)}
-                <div className="jkl-message__content" data-theme="light">
+                <div className="jkl-message__content">
                     {title && <p className="jkl-message__title">{title}</p>}
                     <div className="jkl-message__message">{newChildren}</div>
                 </div>
@@ -124,7 +124,7 @@ export const Message = forwardRef<HTMLDivElement, MessageProps>(
                 role={role}
             >
                 {getIcon(variant)}
-                <div className="jkl-message__content" data-theme="light">
+                <div className="jkl-message__content">
                     {title && <p className="jkl-message__title">{title}</p>}
                     <div className="jkl-message__message">{newChildren}</div>
                 </div>
