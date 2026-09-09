c305add18f9cf24f71bb319da64c4be90b84ed85
Pio Rasch-Halvorsen <pio.rasch-halvorsen@fremtind.no>
2026-09-09 11:05:55 +0200

    Align bilder i kortene i komponentoversikten

--- portal/src/components/overview/card.tsx
+++ portal/src/components/overview/card.tsx
@@ -1,5 +1,7 @@
 import type { SanityImageLike } from "@/sanity/lib/image";
 import { Card } from "@fremtind/jokul/card";
+import { Flex } from "@fremtind/jokul/flex";
+import { Text } from "@fremtind/jokul/typography";
 import NextLink from "next/link";
 import { type ReactNode, useId } from "react";
 import { OverviewThumbnail } from "./thumbnail";
@@ -29,34 +31,43 @@ export const OverviewCard = (props: OverviewCardProps) => {
 
     return (
         <li>
-            <Card
-                as={NextLink}
-                href={link}
-                padding="l"
-                className={styles.card}
-                aria-labelledby={`${id}-title`}
-                aria-describedby={describedBy}
-            >
-                <p className={styles.name} id={`${id}-title`}>
-                    {title}
-                </p>
-                {description && (
-                    <p className={styles.description} id={descriptionId}>
-                        {description}
-                    </p>
-                )}
-                {image && (
-                    <OverviewThumbnail
-                        darkImage={image.dark}
-                        lightImage={image.light}
-                    />
-                )}
-                {footer && (
-                    <footer className={styles.footer} id={footerId}>
-                        {footer}
-                    </footer>
-                )}
-            </Card>
+            <Flex direction="column" asChild gap="16 0">
+                <Card
+                    as={NextLink}
+                    href={link}
+                    padding="l"
+                    className={styles.card}
+                    aria-labelledby={`${id}-title`}
+                    aria-describedby={describedBy}
+                >
+                    <Flex direction="column" gap="8 0">
+                        <p className={styles.name} id={`${id}-title`}>
+                            {title}
+                        </p>
+                        {description && (
+                            <Text
+                                subdued
+                                size="s"
+                                className={styles.description}
+                                id={descriptionId}
+                            >
+                                {description}
+                            </Text>
+                        )}
+                    </Flex>
+                    {image && (
+                        <OverviewThumbnail
+                            darkImage={image.dark}
+                            lightImage={image.light}
+                        />
+                    )}
+                    {footer && (
+                        <footer className={styles.footer} id={footerId}>
+                            {footer}
+                        </footer>
+                    )}
+                </Card>
+            </Flex>
         </li>
     );
 };
