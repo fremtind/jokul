369d97b444b766c5cea16f8bc32fd2cb14800437
Kristian Ulvund <kristian.ulvund@fremtind.no>
2026-09-23 09:24:48 +0200

    Unngå at Tag strekkes til full bredde i en flex-container

--- packages/jokul/src/components/tag/development/Example.tsx
+++ packages/jokul/src/components/tag/development/Example.tsx
@@ -1,7 +1,16 @@
 import React from "react";
 import { DevExample } from "utils/dev-example/DevExample.js";
-import { TagExample } from "./TagExample.js";
+
+import { TagExample, TagInFlexExample } from "./TagExample.js";
 
 export default function Example() {
-    return <DevExample title="Tag" component={TagExample} />;
+    return (
+        <>
+            <DevExample title="Tag" component={TagExample} />
+            <DevExample
+                title="Tag i flex-container"
+                component={TagInFlexExample}
+            />
+        </>
+    );
 }
